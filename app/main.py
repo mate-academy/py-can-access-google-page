f2fabe52b3c213ff361abfdf303053fab0c4f2dc
Oleksii Proshchenko <80070761+MasterpieceElbow@users.noreply.github.com>
2022-02-03 13:41:39 +0200

    Can assess Google page

--- app/main.py
+++ app/main.py
@@ -1,3 +1,21 @@
-# TODO: add initial code
-def hello_world():
-    return "Hello, world!"
+import time
+import datetime
+
+
+def valid_google_url(url):
+    valid_urls = [
+        "https://www.google.com"
+        "google.com",
+        "google.com.ua",
+    ]
+    time.sleep(5)
+    return True if url in valid_urls else False
+
+
+def has_internet_connection():
+    current_time = datetime.datetime.now()
+    return True if current_time.hour in range(6, 23) else False
+
+
+def can_access_google_page(url):
+    return "Accessible" if valid_google_url(url) and has_internet_connection() else "Not accessible"
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,1 @@
-# TODO: add tests
-from app.main import hello_world
-
-
-def test_hello_world():
-    assert hello_world() == "Hello, world!"
+# write your code here
