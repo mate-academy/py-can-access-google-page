142a3c52c46b1f11b113e5b1d25a4ff9d980fb93
Serhii Pastukhov <pastuhovsergey@gmail.com>
2022-02-04 17:30:33 +0200

    'Solution'

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,35 @@
-# write your code here
+from unittest import mock
+from app.main import can_access_google_page, valid_google_url, has_internet_connection
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_url_true_connection_true(internet_connection, valid_url):
+    internet_connection.return_value = True
+    valid_url.return_value = True
+    assert can_access_google_page("https://www.google.com.ua/") == "Accessible"
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_url_false_connection_false(internet_connection, valid_url):
+    internet_connection.return_value = False
+    valid_url.return_value = False
+    assert can_access_google_page("https://www.google.com.ua/") == "Not accessible"
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_url_true_connection_false(internet_connection, valid_url):
+    internet_connection.return_value = True
+    valid_url.return_value = False
+    assert can_access_google_page("https://www.google.com.ua/") == "Not accessible"
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_url_false_connection_true(internet_connection, valid_url):
+    internet_connection.return_value = False
+    valid_url.return_value = True
+    assert can_access_google_page("https://www.google.com.ua/") == "Not accessible"
+
