03c412c351b9398bcf153edac64e915048740806
Nazar-kob <kobrinnazar05@mail.com>
2022-02-05 15:37:06 +0200

    Solution

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,7 +5,10 @@ from app.main import can_access_google_page
 
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
-def test_connection_is_good_url_is_good(mocked_has_internet_connection, mocked_valid_google_url):
+def test_connection_is_good_url_is_good(
+        mocked_has_internet_connection,
+        mocked_valid_google_url
+):
     mocked_has_internet_connection.return_value = True
     mocked_valid_google_url.return_value = True
 
@@ -14,7 +17,10 @@ def test_connection_is_good_url_is_good(mocked_has_internet_connection, mocked_v
 
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
-def test_connection_is_good_url_is_bad(mocked_has_internet_connection, mocked_valid_google_url):
+def test_connection_is_good_url_is_bad(
+        mocked_has_internet_connection,
+        mocked_valid_google_url
+):
     mocked_has_internet_connection.return_value = True
     mocked_valid_google_url.return_value = False
 
@@ -23,7 +29,10 @@ def test_connection_is_good_url_is_bad(mocked_has_internet_connection, mocked_va
 
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
-def test_connection_is_bad_url_is_good(mocked_has_internet_connection, mocked_valid_google_url):
+def test_connection_is_bad_url_is_good(
+        mocked_has_internet_connection,
+        mocked_valid_google_url
+):
     mocked_has_internet_connection.return_value = False
     mocked_valid_google_url.return_value = True
 
@@ -32,7 +41,10 @@ def test_connection_is_bad_url_is_good(mocked_has_internet_connection, mocked_va
 
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
-def test_connection_is_bad_url_is_bad(mocked_has_internet_connection, mocked_valid_google_url):
+def test_connection_is_bad_url_is_bad(
+        mocked_has_internet_connection,
+        mocked_valid_google_url
+):
     mocked_has_internet_connection.return_value = False
     mocked_valid_google_url.return_value = False
 
