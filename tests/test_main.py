efe9f49eb344b30ddecee021a4e6cc8cce25ec01
Oleksandr Zhukov <alexanderzhukov88@gmail.com>
2022-02-06 21:53:50 +0200

    'Solution'

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,44 @@
 from unittest import mock
+import pytest
 
 from app.main import can_access_google_page
 
 
+@pytest.mark.parametrize(
+    "is_valid,has_connection,expected_advice",
+    [
+        (
+            True,
+            True,
+            "Accessible"
+        ),
+        (
+            True,
+            False,
+            "Not accessible"
+        ),
+        (
+            False,
+            True,
+            "Not accessible"
+        ),
+(
+            False,
+            False,
+            "Not accessible"
+        )
+    ]
+)
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
-def test_can_access_google(mocked_request, mocked_connection):
-    mocked_request.return_value = True
-    mocked_connection.return_value = True
+def test_can_access_google(
+                        mocked_request,
+                        mocked_connection,
+                        is_valid,
+                        has_connection,
+                        expected_advice
+                    ):
+    mocked_request.return_value = is_valid
+    mocked_connection.return_value = has_connection
 
-    can_access_google_page("http://www.google.com")
-
-    mocked_request.assert_called_once_with("http://www.google.com")
-    mocked_connection.assert_called_once()
+    assert can_access_google_page("http://www.google.com") == expected_advice
