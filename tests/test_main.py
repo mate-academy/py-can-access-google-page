830aa5a975502eab907dbbd3be1901559a0a8443
polinatytarenko <p.tytarenko.20@gmail.com>
2022-02-07 09:14:52 +0200

    Solution

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,46 @@
-# write your code here
+from unittest import mock
+from app.main import can_access_google_page
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_accessible_when_valid_google_url_and_has_internet_connection(
+        mocked_valid_google_url, mocked_has_internet_connection
+):
+    mocked_valid_google_url.return_value = True
+    mocked_has_internet_connection.return_value = True
+
+    assert can_access_google_page("http://google.com") == "Accessible"
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_not_accessible_when_valid_google_url_but_no_internet_connection(
+        mocked_valid_google_url, mocked_has_internet_connection
+):
+    mocked_valid_google_url.return_value = True
+    mocked_has_internet_connection.return_value = False
+
+    assert can_access_google_page("http://google.com") == "Not accessible"
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_not_accessible_when_has_internet_connection_but_not_valid_google_url(
+        mocked_valid_google_url, mocked_has_internet_connection
+):
+    mocked_valid_google_url.return_value = False
+    mocked_has_internet_connection.return_value = True
+
+    assert can_access_google_page("http://google.com") == "Not accessible"
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_not_accessible_when_not_valid_google_url_and_no_internet_connection(
+        mocked_valid_google_url, mocked_has_internet_connection
+):
+    mocked_valid_google_url.return_value = False
+    mocked_has_internet_connection.return_value = False
+
+    assert can_access_google_page("http://google.com") == "Not accessible"
