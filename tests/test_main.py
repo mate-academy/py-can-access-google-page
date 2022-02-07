204f1ca72d7f606099bb9b29e2d1f931e082ea4d
Anita Posledova <anitaposliedova@gmail.com>
2022-02-07 20:34:59 +0200

    Solution

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,62 @@
-# write your code here
+from unittest import mock
+from app.main import can_access_google_page
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_connection_and_valid_url(
+        mocked_valid_google_url,
+        mocked_internet_connection
+):
+    mocked_valid_google_url.return_value = True
+    mocked_internet_connection.return_value = True
+
+    assert can_access_google_page("https://google.com") == "Accessible"
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_no_connection_and_valid_url(
+        mocked_valid_google_url,
+        mocked_has_internet_connection
+):
+    mocked_valid_google_url.return_value = True
+    mocked_has_internet_connection.return_value = False
+
+    assert can_access_google_page("http://google.com") == "Not accessible"
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_connection_and_invalid_url(
+        mocked_valid_google_url,
+        mocked_internet_connection
+):
+    mocked_valid_google_url.return_value = False
+    mocked_internet_connection.return_value = True
+
+    assert can_access_google_page("https://fsfsdgle.com") == "Not accessible"
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_no_connection_and_invalid_url(
+        mocked_valid_google_url,
+        mocked_internet_connection
+):
+    mocked_valid_google_url.return_value = False
+    mocked_internet_connection.return_value = False
+
+    assert can_access_google_page("https://gsdgle.com") == "Not accessible"
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_incorrect_time_less_6_and_more_23(
+        mocked_valid_google_url,
+        mocked_internet_connection
+):
+    mocked_valid_google_url.return_value = True
+    mocked_internet_connection.return_value = False
+
+    assert can_access_google_page("https://google.com") == "Not accessible"
