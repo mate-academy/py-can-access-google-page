d94f2a5523e025302d16554242a2ba6d10efed8c
Alex Gergel <alexgergel@gmail.com>
2022-02-09 11:42:04 +0200

    "Solution"

--- tests/test_main.py
+++ tests/test_main.py
@@ -37,3 +37,15 @@ def test_if_connection_is_off_and_url_is_correct(
     mocked_internet_connection.return_value = False
 
     assert can_access_google_page("https://google.com") == "Not accessible"
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_if_connection_is_off_and_url_is_incorrect(
+        mocked_valid_google_url,
+        mocked_internet_connection
+):
+    mocked_valid_google_url.return_value = False
+    mocked_internet_connection.return_value = False
+
+    assert can_access_google_page("https://google.com") == "Not accessible"
