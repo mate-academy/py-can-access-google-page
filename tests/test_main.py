b15027ba94c73d6f07c2e3b6455d89d7fc7f8d87
Vlad Tsvigun <vl.tsv@outlook.com>
2022-03-12 16:12:45 +0200

    flake8 fix

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,7 +5,8 @@ from app.main import can_access_google_page
 
 @pytest.fixture()
 def mocked_internet_connection():
-    with mock.patch("app.main.has_internet_connection") as mocked_internet_connection:
+    with mock.patch("app.main.has_internet_connection") as\
+            mocked_internet_connection:
         yield mocked_internet_connection
 
 
@@ -29,14 +30,16 @@ def test_no_connection(mocked_valid_google_url, mocked_internet_connection):
     assert can_access_google_page("https://google.com") == "Not accessible"
 
 
-def test_no_connection_with_invalid_url(mocked_valid_google_url, mocked_internet_connection):
+def test_no_connection_with_invalid_url(mocked_valid_google_url,
+                                        mocked_internet_connection):
     mocked_valid_google_url.return_value = False
     mocked_internet_connection.return_value = False
 
     assert can_access_google_page("https://qwerty2323.com") == "Not accessible"
 
 
-def test_connection_with_valid_url(mocked_valid_google_url, mocked_internet_connection):
+def test_connection_with_valid_url(mocked_valid_google_url,
+                                   mocked_internet_connection):
     mocked_valid_google_url.return_value = True
     mocked_internet_connection.return_value = True
 
