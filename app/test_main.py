55ead4fffe2d2c4a38a445ed99bca2e038bfbaa1
Misha Hrynko <misha@mate.academy>
2022-07-26 13:26:12 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -39,7 +39,7 @@ class TestCanAccessGoogle(unittest.TestCase):
     def test_can_access_google_page_2_False(
             self, mock_valid_url, mock_internet_connection):
         mock_internet_connection.return_value = False
-        mock_valid_url.return_value = True
+        mock_valid_url.return_value = False
         assert \
             can_access_google_page("https://www.google.com/") == \
             "Not accessible"
@@ -66,7 +66,7 @@ class TestCanAccessGoogle(unittest.TestCase):
 
     @patch("app.main.valid_google_url")
     @patch("app.main.has_internet_connection")
-    def test_can_access_google_page_1_True_1_False(
+    def test_can_access_google_page_1_False_1_True(
             self, mock_valid_url, mock_internet_connection):
         mock_internet_connection.return_value = True
         mock_valid_url.return_value = False
