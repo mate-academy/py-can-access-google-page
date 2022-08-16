bbba86a2d415e4a32c0ce546b3a34b284ec4b580
Dima Povkh <dimapovkh@gmail.com>
2022-08-16 13:30:18 +0200

    can_access_google_page_v1

--- app/test_main.py
+++ app/test_main.py
@@ -7,7 +7,7 @@ from app.main import can_access_google_page
 def test_when_url_and_connection_are_valid(mocked_url, mocked_connection):
     mocked_url.return_value = True
     mocked_connection.return_value = True
-    assert can_access_google_page(1) == "Accessible"
+    assert can_access_google_page("https://www.google.com/") == "Accessible"
 
 
 @mock.patch("app.main.valid_google_url")
