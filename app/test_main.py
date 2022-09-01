4b340f28d48726e1fcf0b9c42dfb40e74ae8b2ba
Oleg Usenko <Oleg911Usenko@gmail.com>
2022-09-01 19:25:41 +0300

    Task Complete v2.0

--- app/test_main.py
+++ app/test_main.py
@@ -26,32 +26,3 @@ class TestFunc:
         mock_google_url.return_value = response
         mock_internet_connection.return_value = current_time
         assert can_access_google_page("google.com") == result
-
-
-# class TestFunc:
-#     @mock.patch("app.main.valid_google_url")
-#     @mock.patch("app.main.has_internet_connection")
-#     def test_can_access_google_page_1(self,
-#                                       mock_google_url,
-#                                       mock_internet_connection):
-#         mock_internet_connection.return_value = datetime.time(6, 0, 0)
-#         mock_google_url.return_value = True
-#         assert can_access_google_page("google.com") == "Accessible"
-#
-#     @mock.patch("app.main.valid_google_url")
-#     @mock.patch("app.main.has_internet_connection")
-#     def test_can_access_google_page_2(self,
-#                                       mock_google_url,
-#                                       mock_internet_connection):
-#         mock_internet_connection.return_value = datetime.time(10, 0, 0)
-#         mock_google_url.return_value = False
-#         assert can_access_google_page("google.com") == "Not accessible"
-#
-#     @mock.patch("app.main.valid_google_url")
-#     @mock.patch("app.main.has_internet_connection")
-#     def test_can_access_google_page_3(self,
-#                                       mock_google_url,
-#                                       mock_internet_connection):
-#         mock_internet_connection.return_value = False
-#         mock_google_url.return_value = True
-#         assert can_access_google_page("google.com") == "Not accessible"
