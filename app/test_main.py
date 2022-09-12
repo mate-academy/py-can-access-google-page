026f591c687e335ecac0fff533d2bb67012ae414
Paul Starodub <amorallex@gmail.com>
2022-09-12 11:03:10 +0300

    no comment

--- app/test_main.py
+++ app/test_main.py
@@ -45,38 +45,3 @@ def test_can_access_google_page(
     mocked_url.return_value = response
     mocked_connection.return_value = current_time
     assert can_access_google_page("url") == result
-
-# def test_full_access():
-#     with mock.patch("app.main.valid_google_url") \
-#             as mocked_url_func:
-#         mocked_url_func.return_value = True
-#
-#         with mock.patch("app.main.has_internet_connection") \
-#                 as mocked_internet_func:
-#             mocked_internet_func.return_value = True
-#
-#             assert can_access_google_page("url") == "Accessible"
-#
-#
-# def test_not_is_valid_url():
-#     with mock.patch("app.main.valid_google_url") \
-#             as mocked_url_func:
-#         mocked_url_func.return_value = False
-#
-#         with mock.patch("app.main.has_internet_connection") \
-#                 as mocked_internet_func:
-#             mocked_internet_func.return_value = True
-#
-#             assert can_access_google_page("url") == "Not accessible"
-#
-#
-# def test_not_connection_internet():
-#     with mock.patch("app.main.valid_google_url") \
-#             as mocked_url_func:
-#         mocked_url_func.return_value = True
-#
-#         with mock.patch("app.main.has_internet_connection") \
-#                 as mocked_internet_func:
-#             mocked_internet_func.return_value = False
-#
-#             assert can_access_google_page("url") == "Not accessible"
