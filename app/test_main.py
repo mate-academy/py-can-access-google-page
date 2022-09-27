7555163f0d7a132432afac91db641ae627fbebf7
Davyd Kryvko <davidkrivko@gmail.com>
2022-09-27 18:43:23 +0200

    'Solution_2'

--- app/test_main.py
+++ app/test_main.py
@@ -24,14 +24,16 @@ def test_return_accessible(mocked_internet, mocked_valid_url):
     assert can_access_google_page("") == "Accessible"
 
 
-def test_return_not_accessible_if_internet_is_false(mocked_internet, mocked_valid_url):
+def test_return_not_accessible_if_internet_is_false(mocked_internet,
+                                                    mocked_valid_url):
     mocked_internet.return_value = False
     mocked_valid_url.return_value = True
 
     assert can_access_google_page("") == "Not accessible"
 
 
-def test_return_not_accessible_if_url_is_false(mocked_internet, mocked_valid_url):
+def test_return_not_accessible_if_url_is_false(mocked_internet,
+                                               mocked_valid_url):
     mocked_internet.return_value = True
     mocked_valid_url.return_value = False
 
