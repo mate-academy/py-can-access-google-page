49496bd981acd867e3f21a0243b454ef3394033a
Olia Trofymchuk <oliatrofymchuk@knu.ua>
2022-09-30 00:35:38 +0300

    Edit flake8 errors

--- app/test_main.py
+++ app/test_main.py
@@ -20,29 +20,34 @@ def test_for_correct_values(mocked_valid_google_url,
                             mocked_has_internet_connection):
     mocked_valid_google_url.return_value = True
     mocked_has_internet_connection.return_value = True
-    assert can_access_google_page("https://mate.academy/learn?course=python") == "Accessible", \
-        "Function must return 'Accessible' if url is valid and current time is correct!"
+    assert can_access_google_page("https://mate.academy/learn?course=python") \
+           == "Accessible", "Function must return 'Accessible' " \
+                            "if url is valid and current time is correct!"
 
 
 def test_for_not_valid_url(mocked_valid_google_url,
                            mocked_has_internet_connection):
     mocked_valid_google_url.return_value = False
     mocked_has_internet_connection.return_value = True
-    assert can_access_google_page("http://twitter.com/[username].") == "Not accessible", \
-        "Function must return 'Not accessible' if url is not valid!"
+    assert can_access_google_page("http://twitter.com/[username].") \
+           == "Not accessible", "Function must return 'Not accessible' " \
+                                "if url is not valid!"
 
 
 def test_for_incorrect_time(mocked_valid_google_url,
                             mocked_has_internet_connection):
     mocked_valid_google_url.return_value = True
     mocked_has_internet_connection.return_value = False
-    assert can_access_google_page("https://www.google.com/") == "Not accessible", \
-        "Function must return 'Not accessible' if current time is not correct!"
+    assert can_access_google_page("https://www.google.com/") \
+           == "Not accessible", "Function must return 'Not accessible' " \
+                                "if current time is not correct!"
 
 
 def test_for_not_correct_values(mocked_valid_google_url,
                                 mocked_has_internet_connection):
     mocked_valid_google_url.return_value = False
     mocked_has_internet_connection.return_value = False
-    assert can_access_google_page("http://www.example.com/space%20here.html") == "Not accessible", \
-        "Function must return 'Not accessible' if url is not valid and current time is incorrect!"
+    assert can_access_google_page("http://www.example.com/space%20here.html") \
+           == "Not accessible", "Function must return 'Not accessible' if " \
+                                "url is not valid and current time is " \
+                                "incorrect!"
