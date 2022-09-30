f1aed7f49a2ca2f859ca499e111df33919752927
Soroka Igor <copokaigor@gmail.com>
2022-09-30 13:45:55 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -15,19 +15,22 @@ def mocked_valid_url():
         yield mock_func
 
 
-def test_cannot_access_if_connection_or_valid_url_is_true(mocked_internet_connection, mocked_valid_url):
+def test_cannot_access_if_connection_or_valid_url_is_true(
+        mocked_internet_connection, mocked_valid_url):
     mocked_valid_url.return_value = False
     mocked_internet_connection.return_value = False
     assert can_access_google_page("") == "Not accessible"
 
 
-def test_cannot_access_if_only_connection(mocked_internet_connection, mocked_valid_url):
+def test_cannot_access_if_only_connection(
+        mocked_internet_connection, mocked_valid_url):
     mocked_valid_url.return_value = False
     mocked_internet_connection.return_value = True
     assert can_access_google_page("") == "Not accessible"
 
 
-def test_cannot_access_if_only_valid_url(mocked_internet_connection, mocked_valid_url):
+def test_cannot_access_if_only_valid_url(
+        mocked_internet_connection, mocked_valid_url):
     mocked_valid_url.return_value = True
     mocked_internet_connection.return_value = False
     assert can_access_google_page("") == "Not accessible"
