c0fde49dbdb409d4f3c584390eed6d67b16147a6
Kateryna Kilyk <katekilik@gmail.com>
2022-09-30 16:41:14 +0300

    Fixed line break

--- app/test_main.py
+++ app/test_main.py
@@ -18,26 +18,30 @@ def mocked_has_internet_connection():
 def test_all_works(mocked_valid_url, mocked_has_internet_connection):
     mocked_valid_url.return_value = True
     mocked_has_internet_connection.return_value = True
-    assert can_access_google_page("www.google.com/") == "Accessible", \
+    assert can_access_google_page("www.google.com/") == "Accessible", (
         "Can get access only with internet connection and valid url"
+    )
 
 
 def test_no_connection(mocked_valid_url, mocked_has_internet_connection):
     mocked_valid_url.return_value = True
     mocked_has_internet_connection.return_value = False
-    assert can_access_google_page("www.google.com/") == "Not accessible", \
+    assert can_access_google_page("www.google.com/") == "Not accessible", (
         "Cannot get access without internet connection"
+    )
 
 
 def test_not_response(mocked_valid_url, mocked_has_internet_connection):
     mocked_valid_url.return_value = False
     mocked_has_internet_connection.return_value = True
-    assert can_access_google_page("www.google.com/") == "Not accessible", \
+    assert can_access_google_page("www.google.com/") == "Not accessible", (
         "Cannot get access with invalid url"
+    )
 
 
 def test_all_false(mocked_valid_url, mocked_has_internet_connection):
     mocked_valid_url.return_value = False
     mocked_has_internet_connection.return_value = False
-    assert can_access_google_page("www.google.com/") == "Not accessible", \
+    assert can_access_google_page("www.google.com/") == "Not accessible", (
         "Cannot get access without internet connection and with invalid url"
+    )
