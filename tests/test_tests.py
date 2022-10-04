f98ec9ba6a19d7fb9539fc2bf72d283799419c69
Kirill Drago <dragoswim7@gmail.com>
2022-10-04 18:09:10 +0300

    test1

--- tests/test_tests.py
+++ tests/test_tests.py
@@ -1,8 +1,6 @@
-import inspect
-
 import pytest
 
-from app import main, test_main
+from app import main
 
 
 def test_cannot_access_if_connection_or_valid_url_is_true(monkeypatch):
@@ -54,8 +52,3 @@ def test_cannot_access_if_only_valid_url(monkeypatch):
 
     test_result = pytest.main(["app/test_main.py"])
     assert test_result.value == 1, "You cannot access page if only 'valid url' is True."
-
-
-def test_comment_deleted():
-    lines = inspect.getsource(test_main)
-    assert "# write your code here" not in lines
