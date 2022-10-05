3389015dfe563ec8d539129e688b7608628da0c1
Lina Samoilenko <salina.amoylenko@gmail.com>
2022-10-05 16:38:29 +0300

    Tests cleaned

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,5 @@
 import pytest
-# import requests
+from typing import Callable
 from unittest import mock
 from app.main import can_access_google_page
 
@@ -10,43 +10,39 @@ def mocked_url():
         yield mocked_function_url
 
 
-def test_should_check_url(mocked_url):
-    mocked_url.return_value = True
-    can_access_google_page("www.google.com")
-    mocked_url.assert_called_once_with("www.google.com")
-
-
 @pytest.fixture()
 def mocked_connection():
     with mock.patch("app.main.has_internet_connection") as mocked_function_connection:
         yield mocked_function_connection
 
 
-def test_should_check_connection(mocked_connection):
-    mocked_connection.return_value = True
-    can_access_google_page("www.google.com")
-    mocked_connection.assert_called_once()
-
-
-def test_no_internet_connection(mocked_connection, mocked_url):
+def test_no_internet_connection(
+        mocked_connection: Callable, mocked_url: Callable
+) -> None:
     mocked_connection.return_value = False
     mocked_url.return_value = True
     assert can_access_google_page("www.google.com") == "Not accessible"
 
 
-def test_no_valid_url(mocked_connection, mocked_url):
+def test_no_valid_url(
+        mocked_connection: Callable, mocked_url: Callable
+) -> None:
     mocked_connection.return_value = True
     mocked_url.return_value = False
     assert can_access_google_page("www.google.com") == "Not accessible"
 
 
-def test_no_internet_connection_no_valid_url(mocked_connection, mocked_url):
+def test_no_internet_connection_no_valid_url(
+        mocked_connection: Callable, mocked_url: Callable
+) -> None:
     mocked_connection.return_value = False
     mocked_url.return_value = False
     assert can_access_google_page("www.google.com") == "Not accessible"
 
 
-def test_internet_connection_and_valid_url(mocked_connection, mocked_url):
+def test_internet_connection_and_valid_url(
+        mocked_connection: Callable, mocked_url: Callable
+) -> None:
     mocked_connection.return_value = True
     mocked_url.return_value = True
     assert can_access_google_page("www.google.com") == "Accessible"
