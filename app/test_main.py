862775fdd20faa6923ac9316546a3e92cfa83483
vadymsabadyr <Sabadyrvv94@gmail.com>
2022-10-05 13:48:45 +0300

    add annotation_2

--- app/test_main.py
+++ app/test_main.py
@@ -16,21 +16,28 @@ def mocked_internet_connection():
         yield mock_connection
 
 
-def test_google_page_true(mocked_google_url: str, mocked_internet_connection: bool):
+def test_google_page_true(
+        mocked_google_url: str,
+        mocked_internet_connection: bool
+) -> None:
     mocked_google_url.return_value = True
     mocked_internet_connection.return_value = True
     assert can_access_google_page("www.mataacademy.com") == "Accessible"
 
 
-def test_google_page_false_google_url(mocked_google_url: str,
-                                      mocked_internet_connection: bool):
+def test_google_page_false_google_url(
+        mocked_google_url: str,
+        mocked_internet_connection: bool
+) -> None:
     mocked_google_url.return_value = False
     mocked_internet_connection.return_value = True
     assert can_access_google_page("www.404.com123") == "Not accessible"
 
 
-def test_google_page_false_internet_connection(mocked_google_url: str,
-                                               mocked_internet_connection: bool):
+def test_google_page_false_internet_connection(
+        mocked_google_url: str,
+        mocked_internet_connection: bool
+) -> None:
     mocked_google_url.return_value = True
     mocked_internet_connection.return_value = False
     assert can_access_google_page("www.mataacademy.com") == "Not accessible"
