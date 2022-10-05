75b83248f7f62e2611bd98fadb241ba301e7c54a
Rostyslav Nikulin <rostik.nikss@gmail.com>
2022-10-05 16:47:59 +0300

    Fix flake8 annotation problems

--- app/test_main.py
+++ app/test_main.py
@@ -1,51 +1,52 @@
 import pytest
 from unittest import mock
+from typing import Callable
 from app.main import can_access_google_page
 
 
 @pytest.fixture()
-def mocked_valid_url():
+def mocked_valid_url() -> Callable:
     with mock.patch("app.main.valid_google_url") as mocked_url:
         yield mocked_url
 
 
 @pytest.fixture()
-def mocked_has_internet_connection():
+def mocked_has_internet_connection() -> Callable:
     with mock.patch("app.main.has_internet_connection") as mocked_connection:
         yield mocked_connection
 
 
 def test_if_all_work(
-        mocked_valid_url,
-        mocked_has_internet_connection
-):
+        mocked_valid_url: Callable,
+        mocked_has_internet_connection: Callable
+) -> None:
     mocked_has_internet_connection.return_value = True
     mocked_valid_url.return_value = True
     assert can_access_google_page("google.com") == "Accessible"
 
 
 def test_if_url_is_not_valid(
-        mocked_valid_url,
-        mocked_has_internet_connection
-):
+        mocked_valid_url: Callable,
+        mocked_has_internet_connection: Callable
+) -> None:
     mocked_has_internet_connection.return_value = True
     mocked_valid_url.return_value = False
     assert can_access_google_page("google.com") == "Not accessible"
 
 
 def test_if_has_not_internet_connection(
-        mocked_valid_url,
-        mocked_has_internet_connection
-):
+        mocked_valid_url: Callable,
+        mocked_has_internet_connection: Callable
+) -> None:
     mocked_has_internet_connection.return_value = False
     mocked_valid_url.return_value = True
     assert can_access_google_page("google.com") == "Not accessible"
 
 
 def test_if_all_do_not_work(
-        mocked_valid_url,
-        mocked_has_internet_connection
-):
+        mocked_valid_url: Callable,
+        mocked_has_internet_connection: Callable
+) -> None:
     mocked_has_internet_connection.return_value = False
     mocked_valid_url.return_value = False
     assert can_access_google_page("google.com") == "Not accessible"
