750bbf993daed6dd13fd65ad78db4232943ba9ac
Lina Samoilenko <salina.amoylenko@gmail.com>
2022-10-05 16:46:30 +0300

    Annotations for functions added

--- app/main.py
+++ app/main.py
@@ -2,17 +2,17 @@ import datetime
 import requests
 
 
-def valid_google_url(url):
+def valid_google_url(url) -> bool:
     response = requests.get(url)
     return True if response.status_code == 200 else False
 
 
-def has_internet_connection():
+def has_internet_connection() -> bool:
     current_time = datetime.datetime.now()
     return True if current_time.hour in range(6, 23) else False
 
 
-def can_access_google_page(url):
+def can_access_google_page(url) -> str:
     if has_internet_connection() and valid_google_url(url):
         return "Accessible"
     else:
