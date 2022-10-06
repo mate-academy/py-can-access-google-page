56296e3a12601f52859dae3a234dba4e3f218001
Oleksandr Murynskyi <a.murinskiy@gmail.com>
2022-10-06 09:34:09 +0300

    Solution with brackets

--- app/test_main.py
+++ app/test_main.py
@@ -18,26 +18,30 @@ def mock_connection():
 def test_both_func_work(mock_valid_url, mock_connection):
     mock_valid_url.return_value = True
     mock_connection.return_value = True
-    assert can_access_google_page("www.mate.academy") == "Accessible", \
+    assert can_access_google_page("www.mate.academy") == "Accessible", (
         "the page is accessible if all working correct"
+    )
 
 
 def test_not_valid_url(mock_valid_url, mock_connection):
     mock_valid_url.return_value = False
     mock_connection.return_value = True
-    assert can_access_google_page("www.mate.academy") == "Not accessible", \
+    assert can_access_google_page("www.mate.academy") == "Not accessible", (
         "the page is inaccessible if the url is not correct"
+    )
 
 
 def test_no_internet_connection(mock_valid_url, mock_connection):
     mock_valid_url.return_value = True
     mock_connection.return_value = False
-    assert can_access_google_page("www.mate.academy") == "Not accessible", \
+    assert can_access_google_page("www.mate.academy") == "Not accessible", (
         "the page is inaccessible if the Internet is not working"
+    )
 
 
 def test_both_func_do_not_work(mock_valid_url, mock_connection):
     mock_valid_url.return_value = False
     mock_connection.return_value = False
-    assert can_access_google_page("www.mate.academy") == "Not accessible", \
+    assert can_access_google_page("www.mate.academy") == "Not accessible", (
         "the page is inaccessible if all is not working"
+    )
