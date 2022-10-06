691a86b7ef44abe2a0a4173891298b228009ed9a
Vitalii Shevchuk <shevch.nl@gmail.com>
2022-10-06 23:15:02 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -6,36 +6,40 @@ from app.main import can_access_google_page
 
 
 @pytest.fixture
-def mocked_internet_connection():
+def mocked_internet_connection() -> None:
     with mock.patch("app.main.has_internet_connection") as mocked_connection:
         yield mocked_connection
 
 
 @pytest.fixture
-def mocked_valid_google_url():
+def mocked_valid_google_url() -> None:
     with mock.patch("app.main.valid_google_url") as mocked_url:
         yield mocked_url
 
 
-def test_everything_works(mocked_internet_connection, mocked_valid_google_url):
+def test_everything_works(mocked_internet_connection: None,
+                          mocked_valid_google_url: None) -> None:
     mocked_internet_connection.return_value = True
     mocked_valid_google_url.return_value = True
     assert can_access_google_page("https://www.google.com") == "Accessible"
 
 
-def test_no_connection(mocked_internet_connection, mocked_valid_google_url):
+def test_no_connection(mocked_internet_connection: None,
+                       mocked_valid_google_url: None) -> None:
     mocked_internet_connection.return_value = False
     mocked_valid_google_url.return_value = True
     assert can_access_google_page("https://www.google.com") == "Not accessible"
 
 
-def test_invalid_url(mocked_internet_connection, mocked_valid_google_url):
+def test_invalid_url(mocked_internet_connection: None,
+                     mocked_valid_google_url: None) -> None:
     mocked_internet_connection.return_value = True
     mocked_valid_google_url.return_value = False
     assert can_access_google_page("https://www.google.com") == "Not accessible"
 
 
-def test_everything_is_not_working(mocked_internet_connection, mocked_valid_google_url):
+def test_everything_is_not_working(mocked_internet_connection: None,
+                                   mocked_valid_google_url: None) -> None:
     mocked_internet_connection.return_value = False
     mocked_valid_google_url.return_value = False
     assert can_access_google_page("https://www.google.com") == "Not accessible"
