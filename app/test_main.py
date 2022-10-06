8ef7999a97a23728996cba657fed6e4e38a51cd6
Dima Ursatii <Dimon4ir@gmail.com>
2022-10-06 18:42:09 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -2,12 +2,13 @@ from unittest import mock
 
 from app.main import can_access_google_page
 
+from typing import Callable
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 def test_google_page_valid_with_connection(
-        mock_valid_google_url: bool,
-        mock_internet_connection: bool
+        mock_valid_google_url: Callable,
+        mock_internet_connection: Callable
 ) -> None:
     mock_internet_connection.return_value = True
     mock_valid_google_url.return_value = True
@@ -17,8 +18,8 @@ def test_google_page_valid_with_connection(
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 def test_google_not_connection_and_not_valid(
-        mock_valid_google_url: bool,
-        mock_internet_connection: bool
+        mock_valid_google_url: Callable,
+        mock_internet_connection: Callable
 ) -> None:
     mock_internet_connection.return_value = False
     mock_valid_google_url.return_value = False
@@ -28,8 +29,8 @@ def test_google_not_connection_and_not_valid(
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 def test_google_with_connection_not_valid_page(
-        mock_valid_google_url: bool,
-        mock_internet_connection: bool
+        mock_valid_google_url: Callable,
+        mock_internet_connection: Callable
 ) -> None:
     mock_internet_connection.return_value = True
     mock_valid_google_url.return_value = False
@@ -39,8 +40,8 @@ def test_google_with_connection_not_valid_page(
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 def test_google_without_connection_but_valid_page(
-        mock_valid_google_url: bool,
-        mock_internet_connection: bool
+        mock_valid_google_url: Callable,
+        mock_internet_connection: Callable
 ) -> None:
     mock_internet_connection.return_value = False
     mock_valid_google_url.return_value = True
