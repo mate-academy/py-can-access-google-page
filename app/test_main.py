4eb7924cabcabde68fd5dd36b0ca82cfefaff78a
Liudmyla Kulinchenko <liudmilakulinchenko@gmail.com>
2022-10-07 22:51:12 +0200

    Another annotation

--- app/test_main.py
+++ app/test_main.py
@@ -1,35 +1,35 @@
 from app.main import can_access_google_page
 
-from unittest import mock
+from unittest.mock import patch, MagicMock
 
 
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
 def test_invalid_url_and_no_internet(
-        mock_valid_url: mock,
-        mock_internet_connection: mock
+        mock_valid_url: MagicMock,
+        mock_internet_connection: MagicMock
 ) -> None:
     mock_valid_url.return_value = False
     mock_internet_connection.return_value = False
     assert can_access_google_page("https://www.google") == "Not accessible"
 
 
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
 def test_invalid_url(
-        mock_valid_url: mock,
-        mock_internet_connection: mock
+        mock_valid_url: MagicMock,
+        mock_internet_connection: MagicMock
 ) -> None:
     mock_valid_url.return_value = False
     mock_internet_connection.return_value = True
     assert can_access_google_page("https://www.google") == "Not accessible"
 
 
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
 def test_no_internet(
-        mock_valid_url: mock,
-        mock_internet_connection: mock
+        mock_valid_url: MagicMock,
+        mock_internet_connection: MagicMock
 ) -> None:
     mock_valid_url.return_value = True
     mock_internet_connection.return_value = False
@@ -38,11 +38,11 @@ def test_no_internet(
     ) == "Not accessible"
 
 
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
 def test_can_access(
-        mock_valid_url: mock,
-        mock_internet_connection: mock
+        mock_valid_url: MagicMock,
+        mock_internet_connection: MagicMock
 ) -> None:
     mock_valid_url.return_value = True
     mock_internet_connection.return_value = True
