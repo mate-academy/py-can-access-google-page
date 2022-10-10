c451f361124ab14cba7fcd6a63308a0cf456388c
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-10-10 16:27:51 +0300

    Errors descriptions added

--- app/test_main.py
+++ app/test_main.py
@@ -8,10 +8,18 @@ from app.main import can_access_google_page
 @pytest.mark.parametrize(
     "valid_url, has_connection, expected",
     [
-        pytest.param(True, True, "Accessible"),
-        pytest.param(True, False, "Not accessible"),
-        pytest.param(False, True, "Not accessible"),
-        pytest.param(False, False, "Not accessible")
+        pytest.param(True, True, "Accessible",
+                     id="Return 'Accessible' if valid_google_url "
+                        "and has_internet_connection return True"),
+        pytest.param(True, False, "Not accessible",
+                     id="Return 'Not accessible' if valid_google_url is True "
+                        "but has_internet_connection return False"),
+        pytest.param(False, True, "Not accessible",
+                     id="Return 'Not accessible' if valid_google_url is False "
+                        "but has_internet_connection return True"),
+        pytest.param(False, False, "Not accessible",
+                     id="Return 'Not accessible' if valid_google_url is False "
+                        "and also has_internet_connection return False")
     ]
 )
 @mock.patch("app.main.has_internet_connection")
@@ -24,4 +32,4 @@ def test_can_access_google_page(mocked_valid_url: mock.MagicMock,
 
     mocked_valid_url.return_value = valid_url
     mocked_connection.return_value = has_connection
-    assert can_access_google_page("") == expected
+    assert can_access_google_page("www.google.com") == expected
