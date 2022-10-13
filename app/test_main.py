c3b3deed1bc77406b24fa338f7c34b6fcb92e370
Dmytro Kyrpan <d.kirpan@gmail.com>
2022-10-13 14:03:21 +0300

    solution v02

--- app/test_main.py
+++ app/test_main.py
@@ -16,39 +16,47 @@ def mock_connection() -> None:
         yield mock_internet
 
 
-def test_accessible(mock_valid_url, mock_connection) -> None:
+def test_accessible(mock_valid_url: str, mock_connection: int) -> None:
     mock_valid_url.return_value = True
     mock_connection.return_value = True
     assert can_access_google_page("https://github.com/") == "Accessible"
 
 
-def test_not_accessible_url(mock_valid_url, mock_connection) -> None:
+def test_not_accessible_url(
+        mock_valid_url: str, mock_connection: int
+) -> None:
     mock_valid_url.return_value = False
     mock_connection.return_value = True
     assert can_access_google_page("https://github.com/") ==\
            "Not accessible"
 
 
-def test_not_accessible_connection(mock_valid_url, mock_connection) -> None:
+def test_not_accessible_connection(
+        mock_valid_url: str, mock_connection: int
+) -> None:
     mock_valid_url.return_value = True
     mock_connection.return_value = False
     assert can_access_google_page("https://github.com/") ==\
            "Not accessible"
 
 
-def test_not_accessible(mock_valid_url, mock_connection) -> None:
+def test_not_accessible(
+        mock_valid_url: str, mock_connection: int
+) -> None:
     mock_valid_url.return_value = False
     mock_connection.return_value = False
     assert can_access_google_page("https://github.com/") ==\
            "Not accessible"
 
 
-def test_valid_url_and_connection(mock_valid_url, mock_connection) -> None:
+def test_valid_url_and_connection(
+        mock_valid_url: str, mock_connection: int
+) -> None:
     can_access_google_page("https://github.com/")
     mock_valid_url.assert_called_once()
     mock_connection.assert_called_once()
 
 
-def test_raise_missing_schema(mock_connection) -> None:
+def test_raise_missing_schema(mock_connection: int) -> None:
     with pytest.raises(MissingSchema):
         can_access_google_page(23)
