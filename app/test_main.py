61e6597ae9143953f1e138e21b012ea514483588
George <georgepavlej@gmail.com>
2022-10-28 21:47:33 +0100

    Solution to google page

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,66 @@
-# write your code here
+from app.main import can_access_google_page
+from typing import Callable
+import pytest
+from unittest import mock
+
+
+@pytest.fixture()
+def mock_valid_google_url() -> None:
+    with mock.patch("app.main.valid_google_url") \
+            as mock_valid_url:
+        yield mock_valid_url
+
+
+@pytest.fixture()
+def mock_internet_connection() -> None:
+    with mock.patch("app.main.has_internet_connection") \
+            as mock_connection:
+        yield mock_connection
+
+
+def test_true_if_valid_url_and_has_connection(
+        mock_valid_google_url: Callable,
+        mock_internet_connection: Callable) -> None:
+    mock_valid_google_url.return_value = True
+    mock_internet_connection.return_value = True
+
+    assert can_access_google_page("www.facebook.com") == "Accessible", (
+        "It is accessible when URL is valid and there is internet connection!"
+    )
+
+
+def test_test_cannot_access_if_only_valid_url(
+        mock_valid_google_url: Callable,
+        mock_internet_connection: Callable) -> None:
+
+    mock_valid_google_url.return_value = True
+    mock_internet_connection.return_value = False
+
+    assert can_access_google_page("www.facebook.com") == "Not accessible", (
+        "Can't access the page as there is no internet connection"
+    )
+
+
+def test_cannot_access_if_only_connection(
+        mock_valid_google_url: Callable,
+        mock_internet_connection: Callable) -> None:
+
+    mock_valid_google_url.return_value = False
+    mock_internet_connection.return_value = True
+
+    assert can_access_google_page("www.facebook.com") == "Not accessible", (
+        "Can't access the page as URL address is not valid"
+    )
+
+
+def test_cannot_access_not_valid_url_and_no_connection(
+        mock_valid_google_url: Callable,
+        mock_internet_connection: Callable) -> None:
+
+    mock_valid_google_url.return_value = False
+    mock_internet_connection.return_value = False
+
+    assert can_access_google_page("www.facebook.com") == "Not accessible", (
+        "Can't access the page as URL address is not valid"
+        "and no internet connection"
+    )
