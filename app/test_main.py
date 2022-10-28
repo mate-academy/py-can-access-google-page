1c122726299986b2d71d445dd79b762d65a382b4
Andrey Losev <andry07070712@gmail.comless.gitconfiggit config --global user.email andry07070712@gmail.com>
2022-10-28 17:19:51 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,47 @@
-# write your code here
+from unittest import mock
+from app.main import can_access_google_page
+
+
+@mock.patch("app.main.has_internet_connection")
+@mock.patch("app.main.valid_google_url")
+def test_can_access_google_page_when_true_false(mock_connection: callable,
+                                                mock_valid: callable) -> None:
+    mock_connection.return_value = True
+    mock_valid.return_value = False
+    url = "ccs.com"
+    assert can_access_google_page(url) == "Not accessible"
+    mock_connection.mock_valid.assert_called_once()
+
+
+@mock.patch("app.main.has_internet_connection")
+@mock.patch("app.main.valid_google_url")
+def test_can_access_google_page_when_false_true(mock_connection: callable,
+                                                mock_valid: callable) -> None:
+    mock_connection.return_value = False
+    mock_valid.return_value = True
+    url = "mate_academy.com"
+    assert can_access_google_page(url) == "Not accessible"
+    mock_connection.mock_valid.assert_called_once()
+
+
+@mock.patch("app.main.has_internet_connection")
+@mock.patch("app.main.valid_google_url")
+def test_can_access_google_page_when_all_true(mock_connection: callable,
+                                              mock_valid: callable) -> None:
+    mock_connection.return_value = True
+    mock_valid.return_value = True
+    url = "python.com"
+    assert can_access_google_page(url) == "Accessible"
+    mock_connection.mock_valid.assert_called_once()
+
+
+@mock.patch("app.main.has_internet_connection")
+@mock.patch("app.main.valid_google_url")
+def test_can_access_google_page_all_false(mock_connection: callable,
+                                          mock_valid: callable) -> None:
+    mock_connection.return_value = False
+    mock_valid.return_value = False
+    url = "github.com"
+    assert can_access_google_page(url) == "Not accessible"
+    mock_connection.mock_valid.assert_called_once()
+
