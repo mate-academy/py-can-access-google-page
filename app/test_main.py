904b5503556ef2fbf720a12c37cd69953b6e84db
muhactive <muhactive@gmail.com>
2022-10-31 18:34:13 +0200

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -40,4 +40,5 @@ def test_can_access_google_pege(mocked_valid_google_url,
                                 result):
     mocked_valid_google_url.return_value = url_value
     mocked_has_internet_connection.return_value = connection_value
-    assert can_access_google_page("") == resultefe
+    assert can_access_google_page("") == result
+b,
