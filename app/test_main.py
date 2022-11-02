72cdde06ee384cbf10f2b4ac3607b72e994456db
Mykola Zakharov <kolyazakharov94@gmail.com>
2022-11-02 22:30:14 +0200

    solution1

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,5 @@
 from unittest import mock
+
 from app.main import can_access_google_page
 
 
@@ -6,15 +7,33 @@ def test_function_return_correct_data() -> None:
     assert isinstance(can_access_google_page("https://www.google.com"), str)
 
 
+@mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_valid_url_and_connection(mocked_internet: mock) -> None:
-    mocked_internet.return_value = True
+def test_access_url_and_internet(mock_url: bool, mock_int: bool) -> None:
+    mock_url.return_value = True
+    mock_int.return_value = True
     assert can_access_google_page("https://www.google.com") == "Accessible"
-    mocked_internet.return_value = False
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_not_access_no_url_no_internet(mock_url: bool, mock_int: bool) -> None:
+    mock_url.return_value = False
+    mock_int.return_value = False
+    assert can_access_google_page("https://www.gle.com") == "Not accessible"
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_not_access_no_url(mock_url: bool, mock_int: bool) -> None:
+    mock_url.return_value = False
+    mock_int.return_value = True
+    assert can_access_google_page("https://www.gle.com") == "Not accessible"
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_not_access_no_internet(mock_url: bool, mock_int: bool) -> None:
+    mock_url.return_value = True
+    mock_int.return_value = False
     assert can_access_google_page("https://www.google.com") == "Not accessible"
-    mocked_internet.return_value = True
-    with mock.patch("app.main.valid_google_url") as mock_url:
-        mock_url.return_value = True
-        assert can_access_google_page("url") == "Accessible"
-        mock_url.return_value = False
-        assert can_access_google_page("url") == "Not accessible"
