1746f4260adec874e176ebd161c92cea2cf728a1
Taras Martyniuk <bythewaters800@gmail.com>
2022-11-07 15:10:08 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -30,3 +30,13 @@ def test_cannot_access_if_internet_false(mocked_valid: bool,
     mocked_valid.return_value = True
 
     assert can_access_google_page("https://stackoverflow") == "Not accessible"
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_cannot_access_if_all_false(mocked_valid: bool,
+                                    mocked_internet: bool) -> None:
+    mocked_internet.return_value = False
+    mocked_valid.return_value = False
+
+    assert can_access_google_page("https://stackoverflow") == "Not accessible"
