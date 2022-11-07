550b55b8aa8c1547588882d6a3ff4bf7bef70384
Oleksandr Kolomiiets <jumanje82@gmail.com>
2022-11-07 13:20:06 +0200

    'Sol_#2'

--- app/test_main.py
+++ app/test_main.py
@@ -27,3 +27,12 @@ def test_should_return_not_connection(mocked_url: mock,
     mocked_url.return_value = True
     mocked_connection.return_value = False
     assert can_access_google_page("good_url") == "Not accessible"
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_should_return_not_url_connection(mocked_url: mock,
+                                          mocked_connection: mock) -> None:
+    mocked_url.return_value = False
+    mocked_connection.return_value = False
+    assert can_access_google_page("good_url") == "Not accessible"
