cafb0847825cbc660e48caa7d3de41bd074df577
Mazur Mariya <mazurinka29@gmail.com>
2022-12-01 15:55:49 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -4,13 +4,14 @@ from unittest import mock
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_valid_url_and_connection_exists(mock_url: bool,
-                                         mock_connection: bool) -> None:
+def test_valid_url_and_connection_exists(
+        mock_url: bool,
+        mock_connection: bool) -> None:
     mock_url.return_value = True
     mock_connection.return_value = True
-    assert can_access_google_page("https://mate.academy/learn/python-core/"
-                                  "python-core-testing-in-"
-                                  "details") == "Accessible"
+    assert can_access_google_page(
+        "https://mate.academy/learn/python-core/python-core-testing-in-details"
+    ) == "Accessible"
 
 
 @mock.patch("app.main.valid_google_url")
@@ -18,9 +19,9 @@ def test_valid_url_and_connection_exists(mock_url: bool,
 def test_only_valid_url_exists(mock_url: bool, mock_connection: bool) -> None:
     mock_url.return_value = True
     mock_connection.return_value = False
-    assert can_access_google_page("https://mate.academy/learn/python-core/"
-                                  "python-core-testing-in-"
-                                  "details") == "Not accessible"
+    assert can_access_google_page(
+        "https://mate.academy/learn/python-core/python-core-testing-in-details"
+    ) == "Not accessible"
 
 
 @mock.patch("app.main.valid_google_url")
@@ -28,6 +29,6 @@ def test_only_valid_url_exists(mock_url: bool, mock_connection: bool) -> None:
 def test_only_connection_exists(mock_url: bool, mock_connection: bool) -> None:
     mock_url.return_value = False
     mock_connection.return_value = True
-    assert can_access_google_page("https://mate.academy/learn/python-core/"
-                                  "python-core-testing-in"
-                                  "-details") == "Not accessible"
+    assert can_access_google_page(
+        "https://mate.academy/learn/python-core/python-core-testing-in-details"
+    ) == "Not accessible"
