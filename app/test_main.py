f4fb8df8cc0a180ac24a665dbadf1d4f73edcc76
Ivan Mayor <maramorosh2@gmail.com>
2022-12-01 23:14:16 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -8,7 +8,8 @@ from app.main import can_access_google_page
 @mock.patch("app.main.has_internet_connection")
 def test_access_google_page_with_valid_url_and_time(
         mocked_url: bool,
-        mocked_internet_connection: bool) -> None:
+        mocked_internet_connection: bool
+) -> None:
 
     mocked_url.return_value = True
     mocked_internet_connection.return_value = True
@@ -19,7 +20,8 @@ def test_access_google_page_with_valid_url_and_time(
 @mock.patch("app.main.has_internet_connection")
 def test_access_google_page_with_not_valid_url_and_time(
         mocked_url: bool,
-        mocked_internet_connection: bool) -> None:
+        mocked_internet_connection: bool
+) -> None:
 
     mocked_url.return_value = False
     mocked_internet_connection.return_value = False
@@ -30,7 +32,8 @@ def test_access_google_page_with_not_valid_url_and_time(
 @mock.patch("app.main.has_internet_connection")
 def test_access_google_page_with_valid_url_and_not_valid_time(
         mocked_url: bool,
-        mocked_internet_connection: bool) -> None:
+        mocked_internet_connection: bool
+) -> None:
 
     mocked_url.return_value = True
     mocked_internet_connection.return_value = False
@@ -41,7 +44,8 @@ def test_access_google_page_with_valid_url_and_not_valid_time(
 @mock.patch("app.main.has_internet_connection")
 def test_access_google_page_with_not_valid_url_and_valid_time(
         mocked_url: bool,
-        mocked_internet_connection: bool) -> None:
+        mocked_internet_connection: bool
+) -> None:
 
     mocked_url.return_value = False
     mocked_internet_connection.return_value = True
