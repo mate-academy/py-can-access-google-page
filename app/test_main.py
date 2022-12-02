0e4899259b1171365cc2a6a9895506d0d41bfd0f
Vladyslav <jp6@mail.ru>
2022-12-02 10:10:31 +0000

    Fix

--- app/test_main.py
+++ app/test_main.py
@@ -1,7 +1,6 @@
-# git commit -am 'Solution'
 from unittest import mock
-from app.main import can_access_google_page
 
+from app.main import can_access_google_page
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
