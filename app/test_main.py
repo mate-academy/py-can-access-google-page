0932c27e2b43922dc16c4c5822b80b06c380be17
Maksym Maksimov <maksymmaksimov88@gmail.com>
2022-12-09 10:27:21 +0800

    Added test when has no internet and no valid URL

--- app/test_main.py
+++ app/test_main.py
@@ -38,6 +38,15 @@ def test_if_has_no_internet_connection(
     assert can_access_google_page("some url") == "Not accessible"
 
 
+def test_if_url_is_not_valid_and_no_internet_connection(
+        mocked_valid_url: Callable,
+        mocked_has_connection: Callable
+) -> None:
+    mocked_valid_url.return_value = False
+    mocked_has_connection.return_value = False
+    assert can_access_google_page("some url") == "Not accessible"
+
+
 def test_valid_url_and_connection_exists(
         mocked_valid_url: Callable,
         mocked_has_connection: Callable
