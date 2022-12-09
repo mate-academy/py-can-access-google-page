33d0c7f53010942596dfa2e902450542d5544434
soobig666 <darkvader.ukr.net@gmail.com>
2022-12-09 10:42:00 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -24,6 +24,16 @@ def test_url_false_and_connection_true(
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
+def test_url_false_and_connection_false(
+        mocked_url: bool,
+        mocked_check: bool) -> None:
+    mocked_url.return_value = False
+    mocked_check.return_value = False
+    assert can_access_google_page("url") == "Not accessible"
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
 def test_valid_url_and_connection_exists(
         mocked_url: bool,
         mocked_check: bool) -> None:
