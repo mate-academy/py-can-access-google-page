548968bca51524de930ed2fc91e132d566a79593
Kostyantyn Chernyk <chernykko@gmail.com>
2022-12-15 10:00:56 +0000

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -24,3 +24,11 @@ def test_url_is_not_valid(mocked_connection: callable,
                           mocked_url: callable) -> None:
     assert can_access_google_page("https://www.google.com/") == \
            "Not accessible"
+
+
+@mock.patch("app.main.valid_google_url", return_value=False)
+@mock.patch("app.main.has_internet_connection", return_value=False)
+def test_url_is_not_valid_and_no_connection(mocked_connection: callable,
+                                            mocked_url: callable) -> None:
+    assert can_access_google_page("https://www.google.com/") == \
+           "Not accessible"
