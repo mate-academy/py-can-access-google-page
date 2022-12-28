440dd9f4ee5f9a58e44314878f4de5fd4de43ae6
Oleksandr Rozsokha <oleksandrozsokha@gmail.com>
2022-12-28 12:48:26 +0200

    add test block

--- app/test_main.py
+++ app/test_main.py
@@ -34,3 +34,14 @@ def test_should_not_give_access_if_url_is_not_valid(
     mocked_url.return_value = False
     mocked_connection.return_value = True
     assert can_access_google_page(mocked_url) == "Not accessible"
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_should_not_give_access_if_url_and_internet_connection_is_false(
+        mocked_connection: mock,
+        mocked_url: mock
+) -> None:
+    mocked_url.return_value = False
+    mocked_connection.return_value = False
+    assert can_access_google_page(mocked_url) == "Not accessible"
