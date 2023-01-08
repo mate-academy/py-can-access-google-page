6fca381bc99701fe7b140036d9c1842751d102b2
Tania Kharchuk <tkharchuk14@gmail.com>
2023-01-08 20:22:29 +0200

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -1,47 +1,61 @@
 from unittest import mock
 
+import pytest
+
 from app.main import can_access_google_page
 
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page_when_has_internet_and_url_is_valid(
-        mocked_has_internet_connection: callable,
-        mocked_valid_google_url: callable
+@pytest.mark.parametrize(
+    "has_internet_connection, valid_google_url, access_google_page",
+    [
+        (True, True, "Accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible"),
+        (True, False, "Not accessible"),
+    ]
+)
+def test_can_access_google_page(
+        mocked_has_internet_connection: bool,
+        mocked_valid_google_url: bool,
+        has_internet_connection: bool,
+        valid_google_url: bool,
+        access_google_page: str
 ) -> None:
-    mocked_has_internet_connection.return_value = True
-    mocked_valid_google_url.return_value = True
-    assert can_access_google_page("example_of_url") == "Accessible"
+    mocked_has_internet_connection.return_value = has_internet_connection
+    mocked_valid_google_url.return_value = valid_google_url
+    assert can_access_google_page("example_of_url") == access_google_page
 
 
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page_when_has_not_internet_and_url_is_valid(
-        mocked_has_internet_connection: callable,
-        mocked_valid_google_url: callable
-) -> None:
-    mocked_has_internet_connection.return_value = False
-    mocked_valid_google_url.return_value = True
-    assert can_access_google_page("example_of_url") == "Not accessible"
-
-
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page_when_has_internet_and_url_is_not_valid(
-        mocked_has_internet_connection: callable,
-        mocked_valid_google_url: callable
-) -> None:
-    mocked_has_internet_connection.return_value = True
-    mocked_valid_google_url.return_value = False
-    assert can_access_google_page("example_of_url") == "Not accessible"
-
-
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page_when_has_not_internet_and_url_is_not_valid(
-        mocked_has_internet_connection: callable,
-        mocked_valid_google_url: callable
-) -> None:
-    mocked_has_internet_connection.return_value = False
-    mocked_valid_google_url.return_value = False
-    assert can_access_google_page("example_of_url") == "Not accessible"
+# @mock.patch("app.main.valid_google_url")
+# @mock.patch("app.main.has_internet_connection")
+# def test_can_access_google_page_when_has_not_internet_and_url_is_valid(
+#         mocked_has_internet_connection: callable,
+#         mocked_valid_google_url: callable
+# ) -> None:
+#     mocked_has_internet_connection.return_value = False
+#     mocked_valid_google_url.return_value = True
+#     assert can_access_google_page("example_of_url") == "Not accessible"
+#
+#
+# @mock.patch("app.main.valid_google_url")
+# @mock.patch("app.main.has_internet_connection")
+# def test_can_access_google_page_when_has_internet_and_url_is_not_valid(
+#         mocked_has_internet_connection: callable,
+#         mocked_valid_google_url: callable
+# ) -> None:
+#     mocked_has_internet_connection.return_value = True
+#     mocked_valid_google_url.return_value = False
+#     assert can_access_google_page("example_of_url") == "Not accessible"
+#
+#
+# @mock.patch("app.main.valid_google_url")
+# @mock.patch("app.main.has_internet_connection")
+# def test_can_access_google_page_when_has_not_internet_and_url_is_not_valid(
+#         mocked_has_internet_connection: callable,
+#         mocked_valid_google_url: callable
+# ) -> None:
+#     mocked_has_internet_connection.return_value = False
+#     mocked_valid_google_url.return_value = False
+#     assert can_access_google_page("example_of_url") == "Not accessible"
