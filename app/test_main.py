575f32cc71618c2dc25e36abfe98bbf65f6097a3
Tania Kharchuk <tkharchuk14@gmail.com>
2023-01-08 20:23:09 +0200

    Solution3

--- app/test_main.py
+++ app/test_main.py
@@ -26,36 +26,3 @@ def test_can_access_google_page(
     mocked_has_internet_connection.return_value = has_internet_connection
     mocked_valid_google_url.return_value = valid_google_url
     assert can_access_google_page("example_of_url") == access_google_page
-
-
-# @mock.patch("app.main.valid_google_url")
-# @mock.patch("app.main.has_internet_connection")
-# def test_can_access_google_page_when_has_not_internet_and_url_is_valid(
-#         mocked_has_internet_connection: callable,
-#         mocked_valid_google_url: callable
-# ) -> None:
-#     mocked_has_internet_connection.return_value = False
-#     mocked_valid_google_url.return_value = True
-#     assert can_access_google_page("example_of_url") == "Not accessible"
-#
-#
-# @mock.patch("app.main.valid_google_url")
-# @mock.patch("app.main.has_internet_connection")
-# def test_can_access_google_page_when_has_internet_and_url_is_not_valid(
-#         mocked_has_internet_connection: callable,
-#         mocked_valid_google_url: callable
-# ) -> None:
-#     mocked_has_internet_connection.return_value = True
-#     mocked_valid_google_url.return_value = False
-#     assert can_access_google_page("example_of_url") == "Not accessible"
-#
-#
-# @mock.patch("app.main.valid_google_url")
-# @mock.patch("app.main.has_internet_connection")
-# def test_can_access_google_page_when_has_not_internet_and_url_is_not_valid(
-#         mocked_has_internet_connection: callable,
-#         mocked_valid_google_url: callable
-# ) -> None:
-#     mocked_has_internet_connection.return_value = False
-#     mocked_valid_google_url.return_value = False
-#     assert can_access_google_page("example_of_url") == "Not accessible"
