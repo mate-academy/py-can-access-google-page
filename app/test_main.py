5a529729a07a50fe032b3cf2fef3e6456a7ea2cf
6aPa6aH <6apa6ah666@gmail.com>
2023-01-20 15:53:36 +0200

    fix_code

--- app/test_main.py
+++ app/test_main.py
@@ -35,8 +35,8 @@ def test_can_access_to_page(
         current_url: bool,
         exception_message: str
 ) -> None:
-    with mock.patch("app.main.valid_google_url") as mock_url:
-        with mock.patch("app.main.has_internet_connection") as mock_connection:
-            mock_url.return_value = current_url
-            mock_connection.return_value = current_time
-            assert can_access_google_page("google.com") == exception_message
+    with mock.patch("app.main.valid_google_url") as mock_url, \
+         mock.patch("app.main.has_internet_connection") as mock_connection:
+        mock_url.return_value = current_url
+        mock_connection.return_value = current_time
+        assert can_access_google_page("google.com") == exception_message
