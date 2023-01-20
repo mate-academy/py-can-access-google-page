2243a33e9ac1c8953cb4399c93ac3d6872fb1575
6aPa6aH <6apa6ah666@gmail.com>
2023-01-20 14:33:56 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -9,29 +9,34 @@ from app.main import can_access_google_page
     "current_time,current_url,exception_message",
     [
         (
-            7,
-            "https://google.com",
+            True,
+            True,
             "Accessible"
         ),
         (
             False,
-            "http://google.com",
+            True,
             "Not accessible"
         ),
         (
-            9,
+            True,
+            False,
+            "Not accessible"
+        ),
+        (
+            False,
             False,
             "Not accessible"
         )
     ]
 )
 def test_can_access_to_page(
-        current_time: int,
-        current_url: str,
+        current_time: bool,
+        current_url: bool,
         exception_message: str
 ) -> None:
     with mock.patch("app.main.valid_google_url") as mock_url:
         with mock.patch("app.main.has_internet_connection") as mock_connection:
             mock_url.return_value = current_url
             mock_connection.return_value = current_time
-            assert can_access_google_page(current_url) == exception_message
+            assert can_access_google_page("google.com") == exception_message
