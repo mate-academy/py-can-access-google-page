b107864dd87fc76baff9b25a64e4d1dc8586b091
Taras Savchyn <savik1992@gmail.com>
2023-02-06 18:33:24 +0200

    rename variable ic to connection

--- app/test_main.py
+++ app/test_main.py
@@ -13,9 +13,9 @@ def test_test_main_when_internet_connection_and_valid_url(
 
 def test_test_main_when_no_valid_url(
 ) -> None:
-    with (mock.patch("app.main.has_internet_connection") as ic,
+    with (mock.patch("app.main.has_internet_connection") as connection,
           mock.patch("app.main.valid_google_url") as url):
-        ic.return_value = True
+        connection.return_value = True
         url.return_value = False
         assert can_access_google_page(
             "http://www.google.com") == "Not accessible"
@@ -23,9 +23,9 @@ def test_test_main_when_no_valid_url(
 
 def test_test_main_when_no_internet_connection(
 ) -> None:
-    with (mock.patch("app.main.has_internet_connection") as ic,
+    with (mock.patch("app.main.has_internet_connection") as connection,
           mock.patch("app.main.valid_google_url") as url):
-        ic.return_value = False
+        connection.return_value = False
         url.return_value = True
         assert can_access_google_page(
             "http://www.google.com") == "Not accessible"
