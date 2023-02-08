98d2544112c4c1682d16973ac008413a747c7e49
Taras Savchyn <savik1992@gmail.com>
2023-02-08 17:47:56 +0200

    use "pytest.mark.parametrize"

--- app/test_main.py
+++ app/test_main.py
@@ -1,31 +1,23 @@
 from unittest import mock
 from app.main import can_access_google_page
+import pytest
 
 
+@pytest.mark.parametrize(
+    "func_1_result,func_2_result,total_result",
+    [
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible")
+    ]
+)
 def test_test_main_when_internet_connection_and_valid_url(
+        func_1_result: bool,
+        func_2_result: bool,
+        total_result: str
 ) -> None:
     with (mock.patch("app.main.has_internet_connection") as ic,
           mock.patch("app.main.valid_google_url") as url):
-        ic.return_value = True
-        url.return_value = True
-        assert can_access_google_page("http://www.google.com") == "Accessible"
-
-
-def test_test_main_when_no_valid_url(
-) -> None:
-    with (mock.patch("app.main.has_internet_connection") as connection,
-          mock.patch("app.main.valid_google_url") as url):
-        connection.return_value = True
-        url.return_value = False
-        assert can_access_google_page(
-            "http://www.google.com") == "Not accessible"
-
-
-def test_test_main_when_no_internet_connection(
-) -> None:
-    with (mock.patch("app.main.has_internet_connection") as connection,
-          mock.patch("app.main.valid_google_url") as url):
-        connection.return_value = False
-        url.return_value = True
-        assert can_access_google_page(
-            "http://www.google.com") == "Not accessible"
+        ic.return_value = func_1_result
+        url.return_value = func_2_result
+        assert can_access_google_page("http://www.google.com") == total_result
