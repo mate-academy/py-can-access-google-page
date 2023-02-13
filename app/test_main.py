a84decf7ccd2190794b7bc320e8fe036533d7999
Vyacheslav Kungin <acheros4@gmail.com>
2023-02-13 16:52:16 +0200

    Ðremove code duplication

--- app/test_main.py
+++ app/test_main.py
@@ -1,40 +1,41 @@
+import pytest
 from unittest import mock
-from typing import Any
+from _pytest.mark import ParameterSet
 
 from app.main import can_access_google_page
 
 
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_only_connection_false(
-    mocked_url: Any,
-    mocked_connection: Any
-) -> None:
-    mocked_url.return_value = True
-    mocked_connection.return_value = False
+class TestAccessGooglePage:
+    @pytest.mark.parametrize(
+        "url_return_value, connection_return_value",
+        [
+            pytest.param(
+                True,
+                False,
+                id="only connection false"
+            ),
+            pytest.param(
+                False,
+                True,
+                id="only url false"
+            ),
+            pytest.param(
+                False,
+                False,
+                id="connection and url false"
+            )
+        ]
+    )
+    def test_access(
+        self,
+        url_return_value: ParameterSet,
+        connection_return_value: ParameterSet
+    ) -> None:
+        with (
+            mock.patch("app.main.valid_google_url") as mocked_url,
+            mock.patch("app.main.has_internet_connection") as mocked_connection
+        ):
+            mocked_url.return_value = url_return_value
+            mocked_connection.return_value = connection_return_value
 
-    assert can_access_google_page("google.com") == "Not accessible"
-
-
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_only_url_false(
-    mocked_url: Any,
-    mocked_connection: Any
-) -> None:
-    mocked_url.return_value = False
-    mocked_connection.return_value = True
-
-    assert can_access_google_page("google.com") == "Not accessible"
-
-
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_url_and_connection_false(
-    mocked_url: Any,
-    mocked_connection: Any
-) -> None:
-    mocked_url.return_value = False
-    mocked_connection.return_value = False
-
-    assert can_access_google_page("google.com") == "Not accessible"
+            assert can_access_google_page("google.com") == "Not accessible"
