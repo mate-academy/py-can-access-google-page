e492e1158879b84e737203970f5e58e5d1c15742
Roman Senyshyn <senjaskorp@gmail.com>
2023-02-15 20:56:26 +0200

    1 commit

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,28 @@
-# write your code here
+from app.main import can_access_google_page
+
+from unittest import mock
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "valid_url, has_internet, result",
+    [
+        (True, True, "Accessible"),
+        (False, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (True, False, "Not accessible"),
+    ]
+)
+@mock.patch("app.main.has_internet_connection")
+@mock.patch("app.main.valid_google_url")
+def test_access_to_google(
+        mock_has_internet_connection: mock,
+        mock_valid_google_url: mock,
+        valid_url: bool,
+        has_internet: bool,
+        result: str
+) -> None:
+    mock_has_internet_connection.return_value = has_internet
+    mock_valid_google_url.return_value = valid_url
+    assert can_access_google_page("google.com") == result
