dc2b1ecd76b4059c684fde6a94be06d50d7f2fbf
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-02-21 17:58:39 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,27 @@
-# write your code here
+import pytest
+from unittest import mock
+from unittest.mock import MagicMock
+
+import app.main
+
+
+@pytest.mark.parametrize(
+    "internet_connection_flag,response_status,expected_result",
+    [
+        (True, True, "Accessible"),
+        (False, True, "Not accessible"),
+        (True, False, "Not accessible"),
+    ]
+)
+@mock.patch("app.main.has_internet_connection")
+@mock.patch("app.main.valid_google_url")
+def test_can_access_google_page(
+        mocked_has_internet_connection: MagicMock,
+        mocked_valid_google_url: MagicMock,
+        internet_connection_flag: bool,
+        response_status: bool,
+        expected_result: str
+) -> None:
+    mocked_has_internet_connection.return_value = internet_connection_flag
+    mocked_valid_google_url.return_value = response_status
+    assert app.main.can_access_google_page("") == expected_result
