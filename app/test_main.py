4ddecb40cc29fa74edc60e5fc22c6d7eca4cf317
drapail <mv.dibrov@gmail.com>
2023-02-22 22:32:54 +0100

    refactored tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,52 +1,39 @@
-from app import main
-from app.main import can_access_google_page
-from typing import Generator
+from unittest import mock
+from unittest.mock import MagicMock
+
+from app.main import (can_access_google_page)
 
 # write your code here
+valid_url = "https://www.google.com/"
+invalid_url = "google"
 
 
-def test_can_access_google_page_ok(monkeypatch: Generator) -> None:
-    valid_url = "www.google.com"
-
-    def mock_valid_google_url(url: str) -> bool:
-        return True
-
-    def mock_has_internet_connection() -> bool:
-        return True
-
-    monkeypatch.setattr(main, "valid_google_url", mock_valid_google_url)
-    monkeypatch.setattr(main, "has_internet_connection",
-                        mock_has_internet_connection)
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_can_access_google_page_ok(mocked_valid_google_url: MagicMock,
+                                   mock_has_internet_connection: MagicMock)\
+        -> None:
     assert can_access_google_page(valid_url) == "Accessible"
 
 
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_has_internet_connection_is_called_ok(mock_valid_google_url: MagicMock,
+                                              mock_has_internet_connection:
+                                              MagicMock) -> None:
+    can_access_google_page(valid_url)
+    mock_has_internet_connection.assert_called_once()
+
+
+@mock.patch("app.main.has_internet_connection")
 def test_can_access_google_page_without_internet_not_ok(
-        monkeypatch: Generator) -> None:
-    valid_url = "www.google.com"
-
-    def mock_valid_google_url(url: str) -> bool:
-        return True
-
-    def mock_has_internet_connection() -> bool:
-        return False
-
-    monkeypatch.setattr(main, "valid_google_url", mock_valid_google_url)
-    monkeypatch.setattr(main, "has_internet_connection",
-                        mock_has_internet_connection)
+        mock_has_internet_connection: MagicMock) -> None:
+    mock_has_internet_connection.return_value = False
     assert can_access_google_page(valid_url) == "Not accessible"
 
 
+@mock.patch("app.main.valid_google_url")
 def test_can_access_google_page_invalid_url_not_ok(
-        monkeypatch: Generator) -> None:
-    valid_url = "google"
-
-    def mock_valid_google_url(url: str) -> bool:
-        return False
-
-    def mock_has_internet_connection() -> bool:
-        return True
-
-    monkeypatch.setattr(main, "valid_google_url", mock_valid_google_url)
-    monkeypatch.setattr(main, "has_internet_connection",
-                        mock_has_internet_connection)
-    assert can_access_google_page(valid_url) == "Not accessible"
+        mock_valid_google_url: MagicMock) -> None:
+    mock_valid_google_url.return_value = False
+    assert can_access_google_page(invalid_url) == "Not accessible"
