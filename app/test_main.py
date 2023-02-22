143951ec70342c4d78a82a48cb2c70bfa53bb6ba
David Gluschenko <davidglusch@gmail.com>
2023-02-22 11:19:13 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -4,12 +4,12 @@ from app.main import can_access_google_page
 
 
 @pytest.mark.parametrize(
-    "url, valid_google_url, has_internet_connection, result",
+    "valid_google_url, has_internet_connection, result",
     [
-        ("google.com", True, True, "Accessible"),
-        ("1234.com", False, True, "Not accessible"),
-        ("google.com", True, False, "Not accessible"),
-        ("5678.com", False, False, "Not accessible")
+        (True, True, "Accessible"),
+        (False, True, "Not accessible"),
+        (True, False, "Not accessible"),
+        (False, False, "Not accessible")
     ],
     ids=["test for correct url and right time of the day",
          "test for incorrect url and right time of the day",
@@ -21,7 +21,6 @@ from app.main import can_access_google_page
 def test_can_access_google_page(
         mock_valid_google_url: bool,
         mock_has_internet_connection: bool,
-        url: str,
         valid_google_url: bool,
         has_internet_connection: bool,
         result: str
@@ -29,4 +28,4 @@ def test_can_access_google_page(
 ) -> None:
     mock_valid_google_url.return_value = valid_google_url
     mock_has_internet_connection.return_value = has_internet_connection
-    assert can_access_google_page(url) == result
+    assert can_access_google_page("google.com") == result
