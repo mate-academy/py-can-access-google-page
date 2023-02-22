cd3791758bcaa1c3bed56de796eee14f5474151c
kuznetsb <kuznetsbog@gmail.com>
2023-02-22 16:07:46 +0200

    annotations added

--- app/test_main.py
+++ app/test_main.py
@@ -6,28 +6,35 @@ from app.main import can_access_google_page
 
 
 @pytest.fixture()
-def mocked_url_validation():
+def mocked_url_validation() -> None:
     with mock.patch("app.main.valid_google_url") as mock_check_url:
         yield mock_check_url
 
 
 @pytest.fixture()
-def mocked_internet_connection():
+def mocked_internet_connection() -> None:
     with mock.patch("app.main.has_internet_connection") as mock_connection:
         yield mock_connection
 
 
-def test_should_access_google_page(mocked_url_validation, mocked_internet_connection) -> None:
+def test_should_access_google_page(
+        mocked_url_validation: object,
+        mocked_internet_connection: object
+) -> None:
     mocked_url_validation.return_value = True
     mocked_internet_connection.return_value = True
     assert can_access_google_page("youtube.com") == "Accessible"
 
 
-def test_should_not_access_if_url_invalid(mocked_url_validation) -> None:
+def test_should_not_access_if_url_invalid(
+        mocked_url_validation: object
+) -> None:
     mocked_url_validation.return_value = False
     assert can_access_google_page("maate.academy") == "Not accessible"
 
 
-def test_should_not_access_if_no_connection(mocked_internet_connection) -> None:
+def test_should_not_access_if_no_connection(
+        mocked_internet_connection: object
+) -> None:
     mocked_internet_connection.return_value = False
     assert can_access_google_page("facebook.com") == "Not accessible"
