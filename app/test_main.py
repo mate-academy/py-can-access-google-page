7529f547c8f93bc7ef7edce1134982c2cc795db3
Oksana Dushna <oksana.dushna.1996@gmail.com>
2023-02-23 20:49:58 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,42 +1,36 @@
-from unittest import TestCase
+from typing import Callable
 from unittest.mock import patch
 from app.main import can_access_google_page
-from typing import Callable
 
 
-class TestAccess(TestCase):
+url = "http://www.google.com"
 
-    @patch("app.main.valid_google_url")
-    @patch("app.main.has_internet_connection")
-    def test_is_accessible(
-            self,
-            mock_internet: Callable,
-            mock_url: Callable
-    ) -> None:
-        mock_internet.return_value = True
-        mock_url.return_value = True
-        assert can_access_google_page("http://www.google.com") == "Accessible"
 
-    @patch("app.main.valid_google_url")
-    @patch("app.main.has_internet_connection")
-    def test_is_not_accessible_because_of_connection(
-            self,
-            mock_internet: Callable,
-            mock_url: Callable
-    ) -> None:
-        mock_internet.return_value = False
-        mock_url.return_value = True
-        assert (can_access_google_page("http://www.google.com")
-                == "Not accessible")
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
+def test_is_accessible(mock_internet: Callable, mock_url: Callable) -> None:
+    mock_internet.return_value = True
+    mock_url.return_value = True
+    assert can_access_google_page(url) == "Accessible"
 
-    @patch("app.main.valid_google_url")
-    @patch("app.main.has_internet_connection")
-    def test_is_not_accessible_because_of_url(
-            self,
-            mock_internet: Callable,
-            mock_url: Callable
-    ) -> None:
-        mock_internet.return_value = True
-        mock_url.return_value = False
-        assert (can_access_google_page("http://www.notgoogle.com")
-                == "Not accessible")
+
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
+def test_is_not_accessible_because_of_connection(
+        mock_internet: Callable,
+        mock_url: Callable
+) -> None:
+    mock_internet.return_value = False
+    mock_url.return_value = True
+    assert can_access_google_page(url) == "Not accessible"
+
+
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
+def test_is_not_accessible_because_of_url(
+        mock_internet: Callable,
+        mock_url: Callable
+) -> None:
+    mock_internet.return_value = True
+    mock_url.return_value = False
+    assert can_access_google_page(url) == "Not accessible"
