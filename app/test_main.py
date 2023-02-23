23d46dfd8cb9c1ed7c9708003fa182e4363a73c8
Ruslan <noezyboy1@gmail.com>
2023-02-23 18:47:45 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -5,42 +5,24 @@ url = "https://www.notavalidurl.com"
 
 
 def test_can_access_google_page_access_granted() -> None:
-    with patch("app.main.has_internet_connection", return_value=True), \
-         patch("app.main.valid_google_url", return_value=True):
+    with patch("app.main.has_internet_connection", return_value=True), (
+         patch("app.main.valid_google_url", return_value=True)):
         assert can_access_google_page(url) == "Accessible"
 
 
 def test_can_access_google_page_no_internet() -> None:
-    with patch("app.main.has_internet_connection", return_value=False), \
-         patch("app.main.valid_google_url", return_value=True):
+    with patch("app.main.has_internet_connection", return_value=False), (
+         patch("app.main.valid_google_url", return_value=True)):
         assert can_access_google_page(url) == "Not accessible"
 
 
 def test_can_access_google_page_invalid_url() -> None:
-    with patch("app.main.has_internet_connection", return_value=True), \
-         patch("app.main.valid_google_url", return_value=False):
+    with patch("app.main.has_internet_connection", return_value=True), (
+         patch("app.main.valid_google_url", return_value=False)):
         assert can_access_google_page(url) == "Not accessible"
 
 
 def test_cannot_access_if_both_connection_and_valid_url_are_false() -> None:
-    with patch("app.main.has_internet_connection", return_value=False), \
-         patch("app.main.valid_google_url", return_value=False):
+    with patch("app.main.has_internet_connection", return_value=False), (
+         patch("app.main.valid_google_url", return_value=False)):
         assert can_access_google_page(url) == "Not accessible"
-
-
-def test_cannot_access_if_only_connection() -> None:
-    with patch("app.main.has_internet_connection", return_value=True), \
-         patch("app.main.valid_google_url", return_value=False):
-        assert can_access_google_page(url) == "Not accessible"
-
-
-def test_cannot_access_if_only_valid_url() -> None:
-    with patch("app.main.has_internet_connection", return_value=False), \
-         patch("app.main.valid_google_url", return_value=True):
-        assert can_access_google_page(url) == "Not accessible"
-
-
-def test_connection_and_valid_url_are_true() -> None:
-    with patch("app.main.has_internet_connection", return_value=True), \
-         patch("app.main.valid_google_url", return_value=True):
-        assert can_access_google_page(url) == "Accessible"
