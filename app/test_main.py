abba9915b639264afbe156ecb2b39bc04df247e3
oksana-feshchenko <foksanv@gmail.com>
2023-02-23 18:48:47 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,9 @@
 from unittest import mock
 from typing import Callable
+
 from app.main import can_access_google_page
+
+URL = "www.google.com"
 
 
 @mock.patch("app.main.valid_google_url")
@@ -10,7 +13,7 @@ def test_not_valid_url(
 ) -> bool:
     mocked_valid_url.return_value = False
     mocked_internet_connection.return_value = True
-    assert can_access_google_page("www.google.com") == "Not accessible"
+    assert can_access_google_page(URL) == "Not accessible"
 
 
 @mock.patch("app.main.valid_google_url")
@@ -20,7 +23,7 @@ def test_no_internet_connection(
 ) -> bool:
     mocked_valid_url.return_value = True
     mocked_internet_connection.return_value = False
-    assert can_access_google_page("www.google.com") == "Not accessible"
+    assert can_access_google_page(URL) == "Not accessible"
 
 
 @mock.patch("app.main.valid_google_url")
@@ -30,7 +33,7 @@ def test_are_internet_connection_valid_url(
 ) -> bool:
     mocked_valid_url.return_value = True
     mocked_internet_connection.return_value = True
-    assert can_access_google_page("www.google.com") == "Accessible"
+    assert can_access_google_page(URL) == "Accessible"
 
 
 @mock.patch("app.main.valid_google_url")
@@ -40,4 +43,4 @@ def test_no_internet_connection_not_valid_url(
 ) -> bool:
     mocked_valid_url.return_value = False
     mocked_internet_connection.return_value = False
-    assert can_access_google_page("www.google.com") == "Not accessible"
+    assert can_access_google_page(URL) == "Not accessible"
