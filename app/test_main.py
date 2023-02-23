4afdd65c502c2c3f4c71d9d11dabcd9d89f366d7
Ada Kravchenko <ada.t.krav@gmail.com>
2023-02-23 20:29:38 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,32 @@
-# write your code here
+from pytest import mark, MonkeyPatch
+
+from app.main import can_access_google_page
+
+URL = "Google url"
+
+
+@mark.parametrize(
+    "valid_url, connected, can_access",
+    [
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible"),
+    ],
+    ids=[
+        "True + True -> must be Accessible",
+        "True + False -> must be Not accessible",
+        "False + True -> must be Not accessible",
+        "False + False -> must be Not accessible",
+    ],
+)
+def test_sell_all_cryptocurrency(
+    monkeypatch: MonkeyPatch, valid_url: bool, connected: bool, can_access: str
+) -> None:
+    monkeypatch.setattr(
+        "app.main.has_internet_connection", lambda *args: connected
+    )
+    monkeypatch.setattr(
+        "app.main.valid_google_url", lambda *args: valid_url
+    )
+    assert can_access_google_page(URL) == can_access
