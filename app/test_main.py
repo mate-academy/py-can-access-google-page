43474a1450c7882ebb7b87eb4c9af08bd834a573
Teliks <hapyto371@gmail.com>
2023-02-23 20:08:59 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,26 +2,28 @@ from unittest import mock
 
 from app.main import can_access_google_page
 
+link = "https://www.youtube.com/"
+
 
 @mock.patch("app.main.valid_google_url", return_value=True)
 @mock.patch("app.main.has_internet_connection", return_value=True)
 def test_access_google_page(url: mock, internet: mock) -> None:
-    assert can_access_google_page("https://www.google.com/") == "Accessible"
+    assert can_access_google_page(link) == "Accessible"
 
 
 @mock.patch("app.main.valid_google_url", return_value=False)
 @mock.patch("app.main.has_internet_connection", return_value=True)
 def test_with_rong_url_address(url: mock, internet: mock) -> None:
-    assert can_access_google_page("https:/www.smorfle.com") == "Not accessible"
+    assert can_access_google_page(link) == "Not accessible"
 
 
 @mock.patch("app.main.valid_google_url", return_value=True)
 @mock.patch("app.main.has_internet_connection", return_value=False)
 def test_with_no_internet_connection(url: mock, internet: mock) -> None:
-    assert can_access_google_page("https://www.google.com") == "Not accessible"
+    assert can_access_google_page(link) == "Not accessible"
 
 
 @mock.patch("app.main.valid_google_url", return_value=False)
 @mock.patch("app.main.has_internet_connection", return_value=False)
 def test_with_no_connection_and_wrong_url(url: mock, internet: mock) -> None:
-    assert can_access_google_page("https:/www.smorfle.com") == "Not accessible"
+    assert can_access_google_page(link) == "Not accessible"
