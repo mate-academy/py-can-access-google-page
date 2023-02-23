23589103b8b5b2c1ead497b9859f06e4b293df23
name <ksyusha0kubay@gmail.com>
2023-02-23 09:33:37 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,46 @@
-# write your code here
+from unittest import mock
+from app.main import can_access_google_page
+
+
+@mock.patch("app.main.has_internet_connection", result_value=True)
+@mock.patch("app.main.valid_google_url", result_value=True)
+def test_valid_url_and_has_internet_connection(
+        mock_valid_url: mock,
+        mock_has_connection: mock
+) -> None:
+    assert can_access_google_page(
+        "https://mate.academy/learn?course=python"
+    ) == "Accessible"
+
+
+@mock.patch("app.main.has_internet_connection", return_value=False)
+@mock.patch("app.main.valid_google_url", return_value=True)
+def test_valid_url_and_has_no_internet_connection(
+        mock_valid_url: mock,
+        mock_has_connection: mock
+) -> None:
+    assert can_access_google_page(
+        "https://mate.academy/events/english-lesson"
+    ) == "Not accessible"
+
+
+@mock.patch("app.main.has_internet_connection", return_value=False)
+@mock.patch("app.main.valid_google_url", return_value=False)
+def test_invalid_url_and_has_no_internet_connection(
+        mock_valid_url: mock,
+        mock_has_connection: mock
+) -> None:
+    assert can_access_google_page(
+        "https://www.wikipedia.org/"
+    ) == "Not accessible"
+
+
+@mock.patch("app.main.has_internet_connection", return_value=True)
+@mock.patch("app.main.valid_google_url", return_value=False)
+def test_invalid_url_and_has_internet_connection(
+        mock_valid_url: mock,
+        mock_has_connection: mock
+) -> None:
+    assert can_access_google_page(
+        "https://www.youtube.com/"
+    ) == "Not accessible"
