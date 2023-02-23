384d437aa9744e2c2bf3cb8c2b273a5533a40e55
Ruslan <noezyboy1@gmail.com>
2023-02-23 12:05:52 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,11 +1,11 @@
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 from app.main import can_access_google_page
 
 
 @patch("app.main.has_internet_connection", return_value=True)
 @patch("app.main.valid_google_url", return_value=True)
-def test_can_access_google_page_access_granted(mock_valid_url: str,
-                                               mock_internet: str) -> None:
+def test_can_access_google_page_access_granted(mock_valid_url: MagicMock,
+                                               mock_internet: MagicMock) -> None:
     url = "https://www.google.com"
     result = can_access_google_page(url)
     assert result == "Accessible"
@@ -13,8 +13,8 @@ def test_can_access_google_page_access_granted(mock_valid_url: str,
 
 @patch("app.main.has_internet_connection", return_value=False)
 @patch("app.main.valid_google_url", return_value=True)
-def test_can_access_google_page_no_internet(mock_valid_url: str,
-                                            mock_internet: str) -> None:
+def test_can_access_google_page_no_internet(mock_valid_url: MagicMock,
+                                            mock_internet: MagicMock) -> None:
     url = "https://www.google.com"
     result = can_access_google_page(url)
     assert result == "Not accessible"
@@ -22,8 +22,8 @@ def test_can_access_google_page_no_internet(mock_valid_url: str,
 
 @patch("app.main.has_internet_connection", return_value=True)
 @patch("app.main.valid_google_url", return_value=False)
-def test_can_access_google_page_invalid_url(mock_valid_url: str,
-                                            mock_internet: str) -> None:
+def test_can_access_google_page_invalid_url(mock_valid_url: MagicMock,
+                                            mock_internet: MagicMock) -> None:
     url = "https://www.notavalidurl.com"
     result = can_access_google_page(url)
     assert result == "Not accessible"
