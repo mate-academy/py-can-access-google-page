0f6f9d122e401553c5193e591d4690fdc5c11404
Yevhen Kaskov <kaskov.e@gmail.com>
2023-02-23 19:14:51 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,31 @@
-# write your code here
+from unittest import mock
+import pytest
+
+from app.main import can_access_google_page
+
+
+@pytest.mark.parametrize(
+    "valid_url, connection, access",
+    [
+        pytest.param(True, True, "Accessible",
+                     id="Test can access. All valid"),
+        pytest.param(True, False, "Not accessible",
+                     id="Test can access. Only has connection"),
+        pytest.param(False, True, "Not accessible",
+                     id="Test can access. Only valid url"),
+        pytest.param(False, False, "Not accessible",
+                     id="Test can access. Not connect, not valid"),
+    ]
+)
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_can_access_google_page(
+        mocked_valid: mock,
+        mocked_connection: mock,
+        valid_url: bool,
+        connection: bool,
+        access: str
+) -> None:
+    mocked_valid.return_value = valid_url
+    mocked_connection.return_value = connection
+    assert can_access_google_page("https://www.google.com.ua/") == access
