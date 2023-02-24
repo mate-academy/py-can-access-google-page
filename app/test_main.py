a1a7c5f27a6f10a92a953e491be99ab9c9ed5cc5
Oleksandr Leha <legus96@gmail.com>
2023-02-24 12:00:06 +0200

    'Solution_with_parametrize'

--- app/test_main.py
+++ app/test_main.py
@@ -1,50 +1,33 @@
 from app.main import can_access_google_page
 from unittest import mock
+import pytest
 
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page_true_true(
-        mock_validate_google_url: mock,
-        mock_has_internet_connection: mock
+@pytest.mark.parametrize(
+    "validate_google_url, has_internet_connection, expected_result",
+    [
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible"),
+    ],
+    ids=[
+        "validate_url True, internet_connection True",
+        "validate_url True, internet_connection False",
+        "validate_url False, internet_connection True",
+        "validate_url False, internet_connection False"
+    ]
+
+)
+def test_can_access_google_page(
+        mock_validate_google_url: mock.Mock,
+        mock_has_internet_connection: mock.Mock,
+        validate_google_url: bool,
+        has_internet_connection: bool,
+        expected_result: str
 ) -> None:
-    mock_validate_google_url.return_value = True
-    mock_has_internet_connection.return_value = True
-    url = "https://www.google.com/"
-    assert can_access_google_page(url) == "Accessible"
-
-
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page_true_false(
-        mock_validate_google_url: mock,
-        mock_has_internet_connection: mock
-) -> None:
-    mock_validate_google_url.return_value = True
-    mock_has_internet_connection.return_value = False
-    url = "https://www.google.com/"
-    assert can_access_google_page(url) == "Not accessible"
-
-
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page_false_true(
-        mock_validate_google_url: mock,
-        mock_has_internet_connection: mock
-) -> None:
-    mock_validate_google_url.return_value = False
-    mock_has_internet_connection.return_value = True
-    url = "https://www.google.com/"
-    assert can_access_google_page(url) == "Not accessible"
-
-
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page_false_false(
-        mock_validate_google_url: mock,
-        mock_has_internet_connection: mock
-) -> None:
-    mock_validate_google_url.return_value = False
-    mock_has_internet_connection.return_value = False
-    url = "https://www.google.com/"
-    assert can_access_google_page(url) == "Not accessible"
+    mock_validate_google_url.return_value = validate_google_url
+    mock_has_internet_connection.return_value = has_internet_connection
+    assert can_access_google_page("https://www.google.com/") == expected_result
