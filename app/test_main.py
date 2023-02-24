61b298deae33042bf3fcbe6c8aa415a5f447a233
Litvinenko23 <andreylitvinenko20102010@gmail.com>
2023-02-24 12:33:50 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,29 @@
-# write your code here
+from _pytest.monkeypatch import MonkeyPatch
+
+from app.main import can_access_google_page
+
+
+def test_can_access_google_page(monkeypatch: MonkeyPatch) -> None:
+    # Mock the valid_google_url function to always return True
+    monkeypatch.setattr("app.main.valid_google_url", lambda url: True)
+
+    # Mock the has_internet_connection function to always return True
+    monkeypatch.setattr("app.main.has_internet_connection", lambda: True)
+
+    # Test with a valid Google URL
+    assert can_access_google_page("https://www.google.com") == "Accessible"
+
+
+def test_cannot_access_google_page(monkeypatch: MonkeyPatch) -> None:
+    # Test with an invalid URL
+    monkeypatch.setattr("app.main.has_internet_connection", lambda: True)
+    monkeypatch.setattr("app.main.valid_google_url", lambda url: False)
+    assert (
+        can_access_google_page("https://www.invalidurl.com")
+        == "Not accessible"
+    )
+
+    # Test with a valid URL but no internet connection
+    monkeypatch.setattr("app.main.valid_google_url", lambda url: True)
+    monkeypatch.setattr("app.main.has_internet_connection", lambda: False)
+    assert can_access_google_page("https://www.google.com") == "Not accessible"
