6818762a6892f93393b48edf16ebde08df2f15c4
Nazar Hladaniuk <powerlolek46@gmail.com>
2023-02-26 23:15:31 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -5,27 +5,35 @@ from app.main import can_access_google_page
 
 
 class TestCanAccessPage(unittest.TestCase):
+    def setUp(self) -> None:
+        self.url = "https://www.google.com/"
+        self.invalid_url = "https://www.Geag0Le.com/"
+        self.current_time = datetime(2023, 2, 22, 23, 0, 0)
+
     def test_valid_url_and_internet_connection(self) -> None:
-        with patch("app.main.valid_google_url", return_value=True):
-            with patch("app.main.has_internet_connection", return_value=True):
-                url = "https://www.google.com/"
-                result = can_access_google_page(url)
-                self.assertEqual(result, "Accessible")
+        with (
+            patch("app.main.valid_google_url", return_value=True),
+            patch("app.main.has_internet_connection", return_value=True)
+        ):
+            result = can_access_google_page(self.url)
+            self.assertEqual(result, "Accessible",
+                             f"Expected 'Accessible' but got '{result}'.")
 
     def test_invalid_url_valid_internet_connection(self) -> None:
-        with patch("app.main.valid_google_url", return_value=False):
-            with patch("app.main.has_internet_connection", return_value=True):
-                url = "https://www.Geag0Le.com/"
-                result = can_access_google_page(url)
-                self.assertEqual(result, "Not accessible")
+        with (
+            patch("app.main.valid_google_url", return_value=False),
+            patch("app.main.has_internet_connection", return_value=True)
+        ):
+            result = can_access_google_page(self.invalid_url)
+            self.assertEqual(result, "Not accessible",
+                             f"Expected 'Not accessible' but got '{result}'.")
 
     def test_out_of_hours(self) -> None:
-        with patch("app.main.valid_google_url", return_value=True):
-            with patch("app.main.has_internet_connection", return_value=False):
-                current_time = datetime(2023, 2, 22, 23, 0, 0)
-                with patch(
-                        "app.main.datetime.datetime", return_value=current_time
-                ):
-                    url = "https://www.google.com/"
-                    result = can_access_google_page(url)
-                    self.assertEqual(result, "Not accessible")
+        with (
+            patch("app.main.valid_google_url", return_value=True),
+            patch("app.main.has_internet_connection", return_value=False),
+            patch("app.main.datetime.datetime", return_value=self.current_time)
+        ):
+            result = can_access_google_page(self.url)
+            self.assertEqual(result, "Not accessible",
+                             f"Expected 'Not accessible' but got '{result}'.")
