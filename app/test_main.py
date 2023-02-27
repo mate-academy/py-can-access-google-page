52e9ef8b01787f1cdc37ddefcde7a41dbee7f545
Nazar Hladaniuk <powerlolek46@gmail.com>
2023-02-27 14:58:39 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import patch
-from datetime import datetime
+
 from app.main import can_access_google_page
 
 
@@ -8,7 +8,6 @@ class TestCanAccessPage(unittest.TestCase):
     def setUp(self) -> None:
         self.url = "https://www.google.com/"
         self.invalid_url = "https://www.Geag0Le.com/"
-        self.current_time = datetime(2023, 2, 22, 23, 0, 0)
 
     def test_valid_url_and_internet_connection(self) -> None:
         with (
@@ -31,8 +30,7 @@ class TestCanAccessPage(unittest.TestCase):
     def test_out_of_hours(self) -> None:
         with (
             patch("app.main.valid_google_url", return_value=True),
-            patch("app.main.has_internet_connection", return_value=False),
-            patch("app.main.datetime.datetime", return_value=self.current_time)
+            patch("app.main.has_internet_connection", return_value=False)
         ):
             result = can_access_google_page(self.url)
             self.assertEqual(result, "Not accessible",
