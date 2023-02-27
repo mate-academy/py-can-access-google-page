4972cab03f6a6c63cb1714e8412d3e57fa761b61
Ruslan <noezyboy1@gmail.com>
2023-02-27 15:32:46 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,28 +1,24 @@
 from unittest.mock import patch
+
+import pytest
+
 from app.main import can_access_google_page
 
 url = "https://www.notavalidurl.com"
 
 
-def test_can_access_google_page_with_valid_input() -> None:
-    with patch("app.main.has_internet_connection", return_value=True), (
-            patch("app.main.valid_google_url", return_value=True)):
-        assert can_access_google_page(url) == "Accessible"
-
-
-def test_can_access_google_page_no_internet() -> None:
-    with patch("app.main.has_internet_connection", return_value=False), (
-            patch("app.main.valid_google_url", return_value=True)):
-        assert can_access_google_page(url) == "Not accessible"
-
-
-def test_can_access_google_page_invalid_url() -> None:
-    with patch("app.main.has_internet_connection", return_value=True), (
-            patch("app.main.valid_google_url", return_value=False)):
-        assert can_access_google_page(url) == "Not accessible"
-
-
-def test_cannot_access_if_both_connection_and_valid_url_are_false() -> None:
-    with patch("app.main.has_internet_connection", return_value=False), (
-            patch("app.main.valid_google_url", return_value=False)):
-        assert can_access_google_page(url) == "Not accessible"
+@pytest.mark.parametrize("internet, url, expected", [(True,
+                                                      True,
+                                                      "Accessible"),
+                                                     (False, True,
+                                                      "Not accessible"),
+                                                     (True, False,
+                                                      "Not accessible"),
+                                                     (False, False,
+                                                      "Not accessible")])
+def test_can_access_google_page(internet: any,
+                                url: str,
+                                expected: str) -> None:
+    with patch("app.main.has_internet_connection", return_value=internet), \
+            patch("app.main.valid_google_url", return_value=url):
+        assert can_access_google_page(url) == expected
