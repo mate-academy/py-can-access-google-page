9280068078c8b910144017d6eb78ca1fa593c386
usrofgh <mailforgimail@gmail.com>
2023-02-27 17:43:40 +0200

    update

--- app/test_main.py
+++ app/test_main.py
@@ -1,19 +1,16 @@
+import pytest
+
 from unittest import mock
-import pytest as pytest
+
 from app.main import can_access_google_page
 
 
 @pytest.fixture()
-def mocked_has_internet_connection() -> mock.Mock:
-    with mock.patch("app.main.has_internet_connection") \
-            as mock_has_internet_connection:
-        yield mock_has_internet_connection
-
-
-@pytest.fixture()
-def mocked_valid_google_url() -> mock.Mock:
-    with mock.patch("app.main.valid_google_url") as mock_valid_google_url:
-        yield mock_valid_google_url
+def mocked_valid_url_and_connection() -> tuple[mock.Mock, mock.Mock]:
+    with (mock.patch("app.main.has_internet_connection")
+          as mock_has_internet_connection,
+          mock.patch("app.main.valid_google_url") as mock_valid_google_url):
+        yield mock_has_internet_connection, mock_valid_google_url
 
 
 @pytest.mark.parametrize(
@@ -32,13 +29,13 @@ def mocked_valid_google_url() -> mock.Mock:
     ]
 )
 def test_can_access_google_page(
-        mocked_valid_google_url: mock.Mock,
-        mocked_has_internet_connection: mock.Mock,
+        mocked_valid_url_and_connection: tuple[mock.Mock, mock.Mock],
         is_valid_url: bool,
         is_there_connection: bool,
         expected_result: str
 ) -> None:
-    mocked_valid_google_url.return_value = is_valid_url
-    mocked_has_internet_connection.return_value = is_there_connection
 
+    mocked_url, mocked_connection = mocked_valid_url_and_connection
+    mocked_url.return_value = is_valid_url
+    mocked_connection.return_value = is_there_connection
     assert can_access_google_page("https://www.google.com") == expected_result
