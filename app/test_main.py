5550dd6bf1622af6d33dd435a5406037dbc2f3f6
Ruslan <noezyboy1@gmail.com>
2023-02-27 09:18:50 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -6,23 +6,23 @@ url = "https://www.notavalidurl.com"
 
 def test_can_access_google_page_with_valid_input() -> None:
     with patch("app.main.has_internet_connection", return_value=True), (
-         patch("app.main.valid_google_url", return_value=True)):
+            patch("app.main.valid_google_url", return_value=True)):
         assert can_access_google_page(url) == "Accessible"
 
 
 def test_can_access_google_page_no_internet() -> None:
     with patch("app.main.has_internet_connection", return_value=False), (
-         patch("app.main.valid_google_url", return_value=True)):
+            patch("app.main.valid_google_url", return_value=True)):
         assert can_access_google_page(url) == "Not accessible"
 
 
 def test_can_access_google_page_invalid_url() -> None:
     with patch("app.main.has_internet_connection", return_value=True), (
-         patch("app.main.valid_google_url", return_value=False)):
+            patch("app.main.valid_google_url", return_value=False)):
         assert can_access_google_page(url) == "Not accessible"
 
 
 def test_cannot_access_if_both_connection_and_valid_url_are_false() -> None:
     with patch("app.main.has_internet_connection", return_value=False), (
-         patch("app.main.valid_google_url", return_value=False)):
+            patch("app.main.valid_google_url", return_value=False)):
         assert can_access_google_page(url) == "Not accessible"
