8001ef495186c77086731c37ea7f89d7b0577622
Ruslan <noezyboy1@gmail.com>
2023-02-28 14:26:58 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -7,18 +7,18 @@ from app.main import can_access_google_page
 url = "https://www.notavalidurl.com"
 
 
-@pytest.mark.parametrize("internet, url, expected", [(True,
-                                                      True,
-                                                      "Accessible"),
-                                                     (False, True,
-                                                      "Not accessible"),
-                                                     (True, False,
-                                                      "Not accessible"),
-                                                     (False, False,
-                                                      "Not accessible")])
+@pytest.mark.parametrize("internet, url, expected", [
+    (True, True, "Accessible"),
+    (False, True, "Not accessible"),
+    (True, False, "Not accessible"),
+    (False, False, "Not accessible")
+])
 def test_can_access_google_page(internet: any,
                                 url: str,
                                 expected: str) -> None:
-    with patch("app.main.has_internet_connection", return_value=internet), \
-            patch("app.main.valid_google_url", return_value=url):
+    with patch(
+        patch("app.main.has_internet_connection", return_value=internet),
+        patch("app.main.valid_google_url", return_value=url)
+    ):
+
         assert can_access_google_page(url) == expected
