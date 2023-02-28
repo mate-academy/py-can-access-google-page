5ec3c0c516056e5959271d58380f2f7595d528dc
Ruslan <noezyboy1@gmail.com>
2023-02-28 18:27:48 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,19 +1,21 @@
 from unittest.mock import patch
+
 import pytest
+
 from app.main import can_access_google_page
 
 url = "https://www.notavalidurl.com"
 
 
 @pytest.mark.parametrize("internet_connection, valid_url, expected_result", [
-    (True, True, "The Google page is accessible."),
-    (False, True, "The Google page is not accessible "
-                  "due to lack of internet connection."),
-    (True, False, "The provided URL is not a valid Google URL."),
-    (False, False, "The Google page is not accessible "
-                   "due to lack of internet connection "
-                   "and the provided URL is not a valid Google URL.")
-])
+    (True, True, "Accessible"),
+    (False, True, "Not accessible"),
+    (True, False, "Not accessible"),
+    (False, False, "Not accessible")
+], ids=["internet connection - valid URL",
+        "no internet connection - valid URL",
+        "internet connection - invalid URL",
+        "no internet connection - invalid URL"])
 def test_can_access_google_page(internet_connection: bool,
                                 valid_url: bool,
                                 expected_result: str) -> None:
