9cda45b89e8429c7641224b218c4990334deb685
Nazar Hladaniuk <powerlolek46@gmail.com>
2023-02-28 18:02:21 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -7,7 +7,6 @@ from app.main import can_access_google_page
 class TestCanAccessPage(unittest.TestCase):
     def setUp(self) -> None:
         self.url = "https://www.google.com/"
-        self.invalid_url = "https://www.Geag0Le.com/"
 
     def test_valid_url_and_internet_connection(self) -> None:
         with (
@@ -23,7 +22,7 @@ class TestCanAccessPage(unittest.TestCase):
             patch("app.main.valid_google_url", return_value=False),
             patch("app.main.has_internet_connection", return_value=True)
         ):
-            result = can_access_google_page(self.invalid_url)
+            result = can_access_google_page(self.url)
             self.assertEqual(result, "Not accessible",
                              f"Expected 'Not accessible' but got '{result}'.")
 
