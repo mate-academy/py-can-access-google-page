a8894e4e57bd576e092f300cc4d9447a1e5d4e1e
Oleh Chornyi <ochernyi04@gmail.com>
2023-03-01 14:42:21 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,27 +1,53 @@
 from unittest import mock
 
+import pytest
+
 from app.main import can_access_google_page
 
 
+# @pytest.fixture
+# @mock.patch("app.main.valid_google_url")
+# def mocked_url(
+#         mocked_google_url,
+# ) -> None:
+#     return mocked_google_url
+
+
+# @pytest.fixture
+# @mock.patch("app.main.has_internet_connection")
+# def mocked_internet_connection(
+#         mocked_internet_connection,
+# ) -> None:
+#     return mocked_internet_connection
+#
+
+@pytest.mark.parametrize(
+    "check_google_url,check_has_internet_connection,expected_result",
+    [pytest.param(
+        True, True, "Accessible",
+        id="valid url and internet connection exist"
+    ), pytest.param(
+        False, False, "Not accessible",
+        id="not valid url and internet connection does not exist"
+    ), pytest.param(
+        True, False, "Not accessible",
+        id="valid url but internet does not connection exist"
+    ), pytest.param(
+        False, True, "Not accessible",
+        id="not valid url but internet connection exist"
+    ),
+    ]
+)
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_valid_url_when_connection_does_not_exists(
-        valid_google_url: bool,
-        has_internet_connection: bool
+def test_valid_url_and_connection_exists(
+        mocked_url: bool,
+        mocked_internet_connection: bool,
+        check_google_url: bool,
+        check_has_internet_connection: bool,
+        expected_result: str
 ) -> None:
-    valid_google_url.return_value = True
-    has_internet_connection.return_value = False
+    mocked_url.return_value = check_google_url
+    mocked_internet_connection.return_value = check_has_internet_connection
 
-    assert can_access_google_page("google.com") == "Not accessible"
-
-
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_valid_url_when_connection_exists(
-        valid_google_url: bool,
-        has_internet_connection: bool
-) -> None:
-    valid_google_url.return_value = False
-    has_internet_connection.return_value = True
-
-    assert can_access_google_page("google.com") == "Not accessible"
+    assert can_access_google_page("google.com") == expected_result
