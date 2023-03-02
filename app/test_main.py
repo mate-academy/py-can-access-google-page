96941e5bfbbf9d61394a1c89ed0e8acd35ad4a75
Litvinenko23 <andreylitvinenko20102010@gmail.com>
2023-03-02 16:44:43 +0200

    Implemented parametrize for DRY

--- app/test_main.py
+++ app/test_main.py
@@ -1,31 +1,26 @@
+import pytest
 from _pytest.monkeypatch import MonkeyPatch
-
 from app.main import can_access_google_page
 
 
-def test_can_access_google_page(monkeypatch: MonkeyPatch) -> None:
-    monkeypatch.setattr("app.main.valid_google_url", lambda url: True)
-    monkeypatch.setattr("app.main.has_internet_connection", lambda: True)
-    assert can_access_google_page("https://www.google.com") == "Accessible"
-
-
-def test_cannot_access_google_page_invalid_url(
+@pytest.mark.parametrize(
+    "url, is_valid, has_connection, expected",
+    [
+        ("https://www.google.com", True, True, "Accessible"),
+        ("https://www.goo!gle.com", False, True, "Not accessible"),
+        ("https://www.google.com", True, False, "Not accessible"),
+        ("https://www.goole.com", False, False, "Not accessible"),
+    ],
+)
+def test_can_access_google_page(
     monkeypatch: MonkeyPatch,
+    url: str,
+    is_valid: bool,
+    has_connection: bool,
+    expected: str,
 ) -> None:
-    monkeypatch.setattr("app.main.has_internet_connection", lambda: True)
-    monkeypatch.setattr("app.main.valid_google_url", lambda url: False)
-    assert (
-        can_access_google_page("https://www.goo!gle.com") == "Not accessible"
+    monkeypatch.setattr("app.main.valid_google_url", lambda _: is_valid)
+    monkeypatch.setattr(
+        "app.main.has_internet_connection", lambda: has_connection
     )
-
-
-def test_has_not_internet_connection(monkeypatch: MonkeyPatch) -> None:
-    monkeypatch.setattr("app.main.valid_google_url", lambda url: True)
-    monkeypatch.setattr("app.main.has_internet_connection", lambda: False)
-    assert can_access_google_page("https://www.google.com") == "Not accessible"
-
-
-def test_invalid_url_and_has_not_connection(monkeypatch: MonkeyPatch) -> None:
-    monkeypatch.setattr("app.main.valid_google_url", lambda url: False)
-    monkeypatch.setattr("app.main.has_internet_connection", lambda: False)
-    assert can_access_google_page("https://www.goole.com") == "Not accessible"
+    assert can_access_google_page(url) == expected
