05d886c662689b5688cf9416251e0ec81822daa8
Natalie Goriela <natalie.goriela@gmail.com>
2023-03-03 13:31:25 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,15 @@
-# write your code here
+from unittest import mock
+
+from app.main import can_access_google_page
+
+
+def test_valid_google_url_exists() -> None:
+    with mock.patch("app.main.valid_google_url") as mocked_url:
+        can_access_google_page("https://mate.academy")
+        mocked_url.assert_called_once()
+
+
+def test_has_internet_connection_exists() -> None:
+    with mock.patch("app.main.has_internet_connection") as mocked_connection:
+        can_access_google_page("https://mate.academy")
+        mocked_connection.assert_called_once()
