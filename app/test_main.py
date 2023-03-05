8642c32400583982b4725bf57e796caf7e23efe8
Roman <parom95@ukr.net>
2023-03-05 20:12:42 +0200

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -6,34 +6,23 @@ from unittest import mock
 url = "https://www.google.com"
 
 
-@pytest.fixture()
-def mock_called() -> mock:
-    with (
-            mock.patch("app.main.valid_google_url") as valid_url,
-            mock.patch("app.main.has_internet_connection") as connection
-    ):
-        yield [valid_url, connection]
+@pytest.mark.parametrize(
+    "valid_url,connection,expected_value",
+    [(True, True, "Accessible"),
+     (False, True, "Not accessible"),
+     (True, False, "Not accessible"),
+     (False, False, "Not accessible")]
+)
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_can_access_google_page_request(
+        mock_valid_google_url: mock,
+        mock_has_internet_connection: mock,
+        expected_value: str,
+        valid_url: bool,
+        connection: bool
+) -> None:
+    mock_valid_google_url.return_value = valid_url
+    mock_has_internet_connection.return_value = connection
 
-
-def test_can_access_google_page_request(mock_called: mock) -> None:
-    mock_called[0].return_value = True
-    mock_called[1].return_value = True
-    assert can_access_google_page(url) == "Accessible"
-
-
-def test_error_url(mock_called: mock) -> None:
-    mock_called[0].return_value = False
-    mock_called[1].return_value = True
-    assert can_access_google_page(url) == "Not accessible"
-
-
-def test_error_date(mock_called: mock) -> None:
-    mock_called[0].return_value = True
-    mock_called[1].return_value = False
-    assert can_access_google_page(url) == "Not accessible"
-
-
-def test_error_date_and_url(mock_called: mock) -> None:
-    mock_called[0].return_value = False
-    mock_called[1].return_value = False
-    assert can_access_google_page(url) == "Not accessible"
+    assert can_access_google_page(url) == expected_value
