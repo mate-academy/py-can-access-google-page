ca37d051f6aac1de3157106be58b90dab7b02e5a
Andriy <an_mydel@gmail.com>
2023-03-22 22:04:56 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -4,6 +4,8 @@ from unittest import mock
 from app.main import can_access_google_page
 
 
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
 @pytest.mark.parametrize(
     "valid_url,internet_connection,expected",
     [
@@ -34,14 +36,28 @@ from app.main import can_access_google_page
     ]
 )
 def test_can_access_google_page(
+        mocked_valid_url: mock.Mock,
+        mocked_internet_connection: mock.Mock,
         valid_url: bool,
         internet_connection: bool,
-        expected: str) -> None:
-    with (mock.patch("app.main.valid_google_url") as mocked_valid_google_url,
-          mock.patch("app.main.has_internet_connection") as
-          mocked_has_internet_connection):
-        mocked_valid_google_url.return_value = valid_url
-        mocked_has_internet_connection.return_value = internet_connection
-        assert (
-            can_access_google_page("test_url") == expected
-        )
+        expected: str,
+) -> None:
+    mocked_valid_url.return_value = valid_url
+    mocked_internet_connection.return_value = internet_connection
+    assert (
+        can_access_google_page("test_url") == expected
+    )
+
+
+# def test_can_access_google_page(
+#         valid_url: bool,
+#         internet_connection: bool,
+#         expected: str) -> None:
+#     with (mock.patch("app.main.valid_google_url") as mocked_valid_google_url,
+#           mock.patch("app.main.has_internet_connection") as
+#           mocked_has_internet_connection):
+#         mocked_valid_google_url.return_value = valid_url
+#         mocked_has_internet_connection.return_value = internet_connection
+#         assert (
+#             can_access_google_page("test_url") == expected
+#         )
