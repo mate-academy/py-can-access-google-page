e81321789e140701a7b7cd9898b1a18b38984837
WalDragnir <akgk530@gmail.com>
2023-03-29 03:50:19 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,48 +1,37 @@
 from unittest import mock
+from unittest.mock import MagicMock
 import pytest
-from typing import Callable
 from app.main import can_access_google_page
 
 
 @pytest.fixture()
-def mocked_valid_google_url() -> Callable:
+def mocked_valid_google_url() -> MagicMock:
     with mock.patch("app.main.valid_google_url") as mocked_valid_url:
         yield mocked_valid_url
 
 
 @pytest.fixture()
-def has_internet_connection() -> Callable:
+def has_internet_connection() -> MagicMock:
     with mock.patch("app.main.has_internet_connection") as mocked_connection:
         yield mocked_connection
 
 
-def test_not_access_when_only_url_is_valid(
-        mocked_valid_google_url: Callable,
-        has_internet_connection: Callable) -> None:
-    mocked_valid_google_url.return_value = True
-    has_internet_connection.return_value = False
-    assert can_access_google_page("test_url") == "Not accessible"
-
-
-def test_not_access_when_only_connection_is_good(
-        mocked_valid_google_url: Callable,
-        has_internet_connection: Callable) -> None:
-    mocked_valid_google_url.return_value = False
-    has_internet_connection.return_value = True
-    assert can_access_google_page("test_url") == "Not accessible"
-
-
-def test_access_when_url_is_valid_and_connection_is_good(
-        mocked_valid_google_url: Callable,
-        has_internet_connection: Callable) -> None:
-    mocked_valid_google_url.return_value = True
-    has_internet_connection.return_value = True
-    assert can_access_google_page("test_url") == "Accessible"
-
-
-def test_not_access_when_url_invalid_and_connection_is_bad(
-        mocked_valid_google_url: Callable,
-        has_internet_connection: Callable) -> None:
-    mocked_valid_google_url.return_value = False
-    has_internet_connection.return_value = False
-    assert can_access_google_page("test_url") == "Not accessible"
+@pytest.mark.parametrize(
+    "valid_url, internet_connection, expected_output",
+    [
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (True, True, "Accessible"),
+        (False, False, "Not accessible")
+    ]
+)
+def test_accessibility(
+        mocked_valid_google_url: MagicMock,
+        has_internet_connection: MagicMock,
+        valid_url: bool,
+        internet_connection: bool,
+        expected_output: str
+) -> None:
+    mocked_valid_google_url.return_value = valid_url
+    has_internet_connection.return_value = internet_connection
+    assert can_access_google_page("test_url") == expected_output
