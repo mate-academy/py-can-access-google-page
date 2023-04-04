973658467d657e6e4dd5f20b1bc5d247216661ad
Anastasia Martyniuk <anastasia.20.12.1995@gmail.com>
2023-04-04 14:00:34 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,64 +1,58 @@
+from typing import Any
 from unittest import mock
+from unittest.mock import MagicMock
 
 import pytest
 
 from app.main import can_access_google_page
 
 
+URL = "https://www.google.com/"
+
+
 @pytest.fixture()
-def mocked_valid_google_url() -> None:
+def mocked_valid_google_url(request: Any) -> MagicMock:
     with mock.patch("app.main.valid_google_url") as mocked_url:
+        mocked_url.return_value = request.param
         yield mocked_url
 
 
 @pytest.fixture()
-def mocked_has_internet_connection() -> None:
+def mocked_has_internet_connection(request: Any) -> MagicMock:
     with mock.patch("app.main.has_internet_connection") as mocked_connection:
+        mocked_connection.return_value = request.param
         yield mocked_connection
 
 
-def test_should_return_accessible_when_url_is_valid_and_has_connection(
-    mocked_valid_google_url: mock, mocked_has_internet_connection: mock
+@pytest.mark.parametrize(
+    "mocked_valid_google_url, mocked_has_internet_connection, expected_result",
+    [
+        pytest.param(
+            False,
+            False,
+            "Not accessible"
+        ),
+        pytest.param(
+            True,
+            False,
+            "Not accessible"
+        ),
+        pytest.param(
+            False,
+            True,
+            "Not accessible"
+        ),
+        pytest.param(
+            True,
+            True,
+            "Accessible"
+        ),
+    ],
+    indirect=["mocked_valid_google_url", "mocked_has_internet_connection"]
+)
+def test_can_access_google_page(
+        mocked_valid_google_url: mock,
+        mocked_has_internet_connection: mock,
+        expected_result: str
 ) -> None:
-    mocked_valid_google_url.return_value = True
-    mocked_has_internet_connection.return_value = True
-
-    assert (
-        can_access_google_page(url="https://www.google.com/") == "Accessible"
-    )
-
-
-def test_should_return_not_accessible_when_url_is_valid_but_has_not_connection(
-    mocked_valid_google_url: mock, mocked_has_internet_connection: mock
-) -> None:
-    mocked_valid_google_url.return_value = True
-    mocked_has_internet_connection.return_value = False
-
-    assert (
-        can_access_google_page(url="https://www.google.com/")
-        == "Not accessible"
-    )
-
-
-def test_should_return_not_accessible_when_url_is_not_valid_and_no_connection(
-    mocked_valid_google_url: mock, mocked_has_internet_connection: mock
-) -> None:
-    mocked_valid_google_url.return_value = False
-    mocked_has_internet_connection.return_value = False
-
-    assert (
-        can_access_google_page(url="https://www.google.com/")
-        == "Not accessible"
-    )
-
-
-def test_should_return_not_accessible_when_url_is_not_valid_but_has_connection(
-    mocked_valid_google_url: mock, mocked_has_internet_connection: mock
-) -> None:
-    mocked_valid_google_url.return_value = False
-    mocked_has_internet_connection.return_value = True
-
-    assert (
-        can_access_google_page(url="https://www.google.com/")
-        == "Not accessible"
-    )
+    assert can_access_google_page(URL) == expected_result
