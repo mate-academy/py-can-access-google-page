4a7df19ea85ee8224be4a6536c0fb2c61cc88b42
Dmytro Shvets <dirolius@gmail.com>
2023-04-19 14:18:55 +0300

    or better create separated test for every assert?

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,23 @@
-# write your code here
+from unittest import mock
+from app.main import can_access_google_page
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_can_access_google_page(mock_valid_google_url: bool,
+                                mock_has_internet_connection: bool) -> None:
+    mock_valid_google_url.return_value = True
+    mock_has_internet_connection.return_value = True
+    assert can_access_google_page("https://www.google.com") == "Accessible"
+
+    mock_valid_google_url.return_value = False
+    mock_has_internet_connection.return_value = True
+    assert can_access_google_page("https://www.google.com") == "Not accessible"
+
+    mock_valid_google_url.return_value = True
+    mock_has_internet_connection.return_value = False
+    assert can_access_google_page("https://www.google.com") == "Not accessible"
+
+    mock_valid_google_url.return_value = False
+    mock_has_internet_connection.return_value = False
+    assert can_access_google_page("https://www.google.com") == "Not accessible"
