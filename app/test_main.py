069436c74538206c3c064ebd38077c92380dc3fb
Vlad Tetruk <vladteteruk01@gmail.com>
2023-05-09 15:28:55 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -27,7 +27,6 @@ class TestResultsOfMainFunction:
                     can_access_google_page("https://www.google.com.ua")
                     == expected_result
                 )
-
                 if connection.return_value:
                     valid_google_url.assert_called_once_with(
                         "https://www.google.com.ua"
