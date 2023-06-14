c8428de1270a2976f802355c5b8852f35eede37d
Yevhenii Iovchu <persie.iovchu@gmail.com>
2023-06-14 18:51:23 +0300

    Added F/F case and decorated into parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -1,30 +1,49 @@
+import pytest
+
+
 from unittest import mock
-from typing import Callable
 
 
 from app.main import can_access_google_page
 
 
-@mock.patch("app.main.valid_google_url")
+@pytest.mark.parametrize(
+    "internet_connection, valid_url, url, expected",
+    [
+        pytest.param(
+            True, True, "https://www.youtube.com/", "Accessible"
+        ),
+        pytest.param(
+            False, True, "https://www.youtube.com/", "Not accessible"
+        ),
+        pytest.param(
+            True, False, "https:/ww.you1t1u1be.com/", "Not accessible"
+        ),
+        pytest.param(
+            False, False, "https:/ww.you1t1u1be.com/", "Not accessible"
+        ),
+    ],
+    ids=[
+        "You should be able to enter website if url is correct"
+        " and you have internet connection",
+        "You shouldn't be able to enter website if url is correct"
+        " and you don't have internet connection",
+        "You shouldn't be able to enter website if url is incorrect"
+        " and you have internet connection",
+        "You shouldn't be able to enter website if url is incorrect"
+        " and you don't have internet connection",
+    ]
+)
 @mock.patch("app.main.has_internet_connection")
+@mock.patch("app.main.valid_google_url")
 def test_can_access_google_page(
-    mocked_valid: Callable,
-    mocked_connection: Callable
+    mocked_connection: mock.Mock,
+    mocked_valid: mock.Mock,
+    internet_connection: bool,
+    valid_url: bool,
+    url: str,
+    expected: str,
 ) -> None:
-    mocked_connection.return_value = True
-    mocked_valid.return_value = True
-    assert (
-        can_access_google_page("https://www.youtube.com/") == "Accessible"
-    )
-
-    mocked_connection.return_value = False
-    mocked_valid.return_value = True
-    assert (
-        can_access_google_page("https://www.youtube.com/") == "Not accessible"
-    )
-
-    mocked_connection.return_value = True
-    mocked_valid.return_value = False
-    assert (
-        can_access_google_page("https:/ww.you1t1u1be.com/") == "Not accessible"
-    )
+    mocked_connection.return_value = internet_connection
+    mocked_valid.return_value = valid_url
+    assert can_access_google_page(url) == expected
