019a259c3a2fbfc599be18451b01d36456d64488
Anton Komarov <anton_komarov_qa@ukr.net>
2023-06-15 13:16:44 +0300

    'Add @pytest.mark.parametrize'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,5 @@
+import pytest
+
 from unittest import mock
 
 from app.main import can_access_google_page
@@ -5,37 +7,24 @@ from app.main import can_access_google_page
 from typing import Callable
 
 
+@pytest.mark.parametrize(
+    "is_connected,is_valid_url,expected",
+    [
+        (True, True, "Accessible"),
+        (False, True, "Not accessible"),
+        (True, False, "Not accessible")
+    ]
+)
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_access_with_connection_and_valid_url(
+def test_can_access_google_page(
         mocked_connection: Callable,
-        mocked_valid_url: Callable
+        mocked_valid_url: Callable,
+        is_connected: bool,
+        is_valid_url: bool,
+        expected: str
 ) -> None:
-    mocked_connection.return_value = True
-    mocked_valid_url.return_value = True
+    mocked_connection.return_value = is_connected
+    mocked_valid_url.return_value = is_valid_url
 
-    assert can_access_google_page("url") == "Accessible"
-
-
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_can_not_access_without_connection(
-        mocked_connection: Callable,
-        mocked_valid_url: Callable
-) -> None:
-    mocked_connection.return_value = False
-    mocked_valid_url.return_value = True
-
-    assert can_access_google_page("url") == "Not accessible"
-
-
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_can_not_access_with_invalid_url(
-        mocked_connection: Callable,
-        mocked_valid_url: Callable
-) -> None:
-    mocked_connection.return_value = True
-    mocked_valid_url.return_value = False
-
-    assert can_access_google_page("url") == "Not accessible"
+    assert can_access_google_page("url") == expected
