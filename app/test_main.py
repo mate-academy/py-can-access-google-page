0b5319183e10118a8d53bbc9e6707e9354fe969e
Oleksii Kolii <igravkompik@gmail.com>
2023-06-15 16:39:49 +0300

    changed indentation renamed context manager

--- app/test_main.py
+++ app/test_main.py
@@ -8,14 +8,17 @@ from app.main import can_access_google_page
 @pytest.mark.parametrize("url,valid,connection,expected", [
     ("https://www.google.com", True, True, "Accessible"),
     ("https://www.google.com", True, False, "Not accessible"),
-    ("https://www.google.com", False, True, "Not accessible")
+    ("https://www.google.com", False, True, "Not accessible"),
+    ("https://www.google.com", False, False, "Not accessible")
 ])
 def test_valid_url_and_connection_exists(monkeypatch: MonkeyPatch,
                                          url: str,
                                          expected: str,
                                          valid: bool,
-                                         connection: bool) -> None:
-    with monkeypatch.context() as m:
-        m.setattr("app.main.valid_google_url", lambda *args: valid)
-        m.setattr("app.main.has_internet_connection", lambda *args: connection)
+                                         connection: bool
+                                         ) -> None:
+    with monkeypatch.context() as mocked:
+        mocked.setattr("app.main.valid_google_url", lambda *args: valid)
+        mocked.setattr("app.main.has_internet_connection",
+                       lambda *args: connection)
         assert can_access_google_page("https://www.google.com") == expected
