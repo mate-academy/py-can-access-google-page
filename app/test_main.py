22fddbdde1469c8dad3c392d610f7365d7b6b7fe
Denys Seliverstov <catsperr322@gmail.com>
2023-06-19 17:47:28 +0300

    Fixed

--- app/test_main.py
+++ app/test_main.py
@@ -10,33 +10,29 @@ TEST_URL = "https://github.com/Kirontiko/"
 
 
 @pytest.mark.parametrize(
-    "mock_has_connection, mock_is_valid_url, url, expected",
+    "mock_has_connection, mock_is_valid_url, expected",
     [
         pytest.param(
             True,
             True,
-            TEST_URL,
             "Accessible",
             id="accessible when has connection and url is valid"
         ),
         pytest.param(
             False,
             True,
-            TEST_URL,
             "Not accessible",
             id="not accessible when has no connection"
         ),
         pytest.param(
             True,
             False,
-            TEST_URL,
             "Not accessible",
             id="not accessible when url is not valid"
         ),
         pytest.param(
             False,
             False,
-            TEST_URL,
             "Not accessible",
             id="not accessible when url is not valid and no connection"
         )
@@ -49,9 +45,8 @@ def test_can_access_google_page(
         valid_google_url: Callable,
         mock_has_connection: bool,
         mock_is_valid_url: bool,
-        url: str,
         expected: str
 ) -> None:
     has_internet_connection.return_value = mock_has_connection
     valid_google_url.return_value = mock_is_valid_url
-    assert can_access_google_page(url) == expected
+    assert can_access_google_page(TEST_URL) == expected
