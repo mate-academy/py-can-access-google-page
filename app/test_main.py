98c6a5a095ca049271f54c1ec1c0b2ba164b5743
Makeyev Yevhen <makeyev.ye@gmail.com>
2023-06-19 12:32:14 +0300

    god-damn flake update

--- app/test_main.py
+++ app/test_main.py
@@ -15,6 +15,9 @@ from app.main import can_access_google_page
 def test_can_access_google_page(
     url: str, has_internet: bool, is_valid: bool, expected: str
 ) -> None:
-    with mock.patch("app.main.has_internet_connection", return_value=has_internet):
+    with mock.patch(
+            "app.main.has_internet_connection",
+            return_value=has_internet
+    ):
         with mock.patch("app.main.valid_google_url", return_value=is_valid):
             assert can_access_google_page(url) == expected
