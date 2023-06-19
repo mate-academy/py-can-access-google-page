5ee858d968d023699f3b1573641ab94ce2e58a4d
Nazariy <mac@Nazariys-MacBook-Pro.local>
2023-06-19 12:42:33 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -7,20 +7,21 @@ from app.main import can_access_google_page
 
 
 @pytest.mark.parametrize(
-    "expected_url, connection, expected_result",
+    "url, expected_url, connection, expected_result",
     [
-        pytest.param(True, False, "Not accessible"),
-        pytest.param(False, True, "Not accessible"),
-        pytest.param(True, True, "Accessible")
+        pytest.param("https://www.google.com/", True, False, "Not accessible"),
+        pytest.param("https://www.google.com/", False, True, "Not accessible"),
+        pytest.param("https://www.google.com/", True, True, "Accessible")
     ]
 )
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 def test_can_access_google_page(mocked_valid_google_url: Callable,
                                 mocked_has_internet_connection: Callable,
+                                url: str,
                                 expected_url: bool,
                                 connection: bool,
                                 expected_result: str) -> None:
     mocked_valid_google_url.return_value = expected_url
     mocked_has_internet_connection.return_value = connection
-    assert can_access_google_page("https://www.google.com") == expected_result
+    assert can_access_google_page(url) == expected_result
