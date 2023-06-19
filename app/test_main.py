8c30d198a5bb9c3b355e432772a62694d327dfa6
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-06-19 18:06:28 +0300

    removed all urls but google(had to remove rickroll:(( )

--- app/test_main.py
+++ app/test_main.py
@@ -8,24 +8,17 @@ from app.main import can_access_google_page
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 @pytest.mark.parametrize(
-    "url,is_valid,internet_connection,expected_status",
+    "is_valid,internet_connection,expected_status",
     [
-        ("https://www.google.com", True, False, "Not accessible"),
-        ("http:///a", False, True, "Not accessible"),
-        ("https://mate.academy", True, False, "Not accessible"),
-
-        ("https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-         True, True, "Accessible"),
-
-        ("https://www.twitch.tv", True, True, "Accessible"),
-        ("https://gidhub.com", False, False, "Not accessible")
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (True, True, "Accessible"),
+        (False, False, "Not accessible")
     ],
     ids=[
         "should not be accessible when there's no internet connection",
-        "should not be accessible when url is not valid",
-        "should not be accessible when there's no internet connection",
 
-        "should be accessible when url is valid "
+        "should be accessible when url isn't valid "
         "and user has internet connection",
 
         "should be accessible when url is valid "
@@ -37,10 +30,9 @@ from app.main import can_access_google_page
 )
 def test_accesses_pages_correctly(mocked_valid_url: mock.Mock,
                                   mocked_has_internet_connection: mock.Mock,
-                                  url: str,
                                   is_valid: bool,
                                   internet_connection: bool,
                                   expected_status: str) -> None:
     mocked_valid_url.return_value = is_valid
     mocked_has_internet_connection.return_value = internet_connection
-    assert can_access_google_page(url) == expected_status
+    assert can_access_google_page("https://www.google.com") == expected_status
