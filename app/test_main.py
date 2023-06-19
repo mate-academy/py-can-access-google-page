9279265f607f7eeb06d4e037ec5d8d0e3fa70fac
Vlad <vladislav.zhyla@gmail.com>
2023-06-19 19:48:20 +0300

    Code was optimized. One test case was added

--- app/test_main.py
+++ app/test_main.py
@@ -1,53 +1,28 @@
+import pytest
+
 from app.main import can_access_google_page
 from _pytest.monkeypatch import MonkeyPatch
 
-
-def test_can_access_google_page_if_both_true(monkeypatch: MonkeyPatch) -> None:
-    def mock_valid_google_url(url: str) -> bool:
-        return True
-
-    def mock_has_internet_connection() -> bool:
-        return True
-    monkeypatch.setattr(
-        "app.main.valid_google_url",
-        mock_valid_google_url
-    )
-    monkeypatch.setattr(
-        "app.main.has_internet_connection",
-        mock_has_internet_connection
-    )
-    assert can_access_google_page(
-        "https://www.google.com/"
-    ) == "Accessible"
-
-
-def test_can_access_google_page_if_not_valid(monkeypatch: MonkeyPatch) -> None:
-    def mock_valid_google_url(url: str) -> bool:
-        return False
-
-    def mock_has_internet_connection() -> bool:
-        return True
-    monkeypatch.setattr(
-        "app.main.valid_google_url",
-        mock_valid_google_url
-    )
-    monkeypatch.setattr(
-        "app.main.has_internet_connection",
-        mock_has_internet_connection
-    )
-    assert can_access_google_page(
-        "https://www.google.com/"
-    ) == "Not accessible"
-
-
-def test_can_access_google_page_if_no_connection(
-        monkeypatch: MonkeyPatch
+@pytest.mark.parametrize(
+    "valid_url_return,has_internet_return,expected_result",
+    [
+        (True, True, "Accessible"),
+        (False, False, "Not accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible")
+    ]
+)
+def test_can_access_google_page_if_both_true(
+        monkeypatch: MonkeyPatch,
+        valid_url_return: bool,
+        has_internet_return: bool,
+        expected_result: str
 ) -> None:
     def mock_valid_google_url(url: str) -> bool:
-        return True
+        return valid_url_return
 
     def mock_has_internet_connection() -> bool:
-        return False
+        return has_internet_return
     monkeypatch.setattr(
         "app.main.valid_google_url",
         mock_valid_google_url
@@ -58,4 +33,4 @@ def test_can_access_google_page_if_no_connection(
     )
     assert can_access_google_page(
         "https://www.google.com/"
-    ) == "Not accessible"
+    ) == expected_result
