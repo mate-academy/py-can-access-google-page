0fec3f6b69aae98c52c4e406760fcea5fd2dd4af
Yaryk Konoplyov <kannabis252@gmail.com>
2023-06-19 19:09:33 +0300

    Unnecessary constants removed, url placed directly into function

--- app/test_main.py
+++ app/test_main.py
@@ -3,17 +3,14 @@ from pytest import MonkeyPatch, mark
 
 from app.main import can_access_google_page
 
-GOOGLE_URL = "Some google url"
-NOT_GOOGLE_URL = "Not google url"
-
 
 @mark.parametrize(
-    "url, url_validation,internet,result",
+    "url_validation,internet,result",
     [
-        (GOOGLE_URL, False, True, "Not accessible"),
-        (GOOGLE_URL, True, True, "Accessible"),
-        (NOT_GOOGLE_URL, False, False, "Not accessible"),
-        (GOOGLE_URL, True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (True, True, "Accessible"),
+        (False, False, "Not accessible"),
+        (True, False, "Not accessible"),
     ],
     ids=[
         "Should return false when not google url",
@@ -22,12 +19,11 @@ NOT_GOOGLE_URL = "Not google url"
         "Should return false when google url, no internet"
     ]
 )
-def test_can_access_google_page(url: str,
-                                url_validation: bool,
+def test_can_access_google_page(url_validation: bool,
                                 internet: bool,
                                 result: str,
                                 monkeypatch: MonkeyPatch) -> None:
-
+    url = "some_url"
     monkeypatch.setattr("app.main.valid_google_url",
                         lambda *args: url_validation)
     monkeypatch.setattr("app.main.has_internet_connection",
