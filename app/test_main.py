52106d8815176f8c95cc6f11beabf03a4dc0151b
Olha Tryhub <olha.tryhub@nure.ua>
2023-06-19 22:09:37 +0300

    Solution with using parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -1,26 +1,50 @@
 from unittest import mock
+from typing import Callable
 from app.main import can_access_google_page
 
 
-def test_correct_access() -> None:
-    with (mock.patch("app.main.has_internet_connection", lambda: True),
-          mock.patch("app.main.valid_google_url", lambda a: True)):
-        assert can_access_google_page("some_url") == "Accessible"
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_correct_access(mocked_has_internet_connection: Callable,
+                        mocked_valid_google_url: Callable) -> None:
+
+    mocked_has_internet_connection.return_value = True
+    mocked_valid_google_url.return_value = True
+
+    assert can_access_google_page("some_url") == "Accessible"
 
 
-def test_incorrect_access_has_not_internet_connection() -> None:
-    with (mock.patch("app.main.has_internet_connection", lambda: False),
-          mock.patch("app.main.valid_google_url", lambda a: True)):
-        assert can_access_google_page("some_url") == "Not accessible"
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_incorrect_access_has_not_internet_connection(
+        mocked_has_internet_connection: Callable,
+        mocked_valid_google_url: Callable) -> None:
+
+    mocked_has_internet_connection.return_value = False
+    mocked_valid_google_url.return_value = True
+
+    assert can_access_google_page("some_url") == "Not accessible"
 
 
-def test_incorrect_access_invalid_google_url() -> None:
-    with (mock.patch("app.main.has_internet_connection", lambda: True),
-          mock.patch("app.main.valid_google_url", lambda a: False)):
-        assert can_access_google_page("some_url") == "Not accessible"
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_incorrect_access_invalid_google_url(
+        mocked_has_internet_connection: Callable,
+        mocked_valid_google_url: Callable) -> None:
+
+    mocked_has_internet_connection.return_value = True
+    mocked_valid_google_url.return_value = False
+
+    assert can_access_google_page("some_url") == "Not accessible"
 
 
-def test_incorrect_access_no_internet_invalid_url() -> None:
-    with (mock.patch("app.main.has_internet_connection", lambda: False),
-          mock.patch("app.main.valid_google_url", lambda a: False)):
-        assert can_access_google_page("some_url") == "Not accessible"
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_incorrect_access_no_internet_invalid_url(
+        mocked_has_internet_connection: Callable,
+        mocked_valid_google_url: Callable) -> None:
+
+    mocked_has_internet_connection.return_value = False
+    mocked_valid_google_url.return_value = False
+
+    assert can_access_google_page("some_url") == "Not accessible"
