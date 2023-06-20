8adf029e367805d55167f1d75ee61f5add224b4b
Olha Tryhub <olha.tryhub@nure.ua>
2023-06-20 15:16:40 +0300

    using correct parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -1,50 +1,47 @@
 from unittest import mock
 from typing import Callable
+import pytest
 from app.main import can_access_google_page
 
 
+@pytest.mark.parametrize(
+    "can_access,is_valid,connection_status",
+    [
+        pytest.param(
+            True,
+            True,
+            "Accessible",
+            id="It must be correct connection if user has internet connection and valid URL"
+        ),
+        pytest.param(
+            False,
+            True,
+            "Not accessible",
+            id="It mustn't be correct connection if user hasn't internet connection but has valid URL"
+        ),
+        pytest.param(
+            True,
+            False,
+            "Not accessible",
+            id="It mustn't be correct connection if user has internet connection but don't has valid URL"
+        ),
+        pytest.param(
+            False,
+            False,
+            "Not accessible",
+            id="It mustn't be correct connection if user hasn't internet connection and valid URL"
+        )
+    ]
+)
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 def test_correct_access(mocked_has_internet_connection: Callable,
-                        mocked_valid_google_url: Callable) -> None:
+                        mocked_valid_google_url: Callable,
+                        can_access: bool,
+                        is_valid: bool,
+                        connection_status: str) -> None:
 
-    mocked_has_internet_connection.return_value = True
-    mocked_valid_google_url.return_value = True
+    mocked_has_internet_connection.return_value = can_access
+    mocked_valid_google_url.return_value = is_valid
 
-    assert can_access_google_page("some_url") == "Accessible"
-
-
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_incorrect_access_has_not_internet_connection(
-        mocked_has_internet_connection: Callable,
-        mocked_valid_google_url: Callable) -> None:
-
-    mocked_has_internet_connection.return_value = False
-    mocked_valid_google_url.return_value = True
-
-    assert can_access_google_page("some_url") == "Not accessible"
-
-
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_incorrect_access_invalid_google_url(
-        mocked_has_internet_connection: Callable,
-        mocked_valid_google_url: Callable) -> None:
-
-    mocked_has_internet_connection.return_value = True
-    mocked_valid_google_url.return_value = False
-
-    assert can_access_google_page("some_url") == "Not accessible"
-
-
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_incorrect_access_no_internet_invalid_url(
-        mocked_has_internet_connection: Callable,
-        mocked_valid_google_url: Callable) -> None:
-
-    mocked_has_internet_connection.return_value = False
-    mocked_valid_google_url.return_value = False
-
-    assert can_access_google_page("some_url") == "Not accessible"
+    assert can_access_google_page("some_url") == connection_status
