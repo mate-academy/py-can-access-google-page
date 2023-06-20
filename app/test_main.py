d1ac2ebc79452a917c9cdff1a5c93b0f2b6b452e
Makeyev Yevhen <makeyev.ye@gmail.com>
2023-06-20 08:13:53 +0300

    updated so the test would use a dummy url

--- app/test_main.py
+++ app/test_main.py
@@ -7,18 +7,17 @@ from app.main import can_access_google_page
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
 @pytest.mark.parametrize(
-    "url, has_internet, is_valid, expected",
+    "has_internet, is_valid, expected",
     [
-        ("https://www.google.com", True, True, "Accessible"),
-        ("https://www.google.com", False, True, "Not accessible"),
-        ("https://somewebsitename.com", True, False, "Not accessible"),
-        ("https://somewebsitename.com", False, False, "Not accessible")
+        (True, True, "Accessible"),
+        (False, True, "Not accessible"),
+        (True, False, "Not accessible"),
+        (False, False, "Not accessible")
     ]
 )
 def test_can_access_google_page(
         valid_google_url: mock.MagicMock,
         has_internet_connection: mock.MagicMock,
-        url: str,
         has_internet: bool,
         is_valid: bool,
         expected: str,
@@ -26,4 +25,4 @@ def test_can_access_google_page(
 ) -> None:
     has_internet_connection.return_value = has_internet
     valid_google_url.return_value = is_valid
-    assert can_access_google_page(url) == expected
+    assert can_access_google_page("dummy url") == expected
