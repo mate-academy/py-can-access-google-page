68ad4ed30a3c9bf76a0515d475095105bbd036d5
Alex Yatsun <starcrafter4444@gmail.com>
2023-06-21 17:47:39 +0300

    Improvment

--- app/test_main.py
+++ app/test_main.py
@@ -8,10 +8,9 @@ from app.main import can_access_google_page
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
 @pytest.mark.parametrize(
-    "url,expected_result,internet_connection_value,valid_url_value",
+    "expected_result,internet_connection_value,valid_url_value",
     [
         pytest.param(
-            "https://www.google.com",
             "Accessible",
             True,
             True,
@@ -19,7 +18,6 @@ from app.main import can_access_google_page
                 "connection and url is valid")
         ),
         pytest.param(
-            "https://www.google.com",
             "Not accessible",
             True,
             False,
@@ -27,7 +25,6 @@ from app.main import can_access_google_page
                 "connection and url is NOT valid")
         ),
         pytest.param(
-            "https://www.google.com",
             "Not accessible",
             False,
             True,
@@ -39,10 +36,9 @@ from app.main import can_access_google_page
 )
 def test_can_access_google_page(mock_valid_google_url: Callable,
                                 mock_has_internet_connection: Callable,
-                                url: str,
                                 expected_result: str,
                                 internet_connection_value: bool,
                                 valid_url_value: bool) -> None:
     mock_has_internet_connection.return_value = internet_connection_value
     mock_valid_google_url.return_value = valid_url_value
-    assert can_access_google_page(url) == expected_result
+    assert can_access_google_page("https://www.google.com") == expected_result
