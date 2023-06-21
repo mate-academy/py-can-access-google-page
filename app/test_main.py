47f5b522fd471f91c58f1b404e6d142861838fec
Denys Gordiichuk <dd111320@gmail.com>
2023-06-21 16:59:30 +0300

    finish

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,28 @@
-# write your code here
+from unittest import mock
+import pytest
+
+from app.main import can_access_google_page
+
+
+@mock.patch("app.main.has_internet_connection")
+@mock.patch("app.main.valid_google_url")
+@pytest.mark.parametrize(
+    "url,connection,result",
+    [
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible")
+    ]
+)
+def test_accessibility_to_page(
+        mocked_valid_google_url: mock.MagicMock,
+        mocked_has_internet_connection: mock.MagicMock,
+        url: bool,
+        connection: bool,
+        result: str
+) -> None:
+    mocked_valid_google_url.return_value = url
+    mocked_has_internet_connection.return_value = connection
+
+    assert can_access_google_page("a") == result
