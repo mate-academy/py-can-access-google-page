17afd45bf8e41b983a3e2660b33664e8a6370854
Denys Raboshchuk <kontroler444@gmail.com>
2023-06-21 15:33:20 +0300

    Done!

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,23 @@
-# write your code here
+from unittest import mock
+
+from app.main import can_access_google_page
+
+
+def test_of_calling_has_internet_connection() -> None:
+    with (
+        mock.patch("app.main.has_internet_connection")
+        as mocked_has_internet_connection
+    ):
+
+        can_access_google_page("https://www.google.com/")
+        mocked_has_internet_connection.assert_called_once()
+
+
+def test_of_calling_valid_google_url() -> None:
+    with (
+        mock.patch("app.main.valid_google_url")
+        as mocked_has_internet_connection
+    ):
+
+        can_access_google_page("https://www.google.com/")
+        mocked_has_internet_connection.assert_called_once()
