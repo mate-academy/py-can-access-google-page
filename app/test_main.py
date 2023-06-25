0170f29b6646fa68c0deb048f3a6986400d174a1
laskarj <koks.adekvat@gmail.com>
2023-06-25 19:18:59 +0200

    Add parametrize and rewrite functions.

--- app/test_main.py
+++ app/test_main.py
@@ -1,36 +1,58 @@
 from typing import Callable
 from unittest import mock
 
+import pytest
 
 from app.main import can_access_google_page
 
 
+@pytest.mark.parametrize(
+    "url, value_connection, value_validator, result, error",
+    [
+        (
+            "https://www.google.com",
+            True,
+            True,
+            "Accessible",
+            (
+                    "Function should return "
+                    "`Accessible` if all function passed."
+            )
+        ),
+        (
+            "google.com",
+            False,
+            True,
+            "Not accessible",
+            (
+                    "Function should return "
+                    "`Not accessible` if url doesn't valid."
+            )
+        ),
+        (
+            "https://www.google.com",
+            True,
+            False,
+            "Not accessible",
+            "Function should return `Not accessible` "
+            "If no connection."
+        )
+    ]
+)
 @mock.patch("app.main.valid_google_url", create=True)
-def test_can_access_google_page_url_validator(
-        mocked_validator: Callable,
-) -> None:
-    url = "google"
-    mocked_validator.return_value = False
-
-    test_func = can_access_google_page(url)
-    mocked_validator.assert_called_once_with(url)
-
-    assert test_func == "Not accessible", (
-        "Function should return `Not accessible` "
-        "if url doesn't valid."
-    )
-
-
 @mock.patch("app.main.has_internet_connection", create=True)
-def test_can_access_google_page_internet_connection(
-        mock_internet_connection: Callable
+def test_return_can_access_google_page(
+        mocked_connection: Callable,
+        mocked_validator: Callable,
+        url: str,
+        value_validator: bool,
+        value_connection: bool,
+        result: str,
+        error: str,
 ) -> None:
-    mock_internet_connection.return_value = False
+    mocked_validator.return_value = value_validator
+    mocked_connection.return_value = value_connection
 
-    test_func = can_access_google_page("https://www.google.com/")
-    mock_internet_connection.assert_called_once()
+    result_function = can_access_google_page(url)
 
-    assert test_func == "Not accessible", (
-        "Function should return `Not accessible` "
-        "If no connection."
-    )
+    assert result_function == result, error
