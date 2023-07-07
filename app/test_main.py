68292395522f135f6a1b729f5f90c1b6dab20de3
Nazar Psheiuk <nazar.psheiuk@gmail.com>
2023-07-07 16:44:02 +0300

    test_try_from_google_page

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,24 @@
-# write your code here
+from typing import Callable
+from unittest import mock
+
+from .main import can_access_google_page
+
+
+@mock.patch("main.valid_google_url")
+def test_valid_google_has_called(mocked_valid_url: Callable) -> None:
+
+    can_access_google_page("https://www.google.com/")
+
+    mocked_valid_url.assert_called_once
+
+
+@mock.patch("main.has_internet_connection")
+def test_has_internet_connection(mocked_valid_inet: Callable) -> None:
+
+    can_access_google_page()
+
+    mocked_valid_inet.assert_called_once
+
+
+def test_can_access_page() -> None:
+    assert can_access_google_page("https://www.google.com/")
