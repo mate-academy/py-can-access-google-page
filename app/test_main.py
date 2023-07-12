c5ea74d5528e23c61200653105bc7373b4bb1aee
Max Lapchuk <makslapchuk@gmail.com>
2023-07-12 19:17:35 +0300

    Test one more case

--- app/test_main.py
+++ app/test_main.py
@@ -10,20 +10,18 @@ from app.main import can_access_google_page
     [
         (True, True, "mate.academy", "Accessible"),
         (True, False, "mate.academy", "Not accessible"),
-        (False, True, "bro.academy", "Not accessible")
+        (False, True, "bro.academy", "Not accessible"),
+        (False, False, "bro.academy", "Not accessible")
     ],
     ids=[
         "test when valid url and connection exists",
         "test when valid url but no connection",
-        "test when connection exists but url invalid"
+        "test when connection exists but url invalid",
+        "test when invalid url and no connection"
     ]
 )
-@mock.patch(
-    "app.main.valid_google_url"
-)
-@mock.patch(
-    "app.main.has_internet_connection"
-)
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
 def test_access_with_different_urls_and_internet_connection(
         mocked_url: Callable[[str], bool],
         mocked_connection: Callable[[], bool],
