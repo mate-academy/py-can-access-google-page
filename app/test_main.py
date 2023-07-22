b01c609d819fefaaba9c0772835844c3da25f7c0
Volodymyr Yerokhov <kavbian@gmail.com>
2023-07-22 18:17:48 +0300

    'pytest.mark.parametrize'

--- app/test_main.py
+++ app/test_main.py
@@ -1,54 +1,68 @@
 from unittest import mock
-from typing import Any
+import pytest
 
 from app.main import can_access_google_page
 
 
-@mock.patch("app.main.has_internet_connection")
-def test_valid_url_available_internet(mocked_internet_connection: Any) -> None:
-    mocked_internet_connection.return_value = True
-    assert can_access_google_page("https://google.com") == "Accessible"
-
-
-@mock.patch("app.main.has_internet_connection")
-def test_valid_url_unavailable_internet(
-        mocked_internet_connection: Any
+@pytest.mark.parametrize(
+    "internet,valid_url,result",
+    [
+        pytest.param(
+            True,
+            True,
+            "Accessible",
+            id="Available internet and valid url"
+        ),
+        pytest.param(
+            True,
+            False,
+            "Not accessible",
+            id="Available internet and invalid url"
+        ),
+        pytest.param(
+            False,
+            True,
+            "Not accessible",
+            id="Unavailable internet and valid url"
+        ),
+        pytest.param(
+            False,
+            False,
+            "Not accessible",
+            id="Unavailable internet and invalid url"
+        )
+    ]
+)
+def test_output_with_different_values_of_url_internet(
+        internet: bool,
+        valid_url: bool,
+        result: str
 ) -> None:
-    mocked_internet_connection.return_value = False
-    assert can_access_google_page("https://google.com") == "Not accessible"
+    with mock.patch("app.main.has_internet_connection") as mocked_internet:
+        mocked_internet.return_value = internet
+        with mock.patch("app.main.valid_google_url") as mocked_url_validator:
+            mocked_url_validator.return_value = valid_url
+            assert (
+                can_access_google_page("") == result
+            ), f"Function should return {result} when " \
+               f"`has_internet_connection()` is {internet} and " \
+               f"`valid_google_url()` is {valid_url}"
 
 
-@mock.patch("app.main.has_internet_connection")
-@mock.patch("app.main.valid_google_url")
-def test_invalid_link_available_internet(
-        mocked_valid_url: Any,
-        mocked_internet_connection: Any
-) -> None:
-    mocked_internet_connection.return_value = True
-    mocked_valid_url.return_value = False
-    assert can_access_google_page("https/google.com") == "Not accessible"
-
-
-@mock.patch("app.main.has_internet_connection")
-@mock.patch("app.main.valid_google_url")
-def test_invalid_link_unavailable_internet(
-        mocked_valid_url: Any,
-        mocked_internet_connection: Any
-) -> None:
-    mocked_valid_url.return_value = False
-    mocked_internet_connection.return_value = False
-    assert can_access_google_page("https/google.com") == "Not accessible"
-
-
-@mock.patch("app.main.valid_google_url")
-def test_if_the_valid_google_url_called(mocked_valid_url: Any) -> None:
-    can_access_google_page("https://google.com")
-    mocked_valid_url.assert_called_once()
-
-
-@mock.patch("app.main.has_internet_connection")
-def test_if_the_has_internet_connection_called(
-        mocked_internte_connection: Any
-) -> None:
-    can_access_google_page("https://google.com")
-    mocked_internte_connection.assert_called_once()
+@pytest.mark.parametrize(
+    "function",
+    [
+        pytest.param(
+            "app.main.valid_google_url",
+            id="`valid_google_url()` should be called"
+        ),
+        pytest.param(
+            "app.main.has_internet_connection",
+            id="`has_internet_connection()` should be called"
+        )
+    ]
+)
+def test_functions_should_be_called(function: str) -> None:
+    with mock.patch(function) as mocked_function:
+        can_access_google_page("https://google.com")
+        mocked_function.assert_called_once()
