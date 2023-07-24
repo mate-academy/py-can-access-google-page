eed60935bad27806a05ac91add16e2a104e22408
Tanya Burdenko <burdenko.tetyana@gmail.com>
2023-07-24 13:47:21 +0300

    V4

--- app/test_main.py
+++ app/test_main.py
@@ -4,26 +4,24 @@ import pytest
 from app.main import can_access_google_page
 
 
+@mock.patch("app.main.has_internet_connection")
+@mock.patch("app.main.valid_google_url")
 @pytest.mark.parametrize(
-    "internet_connection, is_valid_url, outcome",
-    [
-        (True, True, "Accessible"),
-        (False, True, "Not accessible"),
-        (True, False, "Not accessible"),
-        (False, False, "Not accessible")
-    ]
+        "internet_connection, is_valid_url, outcome",
+        [
+            (True, True, "Accessible"),
+            (True, False, "Not accessible"),
+            (False, True, "Not accessible"),
+            (False, False, "Not accessible")
+        ]
 )
-def test_if_one_can_access_google_page(
+def test_how_the_function_works(
+        mocked_connection,
+        mocked_valid_url,
         internet_connection: bool,
         is_valid_url: bool,
         outcome: str
-) -> None:
-    with mock.patch(
-            "app.main.has_internet_connection"
-    ) as mock_has_internet_connection,\
-        mock.patch(
-            "app.main.valid_google_url"
-    ) as mock_valid_url:
-        mock_has_internet_connection.return_value = internet_connection
-        mock_valid_url.return_value = is_valid_url
-        assert can_access_google_page("") == outcome
+    ) -> None:
+    mocked_connection.return_value = internet_connection
+    mocked_valid_url.return_value = is_valid_url
+    assert can_access_google_page("") == outcome
