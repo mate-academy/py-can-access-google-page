a8329f5325dfdc84bab8d4c2962bec66242c6b7e
Tanya Burdenko <burdenko.tetyana@gmail.com>
2023-07-24 13:51:18 +0300

    V5

--- app/test_main.py
+++ app/test_main.py
@@ -7,21 +7,21 @@ from app.main import can_access_google_page
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
 @pytest.mark.parametrize(
-        "internet_connection, is_valid_url, outcome",
-        [
-            (True, True, "Accessible"),
-            (True, False, "Not accessible"),
-            (False, True, "Not accessible"),
-            (False, False, "Not accessible")
-        ]
+    "internet_connection, is_valid_url, outcome",
+    [
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible")
+    ]
 )
 def test_how_the_function_works(
-        mocked_connection,
-        mocked_valid_url,
+        mocked_connection: bool,
+        mocked_valid_url: bool,
         internet_connection: bool,
         is_valid_url: bool,
         outcome: str
-    ) -> None:
+) -> None:
     mocked_connection.return_value = internet_connection
     mocked_valid_url.return_value = is_valid_url
     assert can_access_google_page("") == outcome
