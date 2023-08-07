3c4daddcb3c64cb164b621f7cfb7631da166e07e
Nazarii Schcur <nazae530@gmail.com>
2023-08-07 20:45:53 +0300

    del url, usr True/Galse

--- app/test_main.py
+++ app/test_main.py
@@ -3,15 +3,20 @@ from unittest import mock
 from app import main
 
 
-@pytest.mark.parametrize("url, expected_result", [
-    ("https://www.google.com", "Accessible"),
-    ("https://www.this-url-does-not-exist.com", "Not accessible"),
-    ("https://www.google.com/nonexistentpage", "Not accessible")
+@pytest.mark.parametrize("has_internet, is_valid_url, expected_result", [
+    (True, True, "Accessible"),
+    (True, False, "Not accessible"),
+    (False, True, "Not accessible"),
+    (False, False, "Not accessible")
 ])
-def test_can_access_google_page(url: str, expected_result: str) -> None:
+def test_can_access_google_page(
+        has_internet: bool,
+        is_valid_url: bool,
+        expected_result: str
+) -> None:
+
     with mock.patch("app.main.has_internet_connection",
-                    return_value=True if "nonexistentpage"
-                                         not in url else False):
+                    return_value=has_internet):
         with mock.patch("app.main.valid_google_url",
-                        return_value=True if "google" in url else False):
-            assert main.can_access_google_page(url) == expected_result
+                        return_value=is_valid_url):
+            assert main.can_access_google_page("") == expected_result
