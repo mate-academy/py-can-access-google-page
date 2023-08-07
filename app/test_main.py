b9b7387c36cfef1ed60eb00653e3008daa77befa
Vitalii2906 <fahivchik1@gmail.com>
2023-08-07 11:52:00 +0300

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -12,7 +12,12 @@ from app.main import can_access_google_page
         ("https://invalid-url.com", False, False, "Not accessible"),
     ],
 )
-def test_can_access_google_page(url, internet, valid, expected):
+def test_can_access_google_page(
+        url: str,
+        internet: bool,
+        valid: bool,
+        expected: str
+) -> None:
     with patch("app.main.valid_google_url", return_value=valid), patch(
         "app.main.has_internet_connection", return_value=internet
     ):
