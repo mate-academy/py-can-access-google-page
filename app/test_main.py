d9a8f7a2e468b0e1e3cf8c974f1e142d1bc4f982
Viktor Deyneka <3lobites@gmail.com>
2023-08-09 21:57:53 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,31 @@
-from app.main import can_access_google_page
-import pytest
-from unittest import mock
-
-#1 - проверить временной променжуток с 6.00 по 23.00
-#2 - проверить работоспособность сайта
-#3 - проверить временной променжуток
-#4 - проверить тип возвращаемых данных
-#5 - проверить что возвращает на неправильный тип данных (не строка ине путь)
+from app.main import (
+    can_access_google_page,
+    valid_google_url,
+    has_internet_connection
+)
 
 
-class CanAccessGooglePage:
-    def test_cannot_access_if_connection_or_valid_url_is_true(self) -> None:
-        pass
+def test_cannot_access_if_connection_or_valid_url_is_true() -> None:
+    url = "http://google.com"
+    result_connection = has_internet_connection()
+    result_valid = valid_google_url(url)
+    if result_connection and result_valid:
+        assert can_access_google_page(url) == "Accessible"
+    else:
+        assert can_access_google_page(url) == "Not accessible"
 
-    def test_cannot_access_if_only_connection(self) -> None:
-        pass
 
-    def test_cannot_access_if_only_valid_url(self) -> None:
-        pass
+def test_cannot_access_if_only_connection() -> None:
+    url = "http://google.com"
+    result_connection = has_internet_connection()
+    result_valid = valid_google_url(url)
+    if result_connection and not result_valid:
+        assert can_access_google_page(url) == "Not accessible"
 
+
+def test_cannot_access_if_only_valid_url() -> None:
+    url = "http://google.com"
+    result_connection = has_internet_connection()
+    result_valid = valid_google_url(url)
+    if not result_connection and result_valid:
+        assert can_access_google_page(url) == "Not accessible"
