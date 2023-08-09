35556d58138b2422e5870691269fac666cd0149b
Alexander Izmashkin <alexandr.izmashkin@gmail.com>
2023-08-09 10:37:58 +0300

    Solution/Alex/2

--- app/test_main.py
+++ app/test_main.py
@@ -6,15 +6,16 @@ from app.main import can_access_google_page
 
 
 @pytest.mark.parametrize(
-    "valid_url,has_internet_connection,expected_result",
+    "test_url,valid_url,has_internet_connection,expected_result",
     [
-        (True, True, "Accessible"),
-        (True, False, "Not accessible"),
-        (False, True, "Not accessible"),
-        (False, False, "Not accessible")
+        ("https://www.google.com/", True, True, "Accessible"),
+        ("https://www.google.com/", True, False, "Not accessible"),
+        ("https://mate.academy/", False, True, "Not accessible"),
+        ("https://mate.academy/", False, False, "Not accessible")
     ]
 )
 def test_can_access_google_page(
+        test_url: str,
         valid_url: bool,
         has_internet_connection: bool,
         expected_result: str
@@ -23,4 +24,14 @@ def test_can_access_google_page(
                      return_value=valid_url),
           mock.patch("app.main.has_internet_connection",
                      return_value=has_internet_connection)):
-        assert can_access_google_page("url") == expected_result
+        assert can_access_google_page(test_url) == expected_result
+
+
+def test_internal_functions_called_only_once() -> None:
+    with (mock.patch("app.main.valid_google_url") as valid_url,
+          mock.patch("app.main.has_internet_connection") as connection):
+
+        can_access_google_page("https://www.google.com/")
+
+        connection.assert_called_once()
+        valid_url.assert_called_once()
