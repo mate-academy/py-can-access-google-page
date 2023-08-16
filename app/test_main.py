d8b34054496f4282a346b536eefb071c2ee75b49
Ivan Proskurnytskyi <prsky.ivan@gmail.com>
2023-08-16 21:23:36 +0200

    Refactored

--- app/test_main.py
+++ app/test_main.py
@@ -10,16 +10,19 @@ from app.main import can_access_google_page
     [
         ("https://google.com", True, True, "Accessible"),
         ("https://some_example.com", False, True, "Not accessible"),
+        ("https://google.com", True, False, "Not accessible")
     ]
 )
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
 def test_is_valid_url_and_internet(
+        moke_url: mock,
+        moke_internet: mock,
         url: str,
         exists: bool,
         internet: bool,
         result: str
 ) -> None:
-    with (
-        mock.patch("app.main.valid_google_url", return_value=exists),
-        mock.patch("app.main.has_internet_connection", return_value=internet)
-    ):
-        assert can_access_google_page(url) == result
+    moke_url.return_value = exists
+    moke_internet.return_value = internet
+    assert can_access_google_page(url) == result
