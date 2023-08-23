2707d5c1da62a1cc29beabd9cc889a7f66ef94ea
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-08-23 16:21:03 +0300

    refactored with pytest.mark.parametrize, flake8 error fixed

--- app/test_main.py
+++ app/test_main.py
@@ -6,28 +6,38 @@ import app.main as main
     "url, expected, mock_valid, mock_internet",
     [
         pytest.param(
-            "https://google.com", "Not accessible",
-            lambda url: True, lambda: False,
-            id="No Internet"
+            "https://google.com",
+            "Not accessible",
+            lambda url: True,
+            lambda: False,
+            id="No Internet",
         ),
         pytest.param(
-            "", "Not accessible",
-            lambda url: False, lambda: True,
-            id="Invalid URL"
+            "",
+            "Not accessible",
+            lambda url: False,
+            lambda: True,
+            id="Invalid URL",
         ),
         pytest.param(
-            "https://google.com", "Accessible",
-            lambda url: True, lambda: True,
-            id="Access Google"
+            "https://google.com",
+            "Accessible",
+            lambda url: True,
+            lambda: True,
+            id="Access Google",
         ),
         pytest.param(
-            "https://www.google.com", "Accessible",
-            lambda url: True, lambda: True,
-            id="Access www.google"
-        )
-    ]
+            "https://www.google.com",
+            "Accessible",
+            lambda url: True,
+            lambda: True,
+            id="Access www.google",
+        ),
+    ],
 )
-def test_can_access_google_page(monkeypatch, url, expected, mock_valid, mock_internet):
+def test_can_access_google_page(
+    monkeypatch, url, expected, mock_valid, mock_internet
+):
     monkeypatch.setattr(main, "has_internet_connection", mock_internet)
     monkeypatch.setattr(main, "valid_google_url", mock_valid)
 
