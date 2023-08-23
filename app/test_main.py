ba30679e5ea72bc3956e048f8ce7e5c553d3531d
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-08-23 16:19:30 +0300

    refactored with pytest.mark.parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -1,66 +1,34 @@
+import pytest
 import app.main as main
 
 
-def test_cant_access_google_page_no_internet(monkeypatch: None) -> None:
-    def mock_valid_google_url(url: str) -> bool:
-        return True
+@pytest.mark.parametrize(
+    "url, expected, mock_valid, mock_internet",
+    [
+        pytest.param(
+            "https://google.com", "Not accessible",
+            lambda url: True, lambda: False,
+            id="No Internet"
+        ),
+        pytest.param(
+            "", "Not accessible",
+            lambda url: False, lambda: True,
+            id="Invalid URL"
+        ),
+        pytest.param(
+            "https://google.com", "Accessible",
+            lambda url: True, lambda: True,
+            id="Access Google"
+        ),
+        pytest.param(
+            "https://www.google.com", "Accessible",
+            lambda url: True, lambda: True,
+            id="Access www.google"
+        )
+    ]
+)
+def test_can_access_google_page(monkeypatch, url, expected, mock_valid, mock_internet):
+    monkeypatch.setattr(main, "has_internet_connection", mock_internet)
+    monkeypatch.setattr(main, "valid_google_url", mock_valid)
 
-    def mock_has_internet_connection() -> bool:
-        return False
-
-    monkeypatch.setattr(
-        main, "has_internet_connection", mock_has_internet_connection
-    )
-    monkeypatch.setattr(main, "valid_google_url", mock_valid_google_url)
-
-    assert (
-        main.can_access_google_page("https://google.com") == "Not accessible"
-    )
-
-
-def test_cant_access_google_page_invalid_url(monkeypatch: None) -> None:
-    def mock_valid_google_url(url: str) -> bool:
-        return False
-
-    def mock_has_internet_connection() -> bool:
-        return True
-
-    monkeypatch.setattr(
-        main, "has_internet_connection", mock_has_internet_connection
-    )
-    monkeypatch.setattr(main, "valid_google_url", mock_valid_google_url)
-
-    assert (
-        main.can_access_google_page("https://google.com") == "Not accessible"
-    )
-
-    def mock_valid_google_url(url: str) -> bool:
-        return False
-
-    def mock_has_internet_connection() -> bool:
-        return True
-
-    monkeypatch.setattr(
-        main, "has_internet_connection", mock_has_internet_connection
-    )
-    monkeypatch.setattr(main, "valid_google_url", mock_valid_google_url)
-
-    assert main.can_access_google_page("") == "Not accessible"
-
-
-def test_can_access_google_page(monkeypatch: None) -> None:
-    def mock_valid_google_url(url: str) -> bool:
-        return True
-
-    def mock_has_internet_connection() -> bool:
-        return True
-
-    monkeypatch.setattr(
-        main, "has_internet_connection", mock_has_internet_connection
-    )
-    monkeypatch.setattr(main, "valid_google_url", mock_valid_google_url)
-
-    assert main.can_access_google_page("https://google.com") == "Accessible"
-    assert (
-        main.can_access_google_page("https://www.google.com") == "Accessible"
-    )
+    assert main.can_access_google_page(url) == expected
