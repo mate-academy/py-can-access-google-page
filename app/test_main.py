e2e8431d0ba478b290267d65efecfb9ae25d4e70
jinjo <vitaly.tyagunov@gmail.com>
2023-08-24 16:24:27 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -6,8 +6,11 @@ from unittest import mock
 @mock.patch("app.main.has_internet_connection")
 def test_can_access_google_page(mock_has_internet: mock,
                                 mock_valid_google_url: mock) -> None:
-    mock_has_internet.side_effect = [True, False, True]
-    mock_valid_google_url.side_effect = [False, True, True]
-    answers = ["Not accessible", "Not accessible", "Accessible"]
+    mock_has_internet.side_effect = [True, False, True, False]
+    mock_valid_google_url.side_effect = [False, True, True, False]
+    answers = ["Not accessible",
+               "Not accessible",
+               "Accessible",
+               "Not accessible"]
     for answer in answers:
         assert can_access_google_page("some url") == answer
