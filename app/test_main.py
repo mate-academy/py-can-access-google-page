febb0801aec7c0ce7cdf7f4cac3e35d38e085888
oreny <orenycdota@gmail.com>
2023-08-28 15:07:46 +0300

    Added ID in pytest.param()

--- app/test_main.py
+++ app/test_main.py
@@ -5,21 +5,34 @@ from app.main import can_access_google_page
 @pytest.mark.parametrize(
     "functions_result, response",
     [
-        ([True, True], "Accessible"),
-        ([False, True], "Not accessible"),
-        ([True, False], "Not accessible"),
+        pytest.param(
+            [True, True],
+            "Accessible",
+            id=(
+                "Should return 'Accessible' if "
+                "url and connection functions return True"
+            ),
+        ),
+        pytest.param(
+            [False, True],
+            "Not accessible",
+            id="Should return 'Not accessible' if invalid url",
+        ),
+        pytest.param(
+            [True, False],
+            "Not accessible",
+            id="Should return 'Not accessible' if no connection",
+        ),
     ],
 )
 def test_can_access_google_page_with_different_url_and_connection_status(
-        functions_result: list[bool], response: str
+    functions_result: list[bool], response: str
 ) -> None:
     monkeypatch = pytest.MonkeyPatch()
     monkeypatch.setattr(
-        "app.main.valid_google_url",
-        lambda *args: functions_result[0]
+        "app.main.valid_google_url", lambda *args: functions_result[0]
     )
     monkeypatch.setattr(
-        "app.main.has_internet_connection",
-        lambda: functions_result[1]
+        "app.main.has_internet_connection", lambda: functions_result[1]
     )
     assert can_access_google_page("") == response
