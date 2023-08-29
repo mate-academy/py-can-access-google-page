ead672896f614f67ca1bce9c2056370baf508e2a
OlegatorLE <oleh.travin@gmail.com>
2023-08-29 19:51:39 +0300

    fixed solution

--- app/test_main.py
+++ app/test_main.py
@@ -17,6 +17,9 @@ from app.main import can_access_google_page
 def test_can_access_google_page(
     connection_status: bool, valid_url: bool, expected: str
 ) -> None:
-    with mock.patch("app.main.has_internet_connection", return_value=connection_status):
+    with mock.patch(
+            "app.main.has_internet_connection",
+            return_value=connection_status
+    ):
         with mock.patch("app.main.valid_google_url", return_value=valid_url):
             assert can_access_google_page("http://google.com") == expected
