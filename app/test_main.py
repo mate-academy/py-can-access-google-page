1da600ca902462c75b788e4aad4b6897c5331116
Oleh Chernous <chernous.oleh@gmail.com>
2023-08-29 20:48:51 +0200

    added parametrize, deleted redundant asserts

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,5 @@
+import pytest
+
 from unittest import mock
 from typing import Callable
 
@@ -6,25 +8,43 @@ from app.main import can_access_google_page
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
+@pytest.mark.parametrize(
+    "valid_google_url, has_internet_connection, expected_message",
+    [
+        pytest.param(
+            True,
+            True,
+            "Accessible",
+            id="test access if valid url and has connection"
+        ),
+        pytest.param(
+            False,
+            True,
+            "Not accessible",
+            id="test access if not valid url and has connection"
+        ),
+        pytest.param(
+            True,
+            False,
+            "Not accessible",
+            id="test access if valid url and has not connection"
+        ),
+        pytest.param(
+            False,
+            False,
+            "Not accessible",
+            id="test access if not valid url and has not connection"
+        )
+    ]
+)
 def test_can_access_if_has_connection_and_valid_url(
         mocked_valid_google_url: Callable,
-        mocked_has_internet_connection: Callable
+        mocked_has_internet_connection: Callable,
+        valid_google_url: bool,
+        has_internet_connection: bool,
+        expected_message: str
 ) -> None:
-    mocked_valid_google_url.return_value = True
-    mocked_has_internet_connection.return_value = True
+    mocked_valid_google_url.return_value = valid_google_url
+    mocked_has_internet_connection.return_value = has_internet_connection
 
-    assert can_access_google_page("url") == "Accessible"
-
-    mocked_valid_google_url.return_value = False
-    mocked_has_internet_connection.return_value = True
-
-    assert can_access_google_page("url") == "Not accessible"
-
-    mocked_valid_google_url.return_value = True
-    mocked_has_internet_connection.return_value = False
-
-    assert can_access_google_page("url") == "Not accessible"
-
-    mocked_valid_google_url.return_value = False
-    mocked_has_internet_connection.return_value = False
-    assert can_access_google_page("url") == "Not accessible"
+    assert can_access_google_page("url") == expected_message
