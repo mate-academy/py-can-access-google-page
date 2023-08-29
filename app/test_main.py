93a5819d1f95b98f21dd414c43f99c637d3fd377
Oleh Chernous <chernous.oleh@gmail.com>
2023-08-29 19:08:01 +0200

    fixed type annotation

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,5 @@
 from unittest import mock
+from typing import Callable
 
 from app.main import can_access_google_page
 
@@ -6,8 +7,8 @@ from app.main import can_access_google_page
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 def test_can_access_if_has_connection_and_valid_url(
-        mocked_valid_google_url,
-        mocked_has_internet_connection
+        mocked_valid_google_url: Callable,
+        mocked_has_internet_connection: Callable
 ) -> None:
     mocked_valid_google_url.return_value = True
     mocked_has_internet_connection.return_value = True
