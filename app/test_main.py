589539e7290d6022692e1ac1ec095a71e78696ce
Vasyl Dmytruk <vasyldmytruk104@gmail.com>
2023-08-30 00:53:03 +0300

    google_v2

--- app/test_main.py
+++ app/test_main.py
@@ -1,22 +1,56 @@
-from unittest import mock
 import pytest
+from pytest import MonkeyPatch
+
 from app.main import can_access_google_page
 
 
-@pytest.mark.parametrize("connection_status, valid_url, expected",
-                         [
-                             (True, True, "Accessible"),
-                             (False, False, "Not accessible"),
-                             (True, False, "Not accessible"),
-                             (False, True, "Not accessible"),
-                         ])
+@pytest.mark.parametrize(
+    "valid_google_url_value,has_internet_connection_value,expected_result",
+    [
+        pytest.param(
+            True,
+            True,
+            "Accessible",
+            id="should return 'Accessible' when"
+               " both functions return True",
+        ),
+        pytest.param(
+            True,
+            False,
+            "Not accessible",
+            id="should return 'Not accessible' when"
+               " 'has_internet_connection' return False",
+        ),
+        pytest.param(
+            False,
+            True,
+            "Not accessible",
+            id="should return 'Not accessible' when"
+               " 'valid_google_url_value' return False",
+        ),
+        pytest.param(
+            False,
+            False,
+            "Not accessible",
+            id="should return 'Not accessible' when"
+               " both functions return False",
+        ),
+    ],
+)
 def test_can_access_google_page(
-        connection_status: bool,
-        valid_url: bool,
-        expected: str
+    monkeypatch: MonkeyPatch,
+    valid_google_url_value: bool,
+    has_internet_connection_value: bool,
+    expected_result: str,
 ) -> None:
-    with mock.patch("app.main.has_internet_connection",
-                    return_value=connection_status):
-        with mock.patch("app.main.valid_google_url",
-                        return_value=valid_url):
-            assert can_access_google_page("http://google.com") == expected
+    monkeypatch.setattr(
+        "app.main.valid_google_url",
+        lambda url: valid_google_url_value
+    )
+    monkeypatch.setattr(
+        "app.main.has_internet_connection",
+        lambda: has_internet_connection_value,
+    )
+
+    assert can_access_google_page("") == expected_result
+
