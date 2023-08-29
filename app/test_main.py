587915b54e29c06f6b402b3c7ef3b84471404ab2
Oleh Chernous <chernous.oleh@gmail.com>
2023-08-29 21:34:08 +0200

    deleted redundant mocks

--- app/test_main.py
+++ app/test_main.py
@@ -1,13 +1,8 @@
 import pytest
 
-from unittest import mock
-from typing import Callable
-
 from app.main import can_access_google_page
 
 
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
 @pytest.mark.parametrize(
     "valid_google_url, has_internet_connection, expected_message",
     [
@@ -38,13 +33,8 @@ from app.main import can_access_google_page
     ]
 )
 def test_can_access_if_has_connection_and_valid_url(
-        mocked_valid_google_url: Callable,
-        mocked_has_internet_connection: Callable,
         valid_google_url: bool,
         has_internet_connection: bool,
         expected_message: str
 ) -> None:
-    mocked_valid_google_url.return_value = valid_google_url
-    mocked_has_internet_connection.return_value = has_internet_connection
-
     assert can_access_google_page("url") == expected_message
