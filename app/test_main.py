bd3a94aa3806783004e5c054a7959a9dff5287cc
OlegatorLE <oleh.travin@gmail.com>
2023-08-29 19:36:33 +0300

    fixed imports

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,7 @@
 from unittest import mock
+
 import pytest
+
 from app.main import can_access_google_page
 
 
@@ -10,15 +12,11 @@ from app.main import can_access_google_page
         (False, False, "Not accessible"),
         (True, False, "Not accessible"),
         (False, True, "Not accessible"),
-    ]
+    ],
 )
 def test_can_access_google_page(
-        connection_status: bool,
-        valid_url: bool,
-        expected: str
+    connection_status: bool, valid_url: bool, expected: str
 ) -> None:
-    with mock.patch("app.main.has_internet_connection",
-                    return_value=connection_status):
-        with mock.patch("app.main.valid_google_url",
-                        return_value=valid_url):
+    with mock.patch("app.main.has_internet_connection", return_value=connection_status):
+        with mock.patch("app.main.valid_google_url", return_value=valid_url):
             assert can_access_google_page("http://google.com") == expected
