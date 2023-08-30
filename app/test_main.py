059428feb56fa9b7017ac484d56e5f381c735510
oreny <orenycdota@gmail.com>
2023-08-30 10:24:24 +0300

    Removed lists

--- app/test_main.py
+++ app/test_main.py
@@ -3,38 +3,38 @@ from app.main import can_access_google_page
 
 
 @pytest.mark.parametrize(
-    "functions_result, response",
+    "url_test, connection_test, response",
     [
         pytest.param(
-            [True, True],
+            True, True,
             "Accessible",
             id="Should return 'Accessible' if both funcs return True",
         ),
         pytest.param(
-            [False, True],
+            False, True,
             "Not accessible",
             id="Should return 'Not accessible' if invalid url",
         ),
         pytest.param(
-            [True, False],
+            True, False,
             "Not accessible",
             id="Should return 'Not accessible' if no connection",
         ),
         pytest.param(
-            [False, False],
+            False, False,
             "Not accessible",
             id="Should return 'Not accessible' if both funcs return False",
         ),
     ],
 )
 def test_can_access_google_page_with_different_url_and_connection_status(
-    functions_result: list[bool], response: str
+    url_test: bool, connection_test: bool, response: str
 ) -> None:
     monkeypatch = pytest.MonkeyPatch()
     monkeypatch.setattr(
-        "app.main.valid_google_url", lambda *args: functions_result[0]
+        "app.main.valid_google_url", lambda *args: url_test
     )
     monkeypatch.setattr(
-        "app.main.has_internet_connection", lambda: functions_result[1]
+        "app.main.has_internet_connection", lambda: connection_test
     )
     assert can_access_google_page("") == response
