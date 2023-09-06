d16829d6b632c5ae4c373813452376e4a71f2d74
struhanets <posseydon87@gmail.com>
2023-09-06 14:58:38 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -34,6 +34,6 @@ def test_can_access_google_page_when_when_all_checks_is_false() -> None:
     with mock.patch("app.main.valid_google_url") as mocked_valid, \
             mock.patch("app.main.has_internet_connection") as mocked_connect:
         mocked_valid.return_value = False
-        mocked_connect.return_value = True
+        mocked_connect.return_value = False
 
         assert can_access_google_page("") == "Not accessible"
