42166b5a85dcc5b0f8bdf8659cb128681ca637c4
anna-lybid <anna.lybid@gmail.com>
2023-09-07 12:12:49 +0300

    Flake update

--- app/test_main.py
+++ app/test_main.py
@@ -7,15 +7,20 @@ from app.main import can_access_google_page
 @pytest.mark.parametrize(
     "has_internet_connection, valid_url, expected",
     [
+        (True, True, "Accessible"),
         (True, False, "Not accessible"),
         (False, False, "Not accessible"),
         (False, True, "Not accessible"),
-        (True, True, "Accessible")
     ]
 )
-def test_can_access_google_page(has_internet_connection, valid_url, expected):
+def test_can_access_google_page(
+        has_internet_connection: bool,
+        valid_url: bool,
+        expected: str
+) -> None:
     with (
-        mock.patch("app.main.has_internet_connection", return_value=has_internet_connection),
+        mock.patch("app.main.has_internet_connection",
+                   return_value=has_internet_connection),
         mock.patch("app.main.valid_google_url", return_value=valid_url)
     ):
         assert can_access_google_page("http://google.com") == expected
