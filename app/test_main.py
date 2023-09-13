e9d376fd5aea72b7563576cd6be563779b7babc8
Artem <hartem472@gmail.com>
2023-09-13 14:47:01 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,43 @@
 from unittest import mock
+import pytest
 
 from app.main import can_access_google_page
 
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
+@pytest.mark.parametrize(
+    "connection, url, result",
+    [
+        pytest.param(
+            True,
+            True,
+            "Accessible",
+            id="should return `Accessible` when all is ok"
+        ),
+        pytest.param(
+            False,
+            True,
+            "Not accessible",
+            id="should return `Not accessible` when lost connection"
+        ),
+        pytest.param(
+            True,
+            False,
+            "Not accessible",
+            id="should return `Not accessible` when url is false"
+        )
+    ]
+)
 def test_can_access_google_page(
-        mocked_has_internet_connection: bool,
-        mocked_valid_google_url: bool
+        mocked_has_internet_connection: mock.MagicMock,
+        mocked_valid_google_url: mock.MagicMock,
+        connection: bool,
+        url: bool,
+        result: str
 ) -> None:
 
-    mocked_has_internet_connection.return_value = True
-    mocked_valid_google_url.return_value = True
+    mocked_has_internet_connection.return_value = connection
+    mocked_valid_google_url.return_value = url
 
-    assert can_access_google_page("https://www.youtube.com") == "Accessible"
-
-    mocked_has_internet_connection.return_value = False
-
-    assert can_access_google_page("https://www.google.com") == "Not accessible"
-
-    mocked_has_internet_connection.return_value = True
-    mocked_valid_google_url.return_value = False
-
-    assert can_access_google_page("https://www.google.com") == "Not accessible"
+    assert can_access_google_page("https://www.google.com") == result
