5024b5ead1198e276df2f69995fa694eac521e77
Ostap <ostapbojko04@gmail.com>
2023-09-16 16:26:12 +0300

    py-can2

--- app/test_main.py
+++ app/test_main.py
@@ -1,40 +1,25 @@
-import pytest
 from unittest.mock import patch
 from app.main import can_access_google_page
+import pytest
 
 
-@pytest.fixture
-def mock_functions():
-    with patch("app.main.valid_google_url", return_value=True), \
-         patch("app.main.has_internet_connection", return_value=True):
-        yield
-
-
-def test_can_access_google_page_valid_url_and_internet(mock_functions):
+@pytest.mark.parametrize("has_internet,valid_url,expected_result", [
+    (True, True, "Accessible"),
+    (False, True, "Not accessible"),
+    (True, False, "Not accessible"),
+    (False, False, "Not accessible")
+])
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
+def test_can_access_google_page(
+        has_internet_connextion_mock: bool,
+        valid_google_url_mock: bool,
+        has_internet: str,
+        valid_url: str,
+        expected_result: list
+) -> list:
     url = "https://www.google.com"
+    has_internet_connextion_mock.return_value = has_internet
+    valid_google_url_mock.return_value = valid_url
     result = can_access_google_page(url)
-    assert result == "Accessible"
-
-
-def test_car_access_google_page_valid_url_no_internet(mock_functions):
-    url = "https://www.google.com"
-    result = can_access_google_page(url)
-    assert result == "Accessible"
-
-
-def test_can_access_google_page_invalid_url_with_internet(mock_functions):
-    url = "https://example.com"
-    result = can_access_google_page(url)
-    assert result == "Accessible"
-
-
-def test_can_access_google_page_invalid_url_no_internet(mock_functions):
-    url = "https://example.com"
-    result = can_access_google_page(url)
-    assert result == "Accessible"
-
-
-def test_can_access_google_page_invalid_url_not_internet(mock_functions):
-    url = ""
-    result = can_access_google_page(url)
-    assert result == "Not accessible"
+    assert result == expected_result
