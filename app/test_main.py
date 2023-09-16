cd4fbc947cb082c7dd457fce8947a01bedd7ad06
Ruslan <rusipbox@gmail.com>
2023-09-16 19:15:17 +0300

    Test(py-can-access-google-page)

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,23 @@
-# write your code here
+from app.main import can_access_google_page
+
+
+def test_can_access_google_page_accessible(mocker) -> None:
+    url = "https://www.google.com"
+
+    mocker.patch("main.valid_google_url", return_value=True)
+    mocker.patch("main.has_internet_connection", return_value=True)
+
+    result = can_access_google_page(url)
+
+    assert result == "Accessible"
+
+
+def test_can_access_google_page_not_accessible_due_to_url(mocker) -> None:
+    url = "https://www.smth-incorrect.com"
+
+    mocker.patch("main.valid_google_url", return_value=False)
+    mocker.patch("main.has_internet_connection", return_value=True)
+
+    result = can_access_google_page(url)
+
+    assert result == "Not accessible"
