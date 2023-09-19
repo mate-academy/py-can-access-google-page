50b98f2050a3f49a8d4e1955c5de5ef4530d1f37
Olgierrd <oolgierrd@gmail.com>
2023-09-19 18:26:30 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,8 +1,7 @@
-from typing import Callable
-
 import pytest
 from unittest.mock import patch
 from app.main import can_access_google_page
+from typing import Callable
 
 
 @pytest.fixture
@@ -17,24 +16,29 @@ def has_internet_connection_mock() -> None:
         yield mock
 
 
-def test_accessible(
-        valid_google_url_mock: Callable,
-        has_internet_connection_mock: Callable
+@pytest.mark.parametrize(
+    "valid_url, has_internet, url, expected_result",
+    [
+        (True, True, "https://www.google.com", "Accessible"),
+        (True, False, "https://www.google.com", "Not accessible"),
+        (False, False, "https://example.com", "Not accessible"),
+        (False, True, "https://example.com", "Not accessible")
+    ],
+    ids=[
+        "Accessible with Internet",
+        "Not Accessible without Internet",
+        "Not Accessible without Internet and Invalid URL",
+        "Not Accessible with Invalid URL",
+    ],
+)
+def test_can_access_google_page(
+    valid_google_url_mock: Callable,
+    has_internet_connection_mock: Callable,
+    valid_url: bool,
+    has_internet: bool,
+    url: str,
+    expected_result: str
 ) -> None:
-    valid_google_url_mock.return_value = True
-    has_internet_connection_mock.return_value = True
-    assert can_access_google_page("https://www.google.com") == "Accessible"
-
-
-def test_not_accessible_no_internet(
-        valid_google_url_mock: Callable,
-        has_internet_connection_mock: Callable
-) -> None:
-    valid_google_url_mock.return_value = True
-    has_internet_connection_mock.return_value = False
-    assert can_access_google_page("https://www.google.com") == "Not accessible"
-
-
-def test_not_accessible_invalid_url(valid_google_url_mock: Callable) -> None:
-    valid_google_url_mock.return_value = False
-    assert can_access_google_page("https://example.com") == "Not accessible"
+    valid_google_url_mock.return_value = valid_url
+    has_internet_connection_mock.return_value = has_internet
+    assert can_access_google_page(url) == expected_result
