a60a09432038a478c61c46f44352251967cf06d5
Misha Shushman <endernagashushman@gmail.com>
2023-09-21 22:27:39 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,29 @@
-# write your code here
+import datetime
+from unittest import mock
+
+import requests
+from .main import *
+
+
+@mock.patch("app.main.has_internet_connection")
+@mock.patch("app.main.valid_google_url")
+def test_valid_time_and_url(internet, google_valid):
+    internet.return_value = True
+    google_valid.return_value = True
+    assert can_access_google_page("https://www.google.com/") == "Accessible"
+
+
+@mock.patch("app.main.has_internet_connection")
+@mock.patch("app.main.valid_google_url")
+def test_invalid_time_valid_url(internet, google_valid):
+    internet.return_value = False
+    google_valid.return_value = True
+    assert can_access_google_page("https://www.google.com/") == "Not accessible"
+
+
+@mock.patch("app.main.has_internet_connection")
+@mock.patch("app.main.valid_google_url")
+def test_valid_time_invalid_url(internet, google_valid):
+    internet.return_value = True
+    google_valid.return_value = False
+    assert can_access_google_page("https://www.google.com/") == "Not accessible"
