1a2cb050e7dd8823ca246cdbf243945a1dfa9740
Misha Shushman <endernagashushman@gmail.com>
2023-09-21 22:43:41 +0300

    Flake8 fixes

--- app/test_main.py
+++ app/test_main.py
@@ -1,13 +1,13 @@
-import datetime
+from typing import Callable
 from unittest import mock
 
-import requests
-from .main import *
+from app.main import can_access_google_page
 
 
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
-def test_valid_time_and_url(internet, google_valid):
+def test_valid_time_and_url(internet: Callable,
+                            google_valid: Callable) -> None:
     internet.return_value = True
     google_valid.return_value = True
     assert can_access_google_page("https://www.google.com/") == "Accessible"
@@ -15,15 +15,19 @@ def test_valid_time_and_url(internet, google_valid):
 
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
-def test_invalid_time_valid_url(internet, google_valid):
+def test_invalid_time_valid_url(internet: Callable,
+                                google_valid: Callable) -> None:
     internet.return_value = False
     google_valid.return_value = True
-    assert can_access_google_page("https://www.google.com/") == "Not accessible"
+    assert (can_access_google_page("https://www.google.com/")
+            == "Not accessible")
 
 
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
-def test_valid_time_invalid_url(internet, google_valid):
+def test_valid_time_invalid_url(internet: Callable,
+                                google_valid: Callable) -> None:
     internet.return_value = True
     google_valid.return_value = False
-    assert can_access_google_page("https://www.google.com/") == "Not accessible"
+    assert (can_access_google_page("https://www.google.com/")
+           == "Not accessible")
