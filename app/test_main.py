fcf7dea8f0b4533dee893d5fd748d5c2e80a0f1e
Anton <anton380969658858@gmail.com>
2023-09-26 16:14:04 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,12 @@
-# write your code here
+from unittest import mock
+from app.main import can_access_google_page
+
+
+def test_valid_url_and_connection_exists() -> None:
+    with (
+        mock.patch("app.main.has_internet_connection") as mocked_connection,
+        mock.patch("app.main.valid_google_url") as mocked_valid_google_url
+    ):
+        can_access_google_page("https://google.com")
+        mocked_connection.assert_called_once()
+        mocked_valid_google_url.assert_called_once()
