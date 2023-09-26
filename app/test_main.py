fa1a89dcca807f0fd75cd1598cb3f6a7c15d33fc
Nadiia <nadiia.developer777@gmail.com>
2023-09-26 21:36:06 +0300

    Solution-four-py-can-access-google-page

--- app/test_main.py
+++ app/test_main.py
@@ -19,8 +19,14 @@ class TestGoogleFunctions(unittest.TestCase):
         result = can_access_google_page("https://www.google.com")
         self.assertEqual(result, "Accessible")
         self.mock_valid_google_url.return_value = False
+        self.mock_has_internet_connection.return_value = True
         result = can_access_google_page("https://www.google.com")
         self.assertEqual(result, "Not accessible")
+        self.mock_valid_google_url.return_value = True
+        self.mock_has_internet_connection.return_value = False
+        result = can_access_google_page("https://www.google.com")
+        self.assertEqual(result, "Not accessible")
+        self.mock_valid_google_url.return_value = False
         self.mock_has_internet_connection.return_value = False
         result = can_access_google_page("https://www.google.com")
         self.assertEqual(result, "Not accessible")
