8d0a7d54f63e86d28aa656fb6bd804e858b60e7d
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-10-02 20:07:57 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -3,13 +3,14 @@ from unittest.mock import patch
 from app.main import can_access_google_page
 
 
-@pytest.mark.parametrize("has_internet_connection, "
-                         "valid_google_url, "
-                         "expected_result", [
-    (True, True, "Accessible"),
-    (False, True, "Not accessible"),
-    (True, False, "Not accessible"),
-])
+@pytest.mark.parametrize(
+    "has_internet_connection, valid_google_url, expected_result",
+    [
+        (True, True, "Accessible"),
+        (False, True, "Not accessible"),
+        (True, False, "Not accessible"),
+    ]
+)
 @patch("app.main.has_internet_connection")
 @patch("app.main.valid_google_url")
 def test_can_access_google_page(
