d1479d0728f71fa4257202db62ef63d7d2bf0592
Vadym Tiron <tironvadim1@gmail.com>
2023-10-05 14:58:35 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -10,21 +10,20 @@ from app.main import can_access_google_page
 @pytest.mark.parametrize(
     "valid_url, has_internet, expected_result",
     [
-        ("https://www.google.com", True, "Accessible"),
-        ("https://www.google.com", False, "Not accessible"),
-        ("https://www.invalidurl.com", True, "Not accessible"),
-        ("https://www.invalidurl.com", False, "Not accessible"),
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible"),
     ],
 )
 def test_can_access_google_page(
         mock_has_internet_connection: Mock,
         mock_valid_google_url: Mock,
-        valid_url: str,
+        valid_url: bool,
         has_internet: bool,
         expected_result: str
 ) -> None:
-    mock_valid_google_url.return_value = \
-        True if valid_url == "https://www.google.com" else False
+    mock_valid_google_url.return_value = valid_url
     mock_has_internet_connection.return_value = has_internet
 
     assert can_access_google_page(valid_url) == expected_result
