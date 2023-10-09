31bd6e2e88c7f6a221bcad41f4ba9b501d35d959
yevpy <eugene2piskun@gmail.com>
2023-10-10 01:32:31 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,29 @@
-# write your code here
+from unittest import mock
+from app.main import can_access_google_page
+import pytest
+
+
+@pytest.mark.parametrize(
+    "function_valid_google_url,"
+    "function_has_internet_connection,"
+    "expect_result", [
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, False, "Not accessible"),
+        (False, True, "Not accessible"),
+    ])
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_cannot_access_if_only_internet_connection(
+        mocked_valid_google_url: bool,
+        mocked_has_internet_connection: bool,
+        function_valid_google_url: bool,
+        function_has_internet_connection: bool,
+        expect_result: bool
+) -> None:
+    mocked_valid_google_url.return_value = function_valid_google_url
+    mocked_has_internet_connection.return_value = (
+        function_has_internet_connection
+    )
+    result = can_access_google_page("https://www.google.com.ua/")
+    assert result == expect_result
