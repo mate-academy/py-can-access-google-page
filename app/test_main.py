2c1c385a3f8b7a4887cb551620299bde6e5350e9
ValeriiaShvanska <ladyvaleriya13@gmail.com>
2023-10-19 18:06:32 +0300

    Changes

--- app/test_main.py
+++ app/test_main.py
@@ -12,18 +12,21 @@ def test_cannot_access__with_valid_url_and_without_internet() -> None:
     with (mock.patch("app.main.valid_google_url", return_value=True),
          mock.patch("app.main.has_internet_connection", return_value=False)):
         assert can_access_google_page(
-            "https://www.google.com") == "Not accessible"
+            "https://www.google.com"
+        ) == "Not accessible"
 
 
 def test_cannot_access__with_invalid_url_and_with_internet() -> None:
     with (mock.patch("app.main.valid_google_url", return_value=False),
          mock.patch("app.main.has_internet_connection", return_value=True)):
         assert can_access_google_page(
-            "https://wwwgoogle.com") == "Not accessible"
+            "https://wwwgoogle.com"
+        ) == "Not accessible"
 
 
 def test_cannot_access__with_unvalid_url_and_without_internet() -> None:
     with (mock.patch("app.main.valid_google_url", return_value=False),
          mock.patch("app.main.has_internet_connection", return_value=False)):
         assert can_access_google_page(
-            "https://wwwgoogle.com") == "Not accessible"
+            "https://wwwgoogle.com"
+        ) == "Not accessible"
