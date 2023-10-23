7d69ea9b37b56a7ca6d13f3ce578b93ed8c642d2
Oleksandr Pupchenko <oleksandr.pupchenko@gmail.com>
2023-10-24 00:30:38 +0200

    update

--- tests/test_tests.py
+++ tests/test_tests.py
@@ -1,54 +1,22 @@
+from unittest import mock
 import pytest
-
-from app import main
-
-
-def test_cannot_access_if_connection_or_valid_url_is_true(monkeypatch):
-    def can_access_if_connection_or_valid_url(url):
-        from app.main import has_internet_connection, valid_google_url
-
-        if has_internet_connection() or valid_google_url(url):
-            return "Accessible"
-        else:
-            return "Not accessible"
-
-    monkeypatch.setattr(
-        main, "can_access_google_page", can_access_if_connection_or_valid_url
-    )
-
-    test_result = pytest.main(["app/test_main.py"])
-    assert test_result.value == 1, (
-        "You cannot access page if only one of 'connection' or " "'valid url' is True."
-    )
+from app.main import can_access_google_page
 
 
-def test_cannot_access_if_only_connection(monkeypatch):
-    def can_access_if_connection(url):
-        from app.main import has_internet_connection, valid_google_url
+@pytest.mark.parametrize(
+    "test_url,result",
+    [
+        ("https://www.google.com/", "Accessible"),
+        ("htpp://www.goole.com/", "Not accessible")
+    ]
+)
+def test_valid_url_and_connection_exists(test_url: str, result: str) -> None:
+    with (
+        mock.patch("app.main.valid_google_url") as mocked_valid_google_url,
+        mock.patch("app.main.has_internet_connection")
+        as mocked_has_internet_connection
+    ):
 
-        if has_internet_connection():
-            return "Accessible"
-        else:
-            return "Not accessible"
-
-    monkeypatch.setattr(main, "can_access_google_page", can_access_if_connection)
-
-    test_result = pytest.main(["app/test_main.py"])
-    assert (
-        test_result.value == 1
-    ), "You cannot access page if only 'connection' is True."
-
-
-def test_cannot_access_if_only_valid_url(monkeypatch):
-    def can_access_if_connection(url):
-        from app.main import has_internet_connection, valid_google_url
-
-        if valid_google_url(url):
-            return "Accessible"
-        else:
-            return "Not accessible"
-
-    monkeypatch.setattr(main, "can_access_google_page", can_access_if_connection)
-
-    test_result = pytest.main(["app/test_main.py"])
-    assert test_result.value == 1, "You cannot access page if only 'valid url' is True."
+        can_access_google_page(test_url)
+        mocked_valid_google_url.assert_called_with(test_url)
+        mocked_has_internet_connection.assert_called()
