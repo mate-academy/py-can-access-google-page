0b2f59be19a3633202b62b03966e4eb2f83a267b
Dmytro Prokhach <dprokhach@gmail.com>
2023-10-24 17:20:02 +0300

    united 2 contex managers

--- app/test_main.py
+++ app/test_main.py
@@ -15,6 +15,5 @@ from unittest.mock import patch
 def test_can_access_google_page(mocked_connection: bool,
                                 mocked_url: bool, result: str) -> None:
     with patch("app.main.has_internet_connection",
-               return_value=mocked_connection):
-        with patch("app.main.valid_google_url", return_value=mocked_url):
-            assert can_access_google_page("https://www.google.com") == result
+               return_value=mocked_connection), patch("app.main.valid_google_url", return_value=mocked_url):
+        assert can_access_google_page("https://www.google.com") == result
