ed8dba65e08a79d2d5d28c9a3519cad13da63c5b
naiv <nazarivankiv1@gmail.com>
2023-10-25 11:18:33 +0300

    New solution with parametrized test

--- app/test_main.py
+++ app/test_main.py
@@ -1,49 +1,50 @@
-from unittest.mock import patch, MagicMock
+import pytest
+from unittest.mock import patch
 
 from app.main import can_access_google_page
 
 
-@patch("app.main.valid_google_url")
-@patch("app.main.has_internet_connection")
-def test_access_with_valid_url_and_interned_connection(
-    mocked_valid_google_url: MagicMock,
-    mocked_has_internet_connection: MagicMock
+@pytest.mark.parametrize(
+    "url_valid,has_internet,google_url,expected_result",
+    [
+        pytest.param(
+            True,
+            True,
+            "https://www.google.com/",
+            "Accessible",
+            id="Has internet and valid URL"
+        ),
+        pytest.param(
+            False,
+            True,
+            "https://www.google/",
+            "Not accessible",
+            id="Has internet and invalid URL"
+        ),
+        pytest.param(
+            True,
+            False,
+            "https://www.google.com/",
+            "Not accessible",
+            id="No internet and valid URL"
+        ),
+        pytest.param(
+            False,
+            False,
+            "https://www.google/",
+            "Not accessible",
+            id="No internet and invalid URL"
+        )
+    ]
+)
+def test_can_access_google_page(
+    url_valid: bool,
+    has_internet: bool,
+    google_url: str,
+    expected_result: str,
 ) -> None:
-    mocked_valid_google_url.return_value = True
-    mocked_has_internet_connection.return_value = True
-    assert can_access_google_page("https://www.google.com/") == "Accessible"
-
-
-@patch("app.main.valid_google_url")
-@patch("app.main.has_internet_connection")
-def test_access_with_invalid_url_and_interned_connection(
-    mocked_valid_google_url: MagicMock,
-    mocked_has_internet_connection: MagicMock
-) -> None:
-    mocked_valid_google_url.return_value = False
-    mocked_has_internet_connection.return_value = True
-    assert can_access_google_page("https://www.google/") == "Not accessible"
-
-
-@patch("app.main.valid_google_url")
-@patch("app.main.has_internet_connection")
-def test_access_with_valid_url_and_no_interned_connection(
-    mocked_valid_google_url: MagicMock,
-    mocked_has_internet_connection: MagicMock
-) -> None:
-    mocked_valid_google_url.return_value = True
-    mocked_has_internet_connection.return_value = False
-    assert can_access_google_page(
-        "https://www.google.com/"
-    ) == "Not accessible"
-
-
-@patch("app.main.valid_google_url")
-@patch("app.main.has_internet_connection")
-def test_access_with_invalid_url_and_no_interned_connection(
-    mocked_valid_google_url: MagicMock,
-    mocked_has_internet_connection: MagicMock
-) -> None:
-    mocked_valid_google_url.return_value = False
-    mocked_has_internet_connection.return_value = False
-    assert can_access_google_page("https://www.google/") == "Not accessible"
+    with (
+        patch("app.main.valid_google_url", return_value=url_valid),
+        patch("app.main.has_internet_connection", return_value=has_internet)
+    ):
+        assert can_access_google_page(google_url) == expected_result
