78c95f57e2eb3539ee3e6f3c0b3a4484717164b7
Olena Lisova <lisovausa@gmail.com>
2023-10-30 18:42:06 -0400

    Changed can access google page

--- app/main.py
+++ app/main.py
@@ -17,5 +17,3 @@ def can_access_google_page(url: str) -> str:
         return "Accessible"
     else:
         return "Not accessible"
-
-# print(can_access_google_page("http://seasonvar.ru/serial-11941-Tajny_Broukenvuda.html"))
