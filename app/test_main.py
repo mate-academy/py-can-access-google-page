001d485a1d1b18079e5c9691b6642fa3de8af27e
Olena Lisova <lisovausa@gmail.com>
2023-10-30 18:42:06 -0400

    type annotation mock_internrt: mock

--- app/test_main.py
+++ app/test_main.py
@@ -2,23 +2,45 @@ from unittest import mock
 from app.main import can_access_google_page
 
 
-def test_called_function() -> None:
-    with (mock.patch("app.main.valid_google_url") as mock_valid,
-          mock.patch("app.main.has_internet_connection") as mock_internet):
-        page = "http://seasonvar.ru/serial-11941-Tajny_Broukenvuda.html"
-
-        can_access_google_page(page)
-
-        mock_internet.assert_called_once()
-        mock_valid.assert_called_once_with(page)
-
-
-def test_accessible() -> None:
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_called_function(mock_internet: mock, mock_valid: mock) -> None:
     page = "http://seasonvar.ru/serial-11941-Tajny_Broukenvuda.html"
-    with (mock.patch("app.main.valid_google_url") as mock_valid,
-          mock.patch("app.main.has_internet_connection") as mock_internet):
 
-        mock_internet.return_value = True
-        mock_valid.return_value = True
+    can_access_google_page(page)
 
-        assert can_access_google_page(page) == "Accessible"
+    mock_internet.assert_called_once()
+    mock_valid.assert_called_once_with(page)
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_accessible(mock_internet: mock, mock_valid: mock) -> None:
+    page = "http://seasonvar.ru/serial-11941-Tajny_Broukenvuda.html"
+
+    mock_internet.return_value = True
+    mock_valid.return_value = True
+
+    assert can_access_google_page(page) == "Accessible"
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_internet_not_accessible(mock_internet: mock, mock_valid: mock) -> None:
+    page = "http://seasonvar.ru/serial-11941-Tajny_Broukenvuda.html"
+
+    mock_internet.return_value = False
+    mock_valid.return_value = True
+
+    assert can_access_google_page(page) == "Not accessible"
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_google_url_not_accessible(mock_internet: mock, mock_valid: mock) -> None:
+    page = "http://seasonvar.ru/serial-11941-Tajny_Broukenvuda.html"
+
+    mock_internet.return_value = True
+    mock_valid.return_value = False
+
+    assert can_access_google_page(page) == "Not accessible"
