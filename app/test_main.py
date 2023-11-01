70213791a76157213bb90398bcda289b54f9ec0e
Frezworx <frezworx@gmail.com>
2023-11-01 14:58:05 +0200

    Used mock.patch to create mocks for valid_google_url and has_internet_connection

--- app/test_main.py
+++ app/test_main.py
@@ -1,38 +1,29 @@
 from typing import Any
-
-import pytest
-
+from unittest import TestCase, mock
 from app.main import can_access_google_page
 
 
-@pytest.mark.parametrize(
-    "internet,valid_url,expected",
-    [
-        (True, True, "Accessible"),
-        (True, False, "Not accessible"),
-        (False, True, "Not accessible"),
-        (False, False, "Not accessible"),
-    ],
-)
-def test_access_google_page(
-        monkeypatch: Any,
-        internet: bool,
-        valid_url: bool,
-        expected: Any
-) -> None:
-    def mock_has_internet_connection() -> bool:
-        return internet
+class TestCanAccessGooglePage(TestCase):
+    @mock.patch("app.main.valid_google_url")
+    @mock.patch("app.main.has_internet_connection")
+    def test_can_access_google_page(
+            self,
+            mock_has_internet_connection: Any,
+            mock_valid_google_url: Any
+    ) -> None:
+        test_cases = [
+            {"connection": True, "url": True, "expected": "Accessible"},
+            {"connection": False, "url": True, "expected": "Not accessible"},
+            {"connection": True, "url": False, "expected": "Not accessible"},
+            {"connection": False, "url": False, "expected": "Not accessible"},
+        ]
 
-    def mock_valid_google_url(url: str) -> bool:
-        return valid_url
-
-    monkeypatch.setattr(
-        "app.main.has_internet_connection",
-        mock_has_internet_connection
-    )
-
-    monkeypatch.setattr(
-        "app.main.valid_google_url", mock_valid_google_url
-    )
-
-    assert expected == can_access_google_page("url")
+        for test_case in test_cases:
+            mock_valid_google_url.return_value = test_case.get("url")
+            mock_has_internet_connection.return_value = (
+                test_case.get("connection")
+            )
+            self.assertEqual(
+                can_access_google_page("url"),
+                test_case.get("expected")
+            )
