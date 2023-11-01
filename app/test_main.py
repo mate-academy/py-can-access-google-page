6f886ecf7b03a45e1b9c55a1afd03991e35a5bff
Frezworx <frezworx@gmail.com>
2023-11-01 12:51:07 +0200

    Solution, add notations

--- app/test_main.py
+++ app/test_main.py
@@ -1,39 +1,38 @@
 from typing import Any
 
+import pytest
+
 from app.main import can_access_google_page
 
 
-def test_access_google_page(monkeypatch: Any) -> None:
+@pytest.mark.parametrize(
+    "internet,valid_url,expected",
+    [
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible"),
+    ],
+)
+def test_access_google_page(
+        monkeypatch: Any,
+        internet: bool,
+        valid_url: bool,
+        expected: Any
+) -> None:
     def mock_has_internet_connection() -> bool:
-        return True
+        return internet
 
     def mock_valid_google_url(url: str) -> bool:
-        return True
+        return valid_url
 
-    monkeypatch.setattr(
-        "app.main.valid_google_url", mock_valid_google_url
-    )
     monkeypatch.setattr(
         "app.main.has_internet_connection",
         mock_has_internet_connection
     )
 
-    assert can_access_google_page("url") == "Accessible"
-
-
-def test_not_access_google_page(monkeypatch: Any) -> None:
-    def mock_has_internet_connection() -> bool:
-        return False
-
-    def mock_valid_google_url(url: str) -> bool:
-        return True
-
     monkeypatch.setattr(
         "app.main.valid_google_url", mock_valid_google_url
     )
-    monkeypatch.setattr(
-        "app.main.has_internet_connection",
-        mock_has_internet_connection
-    )
 
-    assert can_access_google_page("url") == "Not accessible"
+    assert expected == can_access_google_page("url")
