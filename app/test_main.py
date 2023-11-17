938e0e306406e37838e2832c67029e1faf04e85c
Alex <oleksa.shcherbakov@gmail.com>
2023-11-17 14:37:28 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -13,16 +13,16 @@ from app.main import can_access_google_page
         (False, False, "Not accessible")
     ]
 )
-@mock.patch("app.main.valid_google_url")
+@monk.patch("app.main.valid_google_url")
 @monk.patch("app.main.has_internet_connection")
 def test_can_access_google_page(
         mock_has_internet: bool,
         mock_valid_url: bool,
         url_validation: bool,
         internet_connection: bool,
-        access_resul: str
+        access_result: str
 ) -> None:
     mock_has_internet.return_value = internet_connection
     mock_valid_url.return_value = url_validation
 
-    assert can_access_google_page("url") ==access_result
+    assert can_access_google_page("url") == access_result
