db3be8efef4dd4c47ccf76b95dcae25c95933ea5
unknown <temakot254@gmail.com>
2023-11-26 11:10:49 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -8,8 +8,13 @@ from app.main import can_access_google_page
     (False, True, "Not accessible"),
     (True, False, "Not accessible"),
 ])
-def test_can_access_google_page(internet_connection, valid_url, expected_result):
-    with patch("app.main.has_internet_connection", return_value=internet_connection), \
+def test_can_access_google_page(
+        internet_connection: bool,
+        valid_url: bool,
+        expected_result: str
+) -> None:
+    with patch("app.main.has_internet_connection",
+               return_value=internet_connection), \
          patch("app.main.valid_google_url", return_value=valid_url):
         result = can_access_google_page("https://www.google.com")
         assert result == expected_result
