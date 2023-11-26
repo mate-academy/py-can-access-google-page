1da89d1752b1a5192d9aa1ec637b8c6e71c0d1ea
unknown <temakot254@gmail.com>
2023-11-26 11:07:30 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,30 +1,15 @@
-import unittest
+import pytest
 from unittest.mock import patch
 from app.main import can_access_google_page
 
 
-class TestCanAccessGooglePage(unittest.TestCase):
-    @patch("app.main.valid_google_url")
-    @patch("app.main.has_internet_connection")
-    def test_can_access_google_page(
-            self,
-            mock_has_internet_connection: bool,
-            mock_valid_google_url: bool
-    ) -> None:
-        mock_has_internet_connection.return_value = True
-        mock_valid_google_url.return_value = True
+@pytest.mark.parametrize("internet_connection, valid_url, expected_result", [
+    (True, True, "Accessible"),
+    (False, True, "Not accessible"),
+    (True, False, "Not accessible"),
+])
+def test_can_access_google_page(internet_connection, valid_url, expected_result):
+    with patch("app.main.has_internet_connection", return_value=internet_connection), \
+         patch("app.main.valid_google_url", return_value=valid_url):
         result = can_access_google_page("https://www.google.com")
-        self.assertEqual(result, "Accessible")
-
-        mock_has_internet_connection.return_value = False
-        result = can_access_google_page("https://www.google.com")
-        self.assertEqual(result, "Not accessible")
-
-        mock_has_internet_connection.return_value = True
-        mock_valid_google_url.return_value = False
-        result = can_access_google_page("https://www.google.com")
-        self.assertEqual(result, "Not accessible")
-
-
-if __name__ == "__main__":
-    unittest.main()
+        assert result == expected_result
