16ba58a6d7b5bd00f3969720746a4fc8565f5b7a
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2023-11-27 23:04:07 +0200

    fix again

--- app/test_main.py
+++ app/test_main.py
@@ -11,25 +11,41 @@ from app.main import (valid_google_url,
 class TestCanAccessGooglePage(unittest.TestCase):
     @patch("app.main.has_internet_connection", return_value=True)
     @patch("app.main.valid_google_url", return_value=True)
-    def test_accessible_when_internet_and_valid_url(self) -> None:
+    def test_accessible_when_internet_and_valid_url(
+            self,
+            has_internet_connection: bool,
+            valid_google_url: bool
+    ) -> None:
         result = can_access_google_page("http://www.google.com")
         self.assertEqual(result, "Accessible")
 
     @patch("app.main.has_internet_connection", return_value=False)
     @patch("app.main.valid_google_url", return_value=True)
-    def test_not_accessible_when_no_internet(self) -> None:
+    def test_not_accessible_when_no_internet(
+            self,
+            has_internet_connection: bool,
+            valid_google_url: bool
+    ) -> None:
         result = can_access_google_page("http://www.google.com")
         self.assertEqual(result, "Not accessible")
 
     @patch("app.main.has_internet_connection", return_value=True)
     @patch("app.main.valid_google_url", return_value=False)
-    def test_not_accessible_when_invalid_url(self) -> None:
+    def test_not_accessible_when_invalid_url(
+            self,
+            has_internet_connection: bool,
+            valid_google_url: bool
+    ) -> None:
         result = can_access_google_page("http://invalid-url.com")
         self.assertEqual(result, "Not accessible")
 
     @patch("app.main.has_internet_connection", return_value=False)
     @patch("app.main.valid_google_url", return_value=False)
-    def test_not_accessible_when_no_internet_and_invalid_url(self) -> None:
+    def test_not_accessible_when_no_internet_and_invalid_url(
+            self,
+            has_internet_connection: bool,
+            valid_google_url: bool
+    ) -> None:
         result = can_access_google_page("http://invalid-url.com")
         self.assertEqual(result, "Not accessible")
 
