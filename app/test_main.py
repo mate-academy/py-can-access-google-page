85187c2bf25716d26c00f0bd4d6e0d77a622ec6d
Andrii Homeniuk <andreygomenuk@gmail.com>
2023-11-30 15:40:28 +0200

    Created tests for can_access_google_page function.

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,36 @@
-# write your code here
+import pytest
+from unittest import mock
+
+from app.main import can_access_google_page
+
+
+class TestCanAccessGooglePage:
+    @pytest.fixture()
+    def url(self) -> str:
+        return "https://www.google.com"
+
+    @mock.patch("app.main.valid_google_url", return_value=False)
+    def test_if_not_valid_google_url(
+        self,
+        mocked_valid_google_url_false: mock.MagicMock,
+        url: str
+    ) -> None:
+        assert can_access_google_page(url) == "Not accessible"
+
+    @mock.patch("app.main.has_internet_connection", return_value=False)
+    def test_if_has_not_internet_connection(
+        self,
+        mocked_has_internet_connection_false: mock.MagicMock,
+        url: str
+    ) -> None:
+        assert can_access_google_page(url) == "Not accessible"
+
+    @mock.patch("app.main.has_internet_connection", return_value=True)
+    @mock.patch("app.main.valid_google_url", return_value=True)
+    def test_valid_google_url_and_has_internet_connection(
+        self,
+        mocked_valid_google_url_true: mock.MagicMock,
+        mocked_has_internet_connection_true: mock.MagicMock,
+        url: str
+    ) -> None:
+        assert can_access_google_page(url) == "Accessible"
