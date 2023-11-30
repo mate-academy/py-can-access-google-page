368a389b251080b1ee2cfd335edf81765cdd94ac
Andrii Homeniuk <andreygomenuk@gmail.com>
2023-11-30 17:03:03 +0200

    Added new test_if_all_is_false. And changed name test_valid_google_url_and_has_internet_connection to test_if_all_is_true

--- app/test_main.py
+++ app/test_main.py
@@ -25,9 +25,19 @@ class TestCanAccessGooglePage:
     ) -> None:
         assert can_access_google_page(url) == "Not accessible"
 
+    @mock.patch("app.main.has_internet_connection", return_value=False)
+    @mock.patch("app.main.valid_google_url", return_value=False)
+    def test_if_all_is_false(
+        self,
+        mocked_valid_google_url_false: mock.MagicMock,
+        mocked_has_internet_connection_false: mock.MagicMock,
+        url: str
+    ) -> None:
+        assert can_access_google_page(url) == "Not accessible"
+
     @mock.patch("app.main.has_internet_connection", return_value=True)
     @mock.patch("app.main.valid_google_url", return_value=True)
-    def test_valid_google_url_and_has_internet_connection(
+    def test_if_all_is_true(
         self,
         mocked_valid_google_url_true: mock.MagicMock,
         mocked_has_internet_connection_true: mock.MagicMock,
