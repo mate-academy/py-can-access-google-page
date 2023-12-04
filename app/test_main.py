196eeceaa81be648309660b2201ec02efffb2430
lyaskov <lyaskov1@gmail.com>
2023-12-04 23:51:38 +0200

    fix mock patch as decorator

--- app/test_main.py
+++ app/test_main.py
@@ -1,30 +1,39 @@
-from unittest import mock
+from unittest.mock import patch, Mock
 
 from app.main import can_access_google_page
 
 
-def test_can_access_google_page_accessible() -> None:
-    with mock.patch("app.main.has_internet_connection") as h_i_c, \
-            mock.patch("app.main.valid_google_url") as v_g_u:
-        h_i_c.return_value = True
-        v_g_u.return_value = True
+@patch("app.main.has_internet_connection")
+@patch("app.main.valid_google_url")
+def test_can_access_google_page_accessible(
+        valid_mock: Mock,
+        connection_mock: Mock
+) -> None:
+    valid_mock.return_value = True
+    connection_mock.return_value = True
 
-        assert can_access_google_page("http://google.com") == "Accessible"
-
-
-def test_should_return_not_accessible_if_not_valid_google_url() -> None:
-    with mock.patch("app.main.has_internet_connection") as h_i_c, \
-            mock.patch("app.main.valid_google_url") as v_g_u:
-        h_i_c.return_value = True
-        v_g_u.return_value = False
-
-        assert can_access_google_page("http://google.com") == "Not accessible"
+    assert can_access_google_page("http://google.com") == "Accessible"
 
 
-def test_should_return_not_accessible_if_not_internet_connection() -> None:
-    with mock.patch("app.main.has_internet_connection") as h_i_c, \
-            mock.patch("app.main.valid_google_url") as v_g_u:
-        h_i_c.return_value = False
-        v_g_u.return_value = True
+@patch("app.main.has_internet_connection")
+@patch("app.main.valid_google_url")
+def test_should_return_not_accessible_if_not_valid_google_url(
+        valid_mock: Mock,
+        connection_mock: Mock
+) -> None:
+    valid_mock.return_value = True
+    connection_mock.return_value = False
 
-        assert can_access_google_page("http://google.com") == "Not accessible"
+    assert can_access_google_page("http://google.com") == "Not accessible"
+
+
+@patch("app.main.has_internet_connection")
+@patch("app.main.valid_google_url")
+def test_should_return_not_accessible_if_not_internet_connection(
+        valid_mock: Mock,
+        connection_mock: Mock
+) -> None:
+    valid_mock.return_value = False
+    connection_mock.return_value = True
+
+    assert can_access_google_page("http://google.com") == "Not accessible"
