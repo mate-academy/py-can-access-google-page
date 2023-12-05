69deec3307d7248139334ca20b8ce648075b8aa8
Denysov Viacheslav <wdenisov113@gmail.com>
2023-12-05 08:19:54 +0200

    Before monkeypatching

--- app/test_main.py
+++ app/test_main.py
@@ -1,57 +1,30 @@
 from unittest import mock
-
+from typing import Any
 import pytest
 
 from app.main import can_access_google_page
 
 
 @pytest.fixture
-def mocked_url() -> None:
+def mocked_url(request: Any) -> None:
     with mock.patch("app.main.valid_google_url") as mocked_url:
+        mocked_url.return_value = request.param
         yield mocked_url
 
 
 @pytest.fixture
-def mocked_internet_connection() -> None:
+def mocked_internet_connection(request: Any) -> None:
     with mock.patch("app.main.has_internet_connection") as mocked_connection:
+        mocked_connection.return_value = request.param
         yield mocked_connection
 
 
-def test_can_access_page_with_valid_url_and_connection(
-        mocked_url: str,
-        mocked_internet_connection: bool
-) -> None:
-    mocked_url.return_value = True
-    mocked_internet_connection.return_value = True
+@pytest.mark.parametrize("mocked_url, mocked_internet_connection, expected_result", [
+    (True, True, "Accessible"),
+    (False, True, "Not accessible"),
+    (True, False, "Not accessible"),
+    (False, False, "Not accessible"),
+])
+def test_can_access_google_page(mocked_url, mocked_internet_connection, expected_result):
     result = can_access_google_page("https://www.google.com")
-    assert result == "Accessible"
-
-
-def test_cannot_access_google_page_with_invalid_url(
-        mocked_url: str,
-        mocked_internet_connection: bool
-) -> None:
-    mocked_url.return_value = False
-    mocked_internet_connection.return_value = True
-    result = can_access_google_page("https://www.google.com")
-    assert result == "Not accessible"
-
-
-def test_cannot_access_google_page_with_no_internet_connection(
-        mocked_url: str,
-        mocked_internet_connection: bool
-) -> None:
-    mocked_url.return_value = True
-    mocked_internet_connection.return_value = False
-    result = can_access_google_page("https://www.google.com")
-    assert result == "Not accessible"
-
-
-def test_cant_access_with_all_arguments_invalid(
-        mocked_url: str,
-        mocked_internet_connection: bool
-) -> None:
-    mocked_url.return_value = False
-    mocked_internet_connection.return_value = False
-    result = can_access_google_page("https://www.google.com")
-    assert result == "Not accessible"
+    assert result == expected_result
