439fe9f04bdc03113a0c6c47d9dc9d97b632401d
Alex Moon <moon.ra.lex@gmail.com>
2023-12-07 15:56:18 +0100

    Done

--- app/test_main.py
+++ app/test_main.py
@@ -9,10 +9,30 @@ from app.main import can_access_google_page
 @pytest.mark.parametrize(
     "valid_url_return,internet_connection,expected_result",
     [
-        pytest.param(False, False, "Not accessible", id="Test with two False"),
-        pytest.param(True, False, "Not accessible", id="Test when connection False"),
-        pytest.param(False, True, "Not accessible", id="Test when valid URL False"),
-        pytest.param(True, True, "Accessible", id="Test with two True"),
+        pytest.param(
+            False,
+            False,
+            "Not accessible",
+            id="Test with two False"
+        ),
+        pytest.param(
+            True,
+            False,
+            "Not accessible",
+            id="Test when connection False"
+        ),
+        pytest.param(
+            False,
+            True,
+            "Not accessible",
+            id="Test when valid URL False"
+        ),
+        pytest.param(
+            True,
+            True,
+            "Accessible",
+            id="Test with two True"
+        ),
     ]
 )
 @patch("app.main.valid_google_url")
@@ -24,8 +44,8 @@ def test_for_the_correct_meaning_of_url_and_connection(
         internet_connection: bool,
         expected_result: str
 ) -> None:
-        mock_valid_url.return_value = valid_url_return
-        mock_internet_connection.return_value = internet_connection
-        assert can_access_google_page(
-            "https://www.google.com"
-        ) == expected_result
+    mock_valid_url.return_value = valid_url_return
+    mock_internet_connection.return_value = internet_connection
+    assert can_access_google_page(
+        "https://www.google.com"
+    ) == expected_result
