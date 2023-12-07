ae3c338e616a9d2a068143ba9050363408cd99ab
Denysov Viacheslav <wdenisov113@gmail.com>
2023-12-07 19:41:53 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,30 +1,28 @@
-from unittest import mock
-from typing import Any
 import pytest
+from unittest import mock
 
-from app.main import can_access_google_page
-
-
-@pytest.fixture
-def mocked_url(request: Any) -> None:
-    with mock.patch("app.main.valid_google_url") as mocked_url:
-        mocked_url.return_value = request.param
-        yield mocked_url
+import app.main
 
 
-@pytest.fixture
-def mocked_internet_connection(request: Any) -> None:
-    with mock.patch("app.main.has_internet_connection") as mocked_connection:
-        mocked_connection.return_value = request.param
-        yield mocked_connection
+@pytest.mark.parametrize(
+    "google_url, internet_connection, expected_result",
+    [
+        (True, True, "Accessible"),
+        (False, True, "Not accessible"),
+        (True, False, "Not accessible"),
+        (False, False, "Not accessible")
+    ]
+)
+def test_can_access_google_page(
+        google_url: bool,
+        internet_connection: bool,
+        expected_result: str) -> None:
+    with (mock.patch("app.main.valid_google_url") as mocked_valid_url,
+          mock.patch("app.main.has_internet_connection")
+          as mocked_has_internet_connection):
+        mocked_valid_url.return_value = google_url
+        mocked_has_internet_connection.return_value = internet_connection
 
+        result = app.main.can_access_google_page("https://www.google.com/")
 
-@pytest.mark.parametrize("mocked_url, mocked_internet_connection, expected_result", [
-    (True, True, "Accessible"),
-    (False, True, "Not accessible"),
-    (True, False, "Not accessible"),
-    (False, False, "Not accessible"),
-])
-def test_can_access_google_page(mocked_url, mocked_internet_connection, expected_result):
-    result = can_access_google_page("https://www.google.com")
-    assert result == expected_result
+        assert result == expected_result
