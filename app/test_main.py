4a15e9f482877834fbfd87662a80f344a888f4f7
Liliya <liliyakhu@ukr.net>
2023-12-11 12:12:30 +0200

    Solution optimized

--- app/test_main.py
+++ app/test_main.py
@@ -1,42 +1,33 @@
 from unittest import mock
 
+import pytest
+
 from app.main import can_access_google_page
 
 
+@pytest.mark.parametrize(
+    "valid_url, internet_connection, expected_result",
+    [
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible")
+    ],
+    ids=[
+        "valid url and has internet connection",
+        "valid url and hasn't internet connection",
+        "invalid url and has internet connection",
+        "invalid url and hasn't internet connection",
+    ]
+)
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 def test_can_access_google_page(
-        mocked_internet: callable,
-        mocked_validator: callable
-) -> None:
-    mocked_internet.return_value = True
-    mocked_validator.return_value = True
-    assert (
-        can_access_google_page("https://www.google.com/") == "Accessible"
-    )
-
-
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_can_not_access_google_page_no_internet_connection(
-        mocked_internet: callable,
         mocked_validator: callable,
-) -> None:
-    mocked_internet.return_value = False
-    mocked_validator.return_value = True
-    assert (
-        can_access_google_page("https://www.google.com/") == "Not accessible"
-    )
-
-
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_can_not_access_google_page_not_valid_site(
         mocked_internet: callable,
-        mocked_validator: callable
-) -> None:
-    mocked_internet.return_value = True
-    mocked_validator.return_value = False
-    assert (
-        can_access_google_page("https://www.google.com/") == "Not accessible"
-    )
+        valid_url: bool,
+        internet_connection: bool,
+        expected_result: str) -> None:
+    mocked_validator.return_value = valid_url
+    mocked_internet.return_value = internet_connection
+    assert can_access_google_page("https://www.google.com/") == expected_result
