b0317d8df30565ad612957b5f47bcbdc64ee8e97
Oleksandr Suligan <suligan.alexander@gmail.com>
2023-12-21 17:32:12 +0100

    Solution: added @pytest.mark.parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,7 @@
 from unittest.mock import patch
 
+import pytest
+
 from app.main import can_access_google_page
 
 
@@ -11,31 +13,32 @@ def test_has_internet_connection() -> None:
     pass
 
 
+@pytest.mark.parametrize(
+    "valid_url, internet_connection, expected_result",
+    [
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible")
+    ],
+    ids=[
+        "valid url and has internet connection",
+        "valid url and hasn't internet connection",
+        "invalid url and has internet connection",
+        "invalid url and hasn't internet connection",
+    ]
+)
 @patch("app.main.has_internet_connection")
 @patch("app.main.valid_google_url")
 def test_can_access_google_page(
-        mocked_has_internet_connection: bool,
-        mocked_valid_google_url: bool
+        mocked_has_internet_connection: callable,
+        mocked_valid_google_url: callable,
+        valid_url: bool,
+        internet_connection: bool,
+        expected_result: str
 ) -> None:
-    mocked_has_internet_connection.return_value = True
-    mocked_valid_google_url.return_value = True
+    mocked_has_internet_connection.return_value = valid_url
+    mocked_valid_google_url.return_value = internet_connection
     assert (
-        can_access_google_page("https://www.google.com/") == "Accessible")
-
-    mocked_has_internet_connection.return_value = True
-    mocked_valid_google_url.return_value = False
-    assert (
-        can_access_google_page("https://www.google.com/") == "Not accessible"
-    )
-
-    mocked_has_internet_connection.return_value = False
-    mocked_valid_google_url.return_value = True
-    assert (
-        can_access_google_page("https://www.google.com/") == "Not accessible"
-    )
-
-    mocked_has_internet_connection.return_value = False
-    mocked_valid_google_url.return_value = False
-    assert (
-        can_access_google_page("https://www.google.com/") == "Not accessible"
+        can_access_google_page("https://www.google.com/") == expected_result
     )
