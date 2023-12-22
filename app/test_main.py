8d8a5e612c32f543a52192519f7665226966a20c
Bohdan Horbachov <bgbro.inc@gmail.com>
2023-12-22 13:18:11 +0200

    fixed flake8

--- app/test_main.py
+++ app/test_main.py
@@ -1,12 +1,16 @@
 import pytest
 from unittest.mock import patch
+from typing import Callable
 
 from app.main import can_access_google_page
 
 
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
-def test_can_access_google_page(mock_has_internet_connection, mock_valid_google_url):
+def test_can_access_google_page(
+        mock_has_internet_connection: Callable,
+        mock_valid_google_url: Callable
+) -> None:
     mock_has_internet_connection.return_value = True
     mock_valid_google_url.return_value = True
 
