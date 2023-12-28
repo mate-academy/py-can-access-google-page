8a0e5007370e2d0b8d522210c9ca76dba5b0d4e5
artem.puhach <artem.puhach@cyberpolice.gov.ua>
2023-12-28 12:23:32 +0200

    ready solution!

--- app/test_main.py
+++ app/test_main.py
@@ -1,8 +1,10 @@
 from unittest import mock
+
+import app
 from app.main import can_access_google_page
 
 
-def test_can_access_google_page():
-    has_internet_connection = mock.MagicMock()
-    valid_google_url = mock.MagicMock()
-    assert can_access_google_page('http://google.com') == "Accessible"
+def test_can_access_google_page() -> None:
+    app.main.has_internet_connection = mock.MagicMock()
+    app.main.valid_google_url = mock.MagicMock()
+    assert can_access_google_page("http://google.com") == "Accessible"
