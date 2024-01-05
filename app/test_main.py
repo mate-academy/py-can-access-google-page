840eeb926b9a60e5e32ca4b1d15e050475529228
Vitalij Chajka <chaika.vitalii@gmail.com>
2024-01-05 21:56:48 +0200

    fixed using mock.patch as a decorator instead of context manager

--- app/test_main.py
+++ app/test_main.py
@@ -2,43 +2,37 @@ from unittest import mock
 import app.main
 
 
+@mock.patch(
+    "app.main.valid_google_url", mock.MagicMock(return_value=True)
+)
+@mock.patch(
+    "app.main.has_internet_connection", mock.MagicMock(return_value=True)
+)
 def test_can_access_google_page_all_true() -> None:
-    with mock.patch(
-            "app.main.valid_google_url"
-    ) as mocked_valid_google_url:
-        with mock.patch(
-                "app.main.has_internet_connection"
-        ) as mocked_has_internet_connection:
-            mocked_valid_google_url.return_value = True
-            mocked_has_internet_connection.return_value = True
-            assert app.main.can_access_google_page(
-                "google.com"
-            ) == "Accessible"
+    assert app.main.can_access_google_page(
+        "google.com"
+    ) == "Accessible"
 
 
+@mock.patch(
+    "app.main.valid_google_url", mock.MagicMock(return_value=False)
+)
+@mock.patch(
+    "app.main.has_internet_connection", mock.MagicMock(return_value=True)
+)
 def test_can_access_google_page_url_false() -> None:
-    with mock.patch(
-            "app.main.valid_google_url"
-    ) as mocked_valid_google_url:
-        with mock.patch(
-                "app.main.has_internet_connection"
-        ) as mocked_has_internet_connection:
-            mocked_valid_google_url.return_value = False
-            mocked_has_internet_connection.return_value = True
-            assert app.main.can_access_google_page(
-                "google.com"
-            ) == "Not accessible"
+    assert app.main.can_access_google_page(
+        "google.com"
+    ) == "Not accessible"
 
 
+@mock.patch(
+    "app.main.valid_google_url", mock.MagicMock(return_value=True)
+)
+@mock.patch(
+    "app.main.has_internet_connection", mock.MagicMock(return_value=False)
+)
 def test_can_access_google_page_internet_connection_false() -> None:
-    with mock.patch(
-            "app.main.valid_google_url"
-    ) as mocked_valid_google_url:
-        with mock.patch(
-                "app.main.has_internet_connection"
-        ) as mocked_has_internet_connection:
-            mocked_valid_google_url.return_value = True
-            mocked_has_internet_connection.return_value = False
-            assert app.main.can_access_google_page(
-                "google.com"
-            ) == "Not accessible"
+    assert app.main.can_access_google_page(
+        "google.com"
+    ) == "Not accessible"
