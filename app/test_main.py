5b6556c22368d4d97c2188437d601724fbf6ad66
Denys Sidorov <zondayttop1ggg@gmail.com>
2024-01-12 17:22:59 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -5,15 +5,20 @@ from typing import Callable
 
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
-def test_can_access_google_page(mock_has_internet_connection: Callable,
-                                mock_valid_google_url: Callable) -> None:
+def test_can_access_google_page(
+    mock_has_internet_connection: Callable,
+    mock_valid_google_url: Callable
+) -> None:
     mock_has_internet_connection.return_value = True
     mock_valid_google_url.return_value = True
     result = can_access_google_page("https://www.google.com")
     assert result == "Accessible"
+
     mock_valid_google_url.return_value = False
     result = can_access_google_page("https://invalid-url.com")
     assert result == "Not accessible"
+
+    mock_valid_google_url.return_value = True
     mock_has_internet_connection.return_value = False
     result = can_access_google_page("https://www.google.com")
     assert result == "Not accessible"
