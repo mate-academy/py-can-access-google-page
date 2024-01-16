bc223d04c70437f87c8e48e555f1092bd4f9649b
Vladyslav Pliashechnyk <vlad.pliashechnyk@gmail.com>
2024-01-17 01:27:33 +0200

    py-can-access-google-page

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,27 @@
-# write your code here
+from unittest.mock import patch, Mock
+from pytest import mark
+from app.main import can_access_google_page
+
+
+@mark.parametrize(
+    "input_url, output_validator, connection_status, expected_result",
+    [
+        ("https://www.google.com/", True, True, "Accessible"),
+        ("httpswww.google.co", False, True, "Not accessible"),
+        ("https://www.google.com/", True, False, "Not accessible"),
+        ("httpswww.google.co", False, False, "Not accessible")
+    ]
+)
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
+def test_can_access_google_page(
+        mocked_internet_connection: Mock,
+        mocked_validator: Mock,
+        input_url: str,
+        output_validator: bool,
+        connection_status: bool,
+        expected_result: str
+) -> None:
+    mocked_internet_connection.return_value = connection_status
+    mocked_validator.return_value = output_validator
+    assert can_access_google_page(input_url) == expected_result
