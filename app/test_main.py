efb4599249dd3b381f7e7b0b2f5ca214e54d95d4
Yura Push <youdeimos1@gmail.com>
2024-01-16 19:34:00 +0200

    -tested main cases

--- app/test_main.py
+++ app/test_main.py
@@ -1,23 +1,46 @@
-from typing import Callable
+from __future__ import annotations
 
-from unittest import mock
+import pytest
+
+from unittest.mock import patch, Mock
 
 from app.main import can_access_google_page
 
 
-@mock.patch("requests.get")
-def test_not_valid_url_has_internet_connection(
-        mock_get: Callable
+@pytest.fixture()
+def mock_valid_google_url() -> Mock:
+    with patch("app.main.valid_google_url") as mock_valid_url:
+        yield mock_valid_url
+
+
+@pytest.fixture()
+def mock_has_internet_connection() -> Mock:
+    with patch("app.main.has_internet_connection") as mock_connection:
+        yield mock_connection
+
+
+def test_invalid_url_but_has_internet_connection(
+        mock_valid_google_url: Mock,
+        mock_has_internet_connection: Mock
 ) -> None:
-    mock_response = mock_get()
-    mock_response.status_code = 300
+    mock_valid_google_url.return_value = False
+    mock_has_internet_connection.return_value = True
     assert can_access_google_page("https://mate.academy/") == "Not accessible"
 
 
-@mock.patch("datetime.datetime")
-def test_has_internet_connection_not_valid_url(
-        mock_datetime: Callable
+def test_no_internet_connection_but_valid_url(
+        mock_valid_google_url: Mock,
+        mock_has_internet_connection: Mock
 ) -> None:
-    mock_datetime = mock_datetime()
-    mock_datetime.now.return_value.hour = 5
+    mock_valid_google_url.return_value = True
+    mock_has_internet_connection.return_value = False
     assert can_access_google_page("https://mate.academy/") == "Not accessible"
+
+
+def test_can_access_google_page(
+        mock_valid_google_url: Mock,
+        mock_has_internet_connection: Mock
+) -> None:
+    mock_valid_google_url.return_value = True
+    mock_has_internet_connection.return_value = True
+    assert can_access_google_page("https://mate.academy/") == "Accessible"
