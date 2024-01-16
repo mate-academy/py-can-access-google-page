58e9438aed035acc9aca5a7a2aac48b8f3eb74c8
kabust <tulyulyuk91@gmail.com>
2024-01-16 20:38:48 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,15 @@
-# write your code here
+from unittest import mock
+
+from app.main import can_access_google_page
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_can_access_google_page(mock_connection: callable,
+                                mock_validator: callable) -> None:
+    url = "http://google.com"
+
+    can_access_google_page(url)
+
+    mock_validator.assert_called_once_with(url)
+    mock_connection.assert_any_call()
