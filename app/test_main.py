609d7c9c8d3dde6a4aef258aadcb89f775729afd
el chibuk <chebukin404@gmail.com>
2024-01-18 15:58:28 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,31 @@
-# write your code here
+import pytest
+from typing import Callable
+from app.main import can_access_google_page
+from unittest import mock
+
+@pytest.mark.parametrize(
+    "url,valid_url,has_internet,expected_output",
+    [
+        ("www.google.com", True, True, "Accessible"),
+        ("www.gоogle.com", False, True, "Not accessible"),
+        ("www.google.com", True, False, "Not accessible"),
+    ],
+    ids=[
+        "Test for 'Accessible' with correct values",
+        "Test for 'Not accessible' with a cyrillic typo in url",
+        "Test for 'Not accessible' with no connection",
+    ]
+)
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_correct_google_access_message(
+        mocked_internet: mock.MagicMock,
+        mocked_url: mock.MagicMock,
+        url: str,
+        valid_url: bool,
+        has_internet: bool,
+        expected_output: str
+) -> None:
+    mocked_internet.return_value = has_internet
+    mocked_url.return_value = valid_url
+    assert can_access_google_page(url) == expected_output
