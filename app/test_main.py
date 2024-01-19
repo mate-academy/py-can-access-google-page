51aae6418aed2c244ac7ea31fc512054b71df79e
Artur Poltser <polcerartur@gmail.com>
2024-01-19 20:03:15 +0200

    Fixed

--- app/test_main.py
+++ app/test_main.py
@@ -1,24 +1,31 @@
 from app.main import can_access_google_page
-from typing import Callable
-import pytest
+from pytest import mark
 from unittest import mock
 
 
-@pytest.mark.parametrize(
+@mark.parametrize(
     "url,has_connection,valid_url,expected_output",
     [
         ("https://www.google.com", True, True, "Accessible"),
         ("https://zweeqf.com", True, False, "Not accessible"),
         ("https://xcbvn.com", False, True, "Not accessible"),
+        ("https://xcbvncom", False, False, "Not accessible"),
+    ],
+    ids=[
+        "Must return 'Accessible', if you have internet and URL is valid",
+        "Must return 'Not accessible', if URL is not valid",
+        "Must return 'Not accessible', if you don't have internet",
+        ("Must return 'Not accessible', if you don't have internet"
+         " and URL isn't valid"),
     ]
 )
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
 def test_should_return_correct_output(
-        mocked_valid_google_url: Callable,
-        mocked_has_internet_connection: Callable,
+        mocked_valid_google_url: mock.MagicMock,
+        mocked_has_internet_connection: mock.MagicMock,
         url: str,
-        valid_url: str,
+        valid_url: bool,
         has_connection: bool,
         expected_output: str
 ) -> None:
