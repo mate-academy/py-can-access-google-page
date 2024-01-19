b3f8ddd12c8b9b8a0d5aa81e5ef2378d1daeb4e4
Pavlo-here <pasha.nasonovhtc@gmail.com>
2024-01-19 17:28:15 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -6,7 +6,8 @@ from app.main import can_access_google_page
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
 def test_can_access_google_page(
-        has_connection: mock.MagicMock, valid_url: mock.MagicMock
+        has_connection: mock.MagicMock,
+        valid_url: mock.MagicMock
 ) -> None:
     res = can_access_google_page("https://www.google.com")
     valid_url.assert_called_once()
