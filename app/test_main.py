28a54cc403e43c4912377ae25ef9a96e1b36f24a
Nataliia <natali4.kucher55@gmail.com>
2024-01-22 13:39:49 +0100

    Solution_2

--- app/test_main.py
+++ app/test_main.py
@@ -1,32 +1,28 @@
 from app.main import can_access_google_page
 from unittest import mock
+import pytest
 
 
+@pytest.mark.parametrize(
+    "valid_google_url, has_internet, result_expected",
+    [
+        (True, True, "Accessible"),
+        (False, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (True, False, "Not accessible")
+    ])
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page(mock_has_internet: None,
-                                mock_valid_google_url: None
-                                ) -> None:
-    mock_valid_google_url.return_value = True
-    mock_has_internet.return_value = True
-    assert can_access_google_page("http://example.com") == "Accessible"
-    mock_valid_google_url.assert_called_with("http://example.com")
-    mock_has_internet.assert_called_once()
+def test_can_access_google_page(
+        mock_has_internet: None,
+        mock_valid_google_url: None,
+        valid_google_url: None,
+        has_internet: None,
+        result_expected: None
+) -> None:
+    mock_valid_google_url.return_value = valid_google_url
+    mock_has_internet.return_value = has_internet
 
-    mock_valid_google_url.return_value = False
-    mock_has_internet.return_value = False
-    assert can_access_google_page("http://example.com") == "Not accessible"
-    mock_valid_google_url.assert_called_with("http://example.com")
-    mock_has_internet.assert_called()
+    result = can_access_google_page("http://example.com")
 
-    mock_valid_google_url.return_value = False
-    mock_has_internet.return_value = True
-    assert can_access_google_page("http://example.com") == "Not accessible"
-    mock_valid_google_url.assert_called_with("http://example.com")
-    mock_has_internet.assert_called()
-
-    mock_valid_google_url.return_value = True
-    mock_has_internet.return_value = False
-    assert can_access_google_page("http://example.com") == "Not accessible"
-    mock_valid_google_url.assert_called_with("http://example.com")
-    mock_has_internet.assert_called()
+    assert result == result_expected
