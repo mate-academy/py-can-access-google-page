3142a0f5810acb849a62cb1d8db3a8cb91b34d32
Zoresław Dmytryszyn <zoreslav.dmytryshyn@gmail.com>
2024-01-23 16:35:38 +0100

    solve can-access-google-page

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,33 @@
-# write your code here
+from unittest import mock
+
+import pytest
+
+from app.main import can_access_google_page
+
+
+class TestCanAccessGooglePage:
+    @pytest.mark.parametrize(
+        "has_connection, valid_url, result",
+        [
+            (True, False, "Not accessible"),
+            (False, True, "Not accessible"),
+            (True, True, "Accessible"),
+            (False, False, "Not accessible")
+        ]
+    )
+    def test_can_access_google_page(
+            self,
+            has_connection: bool,
+            valid_url: bool,
+            result: str
+
+    ) -> None:
+        with (
+            mock.patch(
+                "app.main.has_internet_connection", return_value=has_connection
+            ),
+            mock.patch(
+                "app.main.valid_google_url", return_value=valid_url
+            )
+        ):
+            assert can_access_google_page("https://mate.academy/en") == result
