810facc911ad98c5e23b45f30d0aaef4fa0da92f
Vasyl Kalish <vasyl.documents@yahoo.it>
2024-01-27 19:55:16 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,21 @@
-# write your code here
+from app.main import can_access_google_page
+from unittest import mock
+
+
+def test_check_data_input() -> None:
+    assert (
+        can_access_google_page("https://www.google.com/") == "Accessible"
+    )
+
+
+@mock.patch("app.main.valid_google_url")
+def test_url_function_execution(valid: mock) -> None:
+    can_access_google_page("https://www.google.com")
+    valid.asser_celled_onece()
+
+
+@mock.patch("app.main.can_access_google_page")
+def test_access_function_execution(access: mock) -> None:
+    print(type(access))
+    can_access_google_page("https://www.google.com")
+    access.asser_celled_onece()
