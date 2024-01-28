4a850f06564ed74be13f1df261cfefcd9dbd32be
Vasyl Kalish <vasyl.documents@yahoo.it>
2024-01-28 18:05:00 +0100

    fix problem

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,31 @@
 from app.main import can_access_google_page
 from unittest import mock
+import datetime
 
 
-def test_check_data_input() -> None:
+def test_check_correct_url() -> None:
     assert (
         can_access_google_page("https://www.google.com/") == "Accessible"
     )
 
 
-@mock.patch("app.main.valid_google_url")
-def test_url_function_execution(valid: mock) -> None:
-    can_access_google_page("https://www.google.com")
-    valid.asser_celled_onece()
+def test_check_wrong_url() -> None:
+    assert (
+        can_access_google_page("https://www.amazon.com/") == "Not accessible"
+    )
+
+
+@mock.patch("app.main.datetime.datetime")
+def test_url_func_execution(valid: mock) -> None:
+    valid.return_value = (datetime.datetime.now()).replace(hour=5, minute=0)
+    assert (
+        can_access_google_page("https://www.google.com") == "Not accessible"
+    )
 
 
 @mock.patch("app.main.can_access_google_page")
-def test_access_function_execution(access: mock) -> None:
-    print(type(access))
-    can_access_google_page("https://www.google.com")
-    access.asser_celled_onece()
+def test_access_func_execution(access: mock) -> None:
+    access.return_value = False
+    assert (
+        can_access_google_page("https://www.google.com") == "Accessible"
+    )
