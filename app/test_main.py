51ca12f7f8169c486ca104e56dd13bef8610057d
Yevhen <evgeniy.playid@icloud.com>
2024-01-30 15:43:31 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -4,7 +4,7 @@ from app.main import can_access_google_page
 
 
 @pytest.mark.parametrize(
-    "google_url, internet_connection, expected_result",
+    "google_url, internet_connection, test_result",
     [
         (True, True, "Accessible"),
         (False, False, "Not accessible"),
@@ -13,7 +13,7 @@ from app.main import can_access_google_page
     ],
     ids=[
         "Should be Accessible when url is valid and connection is present",
-        "Should not be Accessible when neither url is valid nor connection is present",
+        "Should not be Accessible when no valid url and no connection",
         "Should not be Accessible when url isn't valid",
         "Should not be Accessible when there is no Internet connection"
     ]
@@ -21,10 +21,10 @@ from app.main import can_access_google_page
 def test_can_access_google_page(
         google_url: str,
         internet_connection: str,
-        expected_result: str
-):
-    with mock.patch("app.main.valid_google_url") as mocked_url_check, mock.patch(
-            "app.main.has_internet_connection") as mocked_internet_check:
+        test_result: str
+) -> None:
+    with (mock.patch("app.main.valid_google_url") as mocked_url_check,
+          mock.patch("app.main.has_internet_connection") as mocked_internet_check):
         mocked_url_check.return_value = google_url
         mocked_internet_check.return_value = internet_connection
-        assert can_access_google_page("https://www.google.com") == expected_result
+        assert can_access_google_page("https://www.google.com") == test_result
