57390a8fbd00b8db1b8360d5be1d0bd2a5731514
Mykola <mykola22dubyna@gmail.com>
2024-01-30 12:45:24 +0200

    refractored code

--- app/test_main.py
+++ app/test_main.py
@@ -1,20 +1,8 @@
 import pytest
 
-from unittest.mock import patch, Mock
+from unittest.mock import patch
 
 from app.main import can_access_google_page
-
-
-@pytest.fixture()
-def mocked_valid_google_url() -> None:
-    with patch("app.main.valid_google_url") as mock_google_url:
-        yield mock_google_url
-
-
-@pytest.fixture()
-def mocked_has_internet_connection() -> None:
-    with patch("app.main.has_internet_connection") as mock_internet_connection:
-        yield mock_internet_connection
 
 
 @pytest.mark.parametrize(
@@ -39,13 +27,17 @@ def mocked_has_internet_connection() -> None:
     ]
 )
 def test_can_access_google_page(
-        mocked_valid_google_url: Mock,
-        mocked_has_internet_connection: Mock,
         is_valid_url: bool,
         is_exist_connection: bool,
         result: str,
         test_id: str
 ) -> None:
-    mocked_valid_google_url.return_value = is_valid_url
-    mocked_has_internet_connection.return_value = is_exist_connection
-    assert can_access_google_page("https://www.google.com.") == result, test_id
+    with (
+        patch("app.main.valid_google_url") as mock_google_url,
+        patch("app.main.has_internet_connection") as mock_internet_connection
+    ):
+        mock_google_url.return_value = is_valid_url
+        mock_internet_connection.return_value = is_exist_connection
+        assert can_access_google_page(
+            "https://www.google.com."
+        ) == result, test_id
