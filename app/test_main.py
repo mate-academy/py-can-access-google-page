b7cc05c0978172bdd46c71bc44421071fee90e9a
Vasyl Kalish <vasyl.documents@yahoo.it>
2024-01-31 21:34:34 +0100

    fix task

--- app/test_main.py
+++ app/test_main.py
@@ -4,18 +4,23 @@ from unittest import mock
 
 
 @pytest.mark.parametrize(
-    "url,connection, result",
+    "url, correct_url, connection, result",
     [
-        ("https://www.amazon.com", True, "Not accessible"),
-        ("https://www.google.com", True, "Accessible"),
-        ("https://www.google.com", False, "Not accessible")
+        ("https://www.amazon.com", False, True, "Not accessible"),
+        ("https://www.google.com", True, True, "Accessible"),
+        ("https://www.google.com", True, False, "Not accessible")
     ]
 )
 @mock.patch("app.main.has_internet_connection")
-def test_access_to_google_page(check_connection: mock, url: str,
+@mock.patch("app.main.valid_google_url")
+def test_access_to_google_page(check_url: mock,
+                               check_connection: mock,
+                               url: str,
+                               correct_url: bool,
                                connection: bool,
                                result: str) -> None:
     check_connection.return_value = connection
+    check_url.return_value = correct_url
     assert (
         can_access_google_page(url) == result
     )
