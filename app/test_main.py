9a27b40aec54409f646a74c787f38a0884c7966b
raychw <bladeryz@gmail.com>
2024-02-03 18:07:47 +0200

    Provided test, checking the functional of can_access_google_page function

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,4 @@
+from typing import Callable
 from unittest.mock import patch
 
 from app.main import can_access_google_page
@@ -5,7 +6,10 @@ from app.main import can_access_google_page
 
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
-def test_can_access_google_page(mock_has_internet, mock_valid_url):
+def test_can_access_google_page(
+        mock_has_internet: Callable,
+        mock_valid_url: Callable
+) -> None:
     mock_valid_url.return_value = True
     mock_has_internet.return_value = True
     assert can_access_google_page("www.google.com") == "Accessible"
