53eee6b4112bd61eb930dd132296849dfe1fba66
raychw <bladeryz@gmail.com>
2024-02-03 18:36:56 +0200

    Provided fixes!

--- app/test_main.py
+++ app/test_main.py
@@ -1,27 +1,37 @@
+import pytest
 from typing import Callable
 from unittest.mock import patch
 
 from app.main import can_access_google_page
 
 
+@pytest.mark.parametrize(
+    "valid_url, internet_connection, expected_result",
+    [
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible"),
+    ],
+    ids=[
+        "can access when valid url and has internet",
+        "can't access when valid url, but no internet",
+        "can't access with internet, but invalid url",
+        "can't access with invalid url and no internet",
+    ]
+)
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
 def test_can_access_google_page(
-        mock_has_internet: Callable,
-        mock_valid_url: Callable
+        mock_has_internet_connection: Callable,
+        mock_valid_google_url: Callable,
+        valid_url: bool,
+        internet_connection: bool,
+        expected_result: str
 ) -> None:
-    mock_valid_url.return_value = True
-    mock_has_internet.return_value = True
-    assert can_access_google_page("www.google.com") == "Accessible"
+    mock_valid_google_url.return_value = valid_url
+    mock_has_internet_connection.return_value = internet_connection
 
-    mock_valid_url.return_value = False
-    mock_has_internet.return_value = True
-    assert can_access_google_page("www.google.com") == "Not accessible"
+    result = can_access_google_page("www.google.com")
 
-    mock_valid_url.return_value = True
-    mock_has_internet.return_value = False
-    assert can_access_google_page("www.google.com") == "Not accessible"
-
-    mock_valid_url.return_value = False
-    mock_has_internet.return_value = False
-    assert can_access_google_page("www.google.com") == "Not accessible"
+    assert result == expected_result
