0dcd69ff32f7ff8e6ccaf65251d4cb99fc32d161
Evgen Milokumov <evgen.milok@gmail.com>
2024-02-06 20:51:20 +0200

    Solution1-fix-flake

--- app/test_main.py
+++ app/test_main.py
@@ -1,9 +1,14 @@
+from unittest import mock
 from unittest.mock import patch
 from app.main import can_access_google_page
 
-@patch('app.main.valid_google_url')
-@patch('app.main.has_internet_connection')
-def test_can_access_google_page(mock_has_internet_connection, mock_valid_google_url):
+
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
+def test_can_access_google_page(
+        mock_has_internet_connection: mock,
+        mock_valid_google_url: mock
+) -> None:
     # Mocking the functions
     mock_has_internet_connection.return_value = True
     mock_valid_google_url.return_value = True
@@ -14,10 +19,11 @@ def test_can_access_google_page(mock_has_internet_connection, mock_valid_google_
     # Test the function with an invalid URL
     mock_has_internet_connection.return_value = True
     mock_valid_google_url.return_value = False
-    assert can_access_google_page("https://www.invalidurl.com") == "Not accessible"
+    assert can_access_google_page(
+        "https://www.invalidurl.com"
+    ) == "Not accessible"
 
     # Test the function without internet connection
     mock_has_internet_connection.return_value = False
     mock_valid_google_url.return_value = True
     assert can_access_google_page("https://www.google.com") == "Not accessible"
-
