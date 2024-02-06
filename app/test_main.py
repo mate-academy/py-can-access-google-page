df149e4ec7772924360763bd71b4830a1903ec0b
Evgen Milokumov <evgen.milok@gmail.com>
2024-02-06 23:41:12 +0200

    implemented-pytest-mark-parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -1,29 +1,30 @@
+import pytest
 from unittest import mock
 from unittest.mock import patch
 from app.main import can_access_google_page
 
 
+@pytest.mark.parametrize(
+    "valid_url, has_connection, expected",
+    [
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible")
+    ]
+)
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
 def test_can_access_google_page(
+        # mock_url_page,
         mock_has_internet_connection: mock,
-        mock_valid_google_url: mock
+        mock_valid_google_url: mock,
+        valid_url: bool,
+        has_connection: bool,
+        expected: str
 ) -> None:
     # Mocking the functions
-    mock_has_internet_connection.return_value = True
-    mock_valid_google_url.return_value = True
+    mock_has_internet_connection.return_value = has_connection
+    mock_valid_google_url.return_value = valid_url
 
-    # Test the function with a valid URL and internet connection
-    assert can_access_google_page("https://www.google.com") == "Accessible"
-
-    # Test the function with an invalid URL
-    mock_has_internet_connection.return_value = True
-    mock_valid_google_url.return_value = False
-    assert can_access_google_page(
-        "https://www.invalidurl.com"
-    ) == "Not accessible"
-
-    # Test the function without internet connection
-    mock_has_internet_connection.return_value = False
-    mock_valid_google_url.return_value = True
-    assert can_access_google_page("https://www.google.com") == "Not accessible"
+    assert can_access_google_page("https://google.com/") == expected
