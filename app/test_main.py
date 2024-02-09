f29889473007b33ff99ee1d82822c33a2480260d
Alex Grytsai <a.grytsai1987@gmail.com>
2024-02-09 10:56:49 +0200

    fixed flake8

--- app/test_main.py
+++ app/test_main.py
@@ -8,8 +8,8 @@ class TestCanAccessGooglePage():
     @mock.patch("app.main.valid_google_url", return_value=True)
     def test_can_access_google_page_without_connection(
             self,
-            mock_valid_google_url,
-            mock_has_internet_connection
+            mock_valid_google_url: mock,
+            mock_has_internet_connection: mock
     ) -> None:
         url = "https://www.google.com"
         assert can_access_google_page(url) == "Not accessible"
@@ -18,8 +18,8 @@ class TestCanAccessGooglePage():
     @mock.patch("app.main.valid_google_url", return_value=False)
     def test_can_access_google_page_unavalid_url(
             self,
-            mock_valid_google_url,
-            mock_has_internet_connection
+            mock_valid_google_url: mock,
+            mock_has_internet_connection: mock
     ) -> None:
         url = "https://www.google.com.ua"
         assert can_access_google_page(url) == "Not accessible"
@@ -28,8 +28,8 @@ class TestCanAccessGooglePage():
     @mock.patch("app.main.valid_google_url", return_value=False)
     def test_can_access_google_page_all_false(
             self,
-            mock_valid_google_url,
-            mock_has_internet_connection
+            mock_valid_google_url: mock,
+            mock_has_internet_connection: mock
     ) -> None:
         url = "https://www.google.com.ua"
         assert can_access_google_page(url) == "Not accessible"
@@ -38,8 +38,8 @@ class TestCanAccessGooglePage():
     @mock.patch("app.main.valid_google_url", return_value=True)
     def test_can_access_google_page_all_true(
             self,
-            mock_valid_google_url,
-            mock_has_internet_connection
+            mock_valid_google_url: mock,
+            mock_has_internet_connection: mock
     ) -> None:
         url = "https://www.google.com"
         assert can_access_google_page(url) == "Accessible"
