6ca363fd192cd45bebecbbe58e0ace75397d05c1
Molchanov Ihor <=>
2024-02-15 23:54:19 +0200

    solved task

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,37 @@
-# write your code here
+from unittest import mock
+from unittest import TestCase
+from app.main import can_access_google_page
+
+
+class TestAccessToGooglePage(TestCase):
+    @mock.patch("app.main.valid_google_url")
+    @mock.patch("app.main.has_internet_connection")
+    def test_return_result_can_access_google_page(
+            self,
+            valid_google_url: mock,
+            has_internet_connection: mock
+    ) -> None:
+        valid_google_url.return_value = True
+        has_internet_connection.return_value = True
+        self.assertEqual(can_access_google_page(""), "Accessible")
+
+    @mock.patch("app.main.valid_google_url")
+    @mock.patch("app.main.has_internet_connection")
+    def test_return_result_when_not_valid_google_url(
+            self, mock_valid_google_url: mock,
+            mock_has_internet_connection: mock
+    ) -> None:
+        mock_valid_google_url.return_value = False
+        mock_has_internet_connection.return_value = True
+        self.assertEqual(can_access_google_page(""), "Not accessible")
+
+    @mock.patch("app.main.valid_google_url")
+    @mock.patch("app.main.has_internet_connection")
+    def test_return_result_when_not_has_internet_connection(
+            self,
+            mock_valid_google_url: mock,
+            mock_has_internet_connection: mock
+    ) -> None:
+        mock_valid_google_url.return_value = True
+        mock_has_internet_connection.return_value = False
+        self.assertEqual(can_access_google_page(""), "Not accessible")
