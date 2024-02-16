87c5240b35e5123f099dbb35bf48f4ab0e3d5865
Andrii Mogush <toltecos@ukr.net>
2024-02-16 15:19:19 +0200

    Fix Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,6 @@
+from unittest.mock import patch, MagicMock
+
 import pytest
-from unittest import mock
 
 from app.main import can_access_google_page
 
@@ -19,15 +20,15 @@ from app.main import can_access_google_page
         "Valid url is False, Internet connection is False",
     ]
 )
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
 def test_can_access_google_page(
+        mock_valid_google_url: MagicMock,
+        mock_has_connection: MagicMock,
         valid_url: bool,
         connection: bool,
         access: str
 ) -> None:
-    with (
-        mock.patch("app.main.valid_google_url") as mock_valid_google_url,
-        mock.patch("app.main.has_internet_connection") as mock_has_connection
-    ):
-        mock_valid_google_url.return_value = valid_url
-        mock_has_connection.return_value = connection
-        assert can_access_google_page("google") == access
+    mock_valid_google_url.return_value = valid_url
+    mock_has_connection.return_value = connection
+    assert can_access_google_page("google") == access
