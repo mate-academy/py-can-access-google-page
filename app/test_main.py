23a326832036c5e78350a56cd67557218857717a
RomanHlodann <roman.hlodann@gmail.com>
2024-02-19 18:50:51 +0200

    improved test quality

--- app/test_main.py
+++ app/test_main.py
@@ -8,22 +8,34 @@ from app.main import can_access_google_page
 @pytest.fixture
 def mock_valid_google_url() -> mock.MagicMock:
     with mock.patch("app.main.valid_google_url") as mocked_url:
-        mocked_url.return_value = True
         yield mocked_url
 
 
 @pytest.fixture
 def mock_has_internet_connection() -> mock.MagicMock:
     with mock.patch("app.main.has_internet_connection") as mocked_connection:
-        mocked_connection.return_value = True
         yield mocked_connection
 
 
-def test_when_valid_url_and_has_connection(
+@pytest.mark.parametrize(
+    "valid_url,has_connection,result",
+    [
+        (True, True, "Accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible"),
+        (True, False, "Not accessible"),
+    ]
+)
+def test_access_with_different_parameters(
         mock_valid_google_url: mock.MagicMock,
-        mock_has_internet_connection: mock.MagicMock
+        mock_has_internet_connection: mock.MagicMock,
+        valid_url: bool,
+        has_connection: bool,
+        result: str
 ) -> None:
-    assert can_access_google_page("/") == "Accessible"
+    mock_valid_google_url.return_value = valid_url
+    mock_has_internet_connection.return_value = has_connection
+    assert can_access_google_page("/") == result
 
 
 def test_func_was_called_once(
@@ -33,15 +45,3 @@ def test_func_was_called_once(
     can_access_google_page("/")
     mock_valid_google_url.assert_called_once_with("/")
     mock_has_internet_connection.assert_called_once()
-
-
-def test_return_not_accessible(
-        mock_valid_google_url: mock.MagicMock,
-        mock_has_internet_connection: mock.MagicMock
-) -> None:
-    mock_valid_google_url.return_value = False
-    assert can_access_google_page("/") == "Not accessible"
-
-    mock_valid_google_url.return_value = True
-    mock_has_internet_connection.return_value = False
-    assert can_access_google_page("/") == "Not accessible"
