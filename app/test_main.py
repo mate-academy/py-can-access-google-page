21fd61c01322233463b49b627e02299b9bacd9f8
Eliza Petrova <elizapetrova8@gmail.com>
2024-02-19 15:47:03 +0200

    py-can-access-google-page

--- app/test_main.py
+++ app/test_main.py
@@ -1,48 +1,30 @@
+import pytest
 from unittest import mock
-
 from app.main import can_access_google_page
 
 
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page_right_time(
-        mocked_internet: callable,
-        mocked_validator: callable,
-        url: str = "some.cite") -> None:
-
-    mocked_internet.return_value = True
-    mocked_validator.return_value = True
-
-    assert (
-        can_access_google_page(url) == "Accessible"
-    )
-
-
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_can_not_access_google_page_wrong_time(
-        mocked_internet: callable,
-        mocked_validator: callable,
-        url: str = "some.cite") -> None:
-
-    mocked_internet.return_value = True
-    mocked_validator.return_value = False
-
-    assert (
-        can_access_google_page(url) == "Not accessible"
-    )
-
-
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_can_not_access_google_page_wrong_cite(
-        mocked_internet: callable,
-        mocked_validator: callable,
-        url: str = "some.cite") -> None:
-
-    mocked_internet.return_value = False
-    mocked_validator.return_value = True
-
-    assert (
-        can_access_google_page(url) == "Not accessible"
-    )
+@pytest.mark.parametrize(
+    "mock_param_valid_google_url, mock_param_internet_connection, result",
+    [pytest.param(True, True, "Accessible",
+                  id="Test when URL and Connection is valid"),
+     pytest.param(True, False, "Not accessible",
+                  id="Test when URL is valid and Connection is invalid"),
+     pytest.param(False, True, "Not accessible",
+                  id="Test when URL is invalid and Connection is valid"),
+     pytest.param(False, False, "Not accessible",
+                  id="Test when URL is invalid and Connection is invalid")
+     ])
+def test_can_access_google_page(mock_param_valid_google_url: bool,
+                                mock_param_internet_connection: bool,
+                                result: str) -> None:
+    with (mock.patch("app.main.valid_google_url") as
+          mock_func_valid_google_url):
+        mock_func_valid_google_url.return_value = mock_param_valid_google_url
+        with (mock.patch("app.main.has_internet_connection")
+              as mock_func_int_connect):
+            mock_func_int_connect.return_value = mock_param_internet_connection
+            if (mock_param_valid_google_url is True
+                    and mock_param_internet_connection is True):
+                assert can_access_google_page("Url") == result
+            else:
+                assert can_access_google_page("Url") == result
