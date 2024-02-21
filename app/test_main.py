dbe290230247804d290a0f7d0a6be74cf368c701
cthusq <cthusq.qusevi@gmail.com>
2024-02-21 20:31:11 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -5,38 +5,17 @@ from app.main import can_access_google_page
 
 
 @pytest.mark.parametrize(
-    "internet_connection,valid_url,message",
+    "internet_connection, valid_url, expected_message",
     [
-        pytest.param(
-            True,
-            True,
-            "Accessible",
-            id="connection: True, url: True",
-        ),
-        pytest.param(
-            True,
-            False,
-            "Not accessible",
-            id="connection: True, url: False",
-        ),
-        pytest.param(
-            False,
-            True,
-            "Not accessible",
-            id="connection: False, url: True",
-        ),
-        pytest.param(
-            False,
-            False,
-            "Not accessible",
-            id="connection: False, url: False",
-        )
-    ]
+        (True, True, "Accessible"),
+        (False, False, "Not accessible"),
+    ],
+    ids=["positive_scenario", "negative_scenario"]
 )
-def test_can_access_google_page(
+def test_can_access_google_page_positive_negative(
         internet_connection: bool,
         valid_url: bool,
-        message: str,
+        expected_message: str,
 ) -> None:
     with (mock.patch("app.main.has_internet_connection") as
           mocked_internet_connection,
@@ -44,4 +23,26 @@ def test_can_access_google_page(
           mocked_valid_url):
         mocked_internet_connection.return_value = internet_connection
         mocked_valid_url.return_value = valid_url
-        assert can_access_google_page("url") == message
+        assert can_access_google_page("url") == expected_message
+
+
+@pytest.mark.parametrize(
+    "internet_connection, valid_url, expected_message",
+    [
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+    ],
+    ids=["invalid_url", "no_internet_connection"]
+)
+def test_can_access_google_page_negative(
+        internet_connection: bool,
+        valid_url: bool,
+        expected_message: str,
+) -> None:
+    with (mock.patch("app.main.has_internet_connection") as
+          mocked_internet_connection,
+          mock.patch("app.main.valid_google_url") as
+          mocked_valid_url):
+        mocked_internet_connection.return_value = internet_connection
+        mocked_valid_url.return_value = valid_url
+        assert can_access_google_page("url") == expected_message
