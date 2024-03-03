8f9cd0c29211adf64bcb6cb26cc885c46983ea3c
martyr <vitaliyburkalo81@gmail.com>
2024-03-04 01:01:04 +0200

    Refactored tests using unittest

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,6 @@
 import pytest
 from app.main import can_access_google_page
-from typing import Any
+from unittest import mock
 
 
 @pytest.mark.parametrize(
@@ -12,12 +12,12 @@ from typing import Any
         (False, False, "Not accessible")
     ]
 )
-def test_can_access(mocker: Any,
-                    internet_connection: bool,
-                    valid_url: bool,
-                    expected_result: str) -> None:
-    mocker.patch("app.main.has_internet_connection",
-                 return_value=internet_connection)
-    mocker.patch("app.main.valid_google_url", return_value=valid_url)
-    result = can_access_google_page("https://www.google.com")
-    assert result == expected_result
+def test_can_access(
+        internet_connection: bool,
+        valid_url: bool,
+        expected_result: str
+) -> None:
+    with mock.patch("app.main.has_internet_connection", return_value=internet_connection), \
+            mock.patch("app.main.valid_google_url", return_value=valid_url):
+        result = can_access_google_page("https://www.google.com")
+        assert result == expected_result
