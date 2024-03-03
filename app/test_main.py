2a5c8d8867882ffe5dcd4330b3adbb19ae217347
martyr <vitaliyburkalo81@gmail.com>
2024-03-04 00:47:26 +0200

    Added return type annotations

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,5 @@
 import pytest
+from pytest_mock import MockFixture
 from app.main import can_access_google_page
 
 
@@ -11,11 +12,12 @@ from app.main import can_access_google_page
         (False, False, "Not accessible")
     ]
 )
-def test_can_access(mocker,
+def test_can_access(mocker: MockFixture,
                     internet_connection: bool,
                     valid_url: bool,
                     expected_result: str) -> None:
-    mocker.patch("app.main.has_internet_connection", return_value=internet_connection)
+    mocker.patch("app.main.has_internet_connection",
+                 return_value=internet_connection)
     mocker.patch("app.main.valid_google_url", return_value=valid_url)
     result = can_access_google_page("https://www.google.com")
     assert result == expected_result
