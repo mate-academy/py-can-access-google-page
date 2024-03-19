49e85477b4fa6dcb3c093f0d9f84f4cda4540b67
Oles Shulzhenko <simplemanua@gmail.com>
2024-03-19 21:40:29 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,11 +1,9 @@
 from unittest.mock import patch
 from app.main import can_access_google_page
-import datetime
 
 
 def test_can_access_google_page() -> None:
     url = "http://www.google.com"
-    current_time = datetime.datetime.now()
     with patch("app.main.requests.get") as mock_get:
         with patch(
                 "app.main.has_internet_connection",
@@ -13,4 +11,4 @@ def test_can_access_google_page() -> None:
         ) as mock_internet:
             can_access_google_page(url)
             mock_get.assert_called_once_with(url)
-            mock_internet.assert_called_once_with(current_time)
+            mock_internet.assert_called_once_with()
