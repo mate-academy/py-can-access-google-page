9cd5b04ca14ece7cef0c1b1ee02a46ac2ff8a0f0
bujhm <Bujhm99@gmail.com>
2024-03-21 21:13:05 +0200

    MagikMock annotation, once_with

--- app/test_main.py
+++ app/test_main.py
@@ -1,16 +1,17 @@
-from typing import Callable
 from unittest import mock
 
 from app.main import can_access_google_page
 
 
 @mock.patch("app.main.has_internet_connection")
-def test_has_internet_connection_call(mocked: Callable) -> None:
+def test_has_internet_connection_call(mocked: mock.MagicMock) -> None:
     can_access_google_page("https://www.google.com/webhp?client=firefox-b-d")
     mocked.assert_called_once()
 
 
 @mock.patch("app.main.valid_google_url")
-def test_have_valid_google_url(mocked: Callable) -> None:
+def test_have_valid_google_url(mocked: mock.MagicMock) -> None:
     can_access_google_page("https://www.google.com/webhp?client=firefox-b-d")
-    mocked.assert_called_once()
+    mocked.assert_called_once_with(
+        "https://www.google.com/webhp?client=firefox-b-d"
+    )
