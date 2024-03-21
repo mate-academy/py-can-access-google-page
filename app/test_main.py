40464551e7cc56f531d3d4f4cf3fa4b5d55916a4
Olga <olgahrytsiuk777@gmail.com>
2024-03-21 14:52:38 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,7 +1,7 @@
-from unittest.mock import patch
-from typing import Any
+from unittest.mock import patch, MagicMock
+
 import pytest
-from unittest.mock import MagicMock
+
 from app.main import can_access_google_page
 
 
@@ -17,22 +17,15 @@ from app.main import can_access_google_page
 @patch("app.main.has_internet_connection")
 @patch("app.main.valid_google_url")
 def test_url_google(
-        mock_valid_google_url: Any,
-        mock_has_internet_connection: Any,
+        mock_valid_google_url: MagicMock ,
+        mock_has_internet_connection: MagicMock,
         valid_google_url: bool,
         has_internet_connection: bool,
         expected_result: str
 ) -> None:
-    with patch("app.main.valid_google_url",
-               MagicMock(return_value=valid_google_url)):
-        with patch("app.main.has_internet_connection",
-                   MagicMock(return_value=has_internet_connection)):
 
-            result = can_access_google_page("https://www.google.com")
-            assert result == expected_result
+    mock_valid_google_url.return_value = valid_google_url
+    mock_has_internet_connection.return_value = has_internet_connection
 
-            mock_valid_google_url.return_value = valid_google_url
-            mock_has_internet_connection.return_value = has_internet_connection
-
-            result = can_access_google_page("https://www.google.com")
-            assert result == expected_result
+    result = can_access_google_page("https://www.google.com")
+    assert result == expected_result
