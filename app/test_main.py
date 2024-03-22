39ed2c512b5d07264f803c9daa240d7168d80c11
Oles Shulzhenko <simplemanua@gmail.com>
2024-03-22 08:57:32 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,14 +1,28 @@
-from unittest.mock import patch
 from app.main import can_access_google_page
+from unittest import mock
+import pytest
 
 
-def test_can_access_google_page() -> None:
-    url = "http://www.google.com"
-    with patch("app.main.requests.get") as mock_get:
-        with patch(
-                "app.main.has_internet_connection",
-                return_value=True
-        ) as mock_internet:
-            can_access_google_page(url)
-            mock_get.assert_called_once_with(url)
-            mock_internet.assert_called_once_with()
+@pytest.mark.parametrize(
+    "valid_google_url, has_internet, result_expected",
+    [
+        (True, True, "Accessible"),
+        (False, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (True, False, "Not accessible")
+    ])
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_can_access_google_page(
+        mock_has_internet: None,
+        mock_valid_google_url: None,
+        valid_google_url: None,
+        has_internet: None,
+        result_expected: None
+) -> None:
+    mock_valid_google_url.return_value = valid_google_url
+    mock_has_internet.return_value = has_internet
+
+    result = can_access_google_page("http://google.com")
+
+    assert result == result_expected
