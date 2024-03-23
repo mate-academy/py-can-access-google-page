53d149f4bf81ac1299063cafc4a602e509777a14
Viesich <v.s.vesich@gmail.com>
2024-03-23 18:06:38 +0200

    add ids parameter

--- app/test_main.py
+++ app/test_main.py
@@ -8,6 +8,10 @@ from app.main import can_access_google_page
     ("https://www.google.com", "Accessible"),
     ("https://www.example.com", "Not accessible"),
     ("invalid_url", "Not accessible")
+],ids=[
+    "Access Google",
+    "Access Example",
+    "Invalid URL"
 ])
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
