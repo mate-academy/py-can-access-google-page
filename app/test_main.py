99254fd4b866990920a9ca3ab61fb150d2f5fe4b
AndriiDraga <148384465+AndriiDraga@users.noreply.github.com>
2024-03-23 21:28:38 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,61 @@
-# write your code here
+import pytest
+from app.main import can_access_google_page
+from _pytest.monkeypatch import MonkeyPatch
+
+
+@pytest.mark.parametrize(
+    "url, expected_status",
+    [
+        ("https://www.google.com", "Not accessible"),
+        ("https://www.google.com", "Not accessible"),
+        ("https://www.google.com", "Not accessible")
+    ]
+)
+def test_cannot_access_if_connection_or_valid_url_is_true(
+        monkeypatch: MonkeyPatch,
+        url: str,
+        expected_status: str
+) -> None:
+    monkeypatch.setattr(
+        "app.main.has_internet_connection", lambda: False
+    )
+    monkeypatch.setattr(
+        "app.main.valid_google_url", lambda url: False
+    )
+    assert can_access_google_page(url) == expected_status
+
+
+@pytest.mark.parametrize(
+    "url, expected_status",
+    [
+        ("https://www.google.com", "Not accessible"),
+        ("https://www.google.com", "Not accessible")
+    ]
+)
+def test_cannot_access_if_only_connection(
+        monkeypatch: MonkeyPatch,
+        url: str,
+        expected_status: str
+) -> None:
+    monkeypatch.setattr(
+        "app.main.has_internet_connection", lambda: False
+    )
+    assert can_access_google_page(url) == expected_status
+
+
+@pytest.mark.parametrize(
+    "url, expected_status",
+    [
+        ("https://www.google.com", "Not accessible"),
+        ("https://www.google.com", "Not accessible")
+    ]
+)
+def test_cannot_access_if_only_valid_url(
+        monkeypatch: MonkeyPatch,
+        url: str,
+        expected_status: str
+) -> None:
+    monkeypatch.setattr(
+        "app.main.valid_google_url", lambda url: False
+    )
+    assert can_access_google_page(url) == expected_status
