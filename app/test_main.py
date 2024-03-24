7ef088c1127c0d1fa4baa86639cbd05b877d5fdf
AndriiDraga <148384465+AndriiDraga@users.noreply.github.com>
2024-03-24 17:00:03 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,63 +1,31 @@
 import pytest
-
-from _pytest.monkeypatch import MonkeyPatch
+from unittest import mock
 
 from app.main import can_access_google_page
 
 
 @pytest.mark.parametrize(
-    "url, expected_status",
+    "valid_url_value, connection_value, expected_value",
     [
-        ("https://www.google.com", "Not accessible"),
-        ("https://www.google.com", "Not accessible"),
-        ("https://www.google.com", "Not accessible")
+        (False, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (True, False, "Not accessible"),
+        (True, True, "Accessible")
     ]
 )
-def test_cannot_access_if_connection_or_valid_url_is_true(
-        monkeypatch: MonkeyPatch,
-        url: str,
-        expected_status: str
+def test_access_google_page(
+        valid_url_value: bool,
+        connection_value: bool,
+        expected_value: str
 ) -> None:
-    monkeypatch.setattr(
-        "app.main.has_internet_connection", lambda: False
-    )
-    monkeypatch.setattr(
-        "app.main.valid_google_url", lambda url: False
-    )
-    assert can_access_google_page(url) == expected_status
-
-
-@pytest.mark.parametrize(
-    "url, expected_status",
-    [
-        ("https://www.google.com", "Not accessible"),
-        ("https://www.google.com", "Not accessible")
-    ]
-)
-def test_cannot_access_if_only_connection(
-        monkeypatch: MonkeyPatch,
-        url: str,
-        expected_status: str
-) -> None:
-    monkeypatch.setattr(
-        "app.main.has_internet_connection", lambda: False
-    )
-    assert can_access_google_page(url) == expected_status
-
-
-@pytest.mark.parametrize(
-    "url, expected_status",
-    [
-        ("https://www.google.com", "Not accessible"),
-        ("https://www.google.com", "Not accessible")
-    ]
-)
-def test_cannot_access_if_only_valid_url(
-        monkeypatch: MonkeyPatch,
-        url: str,
-        expected_status: str
-) -> None:
-    monkeypatch.setattr(
-        "app.main.valid_google_url", lambda url: False
-    )
-    assert can_access_google_page(url) == expected_status
+    with (
+        mock.patch(
+            "app.main.valid_google_url",
+            return_value=valid_url_value
+        ),
+            mock.patch(
+                "app.main.has_internet_connection",
+                return_value=connection_value
+            )):
+        url = "https://www.google.com"
+        assert can_access_google_page(url) == expected_value
