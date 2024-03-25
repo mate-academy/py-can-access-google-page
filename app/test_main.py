cb9c4f84d7a9cc31b206e0cd3dd4c24d1d783f07
Oleh Kolodka <oleh1928@gmail.com>
2024-03-25 20:49:05 +0200

    Used mock decorator for better readability and consistency

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,5 @@
 import pytest
-
+from unittest import mock
 from app.main import can_access_google_page
 
 
@@ -18,24 +18,15 @@ from app.main import can_access_google_page
         "Invalid url and hasn't internet",
     ]
 )
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
 def test_can_access_google_page(
-        monkeypatch: object,
+        mock_valid_google_url: mock.MagicMock,
+        mock_has_internet_connection: mock.MagicMock,
         valid_google_url: bool,
         has_internet_connection: bool,
         expected_value: str
 ) -> None:
-    def mock_valid_google_url(_: None) -> bool:
-        return valid_google_url
-
-    def mock_has_internet_connection() -> bool:
-        return has_internet_connection
-
-    monkeypatch.setattr(
-        "app.main.valid_google_url",
-        mock_valid_google_url
-    )
-    monkeypatch.setattr(
-        "app.main.has_internet_connection",
-        mock_has_internet_connection
-    )
+    mock_valid_google_url.return_value = valid_google_url
+    mock_has_internet_connection.return_value = has_internet_connection
     assert can_access_google_page("https://www.google.com") == expected_value
