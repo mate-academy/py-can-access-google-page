51b1a321a4c1ded28193e2bd071e3dd9294f3a5b
AndriiDraga <148384465+AndriiDraga@users.noreply.github.com>
2024-03-25 13:15:46 +0200

    decorators

--- app/test_main.py
+++ app/test_main.py
@@ -2,6 +2,8 @@ import pytest
 
 from unittest import mock
 
+from _pytest.monkeypatch import MonkeyPatch
+
 from app.main import can_access_google_page
 
 
@@ -14,18 +16,17 @@ from app.main import can_access_google_page
         (True, True, "Accessible")
     ]
 )
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
 def test_access_google_page(
+        mock_has_internet_connection: "MonkeyPatch",
+        mock_valid_google_url: "MonkeyPatch",
         valid_url_value: bool,
         connection_value: bool,
         expected_value: str
 ) -> None:
-    with (
-        mock.patch(
-            "app.main.valid_google_url",
-            return_value=valid_url_value
-        ),
-        mock.patch(
-            "app.main.has_internet_connection",
-            return_value=connection_value)):
-        url = "https://www.google.com"
-        assert can_access_google_page(url) == expected_value
+
+    mock_has_internet_connection.return_value = connection_value
+    mock_valid_google_url.return_value = valid_url_value
+    url = "https://www.google.com"
+    assert can_access_google_page(url) == expected_value
