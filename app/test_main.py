7fbb22789439c013e64f8e1b564d55d08e9c9559
antonver <anton.verbovyi@gmail.com>
2024-03-26 18:38:11 +0100

    Done

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,13 @@
-# write your code here
+from app.main import can_access_google_page
+from _pytest.monkeypatch import MonkeyPatch
+
+
+def test_function_acces(monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setattr("app.main.has_internet_connection", lambda: True)
+    monkeypatch.setattr("app.main.valid_google_url", lambda x: True)
+    assert can_access_google_page("ddd") == "Accessible"
+    monkeypatch.setattr("app.main.valid_google_url", lambda x: False)
+    assert can_access_google_page("ddd") == "Not accessible"
+    monkeypatch.setattr("app.main.has_internet_connection", lambda: False)
+    monkeypatch.setattr("app.main.valid_google_url", lambda x: True)
+    assert can_access_google_page("ddd") == "Not accessible"
