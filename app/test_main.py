43e381c9d4721d4a439d67d8109592a62dd66b19
sudakolha <sudakolha@outlook.com>
2024-03-28 22:06:00 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,23 +1,24 @@
 import pytest
-from unittest.mock import patch
+from typing import Generator
+from unittest.mock import MagicMock, patch
 from app.main import can_access_google_page
 
 
 @pytest.fixture
-def mock_requests_get():
+def mock_requests_get() -> Generator[MagicMock, None, None]:
     with patch("app.main.requests.get") as mock_get:
         yield mock_get
 
 
 @pytest.fixture
-def mock_datetime_now():
+def mock_datetime_now() -> Generator[MagicMock, None, None]:
     with patch("app.main.datetime.datetime") as mock_datetime:
         yield mock_datetime.now
 
 
 def test_can_access_google_page_access_granted(
-        mock_requests_get,
-        mock_datetime_now):
+        mock_requests_get: MagicMock,
+        mock_datetime_now: MagicMock) -> None:
     mock_requests_get.return_value.status_code = 200
     mock_datetime_now.return_value.hour = 12
     result = can_access_google_page("https://google.com")
@@ -25,16 +26,16 @@ def test_can_access_google_page_access_granted(
 
 
 def test_can_access_google_page_no_internet_connection(
-        mock_requests_get,
-        mock_datetime_now):
+        mock_requests_get: MagicMock,
+        mock_datetime_now: MagicMock) -> None:
     mock_datetime_now.return_value.hour = 2
     result = can_access_google_page("https://google.com")
     assert result == "Not accessible"
 
 
 def test_can_access_google_page_invalid_url(
-        mock_requests_get,
-        mock_datetime_now):
+        mock_requests_get: MagicMock,
+        mock_datetime_now: MagicMock) -> None:
     mock_requests_get.return_value.status_code = 404
     mock_datetime_now.return_value.hour = 12
     result = can_access_google_page("https://invalid-url.com")
