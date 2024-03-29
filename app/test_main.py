f6eefec56404c699de92f15ad752e2a15fca76df
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2024-03-29 14:24:13 +0200

    Solution with corect tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,14 +1,42 @@
 from unittest import mock
 
+import pytest
+
 from app.main import can_access_google_page
 
 
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
+@pytest.fixture
+def mocked_valid_google_url():
+    with mock.patch("app.main.valid_google_url") as mocked_valid_url:
+        yield mocked_valid_url
+
+
+@pytest.fixture
+def mocked_has_internet_connection():
+    with mock.patch("app.main.has_internet_connection") as mocked_has_connect:
+        yield mocked_has_connect
+
+
+@pytest.mark.parametrize(
+    "url_correct, internet_connection, can_access",
+    [
+        pytest.param(False, True, "Not accessible",
+                     id="Not valid url"),
+        pytest.param(True, False, "Not accessible",
+                     id="No internet connection"),
+        pytest.param(True, True, "Accessible",
+                     id="Correct url and internet connection for access"),
+        pytest.param(False, False, "Not accessible",
+                     id="Not valid url and internet connection")
+    ]
+)
 def test_can_access_google_page(
+        mocked_valid_google_url: mock.MagicMock,
         mocked_has_internet_connection: mock.MagicMock,
-        mocked_valid_google_url: mock.MagicMock
+        url_correct: bool,
+        internet_connection: bool,
+        can_access: str
 ) -> None:
-    can_access_google_page("https://www.google.com.ua")
-    mocked_valid_google_url.assert_called_once()
-    mocked_has_internet_connection.assert_called_once()
+    mocked_valid_google_url.return_value = url_correct
+    mocked_has_internet_connection.return_value = internet_connection
+    assert can_access_google_page("https://google.com") == can_access
