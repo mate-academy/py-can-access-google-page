3161fca3d28a73cf307c865d2524395ab0891598
Olha <olgazinevych@gmail.com>
2024-03-29 19:43:14 +0200

    Added more tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,15 +1,75 @@
-from unittest import mock
+import pytest
+from _pytest.monkeypatch import MonkeyPatch
 from app.main import can_access_google_page
 
 
-def test_can_access_google_page() -> None:
-    with (
-        mock.patch("app.main.has_internet_connection") as mock_connection,
-        mock.patch("app.main.valid_google_url") as mock_valid_url
-    ):
-        url = ("https://www.google.com/webhp?hl=en&sa=X&ved="
-               "0ahUKEwjlr5aw25eFAxVFFBAIHbjlAdEQPAgJ")
-        can_access_google_page(url)
+@pytest.fixture
+def url() -> str:
+    return ("https://www.google.com/webhp?hl=en&sa="
+            "X&ved=0ahUKEwjlr5aw25eFAxVFFBAIHbjlAdEQPAgJ")
 
-        mock_connection.assert_called_once()
-        mock_valid_url.assert_called_once_with(url)
+
+@pytest.fixture
+def mock_internet_connection_true(monkeypatch: MonkeyPatch) -> None:
+    def mock_internet_connection() -> None:
+        return True
+    monkeypatch.setattr(
+        "app.main.has_internet_connection",
+        mock_internet_connection
+    )
+
+
+@pytest.fixture
+def mock_internet_connection_false(monkeypatch: MonkeyPatch) -> None:
+    def mock_internet_connection() -> None:
+        return False
+    monkeypatch.setattr(
+        "app.main.has_internet_connection",
+        mock_internet_connection
+    )
+
+
+@pytest.fixture
+def mock_valid_google_url_true(monkeypatch: MonkeyPatch, url: str) -> None:
+    def mock_valid_google_url(url: str) -> None:
+        return True
+    monkeypatch.setattr("app.main.valid_google_url", mock_valid_google_url)
+
+
+@pytest.fixture
+def mock_valid_google_url_false(monkeypatch: MonkeyPatch, url: str) -> None:
+    def mock_valid_google_url(url: str) -> bool:
+        return False
+    monkeypatch.setattr("app.main.valid_google_url", mock_valid_google_url)
+
+
+def test_cannot_access_google_page_if_only_connection(
+    url: str,
+    mock_internet_connection_true: bool,
+    mock_valid_google_url_false: bool
+) -> None:
+    assert can_access_google_page(url) == "Not accessible"
+
+
+def test_cannot_access_google_page_if_only_valid_url(
+    url: str,
+    mock_internet_connection_false: bool,
+    mock_valid_google_url_true: bool
+) -> None:
+    assert can_access_google_page(url) == "Not accessible"
+
+
+def test_cannot_access_google_page_if_no_connection_and_valid_url(
+    url: str,
+    mock_internet_connection_false: bool,
+    mock_valid_google_url_false: bool
+) -> None:
+    assert can_access_google_page(url) == "Not accessible"
+
+
+def test_can_access_google_page_when_connection_and_valid_url(
+    url: str,
+    mock_internet_connection_true: bool,
+    mock_valid_google_url_true: bool
+) -> None:
+    assert can_access_google_page(url) == "Accessible"
