063ba033d1575c668f1f9d3aaa72a0ddaee29857
kekW <panicua12@gmail.com>
2024-04-02 21:49:09 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,27 @@
-# write your code here
+import unittest
+from unittest import mock
+from app.main import can_access_google_page
+
+
+class TestCanAccessGooglePage(unittest.TestCase):
+    @mock.patch("requests.get")
+    @mock.patch("datetime.datetime")
+    def test_can_access_google_page(self,
+                                    mock_time: mock.MagicMock,
+                                    mock_get: mock.MagicMock) -> None:
+        # Response check from valid_google_url function
+        mock_response = mock.MagicMock()
+        mock_response.status_code = 200
+        mock_get.return_value = mock_response
+
+        # Time check from has_internet_connection
+        mock_now = mock.MagicMock()
+        mock_now.hour = 21
+        mock_time.now.return_value = mock_now
+
+        main_func_call = can_access_google_page("https://somefakegoogle.net")
+
+        mock_get.assert_called_once_with("https://somefakegoogle.net")
+        mock_time.now.assert_called_once()
+
+        self.assertEqual(main_func_call, "Accessible")
