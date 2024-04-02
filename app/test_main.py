e5072b16c445818d0255a777271138430d1fd8e4
Illia <ilaruslanovich7@gmail.com>
2024-04-02 19:09:33 +0200

    sol

--- app/test_main.py
+++ app/test_main.py
@@ -11,33 +11,17 @@ def mock_functions() -> tuple:
         yield mock_valid_google, mock_has_internet_connection
 
 
-def test_can_access_google_page1(mock_functions: tuple) -> None:
+@pytest.mark.parametrize("valid_google, has_inet_connection, result",
+                         [(False, False, "Not accessible"),
+                          (False, True, "Not accessible"),
+                          (True, False, "Not accessible"),
+                          (True, True, "Accessible")])
+def test_can_access_google_page_universal(mock_functions: tuple,
+                                          valid_google: bool,
+                                          has_inet_connection: bool,
+                                          result: str) -> None:
     mock_valid_google, mock_has_internet_connection = mock_functions
-    mock_valid_google.return_value = True
-    mock_has_internet_connection.return_value = True
-    result = can_access_google_page("/")
-    assert result == "Accessible"
-
-
-def test_can_access_google_page2(mock_functions: tuple) -> None:
-    mock_valid_google, mock_has_internet_connection = mock_functions
-    mock_valid_google.return_value = False
-    mock_has_internet_connection.return_value = True
-    result = can_access_google_page("/1")
-    assert result == "Not accessible"
-
-
-def test_can_access_google_page3(mock_functions: tuple) -> None:
-    mock_valid_google, mock_has_internet_connection = mock_functions
-    mock_valid_google.return_value = False
-    mock_has_internet_connection.return_value = False
-    result = can_access_google_page("/1")
-    assert result == "Not accessible"
-
-
-def test_can_access_google_page4(mock_functions: tuple) -> None:
-    mock_valid_google, mock_has_internet_connection = mock_functions
-    mock_valid_google.return_value = True
-    mock_has_internet_connection.return_value = False
-    result = can_access_google_page("/1")
-    assert result == "Not accessible"
+    mock_valid_google.return_value = valid_google
+    mock_has_internet_connection.return_value = has_inet_connection
+    resulto = can_access_google_page("/1")
+    assert resulto == result
