c0d8371ae83929092229d23a2c9f4467f29b8835
Marharyta Marchenko <ritanika12@gmail.com>
2024-04-02 13:03:25 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,5 @@
 import unittest
 from unittest.mock import patch
-import datetime
 from app import main
 
 
@@ -8,7 +7,10 @@ class TestCanAccessGooglePage(unittest.TestCase):
 
     @patch("app.main.valid_google_url")
     @patch("app.main.has_internet_connection")
-    def test_can_access_google_page_accessible(self, mock_has_internet_connection, mock_valid_google_url):
+    def test_can_access_google_page_accessible(
+            self,
+            mock_has_internet_connection: unittest.mock.Mock,
+            mock_valid_google_url: unittest.mock.Mock) -> None:
         mock_valid_google_url.return_value = True
         mock_has_internet_connection.return_value = True
         result = main.can_access_google_page("https://www.google.com")
@@ -16,7 +18,10 @@ class TestCanAccessGooglePage(unittest.TestCase):
 
     @patch("app.main.valid_google_url")
     @patch("app.main.has_internet_connection")
-    def test_can_access_google_page_not_accessible_invalid_url(self, mock_has_internet_connection, mock_valid_google_url):
+    def test_can_access_google_page_not_accessible_invalid_url(
+            self,
+            mock_has_internet_connection: unittest.mock.Mock,
+            mock_valid_google_url: unittest.mock.Mock) -> None:
         mock_valid_google_url.return_value = False
         mock_has_internet_connection.return_value = True
         result = main.can_access_google_page("https://www.invalidurl.com")
@@ -24,13 +29,16 @@ class TestCanAccessGooglePage(unittest.TestCase):
 
     @patch("app.main.valid_google_url")
     @patch("app.main.has_internet_connection")
-    def test_can_access_google_page_not_accessible_no_internet(self, mock_has_internet_connection, mock_valid_google_url):
+    def test_can_access_google_page_not_accessible_no_internet(
+            self,
+            mock_has_internet_connection: unittest.mock.Mock,
+            mock_valid_google_url: unittest.mock.Mock) -> None:
         mock_valid_google_url.return_value = True
         mock_has_internet_connection.return_value = False
         result = main.can_access_google_page("https://www.google.com")
         self.assertEqual(result, "Not accessible")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import subprocess
-    subprocess.check_call(['pip', 'install', 'requests'])
+    subprocess.check_call(["pip", "install", "requests"])
