88ef53324ac07327b3ddafe4cf919b7544c960fc
Alexander Kisliy <108825919+SashaKisliy@users.noreply.github.com>
2024-04-04 18:32:55 +0300

    small refactor

--- app/test_main.py
+++ app/test_main.py
@@ -8,27 +8,7 @@ from app.main import can_access_google_page
     "has_internet, has_valid_url, expected",
     [
         pytest.param(True, True, "Accessible",
-                     id="Access google page")
-    ]
-)
-@mock.patch("app.main.has_internet_connection")
-@mock.patch("app.main.valid_google_url")
-def test_success_cases(
-        mock_valid_url: callable,
-        mock_internet_connection: callable,
-        has_internet: bool,
-        has_valid_url: bool,
-        expected: str
-) -> None:
-    mock_valid_url.return_value = has_valid_url
-    mock_internet_connection.return_value = has_internet
-
-    assert can_access_google_page("https://www.google.com/") == expected
-
-
-@pytest.mark.parametrize(
-    "has_internet, has_valid_url, expected",
-    [
+                     id="Access google page"),
         pytest.param(True, False, "Not accessible",
                      id="Fail, not valid url"),
         pytest.param(False, True, "Not accessible",
@@ -39,9 +19,9 @@ def test_success_cases(
 )
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
-def test_not_success_cases(
-        mock_valid_url: callable,
-        mock_internet_connection: callable,
+def test_success_cases(
+        mock_valid_url: mock.MagicMock,
+        mock_internet_connection: mock.MagicMock,
         has_internet: bool,
         has_valid_url: bool,
         expected: str
@@ -55,11 +35,12 @@ def test_not_success_cases(
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
 def test_func_callable(
-        mock_valid_url: callable,
-        mock_internet_connection: callable
+        mock_valid_url: mock.MagicMock,
+        mock_internet_connection: mock.MagicMock
 ) -> None:
     url = "https://www.google.com/"
     can_access_google_page(url)
 
-    mock_valid_url.assert_called_once_with(url)
     mock_internet_connection.assert_called_once()
+    mock_valid_url.assert_called_once_with(url)
+
