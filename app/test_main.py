78ba9df998e5684cd7629a8b48e01dff0527c41d
Oleksandr Moroz <molecxander@gmail.com>
2024-04-04 14:28:52 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,24 @@
-# write your code here
+import pytest
+from unittest.mock import patch
+from app.main import can_access_google_page
+
+
+@pytest.mark.parametrize(
+    "valid_url,internet_connection,expected_result",
+    [
+        ("https://www.google.com", True, "Accessible"),
+        ("https://non-existent_google.com", True, "Not accessible"),
+        ("https://www.google.com", False, "Not accessible")
+    ],
+    ids=["Existing URL and internet connection",
+         "Non-existent URL is internet connection",
+         "Existing URL  no internet connection"]
+)
+def test_can_access_google_page(valid_url: str,
+                                internet_connection: bool,
+                                expected_result: str) -> None:
+    with patch("app.main.has_internet_connection",
+               return_value=internet_connection):
+        with patch("app.main.valid_google_url",
+                   return_value=(valid_url == "https://www.google.com")):
+            assert can_access_google_page(valid_url) == expected_result
