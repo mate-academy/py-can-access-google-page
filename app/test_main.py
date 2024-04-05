db81ffba5a42dd03cf6e5558c5c067b83bba4097
Lev <dkotkod@gmail.com>
2024-04-05 15:11:08 +0300

    'Solution_fix'

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,6 @@
-import pytest
 from unittest.mock import MagicMock, patch
+
+import pytest
 
 from app.main import can_access_google_page
 
@@ -11,11 +12,17 @@ from app.main import can_access_google_page
         (False, True, "Not accessible"),
         (True, False, "Not accessible"),
         (False, False, "Not accessible"),
+    ],
+    ids=[
+        "Valid URL and internet connection available",
+        "Valid URL but no internet connection",
+        "Internet connection available but invalid URL",
+        "No internet connection and invalid URL"
     ]
 )
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
-def test_cannot_access_if_connection_or_valid_url_is_true(
+def test_can_access_google_page_with_different_connection_and_url_states(
         mock_valid_google_url: MagicMock,
         mock_has_internet_connection: MagicMock,
         valid_url: bool,
@@ -26,3 +33,8 @@ def test_cannot_access_if_connection_or_valid_url_is_true(
     mock_has_internet_connection.return_value = has_connection
 
     assert can_access_google_page("https://www.google.com/") == expected_output
+    mock_valid_google_url.assert_called_once()
+    if valid_url:
+        mock_has_internet_connection.assert_called_once()
+    else:
+        mock_has_internet_connection.assert_not_called()
