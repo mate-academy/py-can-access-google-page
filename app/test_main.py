0ed1b66729461023bee4ca8326f4d9ebc6dc97a1
Vitalii <vitalikbashkiser@gmail.com>
2024-04-06 15:57:20 +0300

    Solution#2

--- app/test_main.py
+++ app/test_main.py
@@ -1,34 +1,40 @@
-from unittest import mock
 import pytest
+from unittest import mock
 from app.main import can_access_google_page
 
 
 @pytest.mark.parametrize(
-    "url, valid_url, internet_connection, expected",
+    "valid_url, internet_connection, expected",
     [
-        # Valid URL and internet connection
-        ("https://www.google.com", True, True, "Accessible"),
-        # Invalid URL but internet connection
-        ("https://www.example.com", False, True, "Not accessible"),
-        # Valid URL but no internet connection
-        ("https://www.google.com", True, False, "Not accessible"),
-        # Invalid URL and no internet connection
-        ("https://www.example.com", False, False, "Not accessible"),
+        (True, True, "Accessible"),
+        (False, True, "Not accessible"),
+        (True, False, "Not accessible"),
+        (False, False, "Not accessible"),
+    ],
+    ids=[
+        "Valid URL and internet connection",
+        "Invalid URL but internet connection",
+        "Valid URL but no internet connection",
+        "Invalid URL and no internet connection",
     ]
 )
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 def test_can_access_google_page(
-    mock_internet_connection: mock.Mock,
-    mock_valid_url: mock.Mock,
-    url: str,
+    mock_has_internet_connection: mock.MagicMock,
+    mock_valid_url: mock.MagicMock,
     valid_url: bool,
     internet_connection: bool,
     expected: str
 ) -> None:
-    # Setting up the mock return values
     mock_valid_url.return_value = valid_url
-    mock_internet_connection.return_value = internet_connection
+    mock_has_internet_connection.return_value = internet_connection
 
-    # Calling the function with the provided URL
-    assert can_access_google_page(url) == expected
+    can_access_google_page("https://anylink.com")
+
+    mock_has_internet_connection.assert_called_once()
+
+    if mock_has_internet_connection.assert_called_once():
+        mock_valid_url.assert_called_once_with("https://anylink.com")
+
+    assert can_access_google_page("https://anylink.com") == expected
