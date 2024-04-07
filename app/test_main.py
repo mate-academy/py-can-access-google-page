7a719d2a599cff5152fcea9e5588ecf971d38601
Sofiia <sonagm9@gmail.com>
2024-04-07 22:47:18 +0300

    'Solution1'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,17 @@
-# write your code here
+import pytest_mock
+
+from app.main import can_access_google_page
+
+
+def test_can_access_google_page(mocker: pytest_mock.MockFixture) -> None:
+    mocker.patch("app.main.valid_google_url", return_value=True)
+    mocker.patch("app.main.has_internet_connection", return_value=True)
+
+    assert can_access_google_page("https://www.google.com") == "Accessible"
+
+    mocker.patch("app.main.has_internet_connection", return_value=False)
+    assert can_access_google_page("https://www.google.com") == "Not accessible"
+
+    mocker.patch("app.main.valid_google_url", return_value=False)
+    mocker.patch("app.main.has_internet_connection", return_value=True)
+    assert can_access_google_page("https://invalidurl.com") == "Not accessible"
