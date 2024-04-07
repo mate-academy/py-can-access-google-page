6c55621cb358fcf2f4208d5fc3b212041fbd255d
semermaks <semermaks@mail.com>
2024-04-07 13:33:16 +0300

    Solution 2

--- app/test_main.py
+++ app/test_main.py
@@ -1,56 +1,35 @@
+from unittest import mock
 import pytest
-from _pytest.monkeypatch import MonkeyPatch
-from typing import Callable
-from unittest.mock import MagicMock
 from app.main import can_access_google_page
 
 
-@pytest.fixture
-def url() -> str:
-    return ("https://www.google.com/webhp?hl=en&sa="
-            "X&ved=0ahUKEwjlr5aw25eFAxVFFBAIHbjlAdEQPAgJ")
-
-
-@pytest.fixture
-def mock_internet_connection(monkeypatch: MonkeyPatch) -> Callable:
-    mock_internet_connection = MagicMock()
-    monkeypatch.setattr(
-        "app.main.has_internet_connection",
-        mock_internet_connection
-    )
-    return mock_internet_connection
-
-
-@pytest.fixture
-def mock_valid_google_url(monkeypatch: MonkeyPatch) -> Callable:
-    mock_valid_google_url = MagicMock()
-    monkeypatch.setattr("app.main.valid_google_url", mock_valid_google_url)
-    return mock_valid_google_url
-
-
 @pytest.mark.parametrize(
-    "connection_return,valid_url_return,expected_result",
+    "valid_google_url_parameter,has_internet_connection_parameter,expected",
     [
+        (True, True, "Accessible"),
         (True, False, "Not accessible"),
         (False, True, "Not accessible"),
-        (False, False, "Not accessible"),
-        (True, True, "Accessible")
+        (False, False, "Not accessible")
     ],
     ids=[
-        "can't access only with connection",
-        "can't access only with valid url",
-        "can't access with no connection and valid url",
-        "can access with connection and valid url"
+        "Valid URL and internet connection",
+        "Valid URL but no internet connection",
+        "Invalid URL but internet connection available",
+        "Invalid URL and no internet connection"
     ]
 )
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
 def test_can_access_google_page(
-    url: str,
-    mock_internet_connection: Callable,
-    mock_valid_google_url: Callable,
-    connection_return: bool,
-    valid_url_return: bool,
-    expected_result: str
+        valid_google_url: mock.MagicMock,
+        has_internet_connection: mock.MagicMock,
+        valid_google_url_parameter: bool,
+        has_internet_connection_parameter: bool,
+        expected: str
 ) -> None:
-    mock_internet_connection.return_value = connection_return
-    mock_valid_google_url.return_value = valid_url_return
-    assert can_access_google_page(url) == expected_result
+    valid_google_url.return_value = valid_google_url_parameter
+    has_internet_connection.return_value = has_internet_connection_parameter
+    assert can_access_google_page("") == expected
+    valid_google_url.assert_called_once()
+    if not valid_google_url:
+        has_internet_connection.assert_called_once()
