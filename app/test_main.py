8e4c7bf0e8e6a4982dcb72a6eb8f08be65a2607d
Serhii Marynin <serhii.marynin@gmail.com>
2024-04-07 21:31:34 +0300

    update annotations, add tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,4 @@
-from typing import Callable
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 import pytest
 
@@ -24,8 +23,8 @@ from app.main import can_access_google_page
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
 def test_can_access_google_page(
-    mocked_internet_func: Callable,
-    mocked_url_func: Callable,
+    mocked_internet_func: MagicMock,
+    mocked_url_func: MagicMock,
     is_internet: bool,
     is_url: bool,
     expected: str
@@ -33,3 +32,23 @@ def test_can_access_google_page(
     mocked_internet_func.return_value = is_internet
     mocked_url_func.return_value = is_url
     assert can_access_google_page("url") == expected
+
+
+@patch("app.main.valid_google_url", return_value=True)
+@patch("app.main.has_internet_connection", return_value=True)
+class TestInternalFuncCalled:
+    def test_has_internet_connection_called(
+        self,
+        mocked_internet_func: MagicMock,
+        mocked_url_func: MagicMock
+    ) -> None:
+        can_access_google_page("url")
+        mocked_internet_func.assert_called_once()
+
+    def test_valid_google_url_called(
+        self,
+        mocked_internet_func: MagicMock,
+        mocked_url_func: MagicMock
+    ) -> None:
+        can_access_google_page("url")
+        mocked_url_func.assert_called_once()
