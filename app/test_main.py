75a893c867877b7629b817484d844133bb39a78c
Oleksandr Moroz <molecxander@gmail.com>
2024-04-08 09:24:44 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -18,22 +18,23 @@ from app.main import can_access_google_page
          "Existing URL, no internet connection",
          "Non-existent URL, no internet connection"]
 )
+@patch("app.main.has_internet_connection")
+@patch("app.main.valid_google_url")
 def test_can_access_google_page(
-        valid_url: str,
-        internet_connection: bool,
-        expected_result: str
+    mock_valid_google_url: MagicMock,
+    mock_has_internet_connection: MagicMock,
+    valid_url: str,
+    internet_connection: bool,
+    expected_result: str
 ) -> None:
-    mock_has_internet_connection = MagicMock(return_value=internet_connection)
-    mock_valid_google_url = (
-        MagicMock(return_value=(valid_url == "https://www.google.com"))
-    )
+    mock_has_internet_connection.return_value = internet_connection
+    mock_valid_google_url.return_value = \
+        (valid_url == "https://www.google.com")
 
-    with patch("app.main.has_internet_connection",
-               mock_has_internet_connection):
-        with patch("app.main.valid_google_url", mock_valid_google_url):
-            assert can_access_google_page(valid_url) == expected_result
-            mock_has_internet_connection.assert_called_once_with()
-            if internet_connection:
-                mock_valid_google_url.assert_called_once_with(valid_url)
-            else:
-                mock_valid_google_url.assert_not_called()
+    assert can_access_google_page(valid_url) == expected_result
+
+    mock_has_internet_connection.assert_called_once_with()
+    if internet_connection:
+        mock_valid_google_url.assert_called_once_with(valid_url)
+    else:
+        mock_valid_google_url.assert_not_called()
