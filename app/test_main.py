a2f9950f350190da39ae19cedd7c0a0d3cdf8701
Olevksy Ivan <kiperdemiwan@gmail.com>
2024-04-08 19:47:03 +0300

    result

--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,26 @@
-import requests
+import pytest
+from app.main import can_access_google_page
+from unittest import mock
 
-from unittest import TestCase
-from unittest.mock import patch
 
-
-class TestGoogle(TestCase):
-    def setUp(self) -> None:
-        self.url = "https://www.google.com"
-
-    def test_can_access_google_access(self) -> None:
-        with patch(
-                "app.main.has_internet_connection",
-                return_value=True
-        ):
-            with patch(
-                    "app.main.valid_google_url",
-                    return_value=True
-            ):
-                with patch(
-                        "app.main.can_access_google_page",
-                        return_value=True
-                ):
-                    response = requests.get(self.url)
-                    self.assertEqual(response.status_code, 200)
+@pytest.mark.parametrize(
+    "valid_google_url,has_internet_connection,expected_result",
+    [
+        (True, True, "Accessible"),
+        (False, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (True, False, "Not accessible")
+    ]
+)
+@mock.patch("app.main.has_internet_connection")
+@mock.patch("app.main.valid_google_url")
+def test_can_access_google_page(
+        mocked_has_internet_connection: mock,
+        mocked_valid_google_url: mock,
+        valid_google_url: bool,
+        has_internet_connection: bool,
+        expected_result: str
+) -> None:
+    mocked_valid_google_url.return_value = valid_google_url
+    mocked_has_internet_connection.return_value = has_internet_connection
+    assert can_access_google_page("https://www.google.com") == expected_result
