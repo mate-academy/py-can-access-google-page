6f93f91f9fbb11494445051241099b278feecb33
Andrii Chubenko <stalkerock@gmail.com>
2024-04-09 17:07:36 +0300

    fix url variable

--- app/test_main.py
+++ app/test_main.py
@@ -1,11 +1,18 @@
 from unittest import mock
+from unittest.mock import MagicMock
+
 from app.main import can_access_google_page
 
 
 import pytest
 
 
-@pytest.mark.parametrize("valid_situation, connection, result", [
+@pytest.fixture
+def test_url() -> str:
+    return "https://itc.ua/"
+
+
+@pytest.mark.parametrize("valid_url, exisiting_connection, result", [
     (False, False, "Not accessible"),
     (True, True, "Accessible"),
     (False, True, "Not accessible"),
@@ -16,18 +23,18 @@ import pytest
         "Valid but not connected"])
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_connection(mock_has_internet_connection: mock,
-                    mock_valid_google_url: mock,
-                    valid_situation: bool,
-                    connection: bool,
+def test_connection(mock_has_internet_connection: MagicMock,
+                    mock_valid_google_url: MagicMock,
+                    valid_url: bool,
+                    exisiting_connection: bool,
                     result: str) -> None:
-    mock_valid_google_url.return_value = valid_situation
-    mock_has_internet_connection.return_value = connection
+    mock_valid_google_url.return_value = valid_url
+    mock_has_internet_connection.return_value = exisiting_connection
 
-    assert can_access_google_page("https://itc.ua/") == result
+    assert can_access_google_page(test_url) == result
 
     mock_has_internet_connection.assert_called_once()
-    if connection:
-        mock_valid_google_url.assert_called_once_with("https://itc.ua/")
+    if exisiting_connection:
+        mock_valid_google_url.assert_called_once_with(test_url)
     else:
         mock_valid_google_url.assert_not_called()
