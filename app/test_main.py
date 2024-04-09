0de17b22cc08ab1aad0422a3d7dc50600a2505fa
Serhii Marynin <serhii.marynin@gmail.com>
2024-04-09 16:35:52 +0300

    add fixture, update tests

--- app/test_main.py
+++ app/test_main.py
@@ -5,50 +5,72 @@ import pytest
 from app.main import can_access_google_page
 
 
-@pytest.mark.parametrize(
-    "is_url,is_internet,expected",
-    [
-        (True, True, "Accessible"),
-        (False, True, "Not accessible"),
-        (True, False, "Not accessible"),
-        (False, False, "Not accessible")
-    ],
-    ids=[
-        "return 'Accessible' if url is valid and there is internet access",
-        "return 'Not accessible' if url is not valid",
-        "return 'Not accessible' if there is no internet",
-        "return 'Not accessible' if url is not valid and there is no internet",
-    ]
-)
+@pytest.fixture()
+def url() -> str:
+    return "https://fakeurl"
+
+
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
-def test_can_access_google_page(
-    mocked_internet_func: MagicMock,
-    mocked_url_func: MagicMock,
-    is_internet: bool,
-    is_url: bool,
-    expected: str
-) -> None:
-    mocked_internet_func.return_value = is_internet
-    mocked_url_func.return_value = is_url
-    assert can_access_google_page("url") == expected
-
-
-@patch("app.main.valid_google_url", return_value=True)
-@patch("app.main.has_internet_connection", return_value=True)
-class TestInternalFuncCalled:
-    def test_has_internet_connection_called(
+class TestCanAccessGooglePageFunc:
+    @pytest.mark.parametrize(
+        "is_url,is_internet,expected",
+        [
+            (True, True, "Accessible"),
+            (False, True, "Not accessible"),
+            (True, False, "Not accessible"),
+            (False, False, "Not accessible")
+        ],
+        ids=[
+            "return 'Accessible' if url is valid and there is internet access",
+            "return 'Not accessible' if url is not valid",
+            "return 'Not accessible' if there is no internet",
+            "return 'Not accessible' if url is not valid and there is no "
+            "internet",
+        ]
+    )
+    def test_function_should_return_correct_value(
         self,
         mocked_internet_func: MagicMock,
-        mocked_url_func: MagicMock
+        mocked_url_func: MagicMock,
+        is_internet: bool,
+        is_url: bool,
+        expected: str,
+        url: str,
     ) -> None:
-        can_access_google_page("url")
-        mocked_internet_func.assert_called_once()
+        mocked_internet_func.return_value = is_internet
+        mocked_url_func.return_value = is_url
+        assert can_access_google_page(url) == expected
 
-    def test_valid_google_url_called(
+    @pytest.mark.parametrize(
+        "is_url,is_internet,expected",
+        [
+            (True, True, 2),
+            (False, True, 1),
+            (True, False, 1),
+            (False, False, 1)
+        ],
+        ids=[
+            "'valid_google_url' and 'has_internet_connection' "
+            "should have been called once",
+            "'valid_google_url' or 'has_internet_connection' "
+            "should have been called once",
+            "'valid_google_url' or 'has_internet_connection' "
+            "should have been called once",
+            "'valid_google_url' or 'has_internet_connection' "
+            "should have been called once",
+        ]
+    )
+    def test_other_functions_have_been_called(
         self,
         mocked_internet_func: MagicMock,
-        mocked_url_func: MagicMock
+        mocked_url_func: MagicMock,
+        is_internet: bool,
+        is_url: bool,
+        expected: int,
+        url: str,
     ) -> None:
-        can_access_google_page("url")
-        mocked_url_func.assert_called_once()
+        mocked_internet_func.return_value = is_internet
+        mocked_url_func.return_value = is_url
+        can_access_google_page(url)
+        assert mocked_internet_func.called + mocked_url_func.called >= expected
