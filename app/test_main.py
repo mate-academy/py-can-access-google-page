2455585da2583231e5961bb95b921a414b00e390
Oleksandr Moroz <molecxander@gmail.com>
2024-04-09 09:51:15 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -6,12 +6,12 @@ from app.main import can_access_google_page
 
 
 @pytest.mark.parametrize(
-    "valid_url,internet_connection,expected_result",
+    "internet_connection,valid_google_url,expected_result",
     [
-        ("https://www.google.com", True, "Accessible"),
-        ("https://non-existent_google.com", True, "Not accessible"),
-        ("https://www.google.com", False, "Not accessible"),
-        ("https://non-existent_google.com", False, "Not accessible")
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible")
     ],
     ids=["Existing URL, is internet connection",
          "Non-existent URL, is internet connection",
@@ -23,18 +23,17 @@ from app.main import can_access_google_page
 def test_can_access_google_page(
     mock_valid_google_url: MagicMock,
     mock_has_internet_connection: MagicMock,
-    valid_url: str,
     internet_connection: bool,
+    valid_google_url: bool,
     expected_result: str
 ) -> None:
     mock_has_internet_connection.return_value = internet_connection
-    mock_valid_google_url.return_value = \
-        (valid_url == "https://www.google.com")
+    mock_valid_google_url.return_value = valid_google_url
 
-    assert can_access_google_page(valid_url) == expected_result
+    assert can_access_google_page("https://www.google.com") == expected_result
 
     mock_has_internet_connection.assert_called_once_with()
     if internet_connection:
-        mock_valid_google_url.assert_called_once_with(valid_url)
+        mock_valid_google_url.assert_called_once_with("https://www.google.com")
     else:
         mock_valid_google_url.assert_not_called()
