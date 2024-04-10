94fde4613d33416c91cfac027ad9320279606e12
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-04-10 22:11:59 +0300

    fix flake8

--- app/test_main.py
+++ app/test_main.py
@@ -3,7 +3,8 @@ from app.main import can_access_google_page
 
 
 def test_has_internet_connection_was_called() -> None:
-    with mock.patch("app.main.has_internet_connection") as mocked_has_connection, \
+    with mock.patch(
+            "app.main.has_internet_connection") as mocked_has_connection, \
             mock.patch("app.main.valid_google_url") as mocked_valid_url:
         url = "https://www.google.com"
         can_access_google_page(url)
