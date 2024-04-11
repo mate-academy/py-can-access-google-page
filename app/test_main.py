4e9ebb3238ac4a367b95b864b5571522c6a4794f
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-04-11 16:58:11 +0300

    fixed

--- app/test_main.py
+++ app/test_main.py
@@ -1,39 +1,27 @@
 import pytest
+
 from unittest import mock
+
 from app.main import can_access_google_page
 
 
 @pytest.mark.parametrize(
-    "url, expected_result, mocked_hour",
+    "valid_url, has_connection, expected",
     [
-        ("", "Not accessible", 5),
-        ("hsgdfjhg", "Not accessible", 2),
-        ("http://www.google.com", "Accessible", 18),
-        ("http://www.google.com", "Not accessible", 4),
-        ("https://www.gogle.com", "Not accessible", 23),
+        (True, True, "Accessible"),
+        (False, True, "Not accessible"),
+        (True, False, "Not accessible"),
+        (False, False, "Not accessible"),
     ]
 )
-def test_can_access_google_page_get_expected_value(
-        url: str,
-        expected_result: str,
-        mocked_hour: int
-) -> None:
-    with mock.patch("app.main.datetime.datetime") as mocked_datetime:
-        mocked_now = mock.Mock()
-        mocked_now.hour = mocked_hour
-        mocked_datetime.now.return_value = mocked_now
-        assert can_access_google_page(url) == expected_result
-
-
-def test_has_internet_connection_and_can_access_google_page_was_called(
+def test_can_access_google_page(
+    valid_url: bool, has_connection: bool, expected: str
 ) -> None:
     with mock.patch(
+            "app.main.valid_google_url"
+    ) as mocked_valid_url, mock.patch(
         "app.main.has_internet_connection"
-    ) as mocked_has_connection, mock.patch(
-        "app.main.valid_google_url"
-    ) as mocked_valid_url:
-
-        url = "https://www.google.com"
-        can_access_google_page(url)
-        mocked_has_connection.assert_called_once()
-        mocked_valid_url.assert_called_once()
+    ) as mocked_has_connection:
+        mocked_valid_url.return_value = valid_url
+        mocked_has_connection.return_value = has_connection
+        assert can_access_google_page("https://www.google.com") == expected
