5bcb62c3dd334fc47664085517250bd5464d022e
Pavlo <pavlo@MacBook-Pro-de-Pavlo.local>
2024-04-14 16:04:21 +0200

    Solution: fix flake8

--- app/test_main.py
+++ app/test_main.py
@@ -1,7 +1,9 @@
 import pytest
+from unittest.mock import MagicMock
 from unittest import mock
 
 from app.main import can_access_google_page
+
 
 @pytest.mark.parametrize(
     "valid_url, internet_available, expected_result, url_called",
@@ -14,12 +16,12 @@ from app.main import can_access_google_page
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 def test_can_access_google_page(
-        mock_has_internet_connection,
-        mock_valid_google_url,
-        valid_url,
-        internet_available,
-        expected_result,
-        url_called
+        mock_has_internet_connection: MagicMock,
+        mock_valid_google_url: MagicMock,
+        valid_url: bool,
+        internet_available: bool,
+        expected_result: str,
+        url_called: bool
 ) -> None:
     mock_valid_google_url.return_value = valid_url
     mock_has_internet_connection.return_value = internet_available
@@ -34,4 +36,3 @@ def test_can_access_google_page(
         mock_valid_google_url.assert_not_called()
 
     mock_has_internet_connection.assert_called_once()
-
