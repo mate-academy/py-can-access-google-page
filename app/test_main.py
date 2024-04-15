51f38d561848f36f9500b298f7d0e0efbc147ced
Mykola Medved <kradu2001@gmail.com>
2024-04-15 03:12:25 +0300

    add "mocked" and fix imports

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,6 @@
 import pytest
-
 from unittest import mock
+
 from app.main import can_access_google_page
 
 
@@ -17,15 +17,15 @@ from app.main import can_access_google_page
                      id="invalid url and no connection"),
     ]
 )
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
 def test_can_access_google_page(
+        mocked_connection: mock,
+        mocked_valid_url: mock,
         valid_url: bool,
         has_connection: bool,
         expected: str
 ) -> None:
-    with (
-        mock.patch("app.main.valid_google_url") as mocked_valid_url,
-        mock.patch("app.main.has_internet_connection") as mocked_connection
-    ):
-        mocked_valid_url.return_value = valid_url
-        mocked_connection.return_value = has_connection
-        assert can_access_google_page("") == expected
+    mocked_valid_url.return_value = valid_url
+    mocked_connection.return_value = has_connection
+    assert can_access_google_page("") == expected
