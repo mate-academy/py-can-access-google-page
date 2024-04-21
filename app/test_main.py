01a38ae79c1c036cbddb869ada46a9f797e610ba
Yevheniia Sosnovska <sosnovskaevgeniya.2504@gmail.com>
2024-04-21 17:01:19 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -12,14 +12,22 @@ from app.main import can_access_google_page
         (False, False, "Not accessible"),
     ],
     ids=[
-        "Test should return Accessible when valid url and has internet connection",
-        "Test should return Not accessible when valid url but no internet connection",
-        "Test should return Not accessible when not valid url but has internet connection",
-        "Test should return Not accessible when not valid url and no internet connection"
+        "Test should return Accessible when valid url "
+        "and has internet connection",
+        "Test should return Not accessible when valid "
+        "url but no internet connection",
+        "Test should return Not accessible when not "
+        "valid url but has internet connection",
+        "Test should return Not accessible when not "
+        "valid url and no internet connection"
     ]
 )
-def test_can_access_google_page(valid_url, internet_connection, expected_result):
+def test_can_access_google_page(valid_url: bool,
+                                internet_connection: bool,
+                                expected_result: str
+                                ) -> None:
     with mock.patch("app.main.valid_google_url", return_value=valid_url), \
-            mock.patch("app.main.has_internet_connection", return_value=internet_connection):
+            mock.patch("app.main.has_internet_connection",
+                       return_value=internet_connection):
         result = can_access_google_page("https://www.google.com")
         assert result == expected_result
