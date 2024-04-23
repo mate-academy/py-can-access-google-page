68b012308de678306c69846d9113cf9a95f9ef86
Ruslan CHavus <chruslan4@gmail.com>
2024-04-23 16:12:43 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,9 @@
-# write your code here
+from unittest import mock
+
+from app.main import can_access_google_page
+
+
+@mock.patch("app.main.has_internet_connection")
+def test_can_access_google_page(mocked_access: mock.MagicMock) -> None:
+    mocked_access.return_value = False
+    assert can_access_google_page("") == "Accessible"
