31f84658e3eeaa898cd60330570b371c5fce8045
Vladimir Dolhyi <volodymyr_dolgyi@ukr.net>
2024-05-18 17:17:32 +0300

    Solution++

--- app/test_main.py
+++ app/test_main.py
@@ -1,13 +1,32 @@
 from unittest import mock
-from typing import Any
 from app.main import can_access_google_page
 
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page(mock_internet: Any, mock_url: Any) -> None:
+def test_can_access_google_page_accessible(mock_internet, mock_url):
+    mock_internet.return_value = True
+    mock_url.return_value = True
+    can_access_google_page("https://www.google.com")
+    mock_internet.assert_called_once_with()
+    mock_url.assert_called_once_with("https://www.google.com")
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_can_access_google_page_invalid_url(mock_internet, mock_url):
     mock_internet.return_value = True
     mock_url.return_value = False
-    can_access_google_page("https://www.notrealurl.com")
+    can_access_google_page("https://www.invalidurl.com")
     mock_internet.assert_called_once_with()
-    mock_url.assert_called_once_with("https://www.notrealurl.com")
+    mock_url.assert_called_once_with("https://www.invalidurl.com")
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_can_access_google_page_no_internet_connection(mock_internet, mock_url):
+    mock_internet.return_value = False
+    mock_url.return_value = True
+    can_access_google_page("https://www.google.com")
+    mock_internet.assert_called_once_with()
+    mock_url.assert_called_once_with("https://www.google.com")
