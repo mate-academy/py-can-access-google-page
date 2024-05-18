3517665d44456fe21c99d207bda5bbbb8fea5d73
Vladimir Dolhyi <volodymyr_dolgyi@ukr.net>
2024-05-18 20:11:34 +0300

    Solution final+

--- app/test_main.py
+++ app/test_main.py
@@ -6,7 +6,8 @@ from app.main import can_access_google_page
 @pytest.mark.parametrize("valid_url, internet_connection, result", [
     (True, True, "Accessible"),
     (True, False, "Not accessible"),
-    (False, True, "Not accessible")
+    (False, True, "Not accessible"),
+    (False, False, "Not accessible")
 ])
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
