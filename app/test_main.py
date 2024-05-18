3f009c0bcf2dd1925f48fddbbde3d4dcf8d20fc1
Vladimir Dolhyi <volodymyr_dolgyi@ukr.net>
2024-05-18 16:18:09 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,12 @@
-# write your code here
+from unittest import mock
+from app.main import can_access_google_page
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_can_access_google_page(mock_internet, mock_url):
+    mock_internet.return_value = True
+    mock_url.return_value = False
+    can_access_google_page("https://www.notrealurl.com")
+    mock_internet.assert_called_once_with()
+    mock_url.assert_called_once_with("https://www.notrealurl.com")
