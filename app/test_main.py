8280a36905e568ac8ef9879868e1330289328e05
Yakov K <hyakunosuke7@gmail.com>
2024-05-20 15:15:17 +0300

    GoogleUpdated

--- app/test_main.py
+++ app/test_main.py
@@ -1,43 +1,20 @@
-import unittest
+import pytest
 from unittest.mock import patch
-import sys
-import os
 from app.main import can_access_google_page
-from typing import Any
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-
-class TestCanAccessGooglePage(unittest.TestCase):
-
-    @patch("app.main.valid_google_url")
-    @patch("app.main.has_internet_connection")
-    def test_accessible_when_both_conditions_true(self, mock_has_internet_connection: Any, mock_valid_google_url: Any) -> None:  # noqa: E501
-        mock_has_internet_connection.return_value = True
-        mock_valid_google_url.return_value = True
-        self.assertEqual(can_access_google_page("http://www.google.com"), "Accessible")  # noqa: E501
-
-    @patch("app.main.valid_google_url")
-    @patch("app.main.has_internet_connection")
-    def test_not_accessible_when_no_internet_connection(self, mock_has_internet_connection: Any, mock_valid_google_url: Any) -> None:  # noqa: E501
-        mock_has_internet_connection.return_value = False
-        mock_valid_google_url.return_value = True
-        self.assertEqual(can_access_google_page("http://www.google.com"), "Not accessible")  # noqa: E501
-
-    @patch("app.main.valid_google_url")
-    @patch("app.main.has_internet_connection")
-    def test_not_accessible_when_invalid_google_url(self, mock_has_internet_connection: Any, mock_valid_google_url: Any) -> None:  # noqa: E501
-        mock_has_internet_connection.return_value = True
-        mock_valid_google_url.return_value = False
-        self.assertEqual(can_access_google_page("http://www.google.com"), "Not accessible")  # noqa: E501
-
-    @patch("app.main.valid_google_url")
-    @patch("app.main.has_internet_connection")
-    def test_not_accessible_when_both_conditions_false(self, mock_has_internet_connection: Any, mock_valid_google_url: Any) -> None:  # noqa: E501
-        mock_has_internet_connection.return_value = False
-        mock_valid_google_url.return_value = False
-        self.assertEqual(can_access_google_page("http://www.google.com"), "Not accessible")  # noqa: E501
+test_data = [
+    (True, True, "http://www.google.com", "Accessible"),
+    (False, True, "http://www.google.com", "Not accessible"),
+    (True, False, "http://www.google.com", "Not accessible"),
+    (False, False, "http://www.google.com", "Not accessible")
+]
 
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.mark.parametrize(
+    "internet_connection, valid_url, url, expected", test_data)
+def test_can_access_google_page(
+        internet_connection: bool, valid_url: bool, url: str, expected: str)\
+        -> None:
+    with patch("app.main.has_internet_connection", return_value=internet_connection):  # noqa: E501
+        with patch("app.main.valid_google_url", return_value=valid_url):
+            assert can_access_google_page(url) == expected
