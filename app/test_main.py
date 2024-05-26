701f65d68e503791f10c679bea819aae442bffe4
Olena Zaritska <zaritskaos@gmail.com>
2024-05-26 16:45:48 +0300

    add pytest.mark.parametrize and use mock decorators

--- app/test_main.py
+++ app/test_main.py
@@ -1,74 +1,66 @@
 from unittest import mock
-from typing import Any, Callable
+from typing import Callable
 
 import pytest
 
 from app.main import can_access_google_page
 
 
-@pytest.fixture()
-def mocked_valid_url_check() -> Any:
-    with (mock.patch("app.main.valid_google_url")
-          as mock_url_check):
-        yield mock_url_check
+class TestCanAccessGooglePage:
+    @pytest.mark.parametrize(
+        "internet_response,valid_url_response,expected_response",
+        [
+            pytest.param(
+                False, True, "Not accessible",
+                id="should not access page if no internet"
+            ),
+            pytest.param(
+                True, False, "Not accessible",
+                id="should not access page if url invalid"
+            ),
+            pytest.param(
+                False, False, "Not accessible",
+                id="should not access page if no internet and url invalid"
+            ),
+            pytest.param(
+                True, True, "Accessible",
+                id="should access page if internet connection ok and url valid"
+            ),
+        ]
+    )
+    @mock.patch("app.main.has_internet_connection")
+    @mock.patch("app.main.valid_google_url")
+    def test_correct_response(
+            self,
+            mocked_valid_url_check: Callable,
+            mocked_internet_connection_check: Callable,
+            internet_response: bool,
+            valid_url_response: bool,
+            expected_response: str
+    ) -> None:
+        mocked_internet_connection_check.return_value = internet_response
+        mocked_valid_url_check.return_value = valid_url_response
+        assert (can_access_google_page("https://some-page.com/")
+                == expected_response)
 
+    @mock.patch("app.main.has_internet_connection")
+    @mock.patch("app.main.valid_google_url")
+    def test_valid_url_call(
+            self,
+            mocked_valid_url_check: Callable,
+            mocked_internet_connection_check: Callable
+    ) -> None:
+        url = "https://some-page.com/"
+        can_access_google_page(url)
+        mocked_valid_url_check.assert_called_once_with(url)
 
-@pytest.fixture()
-def mocked_internet_connection_check() -> Any:
-    with (mock.patch("app.main.has_internet_connection")
-          as mock_connection_check):
-        yield mock_connection_check
-
-
-def test_valid_url_call(
-        mocked_valid_url_check: Callable,
-        mocked_internet_connection_check: Callable
-) -> None:
-    url = "https://example.com/"
-    can_access_google_page(url)
-    mocked_valid_url_check.assert_called_once_with(url)
-
-
-def test_internet_connection_call(
-        mocked_valid_url_check: Callable,
-        mocked_internet_connection_check: Callable
-) -> None:
-    url = "https://example.com/"
-    can_access_google_page(url)
-    mocked_internet_connection_check.assert_called_once()
-
-
-def test_response_no_internet(
-        mocked_valid_url_check: Callable,
-        mocked_internet_connection_check: Callable
-) -> None:
-    mocked_internet_connection_check.return_value = False
-    url = "https://example.com/"
-    assert can_access_google_page(url) == "Not accessible"
-
-
-def test_response_invalid_url(
-        mocked_valid_url_check: Callable,
-        mocked_internet_connection_check: Callable
-) -> None:
-    mocked_valid_url_check.return_value = False
-    assert can_access_google_page("example") == "Not accessible"
-
-
-def test_response_invalid_url_and_no_internet(
-        mocked_valid_url_check: Callable,
-        mocked_internet_connection_check: Callable
-) -> None:
-    mocked_internet_connection_check.return_value = False
-    mocked_valid_url_check.return_value = False
-    assert can_access_google_page("example") == "Not accessible"
-
-
-def test_response_valid_url_and_internet_connection(
-        mocked_valid_url_check: Callable,
-        mocked_internet_connection_check: Callable
-) -> None:
-    mocked_internet_connection_check.return_value = True
-    mocked_valid_url_check.return_value = True
-    url = "https://example.com/"
-    assert can_access_google_page(url) == "Accessible"
+    @mock.patch("app.main.has_internet_connection")
+    @mock.patch("app.main.valid_google_url")
+    def test_internet_connection_call(
+            self,
+            mocked_valid_url_check: Callable,
+            mocked_internet_connection_check: Callable
+    ) -> None:
+        url = "https://some-page.com/"
+        can_access_google_page(url)
+        mocked_internet_connection_check.assert_called_once()
