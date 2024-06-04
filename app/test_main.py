17fdf6d89bc2f285591c51732b27957fb60c2fed
Mikola Rozhansky <bymikola@gmail.com>
2024-06-04 11:46:36 +0300

    Work 5

--- app/test_main.py
+++ app/test_main.py
@@ -5,64 +5,106 @@ import pytest
 from app.main import can_access_google_page
 
 
-@pytest.fixture()
+@pytest.fixture
 def mock_valid_google_url() -> object:
     with (mock.patch("app.main.valid_google_url")) as mock_valid:
         yield mock_valid
 
 
-@pytest.fixture()
+@pytest.fixture
 def mock_has_internet_connection() -> object:
     with (mock.patch("app.main.has_internet_connection")) as mock_connection:
         yield mock_connection
 
+@pytest.mark.parametrize(
+"init_mock_valid, init_mock_has_internet, init_str, result",
+[
+    pytest.param(
+        True,
+        True,
+        "Test",
+        "Accessible",
+        id="test_can_access_google_page_accsesible",
 
-def test_can_access_google_page_accsesible(
-        mock_valid_google_url: object,
-        mock_has_internet_connection: object
-) -> None:
-    mock_valid_google_url.return_value = True
-    mock_has_internet_connection.return_value = True
+    ),
+    pytest.param(
+        True,
+        False,
+        "",
+        "Not accessible",
+        id="test_can_access_google_page_not_accsesible_1_0",
 
-    can_access_google_page("Test")
+    ),
+    pytest.param(
+        False,
+        True,
+        "",
+        "Not accessible",
+        id="test_can_access_google_page_not_accsesible_0_1",
 
-    mock_has_internet_connection.assert_called_once()
-    mock_valid_google_url.assert_called_once_with("Test")
+    ),
+    pytest.param(
+        False,
+        False,
+        "",
+        "Not accessible",
+        id="test_can_access_google_page_not_accsesible_0_0",
 
-    assert can_access_google_page("") == "Accessible"
+    ),
 
+]
+)
+def test_can_access(
+                mock_valid_google_url: object,
+                mock_has_internet_connection: object,
+                init_mock_valid: bool,
+                init_mock_has_internet: bool,
+                init_str: str,
+                result: str
 
-def test_can_access_google_page_not_accsesible_1_0(
-        mock_valid_google_url: object,
-        mock_has_internet_connection: object
-) -> None:
-    mock_valid_google_url.return_value = True
-    mock_has_internet_connection.return_value = False
+                ) -> None:
+    mock_valid_google_url.return_value = init_mock_valid
+    mock_has_internet_connection.return_value = init_mock_has_internet
 
-    can_access_google_page("")
+    can_access_google_page(init_str)
 
-    assert can_access_google_page("") == "Not accessible"
+    # mock_has_internet_connection.assert_called_once()
+    # mock_valid_google_url.assert_called_once_with(init_str)
 
+    assert can_access_google_page(init_str) == result
 
-def test_can_access_google_page_not_accsesible_0_1(
-        mock_valid_google_url: object,
-        mock_has_internet_connection: object
-) -> None:
-    mock_valid_google_url.return_value = False
-    mock_has_internet_connection.return_value = True
-
-    can_access_google_page("")
-
-    assert can_access_google_page("") == "Not accessible"
-
-
-def test_can_access_google_page_not_accsesible_0(
-        mock_valid_google_url: object,
-        mock_has_internet_connection: object
-) -> None:
-    mock_valid_google_url.return_value = False
-    mock_has_internet_connection.return_value = False
-
-    can_access_google_page("")
-
-    assert can_access_google_page("") == "Not accessible"
+#
+# def test_can_access_google_page_not_accsesible_1_0(
+#         mock_valid_google_url: object,
+#         mock_has_internet_connection: object
+# ) -> None:
+#     mock_valid_google_url.return_value = True
+#     mock_has_internet_connection.return_value = False
+#
+#     can_access_google_page("")
+#
+#     assert can_access_google_page("") == "Not accessible"
+#
+#
+# def test_can_access_google_page_not_accsesible_0_1(
+#         mock_valid_google_url: object,
+#         mock_has_internet_connection: object
+# ) -> None:
+#     mock_valid_google_url.return_value = False
+#     mock_has_internet_connection.return_value = True
+#
+#     can_access_google_page("")
+#
+#     assert can_access_google_page("") == "Not accessible"
+#
+#
+# def test_can_access_google_page_not_accsesible_0(
+#         mock_valid_google_url: object,
+#         mock_has_internet_connection: object
+# ) -> None:
+#     mock_valid_google_url.return_value = False
+#     mock_has_internet_connection.return_value = False
+#
+#     can_access_google_page("")
+#
+#     assert can_access_google_page("") == "Not accessible"
