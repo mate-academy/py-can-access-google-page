8f60fc4b4d368c40d825f008c7b423ca37072aa0
necobra <necobrane105@gmail.com>
2024-06-05 17:29:24 +0300

    split of test

--- app/test_main.py
+++ app/test_main.py
@@ -1,19 +1,25 @@
 import app.main
 
+
 def test_should_access_google_page(monkeypatch) -> None:
-    monkeypatch.setattr(app.main, 'has_internet_connection', lambda: True)
-    monkeypatch.setattr(app.main, 'valid_google_url', lambda url: True)
+    monkeypatch.setattr(app.main, "has_internet_connection", lambda: True)
+    monkeypatch.setattr(app.main, "valid_google_url", lambda url: True)
     assert app.main.can_access_google_page("google.com") == "Accessible"
 
-def test_should_not_access_google_page(monkeypatch) -> None:
-    monkeypatch.setattr(app.main, 'has_internet_connection', lambda: True)
-    monkeypatch.setattr(app.main, 'valid_google_url', lambda url: False)
+
+def test_should_not_access_google_page1(monkeypatch) -> None:
+    monkeypatch.setattr(app.main, "has_internet_connection", lambda: True)
+    monkeypatch.setattr(app.main, "valid_google_url", lambda url: False)
     assert app.main.can_access_google_page("google.com") == "Not accessible"
 
-    monkeypatch.setattr(app.main, 'has_internet_connection', lambda: False)
-    monkeypatch.setattr(app.main, 'valid_google_url', lambda url: True)
+
+def test_should_not_access_google_page2(monkeypatch) -> None:
+    monkeypatch.setattr(app.main, "has_internet_connection", lambda: False)
+    monkeypatch.setattr(app.main, "valid_google_url", lambda url: True)
     assert app.main.can_access_google_page("google.com") == "Not accessible"
 
-    monkeypatch.setattr(app.main, 'has_internet_connection', lambda: False)
-    monkeypatch.setattr(app.main, 'valid_google_url', lambda url: False)
+
+def test_should_not_access_google_page3(monkeypatch) -> None:
+    monkeypatch.setattr(app.main, "has_internet_connection", lambda: False)
+    monkeypatch.setattr(app.main, "valid_google_url", lambda url: False)
     assert app.main.can_access_google_page("google.com") == "Not accessible"
