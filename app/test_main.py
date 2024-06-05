7e677d8f063e1ed3076299f66ff26c50343f78e6
Vadim Babii <judviiioff@gmail.com>
2024-06-05 04:04:56 +0300

    test_case_solution

--- app/test_main.py
+++ app/test_main.py
@@ -3,9 +3,14 @@ from app.main import can_access_google_page
 
 
 def test_can_access_google_page() -> None:
-    with patch(
-            "app.main.has_internet_connection") as mock_has_internet_connection, \
-            patch("app.main.valid_google_url") as mock_valid_google_url:
+    with (
+        patch(
+            "app.main.has_internet_connection"
+        ) as mock_has_internet_connection,
+        patch(
+            "app.main.valid_google_url"
+        ) as mock_valid_google_url
+    ):
         mock_has_internet_connection.return_value = True
         mock_valid_google_url.return_value = True
         assert can_access_google_page("http://www.google.com") == "Accessible"
