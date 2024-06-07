417a0d01aa3b9234493f5f9841e9f6c9ecc982d2
ilgan <2ilgan1601@gmail.com>
2024-06-07 19:54:09 +0300

    1

--- app/test_main.py
+++ app/test_main.py
@@ -8,7 +8,8 @@ test_url = "test_url"
 
 
 @pytest.mark.parametrize(
-    "valid_google_url_return_value, has_internet_connection_return_value, result",
+    "valid_google_url_return_value, "
+    "has_internet_connection_return_value, result",
     [
         (True, True, "Accessible"),
         (False, False, "Not accessible"),
@@ -19,11 +20,12 @@ test_url = "test_url"
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 def test_can_access_google_page_accessible(
-        mocked_has_internet_connection,
-        mocked_valid_google_url,
-        valid_google_url_return_value,
-        has_internet_connection_return_value,
-        result) -> None:
+        mocked_has_internet_connection: mock.Mock,
+        mocked_valid_google_url: mock.Mock,
+        valid_google_url_return_value: bool,
+        has_internet_connection_return_value: bool,
+        result: str) -> None:
     mocked_valid_google_url.return_value = valid_google_url_return_value
-    mocked_has_internet_connection.return_value = has_internet_connection_return_value
+    mocked_has_internet_connection.return_value = (
+        has_internet_connection_return_value)
     assert can_access_google_page(test_url) == result
