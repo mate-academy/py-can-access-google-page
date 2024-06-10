63a20003fdcf64e07b234957781a640689a52392
necobra <necobrane105@gmail.com>
2024-06-10 16:57:29 +0300

    v2

--- app/test_main.py
+++ app/test_main.py
@@ -5,21 +5,16 @@ import pytest
 import app.main
 
 
-def test_should_access_google_page(monkeypatch: Any) -> None:
-    monkeypatch.setattr(app.main, "has_internet_connection", lambda: True)
-    monkeypatch.setattr(app.main, "valid_google_url", lambda url: True)
-    assert app.main.can_access_google_page("google.com") == "Accessible"
-
-
 @pytest.mark.parametrize(
-    "has_internet, valid_url",
+    "has_internet, valid_url, accessibility",
     [
-        (True, False),
-        (False, True),
-        (False, False)
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible"),
+        (True, True, "Accessible")
     ]
 )
-def test_should_not_access_google_page(monkeypatch: Any, has_internet: bool, valid_url: bool) -> None:
+def test_access_google_page(monkeypatch: Any, has_internet: bool, valid_url: bool, accessibility: str) -> None:
     monkeypatch.setattr(app.main, "has_internet_connection", lambda: has_internet)
     monkeypatch.setattr(app.main, "valid_google_url", lambda _: valid_url)
-    assert app.main.can_access_google_page("google.com") == "Not accessible"
+    assert app.main.can_access_google_page("google.com") == accessibility
