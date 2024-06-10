38cbd162445aecc5ce2921dd5c7e86eab87f05a0
Іван Білейчук <ivan.bileichuk@gmail.com>
2024-06-10 19:40:24 +0300

    Solution_V2

--- app/test_main.py
+++ app/test_main.py
@@ -17,15 +17,13 @@ from app.main import can_access_google_page
                      id="test_not_valid_url_and_bad_internet_connection")
     ]
 )
-def test_can_access_google_page(
-        mock_internet: None,
-        mock_valid_url: None,
-        expected_result: bool
-) -> None:
-    with patch("app.main.valid_google_url", return_value=mock_valid_url):
-        with patch(
-                "app.main.has_internet_connection", return_value=mock_internet
-        ):
-            assert can_access_google_page(
-                "https://www.google.com"
-            ) == expected_result
+@patch("app.main.has_internet_connection")
+@patch("app.main.valid_google_url")
+def test_can_access_google_page(mock_valid_google_url: callable,
+                                mock_has_internet_connection: callable,
+                                mock_internet: bool,
+                                mock_valid_url: bool,
+                                expected_result: str) -> None:
+    mock_has_internet_connection.return_value = mock_internet
+    mock_valid_google_url.return_value = mock_valid_url
+    assert can_access_google_page("https://www.google.com") == expected_result
