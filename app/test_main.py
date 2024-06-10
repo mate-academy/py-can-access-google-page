a7fb26504217326663a093ba98c2f0431e77ae8c
Ivan <ivan.seeker6@gmail.com>
2024-06-10 17:47:04 +0300

    sol 2

--- app/test_main.py
+++ app/test_main.py
@@ -6,16 +6,17 @@ from app.main import can_access_google_page
 
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
-def test_can_access_google_page(mock_has_internet_connection: Any,
-                                mock_valid_google_url: Any) -> None:
-    mock_has_internet_connection.return_value = True
+def test_cannot_access_if_only_valid_url(mock_has_internet_connection: Any,
+                                         mock_valid_google_url: Any) -> None:
+    mock_has_internet_connection.return_value = False
     mock_valid_google_url.return_value = True
-    assert can_access_google_page("https://www.google.com") == "Accessible"
+    assert can_access_google_page("https://www.google.com") == "Not accessible"
+    mock_has_internet_connection.return_value = True
     mock_valid_google_url.return_value = False
     assert (can_access_google_page("https://invalid-url.com")
             == "Not accessible")
-    mock_has_internet_connection.return_value = False
-    assert can_access_google_page("https://www.google.com") == "Not accessible"
+    mock_valid_google_url.return_value = True
+    assert can_access_google_page("https://www.google.com") == "Accessible"
 
 
 if __name__ == "__main__":
