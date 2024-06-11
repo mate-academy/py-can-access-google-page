ddc0a7e12a2562cabeed7699b0a2f7c4dd0aea92
VitaliiDats1 <vitalii.8dats@gmail.com>
2024-06-11 12:12:36 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -4,8 +4,10 @@ from app.main import can_access_google_page
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page_accessible(mock_has_internet_connection,
-                                           mock_valid_google_url):
+def test_can_access_google_page_accessible(
+        mock_has_internet_connection: bool,
+        mock_valid_google_url: bool
+) -> None:
     mock_has_internet_connection.return_value = True
     mock_valid_google_url.return_value = True
     result = can_access_google_page("https://google.com")
@@ -14,8 +16,10 @@ def test_can_access_google_page_accessible(mock_has_internet_connection,
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page_not_accessible_no_connection\
-                (mock_has_internet_connection, mock_valid_google_url):
+def test_can_access_google_page_not_accessible_no_connection(
+        mock_has_internet_connection: bool,
+        mock_valid_google_url: bool
+) -> None:
     mock_has_internet_connection.return_value = False
     mock_valid_google_url.return_value = True
     result = can_access_google_page("https://google.com")
@@ -24,8 +28,10 @@ def test_can_access_google_page_not_accessible_no_connection\
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page_not_accessible_invalid_url\
-                (mock_has_internet_connection, mock_valid_google_url):
+def test_can_access_google_page_not_accessible_invalid_url(
+        mock_has_internet_connection: bool,
+        mock_valid_google_url: bool
+) -> None:
     mock_has_internet_connection.return_value = True
     mock_valid_google_url.return_value = False
     result = can_access_google_page("https://example.com")
@@ -34,8 +40,10 @@ def test_can_access_google_page_not_accessible_invalid_url\
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page_not_accessible_invalid_url_no_connection\
-                (mock_has_internet_connection, mock_valid_google_url):
+def test_can_access_google_page_not_accessible_invalid_url_no_connection(
+        mock_has_internet_connection: bool,
+        mock_valid_google_url: bool
+) -> None:
     mock_has_internet_connection.return_value = False
     mock_valid_google_url.return_value = False
     result = can_access_google_page("https://example.com")
