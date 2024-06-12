4d3c87648f062e1a97821df86b50a79cd7bbcd03
Ivan <ivan.seeker6@gmail.com>
2024-06-12 09:55:11 +0300

    sol 2

--- app/test_main.py
+++ app/test_main.py
@@ -4,19 +4,20 @@ from typing import Any
 from app.main import can_access_google_page
 
 
+@pytest.mark.parametrize("internet, valid_url, url, expected", [
+    (False, True, "https://www.google.com", "Not accessible"),
+    (True, False, "https://invalid-url.com", "Not accessible"),
+    (True, True, "https://www.google.com", "Accessible")
+])
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
-def test_cannot_access_if_only_valid_url(mock_has_internet_connection: Any,
-                                         mock_valid_google_url: Any) -> None:
-    mock_has_internet_connection.return_value = False
-    mock_valid_google_url.return_value = True
-    assert can_access_google_page("https://www.google.com") == "Not accessible"
-    mock_has_internet_connection.return_value = True
-    mock_valid_google_url.return_value = False
-    assert (can_access_google_page("https://invalid-url.com")
-            == "Not accessible")
-    mock_valid_google_url.return_value = True
-    assert can_access_google_page("https://www.google.com") == "Accessible"
+def test_can_access_google_page(mock_has_internet_connection: Any,
+                                mock_valid_google_url: Any,
+                                internet: bool, valid_url: bool,
+                                url: str, expected: str) -> None:
+    mock_has_internet_connection.return_value = internet
+    mock_valid_google_url.return_value = valid_url
+    assert can_access_google_page(url) == expected
 
 
 if __name__ == "__main__":
