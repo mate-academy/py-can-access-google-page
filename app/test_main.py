7f264173b0d0d31d66fc94b1e2dde883ad334cd9
Maksym Protsak <maksym.protsak@gmail.com>
2024-06-15 21:02:12 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,14 +1,16 @@
+from typing import Type
 from unittest import mock
-
 import pytest
 
 from app.main import can_access_google_page
 
 
 @pytest.mark.parametrize(
-    "has_net_connection, valid_url",
+    "has_net_connection, valid_url, error",
     [
-        (True, True)
+        (True, True, None),
+        (False, True, AssertionError),
+        (False, False, AssertionError)
     ]
 )
 @mock.patch("app.main.valid_google_url")
@@ -16,30 +18,18 @@ from app.main import can_access_google_page
 def test_can_access_google_page(mock_has_internet_connection: mock,
                                 mock_valid_google_url: mock,
                                 has_net_connection: bool | int,
-                                valid_url: bool | int) -> None:
-    mock_has_internet_connection.return_value = has_net_connection
-    mock_valid_google_url.return_value = valid_url
-    can_access_google_page("https://www.google.com")
-    mock_has_internet_connection.assert_called_once()
-    mock_valid_google_url.assert_called_once()
-
-
-@pytest.mark.parametrize(
-    "has_net_connection, valid_url",
-    [
-        (False, True),
-        (False, False)
-    ]
-)
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_can_not_access_google_page(mock_has_internet_connection: mock,
-                                    mock_valid_google_url: mock,
-                                    has_net_connection: bool | int,
-                                    valid_url: bool | int) -> None:
-    with pytest.raises(AssertionError):
+                                valid_url: bool | int,
+                                error: Type[Exception]) -> None:
+    try:
         mock_has_internet_connection.return_value = has_net_connection
         mock_valid_google_url.return_value = valid_url
         can_access_google_page("https://www.google.com")
         mock_has_internet_connection.assert_called_once()
         mock_valid_google_url.assert_called_once()
+    except Exception:
+        with pytest.raises(error):
+            mock_has_internet_connection.return_value = has_net_connection
+            mock_valid_google_url.return_value = valid_url
+            can_access_google_page("https://www.google.com")
+            mock_has_internet_connection.assert_called_once()
+            mock_valid_google_url.assert_called_once()
