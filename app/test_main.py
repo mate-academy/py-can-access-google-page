663410180343d02f7e4a96311c921a44620307db
Maksym Protsak <maksym.protsak@gmail.com>
2024-06-15 19:38:06 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -8,12 +8,7 @@ from app.main import can_access_google_page
 @pytest.mark.parametrize(
     "has_net_connection, valid_url",
     [
-        (True, True),
-        (6, True),
-        (14, True),
-        (23, True),
-        (6, 200)
-
+        (True, True)
     ]
 )
 @mock.patch("app.main.valid_google_url")
@@ -27,3 +22,24 @@ def test_can_access_google_page(mock_has_internet_connection: mock,
     can_access_google_page("https://www.google.com")
     mock_has_internet_connection.assert_called_once()
     mock_valid_google_url.assert_called_once()
+
+
+@pytest.mark.parametrize(
+    "has_net_connection, valid_url",
+    [
+        (False, True),
+        (False, False)
+    ]
+)
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_can_not_access_google_page(mock_has_internet_connection: mock,
+                                    mock_valid_google_url: mock,
+                                    has_net_connection: bool | int,
+                                    valid_url: bool | int) -> None:
+    with pytest.raises(AssertionError):
+        mock_has_internet_connection.return_value = has_net_connection
+        mock_valid_google_url.return_value = valid_url
+        can_access_google_page("https://www.google.com")
+        mock_has_internet_connection.assert_called_once()
+        mock_valid_google_url.assert_called_once()
