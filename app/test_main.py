59537852b492afc3694bca7a518aa78c46e0d67a
Alex Bul <tarasov57228@gmail.com>
2024-06-18 19:59:13 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -7,23 +7,23 @@ from app.main import can_access_google_page
 
 
 @pytest.mark.parametrize(
-	"return_valid, return_internet_connection, result",
-	[
-		(True, True, "Accessible"),
-		(True, False, "Not accessible"),
-		(False, True, "Not accessible"),
-		(False, False, "Not accessible")
-	]
+    "return_valid, return_internet_connection, result",
+    [
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible")
+    ]
 )
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 def test_function_can_access_google_page(
-		mock_valid_google_url: Callable,
-		mock_has_internet_connection: Callable,
-		return_valid: bool,
-		return_internet_connection: bool,
-		result: str
+        mock_valid_google_url: Callable,
+        mock_has_internet_connection: Callable,
+        return_valid: bool,
+        return_internet_connection: bool,
+        result: str
 ) -> None:
-	mock_has_internet_connection.return_value = return_internet_connection
-	mock_valid_google_url.return_value = return_valid
-	assert can_access_google_page("https://google.com") == result
+    mock_has_internet_connection.return_value = return_internet_connection
+    mock_valid_google_url.return_value = return_valid
+    assert can_access_google_page("https://google.com") == result
