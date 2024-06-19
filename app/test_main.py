a0c951d9761d6a169eb064724ef81c7c614f71f7
Seniv Volodymyr <seniv.05@gmail.com>
2024-06-19 16:28:12 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,19 +1,16 @@
 import pytest
-from unittest.mock import patch
-from app.main import (valid_google_url,
-                  has_internet_connection,
-                  can_access_google_page)
+from app.main import can_access_google_page
 from unittest import mock
 
 
 @pytest.mark.parametrize(
-        "valid_url, internet_connection, expected_result",
-        [
-            (True, True, "Accessible"),
-            (False, True, "Not accessible"),
-            (False, False, "Not accessible"),
-            (True, False, "Not accessible"),
-        ]
+    "valid_url, internet_connection, expected_result",
+    [
+        (True, True, "Accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible"),
+        (True, False, "Not accessible"),
+    ]
 )
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
