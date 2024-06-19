56e8c4dd8f684d59a01c1701a9addec477206b78
Anna Shevtsova <aniashevtsova@gmail.com>
2024-06-19 13:51:11 +0200

    errors fixed

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,4 @@
+import unittest
 from unittest import mock
 from unittest.mock import MagicMock
 
@@ -6,10 +7,30 @@ from app.main import can_access_google_page
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page(
-        mocked_url: MagicMock,
-        mocked_connection: MagicMock
-) -> None:
-    mocked_connection.return_value = True
-    mocked_url.return_value = True
-    assert can_access_google_page("http://google.com") == "Accessible"
+class TestGooglePageAccess(unittest.TestCase):
+    def test_can_access_when_connection_and_url_are_true(
+            self,
+            mocked_url: MagicMock,
+            mocked_connection: MagicMock
+    ) -> None:
+        mocked_connection.return_value = True
+        mocked_url.return_value = True
+        assert can_access_google_page("http://google.com") == "Accessible"
+
+    def test__cannot_access_if_only_valid_url(
+            self,
+            mocked_url: MagicMock,
+            mocked_connection: MagicMock
+    ) -> None:
+        mocked_connection.return_value = False
+        mocked_url.return_value = True
+        assert can_access_google_page("http://google.com") == "Not accessible"
+
+    def test__cannot_access_if_only_connection(
+            self,
+            mocked_url: MagicMock,
+            mocked_connection: MagicMock
+    ) -> None:
+        mocked_connection.return_value = True
+        mocked_url.return_value = False
+        assert can_access_google_page("http://google.com") == "Not accessible"
