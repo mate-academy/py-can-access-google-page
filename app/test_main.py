94d848ac0642ef0f8c4e5c9710f69bc1da1f1605
Anna Shevtsova <aniashevtsova@gmail.com>
2024-06-19 13:27:06 +0200

    all tests pass

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,15 @@
-# write your code here
+from unittest import mock
+from unittest.mock import MagicMock
+
+from app.main import can_access_google_page
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_can_access_google_page(
+        mocked_url: MagicMock,
+        mocked_connection: MagicMock
+) -> None:
+    mocked_connection.return_value = True
+    mocked_url.return_value = True
+    assert can_access_google_page("http://google.com") == "Accessible"
