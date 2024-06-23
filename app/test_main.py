46f1fd5f25359742a3d7ede7ea6388074220356b
Tymur <shmorguntimur@gmail.com>
2024-06-23 14:45:31 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,20 +1,31 @@
 from unittest import mock
-from app.main import can_access_google_page
+from app.main import valid_google_url, has_internet_connection
 
 
-def mocked_valid_google_url(url: str) -> bool:
-    response = mock.patch("requests.get")
-    response_status_code = mock.patch("response.status_code")
-    if response_status_code == 200:
-        return True
-    else:
-        return False
+def mock_valid_google_url(url: str, status_code: int) -> bool:
+    with mock.patch("requests.get") as mock_requests_get:
+        mock_response = mock_requests_get.return_value
+        mock_response.status_code = status_code
+        return valid_google_url(url)
 
 
-def mocked_has_internet_connection() -> bool:
-    current_time = mock.patch("datetime.datetime.now.hour")
-    return True if current_time in range(6, 23) else False
+def mock_has_internet_connection(hour: int) -> bool:
+    with mock.patch("datetime.datetime") as mock_datetime:
+        mock_now = mock_datetime.now.return_value
+        mock_now.hour = hour
+        return has_internet_connection()
 
 
-def test_can_access_google_page():
-    assert can_access_google_page('https://www.google.com') == "Accessible"
+def test_can_access_google_page() -> None:
+    assert mock_valid_google_url("https://www.google.com", 200) is True
+    assert mock_has_internet_connection(10) is True
+
+
+def test_can_access_google_page_wrong_time() -> None:
+    assert mock_valid_google_url("https://www.google.com", 200) is True
+    assert mock_has_internet_connection(2) is False
+
+
+def test_can_access_google_page_wrong_url() -> None:
+    assert mock_valid_google_url("https://www.google.com", 404) is False
+    assert mock_has_internet_connection(10) is True
