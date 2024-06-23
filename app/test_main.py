d9b2fd44ca9eaaa51a25ed1c0e37d718a9ba1fee
Tymur <shmorguntimur@gmail.com>
2024-06-23 18:49:29 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,31 +1,26 @@
+import pytest
 from unittest import mock
-from app.main import valid_google_url, has_internet_connection
+from app.main import can_access_google_page
 
 
-def mock_valid_google_url(url: str, status_code: int) -> bool:
-    with mock.patch("requests.get") as mock_requests_get:
-        mock_response = mock_requests_get.return_value
-        mock_response.status_code = status_code
-        return valid_google_url(url)
-
-
-def mock_has_internet_connection(hour: int) -> bool:
-    with mock.patch("datetime.datetime") as mock_datetime:
-        mock_now = mock_datetime.now.return_value
-        mock_now.hour = hour
-        return has_internet_connection()
-
-
-def test_can_access_google_page() -> None:
-    assert mock_valid_google_url("https://www.google.com", 200) is True
-    assert mock_has_internet_connection(10) is True
-
-
-def test_can_access_google_page_wrong_time() -> None:
-    assert mock_valid_google_url("https://www.google.com", 200) is True
-    assert mock_has_internet_connection(2) is False
-
-
-def test_can_access_google_page_wrong_url() -> None:
-    assert mock_valid_google_url("https://www.google.com", 404) is False
-    assert mock_has_internet_connection(10) is True
+@pytest.mark.parametrize(
+    "url, internet_connection, url_validity, result", [
+        ("https://www.google.com/", True, True, "Accessible"),
+        ("https://www.google.com/", False, True, "Not accessible"),
+        ("https/www.google.c", True, False, "Not accessible"),
+        ("https/www.google.c", False, False, "Not accessible"),
+    ]
+)
+@mock.patch("app.main.has_internet_connection")
+@mock.patch("app.main.valid_google_url")
+def test_can_access_google_page(
+        mocked_has_internet_connection: callable,
+        mocked_valid_google_url: callable,
+        url: str,
+        internet_connection: bool,
+        url_validity: bool,
+        result: bool
+) -> None:
+    mocked_has_internet_connection.return_value = internet_connection
+    mocked_valid_google_url.return_value = url_validity
+    assert can_access_google_page(url) == result
