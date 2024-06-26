a4eb6924c276332b18732740ece7dc119525cf4b
Danylo Minkov <minkov215@gmail.com>
2024-06-26 21:54:23 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -4,31 +4,37 @@ from app.main import can_access_google_page
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page_with_no_internet(
-    mocked_url: mock.Mock,
-    mocked_internet_connection: mock.Mock
+def test_can_access_google_page_accessible(
+    mocked_valid_google_url: mock.Mock,
+    mocked_has_internet_connection: mock.Mock
 ) -> None:
-    mocked_url.return_value = True
-    mocked_internet_connection.return_value = False
+    mocked_valid_google_url.return_value = True
+    mocked_has_internet_connection.return_value = True
+
+    result = can_access_google_page("https://www.google.com")
+    assert result == "Accessible"
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_can_access_google_page_no_internet(
+    mocked_valid_google_url: mock.Mock,
+    mocked_has_internet_connection: mock.Mock
+) -> None:
+    mocked_valid_google_url.return_value = True
+    mocked_has_internet_connection.return_value = False
 
     result = can_access_google_page("https://www.google.com")
     assert result == "Not accessible"
-
-    mocked_url.assert_called()
-    mocked_internet_connection.assert_called()
 
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 def test_can_access_google_page_with_invalid_url(
-    mocked_url: mock.Mock,
-    mocked_internet_connection: mock.Mock
+    mocked_url: mock.Mock, mocked_internet_connection: mock.Mock
 ) -> None:
     mocked_url.return_value = False
     mocked_internet_connection.return_value = True
 
     result = can_access_google_page("https://invalid-url.com")
     assert result == "Not accessible"
-
-    mocked_url.assert_called()
-    mocked_internet_connection.assert_called()
