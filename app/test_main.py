4ad16a9b7899c9f33e8535dbb0a2af60ed21fcb8
Arsen Markotskyi <arsen.markotskui@gmail.com>
2024-06-30 22:29:52 +0300

    "sol"

--- app/test_main.py
+++ app/test_main.py
@@ -12,16 +12,16 @@ from app.main import can_access_google_page
         ("http://www.invalid_url.com", False, False, "Not accessible"),
     ]
 )
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
 def test_can_access_google_page(
+        mock_has_internet: bool,
+        mock_valid_url: bool,
         url: str,
         valid_url: bool,
         has_internet: bool,
         excepted: str
 ) -> None:
-    with (
-        mock.patch(
-            "app.main.valid_google_url", return_value=valid_url),
-        mock.patch(
-            "app.main.has_internet_connection", return_value=has_internet)
-    ):
-        assert can_access_google_page(url) == excepted
+    mock_valid_url.return_value = valid_url
+    mock_has_internet.return_value = has_internet
+    assert can_access_google_page(url) == excepted
