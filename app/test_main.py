926ab32d2fe341f255be93cbecb24ad78dd66f13
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-07-10 18:09:47 +0300

    Deleted redundant test

--- app/test_main.py
+++ app/test_main.py
@@ -47,17 +47,3 @@ class TestCanAccessGooglePage(unittest.TestCase):
             can_access_google_page("https://invalid.url"),
             "Not accessible"
         )
-
-    @patch("app.main.valid_google_url")
-    @patch("app.main.has_internet_connection")
-    def test_not_accessible_without_internet_connection_and_invalid_url(
-            self,
-            mock_has_internet: bool,
-            mock_valid_url: bool
-    ) -> None:
-        mock_has_internet.return_value = False
-        mock_valid_url.return_value = False
-        self.assertEqual(
-            can_access_google_page("https://invalid.url"),
-            "Not accessible"
-        )
