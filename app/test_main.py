3a39d09de87a7d0b6145b4a67157d1cc1ac4f0ba
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-07-10 22:08:38 +0300

    removed duplications of code using parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -1,49 +1,31 @@
-import unittest
+import pytest
 from unittest.mock import patch
-
 from app.main import can_access_google_page
 
 
-class TestCanAccessGooglePage(unittest.TestCase):
-
-    @patch("app.main.valid_google_url")
-    @patch("app.main.has_internet_connection")
-    def test_accessible_with_internet_connection_and_valid_url(
-            self,
-            mock_has_internet: bool,
-            mock_valid_url: bool
-    ) -> None:
-        mock_has_internet.return_value = True
-        mock_valid_url.return_value = True
-        self.assertEqual(
-            can_access_google_page("https://www.google.com"),
-            "Accessible"
-        )
-
-    @patch("app.main.valid_google_url")
-    @patch("app.main.has_internet_connection")
-    def test_not_accessible_without_internet_connection(
-            self,
-            mock_has_internet: bool,
-            mock_valid_url: bool
-    ) -> None:
-        mock_has_internet.return_value = False
-        mock_valid_url.return_value = True
-        self.assertEqual(
-            can_access_google_page("https://www.google.com"),
-            "Not accessible"
-        )
-
-    @patch("app.main.valid_google_url")
-    @patch("app.main.has_internet_connection")
-    def test_not_accessible_with_invalid_url(
-            self,
-            mock_has_internet: bool,
-            mock_valid_url: bool
-    ) -> None:
-        mock_has_internet.return_value = True
-        mock_valid_url.return_value = False
-        self.assertEqual(
-            can_access_google_page("https://invalid.url"),
-            "Not accessible"
-        )
+@pytest.mark.parametrize(
+    "has_internet, valid_url, expected", [
+        (True, True, "Accessible"),
+        (False, True, "Not accessible"),
+        (True, False, "Not accessible"),
+        (False, False, "Not accessible"),
+    ],
+    ids=[
+        "Should return accessible if correct url and has internet",
+        "Should not return accessible if has no internet",
+        "Should not return accessible if invalid url and has no internet",
+        "Should not return accessible if invalid url and has no internet",
+    ]
+)
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
+def test_can_access_google_page(
+        mock_has_internet: bool,
+        mock_valid_url: bool,
+        has_internet: bool,
+        valid_url: str,
+        expected: str
+) -> None:
+    mock_has_internet.return_value = has_internet
+    mock_valid_url.return_value = valid_url
+    assert can_access_google_page("https://www.google.com") == expected
