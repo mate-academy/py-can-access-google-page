87e4bfcd13051e3c80de2bec96c79d252f56174b
Anna Lyubinska <anna.lyubinskagmail.com>
2024-07-10 19:58:35 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -10,14 +10,14 @@ from app.main import can_access_google_page
      (True, False, "Not accessible"),
      (False, False, "Not accessible")]
 )
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
 def test_should_check_can_access_google_page(
+        mocked_url: object,
+        mocked_internet: object,
         valid_ural: bool,
         has_internet: bool,
         result: str) -> None:
-    with (
-        mock.patch("app.main.valid_google_url") as mocked_url,
-        mock.patch("app.main.has_internet_connection") as mocked_internet
-    ):
-        mocked_url.return_value = valid_ural
-        mocked_internet.return_value = has_internet
-        assert can_access_google_page("google.com") == result
+    mocked_url.return_value = valid_ural
+    mocked_internet.return_value = has_internet
+    assert can_access_google_page("google.com") == result
