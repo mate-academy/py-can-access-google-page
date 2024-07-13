35ff0e42d24565ac8dd7a036b91efaf2dfeac635
Valeriy Morkvych <morkvych@gmail.com>
2024-07-13 14:01:55 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,34 @@
-# write your code here
+from unittest import mock
+import pytest
+
+from app.main import can_access_google_page
+
+
+@pytest.fixture()
+def mock_valid_url() -> object:
+    with mock.patch("app.main.valid_google_url") as valid:
+        yield valid
+
+
+@pytest.fixture()
+def mock_connection() -> object:
+    with mock.patch("app.main.has_internet_connection") as connect:
+        yield connect
+
+
+@pytest.mark.parametrize(
+    "value_connection, value_valid, result",
+    [
+        (False, False, "Not accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (True, True, "Accessible")
+    ], ids=["no connection & no valid", "only connection",
+            "only valid", "connection & valid"]
+)
+def test_valid_url_and_connection_exists(
+        mock_valid_url: object, mock_connection: object,
+        value_connection: bool, value_valid: bool, result: bool) -> None:
+    mock_connection.return_value = value_connection
+    mock_valid_url.return_value = value_valid
+    assert can_access_google_page("url") == result
