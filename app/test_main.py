2d39f631df12cdeab162896ad412f243a760def3
Daniil <daniilrizhenko@gmail.com>
2024-07-15 11:38:55 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,29 +1,31 @@
+import pytest
 from unittest import mock
 from app.main import can_access_google_page
 
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
+@pytest.mark.parametrize(
+    "mocked_valid_url_return, "
+    "mocked_has_internet_return, "
+    "url, "
+    "expected_result",
+    [
+        (True, True, "https://www.google.com", "Accessible"),
+        (True, False, "https://www.google.com", "Not accessible"),
+        (False, True, "https://www.invalidurl.com", "Not accessible"),
+        (False, False, "https://www.invalidurl.com", "Not accessible"),
+    ]
+)
 def test_can_access_google_page(
-        mocked_has_internet: bool,
-        mocked_valid_url: bool
+        mocked_has_internet: mock.MagicMock,
+        mocked_valid_url: mock.MagicMock,
+        mocked_valid_url_return: bool,
+        mocked_has_internet_return: bool,
+        url: str,
+        expected_result: str
 ) -> None:
-    mocked_valid_url.return_value = True
-    mocked_has_internet.return_value = True
-    result = can_access_google_page("https://www.google.com")
-    assert result == "Accessible"
-
-    mocked_has_internet.return_value = True
-    mocked_has_internet.return_value = False
-    result = can_access_google_page("https://www.google.com")
-    assert result == "Not accessible"
-
-    mocked_valid_url.return_value = False
-    mocked_has_internet.return_value = True
-    result = can_access_google_page("https://www.invalidurl.com")
-    assert result == "Not accessible"
-
-    mocked_valid_url.return_value = False
-    mocked_has_internet.return_value = False
-    result = can_access_google_page("https://www.invalidurl.com")
-    assert result == "Not accessible"
+    mocked_valid_url.return_value = mocked_valid_url_return
+    mocked_has_internet.return_value = mocked_has_internet_return
+    result = can_access_google_page(url)
+    assert result == expected_result
