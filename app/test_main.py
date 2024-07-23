c1f0edcdff4a0b9c286cc7240777e732d620e820
Anton Bilous <anton.bilosus@gmail.com>
2024-07-23 17:44:09 +0300

    Implement tests for can_access_google_page

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,34 @@
-# write your code here
+from unittest import mock
+
+import pytest
+
+from app.main import can_access_google_page
+
+
+@pytest.mark.parametrize(
+    "has_connection, url_is_valid",
+    [
+        pytest.param(True, True, id="connection exists and url is valid"),
+        pytest.param(True, False, id="connection exists but url is invalid"),
+        pytest.param(False, True, id="no connection but url is valid"),
+        pytest.param(False, False, id="no connection and url is invalid"),
+    ],
+)
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_returns_valid_results(
+    mocked_has_internet_connection: mock.MagicMock,
+    mocked_valid_google_url: mock.MagicMock,
+    has_connection: bool,
+    url_is_valid: bool,
+) -> None:
+    mocked_has_internet_connection.return_value = has_connection
+    mocked_valid_google_url.return_value = url_is_valid
+
+    assert can_access_google_page("http://test.com")
+
+    mocked_has_internet_connection.assert_called_once()
+    if has_connection:
+        mocked_valid_google_url.assert_called_once_with("http://test.com")
+    else:
+        mocked_valid_google_url.assert_not_called()
