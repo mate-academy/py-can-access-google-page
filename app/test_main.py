0cafdcae67a7e3330518987a2598cf70a1efc2e0
Halyna Rohatska <rohatskahr@gmail.com>
2024-08-06 16:11:33 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -6,14 +6,16 @@ from app.main import can_access_google_page
 
 
 @pytest.fixture()
-def mocked_connect():
+def mocked_connect() -> mock.Mock:
     with mock.patch("app.main.has_internet_connection") as mock_test_connect:
         yield mock_test_connect
 
+
 @pytest.fixture()
-def mocked_url():
+def mocked_url() -> mock.Mock:
     with mock.patch("app.main.valid_google_url") as mock_test_url:
         yield mock_test_url
+
 
 @pytest.mark.parametrize(
     "has_connection, valid_url, value",
@@ -28,8 +30,8 @@ def test_can_access_google_page(
         has_connection: bool,
         valid_url: bool,
         value: str,
-        mocked_connect,
-        mocked_url
+        mocked_connect: mock.Mock,
+        mocked_url: mock.Mock
 ) -> None:
     mocked_connect.return_value = has_connection
     mocked_url.return_value = valid_url
