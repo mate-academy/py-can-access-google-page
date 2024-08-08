6721927a3a5426a42e7e062e03ea126cd8f45ead
Denys Piuro <denys.pyuro@gmail.com>
2024-08-08 22:56:33 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,32 @@
-# write your code here
+from typing import Any
+from app.main import can_access_google_page
+
+
+def test_can_access_google_page_accessible(
+        mocker: Any
+) -> None:
+    mocker.patch("app.main.has_internet_connection", return_value=True)
+    mocker.patch("app.main.valid_google_url", return_value=True)
+
+    result = can_access_google_page("http://www.google.com")
+    assert result == "Accessible"
+
+
+def test_can_access_google_page_not_accessible_no_internet(
+        mocker: Any
+) -> None:
+    mocker.patch("app.main.has_internet_connection", return_value=False)
+    mocker.patch("app.main.valid_google_url", return_value=True)
+
+    result = can_access_google_page("http://www.google.com")
+    assert result == "Not accessible"
+
+
+def test_can_access_google_page_not_accessible_invalid_url(
+        mocker: Any
+) -> None:
+    mocker.patch("app.main.has_internet_connection", return_value=True)
+    mocker.patch("app.main.valid_google_url", return_value=False)
+
+    result = can_access_google_page("http://www.google.com")
+    assert result == "Not accessible"
