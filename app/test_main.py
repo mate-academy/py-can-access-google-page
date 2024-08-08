90278f264696343481b889e269f13640c710cca7
Denys Piuro <denys.pyuro@gmail.com>
2024-08-08 23:24:40 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,32 +1,34 @@
-from pytest_mock import MockerFixture
+from unittest.mock import patch
 from app.main import can_access_google_page
 
 
-def test_can_access_google_page_accessible(
-        mocker: MockerFixture
-) -> None:
-    mocker.patch("app.main.has_internet_connection", return_value=True)
-    mocker.patch("app.main.valid_google_url", return_value=True)
-
-    result = can_access_google_page("http://www.google.com")
-    assert result == "Accessible"
+def test_can_access_google_page_accessible() -> None:
+    with patch("app.main.valid_google_url", return_value=True):
+        with patch("app.main.has_internet_connection", return_value=True):
+            assert can_access_google_page(
+                ("https://www.google.com") == "Accessible"
+            )
 
 
-def test_can_access_google_page_not_accessible_no_internet(
-        mocker: MockerFixture
-) -> None:
-    mocker.patch("app.main.has_internet_connection", return_value=False)
-    mocker.patch("app.main.valid_google_url", return_value=True)
-
-    result = can_access_google_page("http://www.google.com")
-    assert result == "Not accessible"
+def test_can_access_google_page_no_internet() -> None:
+    with patch("app.main.valid_google_url", return_value=True):
+        with patch("app.main.has_internet_connection", return_value=False):
+            assert can_access_google_page(
+                ("https://www.google.com") == "Not accessible"
+            )
 
 
-def test_can_access_google_page_not_accessible_invalid_url(
-        mocker: MockerFixture
-) -> None:
-    mocker.patch("app.main.has_internet_connection", return_value=True)
-    mocker.patch("app.main.valid_google_url", return_value=False)
+def test_can_access_google_page_invalid_url() -> None:
+    with patch("app.main.valid_google_url", return_value=False):
+        with patch("app.main.has_internet_connection", return_value=True):
+            assert can_access_google_page(
+                ("https://www.invalidurl.com") == "Not accessible"
+            )
 
-    result = can_access_google_page("http://www.google.com")
-    assert result == "Not accessible"
+
+def test_can_access_google_page_no_internet_and_invalid_url() -> None:
+    with patch("app.main.valid_google_url", return_value=False):
+        with patch("app.main.has_internet_connection", return_value=False):
+            assert can_access_google_page(
+                ("https://www.invalidurl.com") == "Not accessible"
+            )
