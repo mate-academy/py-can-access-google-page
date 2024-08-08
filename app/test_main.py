7adc527f566a41e5b7976b48ac9d63bca59b8fd0
Denys Piuro <denys.pyuro@gmail.com>
2024-08-08 23:29:12 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -6,29 +6,29 @@ def test_can_access_google_page_accessible() -> None:
     with patch("app.main.valid_google_url", return_value=True):
         with patch("app.main.has_internet_connection", return_value=True):
             assert can_access_google_page(
-                ("https://www.google.com") == "Accessible"
-            )
+                "https://www.google.com"
+            ) == "Accessible"
 
 
 def test_can_access_google_page_no_internet() -> None:
     with patch("app.main.valid_google_url", return_value=True):
         with patch("app.main.has_internet_connection", return_value=False):
             assert can_access_google_page(
-                ("https://www.google.com") == "Not accessible"
-            )
+                "https://www.google.com"
+            ) == "Not accessible"
 
 
 def test_can_access_google_page_invalid_url() -> None:
     with patch("app.main.valid_google_url", return_value=False):
         with patch("app.main.has_internet_connection", return_value=True):
             assert can_access_google_page(
-                ("https://www.invalidurl.com") == "Not accessible"
-            )
+                "https://www.invalidurl.com"
+            ) == "Not accessible"
 
 
 def test_can_access_google_page_no_internet_and_invalid_url() -> None:
     with patch("app.main.valid_google_url", return_value=False):
         with patch("app.main.has_internet_connection", return_value=False):
             assert can_access_google_page(
-                ("https://www.invalidurl.com") == "Not accessible"
-            )
+                "https://www.invalidurl.com"
+            ) == "Not accessible"
