efd91d6c15a81af968ffe9ec0d03d54a6f8b497d
Vlada <vlada.belayy@mail.com>
2024-08-08 19:52:10 +0300

    create mock test

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,23 @@
-# write your code here
+from unittest import mock
+import pytest
+from app.main import can_access_google_page
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_can_access_google_page(mock_internet_connection, mock_valid_url):
+    mock_valid_url.return_value = True
+    mock_internet_connection.return_value = True
+    assert can_access_google_page("https://www.google.com") == "Accessible"
+
+    mock_valid_url.return_value = True
+    mock_internet_connection.return_value = False
+    assert can_access_google_page("https://www.google.com") == "Not accessible"
+
+    mock_valid_url.return_value = False
+    mock_internet_connection.return_value = True
+    assert can_access_google_page("https://www.invalid-url.com") == "Not accessible"
+
+    mock_valid_url.return_value = False
+    mock_internet_connection.return_value = False
+    assert can_access_google_page("https://www.invalid-url.com") == "Not accessible"
