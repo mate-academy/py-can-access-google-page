fe7eb186a006e464a8d4df462e1abbce55e272f9
serhii <ockar2018@gmail.com>
2024-08-09 21:53:38 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,60 @@
-# write your code here
+from unittest import mock
+
+import pytest
+
+from app.main import can_access_google_page
+
+
+@pytest.fixture()
+def mock_access():
+    with (
+        mock.patch("app.main.valid_google_url") as mock_valid_google_url,
+        mock.patch("app.main.has_internet_connection") as mock_connection
+    ):
+        yield mock_valid_google_url, mock_connection
+
+
+@pytest.mark.parametrize(
+    "valid_url, has_internet, result, description",
+    [
+        (
+            True,
+            True,
+            "Accessible",
+            "Valid url and internet connection should return 'Accessible'"
+        ),
+        (
+            False,
+            False,
+            "Not accessible",
+            "Invalid url and internet connection "
+            "should return 'Not accessible'"
+        ),
+        (
+            True,
+            False,
+            "Not accessible",
+            "Valid url but no internet connection "
+            "should return 'Not accessible'"
+        ),
+        (
+            False,
+            True,
+            "Not accessible",
+            "Invalid url and working internet connection "
+            "should return 'Not accessible'"
+        ),
+    ]
+)
+def test_can_access_google_page(
+        mock_access,
+        valid_url,
+        has_internet,
+        result,
+        description
+) -> None:
+    mock_valid_google_url, mock_internet_connection = mock_access
+    mock_valid_google_url.return_value = valid_url
+    mock_internet_connection.return_value = has_internet
+
+    assert can_access_google_page("example.com") == result, description
