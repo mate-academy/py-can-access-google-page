6cab3300de1b76a3056f711c1f03a54a9fa663d3
Olexandr Bastov <iamlucky1990@gmail.com>
2024-08-09 20:18:15 +0300

    made new solution from scratch using "pytest.fixture" and "pytest.mark.parametrize"

--- app/test_main.py
+++ app/test_main.py
@@ -1,46 +1,45 @@
-from unittest import mock
-from unittest.mock import Mock
+from unittest.mock import patch, DEFAULT
 
-from app import main
+import pytest
+
+from app.main import can_access_google_page
+
+MAGIC_URL = "magic URL"
 
 
-@mock.patch("app.main.has_internet_connection")
-def test_always_call_inet_connection_func_and_return_not_accessible_if_false(
-    mock_inet_connection: Mock
+@pytest.fixture
+def mocked_funcs() -> dict:
+    with patch.multiple(
+        "app.main",
+        has_internet_connection=DEFAULT,
+        valid_google_url=DEFAULT
+    ) as mocks:
+        yield mocks
+
+
+@pytest.mark.parametrize(
+    "inet_conn_return, valid_url_return, expected",
+    [
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible"),
+    ],
+    ids=[
+        "if inet connection established and google url is valid",
+        "if invalid googl url",
+        "if no internet connection",
+        "if no inet connection and google url is invalid",
+    ]
+)
+def test_should_return_correct_value(
+    mocked_funcs: dict,
+    inet_conn_return: bool,
+    valid_url_return: bool,
+    expected: bool
 ) -> None:
-    mock_inet_connection.return_value = False
 
-    access_status = main.can_access_google_page("magic URL")
+    mocked_funcs["has_internet_connection"].return_value = inet_conn_return
+    mocked_funcs["valid_google_url"].return_value = valid_url_return
 
-    mock_inet_connection.assert_called_once_with()
-    assert access_status == "Not accessible"
-
-
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_call_valid_url_func_with_given_url_and_return_not_accessible_if_false(
-        mock_inet_connection: Mock,
-        mock_valid_url: Mock
-) -> None:
-    mock_inet_connection.return_value = True
-    mock_valid_url.return_value = False
-
-    access_status = main.can_access_google_page("magic URL")
-
-    mock_valid_url.assert_called_once_with("magic URL")
-    assert access_status == "Not accessible"
-
-
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_should_return_accessible_if_both_inner_funcs_returns_true(
-    mock_inet_connection: Mock,
-    mock_valid_url: Mock
-) -> None:
-    mock_inet_connection.return_value = True
-    mock_valid_url.return_value = True
-
-    access_status = main.can_access_google_page("magic URL")
-
-    mock_valid_url.assert_called_once_with("magic URL")
-    assert access_status == "Accessible"
+    assert can_access_google_page(MAGIC_URL) == expected
