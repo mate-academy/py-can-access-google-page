dc9eb4af9966d3ad12999f177d37cbd5acf4d5d7
Valeriia Kyrylenko <valerya.kyrylenko@gmail.com>
2024-08-09 15:21:31 +0200

    implement py-can-access-google-page with changes

--- app/test_main.py
+++ app/test_main.py
@@ -2,6 +2,8 @@ from unittest import mock
 from app.main import can_access_google_page
 import pytest
 
+URL = "http://www.google.com"
+
 
 @pytest.fixture()
 def mocked_current_time() -> None:
@@ -9,45 +11,64 @@ def mocked_current_time() -> None:
         yield mock_current_time
 
 
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
+@pytest.fixture()
+def first_func() -> None:
+    with mock.patch("app.main.valid_google_url") as mocked_valid_google_url:
+        yield mocked_valid_google_url
+
+
+@pytest.fixture()
+def sec_func() -> None:
+    with (mock.patch("app.main.has_internet_connection")
+          as mocked_has_internet_connection):
+        yield mocked_has_internet_connection
+
+
+@pytest.mark.parametrize(
+    "url, value_first_func, value_sec_func, result",
+    [
+        pytest.param(
+            URL,
+            True,
+            True,
+            "Accessible",
+            id="should return accessible when funcs true"
+        ),
+        pytest.param(
+            URL,
+            False,
+            True,
+            "Not accessible",
+            id="should return not accessible when first func false"
+        ),
+        pytest.param(
+            URL,
+            True,
+            False,
+            "Not accessible",
+            id="should return not accessible when second func false"
+        ),
+        pytest.param(
+            URL,
+            False,
+            False,
+            "Not accessible",
+            id="should return not accessible when all funcs false"
+        ),
+    ]
+)
 def test_should_return_accessible_when_all_funcs_return_true(
-        mocked_valid_google_url: mock,
-        mocked_has_internet_connection: mock
+        first_func: mock,
+        sec_func: mock,
+        url: str,
+        value_first_func: bool,
+        value_sec_func: bool,
+        result: str
 ) -> None:
-    mocked_valid_google_url.return_value = True
-    mocked_has_internet_connection.return_value = True
-    assert can_access_google_page("http://www.google.com") == "Accessible"
-
-
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_should_return_not_accessible_when_valid_googl_funcs_return_false(
-        mocked_valid_google_url: mock,
-        mocked_has_internet_connection: mock
-) -> None:
-    mocked_valid_google_url.return_value = False
-    mocked_has_internet_connection.return_value = True
-    assert can_access_google_page("http://www.google.com") == "Not accessible"
-
-
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_should_return_not_accessible_when_has_internet_return_false(
-        mocked_valid_google_url: mock,
-        mocked_has_internet_connection: mock
-) -> None:
-    mocked_valid_google_url.return_value = True
-    mocked_has_internet_connection.return_value = False
-    assert can_access_google_page("http://www.google.com") == "Not accessible"
-
-
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_should_return_not_accessible_when_all_funcs_return_false(
-        mocked_valid_google_url: mock,
-        mocked_has_internet_connection: mock
-) -> None:
-    mocked_valid_google_url.return_value = False
-    mocked_has_internet_connection.return_value = False
-    assert can_access_google_page("http://www.google.com") == "Not accessible"
+    first_func.return_value = value_first_func
+    sec_func.return_value = value_sec_func
+    assert can_access_google_page(url) == result, (
+        f"{result} when valid_google_url returned "
+        f"{value_first_func} and has_internet_connection returned "
+        f"{value_sec_func} but got {can_access_google_page(url)} instead"
+    )
