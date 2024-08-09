f29fbad8fd155b8f65ab935f510e367efa1012a9
Denys Piuro <denys.pyuro@gmail.com>
2024-08-09 16:33:05 +0300

    Fixes according to Olga`s remarks

--- app/test_main.py
+++ app/test_main.py
@@ -1,34 +1,32 @@
+import pytest
 from unittest.mock import patch
 from app.main import can_access_google_page
 
 
-def test_can_access_google_page_accessible() -> None:
-    with patch("app.main.valid_google_url", return_value=True):
-        with patch("app.main.has_internet_connection", return_value=True):
-            assert can_access_google_page(
-                "https://www.google.com"
-            ) == "Accessible"
+GOOGLE_URL = "https://www.google.com"
+INVALID_URL = "https://www.invalidurl.com"
 
 
-def test_can_access_google_page_no_internet() -> None:
-    with patch("app.main.valid_google_url", return_value=True):
-        with patch("app.main.has_internet_connection", return_value=False):
-            assert can_access_google_page(
-                "https://www.google.com"
-            ) == "Not accessible"
-
-
-def test_can_access_google_page_invalid_url() -> None:
-    with patch("app.main.valid_google_url", return_value=False):
-        with patch("app.main.has_internet_connection", return_value=True):
-            assert can_access_google_page(
-                "https://www.invalidurl.com"
-            ) == "Not accessible"
-
-
-def test_can_access_google_page_no_internet_and_invalid_url() -> None:
-    with patch("app.main.valid_google_url", return_value=False):
-        with patch("app.main.has_internet_connection", return_value=False):
-            assert can_access_google_page(
-                "https://www.invalidurl.com"
-            ) == "Not accessible"
+@pytest.mark.parametrize(
+    "url, valid_url, has_internet, expected",
+    [
+        (GOOGLE_URL, True, True, "Accessible"),
+        (GOOGLE_URL, True, False, "Not accessible"),
+        (INVALID_URL, False, True, "Not accessible"),
+        (INVALID_URL, False, False, "Not accessible"),
+    ]
+)
+def test_can_access_google_page(
+    url: str,
+        valid_url: bool,
+        has_internet: bool,
+        expected: str
+) -> None:
+    with patch("app.main.valid_google_url", return_value=valid_url):
+        with patch(
+                "app.main.has_internet_connection", return_value=has_internet
+        ):
+            assert can_access_google_page(url) == expected, (
+                f"Failed for URL: {url}, valid_url: {valid_url}, "
+                f"has_internet: {has_internet}"
+            )
