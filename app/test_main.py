152becd80c67982f299690f9876af5c717f2dd90
Pasha <pavik.varganov@gmail.com>
2024-08-10 16:46:31 +0300

    Solution 2

--- app/test_main.py
+++ app/test_main.py
@@ -1,20 +1,25 @@
 from unittest import mock
 from unittest.mock import Mock
 
+import pytest
+
 from app.main import can_access_google_page
 
 
+@pytest.mark.parametrize(
+    "url",
+    [
+        "http://google.com",
+        "http://abra_cadabra"
+    ]
+)
 @mock.patch("app.main.valid_google_url")
-def test_valid_google_url_has_called(mocked_valid_url: Mock) -> None:
-    url = "http://google.com"
+@mock.patch("app.main.has_internet_connection")
+def test_valid_google_url_has_called(
+        mocked_has_connection: Mock,
+        mocked_valid_url: Mock,
+        url: str
+) -> None:
     can_access_google_page(url)
     mocked_valid_url.assert_called_once_with(url)
-
-
-@mock.patch("app.main.has_internet_connection")
-def test_has_internet_connection_has_called(
-        mocked_has_connection: Mock
-) -> None:
-    url = "http://google.com"
-    can_access_google_page(url)
     mocked_has_connection.assert_called_once()
