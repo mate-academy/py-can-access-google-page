8be6a6ded18bdd0f78ce92e267993fb13c1afbb1
Bogdan <bogdan.rylov422@gmail.com>
2024-08-10 22:12:40 +0300

    Reformat code according to mentor's requirements

--- app/test_main.py
+++ app/test_main.py
@@ -5,6 +5,18 @@ import pytest
 from app.main import can_access_google_page
 
 
+@pytest.fixture()
+def mocked_valid_google_url() -> mock.MagicMock:
+    with mock.patch("app.main.valid_google_url") as mocked_func:
+        yield mocked_func
+
+
+@pytest.fixture()
+def mocked_has_internet_connection() -> mock.MagicMock:
+    with mock.patch("app.main.has_internet_connection") as mocked_func:
+        yield mocked_func
+
+
 @pytest.mark.parametrize(
     "valid_url, has_connection, expected_result",
     [
@@ -21,16 +33,14 @@ from app.main import can_access_google_page
          "url is NOT valid and has NO connection"),
     ]
 )
-@mock.patch("app.main.has_internet_connection")
-@mock.patch("app.main.valid_google_url")
 def test_can_access_google_page(
-        mock_valid_url: mock.MagicMock,
-        mock_has_connection: mock.MagicMock,
+        mocked_valid_google_url: mock.MagicMock,
+        mocked_has_internet_connection: mock.MagicMock,
         valid_url: bool,
         has_connection: bool,
         expected_result: str
 
 ) -> None:
-    mock_valid_url.return_value = valid_url
-    mock_has_connection.return_value = has_connection
+    mocked_valid_google_url.return_value = valid_url
+    mocked_has_internet_connection.return_value = has_connection
     assert can_access_google_page("valid_url") == expected_result
