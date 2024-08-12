da1318000f9a8bcf40c9ad9cc8c9e04567716599
Andrii <andrii.khamaza0@gmail.com>
2024-08-12 13:08:41 +0300

    fixed mistakes

--- app/test_main.py
+++ app/test_main.py
@@ -8,7 +8,7 @@ test_url = "url"
 
 
 @pytest.fixture()
-def mock_functions() -> tuple:
+def mocked_connection_and_valid_url_functions() -> tuple:
     with (
         mock.patch("app.main.has_internet_connection") as mocked_connection,
         mock.patch("app.main.valid_google_url") as mocked_valid_url
@@ -16,8 +16,10 @@ def mock_functions() -> tuple:
         yield mocked_connection, mocked_valid_url
 
 
-def test_inner_functions_should_be_called(mock_functions: tuple) -> None:
-    mock_connection, mock_valid_url = mock_functions
+def test_inner_functions_should_be_called(
+        mocked_connection_and_valid_url_functions: tuple
+) -> None:
+    mock_connection, mock_valid_url = mocked_connection_and_valid_url_functions
 
     can_access_google_page(test_url)
     mock_connection.assert_called_once()
@@ -43,9 +45,9 @@ def test_can_access_google_page(
         connection_func_result: bool,
         valid_url_func_result: bool,
         expected_value: str,
-        mock_functions: tuple
+        mocked_connection_and_valid_url_functions: tuple
 ) -> None:
-    mock_connection, mock_valid_url = mock_functions
+    mock_connection, mock_valid_url = mocked_connection_and_valid_url_functions
 
     mock_connection.return_value = connection_func_result
     mock_valid_url.return_value = valid_url_func_result
