6f3c49efd423e320164eaa103acbcb4e15f88e83
Maxim Turenko <turenkomaksim099@gmail.com>
2024-08-13 15:47:04 +0300

    implement py-can-access-google-page (used parametrize)

--- app/test_main.py
+++ app/test_main.py
@@ -3,51 +3,33 @@ from unittest.mock import patch
 from app.main import can_access_google_page
 
 
-@pytest.fixture
-def mock_validations() -> None:
-    with (
-        patch("app.main.has_internet_connection")
-        as mock_has_internet_connection
-    ):
-        with patch("app.main.valid_google_url") as mock_valid_google_url:
-            yield mock_valid_google_url, mock_has_internet_connection
-
-
-def test_can_access_google_page_accessible(mock_validations: str) -> None:
-    mock_valid_google_url, mock_has_internet_connection = mock_validations
-    mock_valid_google_url.return_value = True
-    mock_has_internet_connection.return_value = True
-
-    assert can_access_google_page("https://www.google.com") == "Accessible"
-
-
-def test_can_access_google_page_no_internet(mock_validations: str) -> None:
-    mock_valid_google_url, mock_has_internet_connection = mock_validations
-    mock_valid_google_url.return_value = True
-    mock_has_internet_connection.return_value = False
-
-    assert can_access_google_page("https://www.google.com") == "Not accessible"
-
-
-def test_can_access_google_page_invalid_url(mock_validations: str) -> None:
-    mock_valid_google_url, mock_has_internet_connection = mock_validations
-    mock_valid_google_url.return_value = False
-    mock_has_internet_connection.return_value = True
-
-    assert (
-        can_access_google_page("https://www.invalid-url.com")
-        == "Not accessible"
-    )
-
-
-def test_can_access_google_page_no_internet_and_invalid_url(
-        mock_validations: str
+@pytest.mark.parametrize(
+    "url, mock_valid_google_url_return, "
+    "mock_has_internet_connection_return, "
+    "expected",
+    [
+        ("https://www.google.com", True, True, "Accessible"),
+        ("https://www.google.com", True, False, "Not accessible"),
+        ("https://www.invalid-url.com", False, True, "Not accessible"),
+        ("https://www.invalid-url.com", False, False, "Not accessible"),
+    ],
+)
+def test_can_access_google_page(
+        url: str,
+        mock_valid_google_url_return: bool,
+        mock_has_internet_connection_return: bool,
+        expected: str
 ) -> None:
-    mock_valid_google_url, mock_has_internet_connection = mock_validations
-    mock_valid_google_url.return_value = False
-    mock_has_internet_connection.return_value = False
+    with (patch("app.main.has_internet_connection")
+          as mock_has_internet_connection,
+            patch("app.main.valid_google_url")
+            as mock_valid_google_url
+          ):
+        mock_valid_google_url.return_value = mock_valid_google_url_return
+        # I had to do this transfer because
+        # it can't be transferred any other way
+        mock_has_internet_connection.return_value = (
+            mock_has_internet_connection_return
+        )
 
-    assert (
-        can_access_google_page("https://www.invalid-url.com")
-        == "Not accessible"
-    )
+        assert can_access_google_page(url) == expected
