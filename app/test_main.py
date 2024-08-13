2985a02c10fb2df19d8298e67d5c50dd855f4fb5
Maxim Turenko <turenkomaksim099@gmail.com>
2024-08-13 12:49:03 +0300

    implement py-can-access-google-page (fixed)

--- app/test_main.py
+++ app/test_main.py
@@ -4,60 +4,50 @@ from app.main import can_access_google_page
 
 
 @pytest.fixture
-def mock_functions() -> None:
+def mock_validations() -> None:
     with (
         patch("app.main.has_internet_connection")
-        as mock_has_internet_connection,
-        patch("app.main.valid_google_url")
-        as mock_valid_google_url
+        as mock_has_internet_connection
     ):
-        yield {
-            "valid_google_url": mock_valid_google_url,
-            "has_internet_connection": mock_has_internet_connection
-        }
+        with patch("app.main.valid_google_url") as mock_valid_google_url:
+            yield mock_valid_google_url, mock_has_internet_connection
 
 
-def test_can_access_google_page_accessible(mock_functions: dict) -> None:
-    mocks = mock_functions
-    mocks["valid_google_url"].return_value = True
-    mocks["has_internet_connection"].return_value = True
+def test_can_access_google_page_accessible(mock_validations: str) -> None:
+    mock_valid_google_url, mock_has_internet_connection = mock_validations
+    mock_valid_google_url.return_value = True
+    mock_has_internet_connection.return_value = True
 
-    assert (
-        can_access_google_page("https://www.google.com") == "Accessible"
-    )
+    assert can_access_google_page("https://www.google.com") == "Accessible"
 
 
-def test_can_access_google_page_no_internet(mock_functions: dict) -> None:
-    mocks = mock_functions
-    mocks["valid_google_url"].return_value = True
-    mocks["has_internet_connection"].return_value = False
+def test_can_access_google_page_no_internet(mock_validations: str) -> None:
+    mock_valid_google_url, mock_has_internet_connection = mock_validations
+    mock_valid_google_url.return_value = True
+    mock_has_internet_connection.return_value = False
 
-    assert (
-        can_access_google_page("https://www.google.com") == "Not accessible"
-    )
+    assert can_access_google_page("https://www.google.com") == "Not accessible"
 
 
-def test_can_access_google_page_invalid_url(mock_functions: dict) -> None:
-    mocks = mock_functions
-    mocks["valid_google_url"].return_value = False
-    mocks["has_internet_connection"].return_value = True
+def test_can_access_google_page_invalid_url(mock_validations: str) -> None:
+    mock_valid_google_url, mock_has_internet_connection = mock_validations
+    mock_valid_google_url.return_value = False
+    mock_has_internet_connection.return_value = True
 
     assert (
-        can_access_google_page(
-            "https://www.invalid-url.com"
-        ) == "Not accessible"
+        can_access_google_page("https://www.invalid-url.com")
+        == "Not accessible"
     )
 
 
 def test_can_access_google_page_no_internet_and_invalid_url(
-        mock_functions: dict
+        mock_validations: str
 ) -> None:
-    mocks = mock_functions
-    mocks["valid_google_url"].return_value = False
-    mocks["has_internet_connection"].return_value = False
+    mock_valid_google_url, mock_has_internet_connection = mock_validations
+    mock_valid_google_url.return_value = False
+    mock_has_internet_connection.return_value = False
 
     assert (
-        can_access_google_page(
-            "https://www.invalid-url.com"
-        ) == "Not accessible"
+        can_access_google_page("https://www.invalid-url.com")
+        == "Not accessible"
     )
