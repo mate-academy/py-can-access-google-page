19e11752fdcfb55c7db6130644ae68805456abc2
Maxim Turenko <turenkomaksim099@gmail.com>
2024-08-13 16:22:24 +0300

    implement py-can-access-google-page (fixed code style)

--- app/test_main.py
+++ app/test_main.py
@@ -6,7 +6,7 @@ from app.main import can_access_google_page
 @pytest.mark.parametrize(
     "url, mock_valid_google_url_return, "
     "mock_has_internet_connection_return, "
-    "expected",
+    "expected_result",
     [
         ("https://www.google.com", True, True, "Accessible"),
         ("https://www.google.com", True, False, "Not accessible"),
@@ -18,18 +18,17 @@ def test_can_access_google_page(
         url: str,
         mock_valid_google_url_return: bool,
         mock_has_internet_connection_return: bool,
-        expected: str
+        expected_result: str
 ) -> None:
-    with (patch("app.main.has_internet_connection")
-          as mock_has_internet_connection,
-            patch("app.main.valid_google_url")
-            as mock_valid_google_url
-          ):
+    with (
+        patch("app.main.has_internet_connection")
+        as mock_has_internet_connection,
+        patch("app.main.valid_google_url")
+        as mock_valid_google_url
+    ):
         mock_valid_google_url.return_value = mock_valid_google_url_return
-        # I had to do this transfer because
-        # it can't be transferred any other way
         mock_has_internet_connection.return_value = (
             mock_has_internet_connection_return
         )
 
-        assert can_access_google_page(url) == expected
+        assert can_access_google_page(url) == expected_result
