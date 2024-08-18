f06b7a06780f2f68c0d4a11e201bddcf3367bac2
Tkachov Mykhailo <mtkachov435@gmail.com>
2024-08-18 20:48:43 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,32 @@
-# write your code here
+from app.main import can_access_google_page
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "url,expected_result",
+    [
+        pytest.param(
+            "http://",
+            "Not accessible",
+            id=""
+        ),
+        pytest.param(
+            "",
+            "Not accessible",
+            id="edge cases"
+        ),
+        pytest.param(
+            "https://www.google.com.ua/?hl=uk",
+            "Accessible",
+            id="edge cases"
+        ),
+        pytest.param(
+            "https://www.googles.com.ua/?hl=uk",
+            "Not accessible",
+            id="edge cases"
+        ),
+    ]
+)
+def test_all(url: str, expected_result: str) -> None:
+    assert can_access_google_page(url) == expected_result
