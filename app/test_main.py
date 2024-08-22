3b15a0d6c52efa04cd89c211de300dd34eb5dab5
Maksim <golevmaksim52@gmail.com>
2024-08-22 22:25:29 +0300

    created url access test

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,27 @@
-# write your code here
+from app.main import can_access_google_page
+from unittest.mock import patch
+
+
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
+def test_cannot_access_if_only_connection(
+        mock_has_internet: any,
+        mock_valid_url: any
+) -> None:
+    mock_valid_url.return_value = False
+    mock_has_internet.return_value = True
+
+    assert can_access_google_page("http://google.com") == "Not accessible", \
+        "You cannot access page if only 'connection' is True."
+
+    mock_valid_url.return_value = True
+    mock_has_internet.return_value = False
+
+    assert can_access_google_page("http://google.com") == "Not accessible", \
+        "You cannot access page if URL is valid but no connection."
+
+    mock_valid_url.return_value = True
+    mock_has_internet.return_value = True
+
+    assert can_access_google_page("http://google.com") == "Accessible", \
+        "Page should be accessible if both URL and connection are valid."
