9d25b88a8299863fcea34fa51bcc7397e38c08b6
Ekaterina <sudarkova.ekaterina@gmail.com>
2024-08-24 07:24:27 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,36 +2,36 @@ from unittest import mock
 from app.main import can_access_google_page
 
 
-def test_can_access_google_page():
+def test_can_access_google_page() -> None:
     with mock.patch(
-            'main.valid_google_url',
+            "main.valid_google_url",
             return_value=True
-    ) as mock_valid_url,\
+    ),\
         mock.patch(
-            'main.has_internet_connection',
+            "main.has_internet_connection",
             return_value=True
-        ) as mock_internet:
+    ):
         result = "http://google.com"
         assert result == "Not accessible"
 
     with mock.patch(
-            'main.valid_google_url',
+            "main.valid_google_url",
             return_value=False
-                    ) as mock_valid_url, \
-            mock.patch(
-                'main.has_internet_connection',
-                return_value=True
-            ) as mock_internet:
+    ), \
+        mock.patch(
+            "main.has_internet_connection",
+            return_value=True
+    ):
         result = can_access_google_page("http://google.com")
         assert result == "Not accessible"
 
     with mock.patch(
-            'main.valid_google_url',
+            "main.valid_google_url",
             return_value=True
-    ) as mock_valid_url, \
-            mock.patch(
-                'main.has_internet_connection',
-                return_value=False
-            ) as mock_internet:
+    ), \
+        mock.patch(
+            "main.has_internet_connection",
+            return_value=False
+    ):
         result = can_access_google_page("http://google.com")
         assert result == "Not accessible"
