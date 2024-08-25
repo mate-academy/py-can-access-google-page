769b72fe894e61ff5d24095d7eeec750052d2aca
Tkachov Mykhailo <mtkachov435@gmail.com>
2024-08-25 17:55:54 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,32 +1,30 @@
+from unittest import mock
+from typing import Any
+
 from app.main import can_access_google_page
 
-import pytest
+
+def test_can_access_google_page_accessible() -> None:
+    assert can_access_google_page(
+        "https://www.google.com.ua/?hl=ukar"
+    ) == "Accessible"
 
 
-@pytest.mark.parametrize(
-    "url,expected_result",
-    [
-        pytest.param(
-            "http://",
-            "Not accessible",
-            id=""
-        ),
-        pytest.param(
-            "",
-            "Not accessible",
-            id="edge cases"
-        ),
-        pytest.param(
-            "https://www.google.com.ua/?hl=uk",
-            "Accessible",
-            id="edge cases"
-        ),
-        pytest.param(
-            "https://www.googles.com.ua/?hl=uk",
-            "Not accessible",
-            id="edge cases"
-        ),
-    ]
-)
-def test_all(url: str, expected_result: str) -> None:
-    assert can_access_google_page(url) == expected_result
+@mock.patch("app.main.valid_google_url")
+def test_can_access_google_page_not_accessible(
+        mocked_valid_google_url: Any
+) -> None:
+    mocked_valid_google_url.return_value = False
+    assert can_access_google_page(
+        "https://www.goosdofighoidjfggle.com.ua/?hl=uk"
+    ) == "Not accessible"
+
+
+@mock.patch("app.main.has_internet_connection")
+def test_has_internet_connection_not_accessible(
+        mocked_has_internet_connection: Any
+) -> None:
+    mocked_has_internet_connection.return_value = False
+    assert can_access_google_page(
+        "https://www.goosdofighoidkjhihjfggle.com.ua/?hl=uk"
+    ) == "Not accessible"
