4920597562516a03266e3d902135107c8bf1f39c
Ekaterina <sudarkova.ekaterina@gmail.com>
2024-08-27 03:50:46 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -4,33 +4,33 @@ from app.main import can_access_google_page
 
 def test_can_access_google_page() -> None:
     with mock.patch(
-            "main.valid_google_url",
+            "app.main.valid_google_url",
             return_value=True
     ),\
         mock.patch(
-            "main.has_internet_connection",
+            "app.main.has_internet_connection",
             return_value=True
     ):
-        result = "http://google.com"
-        assert result == "Not accessible"
+        result = can_access_google_page("http://google.com")
+        assert result == "Accessible"
 
     with mock.patch(
-            "main.valid_google_url",
+            "app.main.valid_google_url",
             return_value=False
     ), \
         mock.patch(
-            "main.has_internet_connection",
+            "app.main.has_internet_connection",
             return_value=True
     ):
         result = can_access_google_page("http://google.com")
         assert result == "Not accessible"
 
     with mock.patch(
-            "main.valid_google_url",
+            "app.main.valid_google_url",
             return_value=True
     ), \
         mock.patch(
-            "main.has_internet_connection",
+            "app.main.has_internet_connection",
             return_value=False
     ):
         result = can_access_google_page("http://google.com")
