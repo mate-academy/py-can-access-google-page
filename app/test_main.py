6e39292a1cc8af26fce7f08da514382d67e21dcf
Kulib <kulibchenkovalera@gmail.com>
2024-08-29 08:50:50 +0300

    second commit

--- app/test_main.py
+++ app/test_main.py
@@ -1,26 +1,23 @@
-import unittest
 from unittest import mock
 
-from app.main import can_access_google_page
+import pytest
+
+from app.main import (can_access_google_page)
 
 
-@mock.patch("app.main.has_internet_connection", return_value=True)
-@mock.patch("app.main.valid_google_url", return_value=True)
-class TestCanAccessGooglePage(unittest.TestCase):
-
-    def test_can_access_with_correct_url_and_time(self,
-                                                  internet_connection: bool,
-                                                  valid_url: bool) -> None:
-        assert can_access_google_page(valid_url) == "Accessible"
-
-    def test_cannot_access_with_incorrect_time(self,
-                                               internet_connection: bool,
-                                               valid_url: bool) -> None:
-        internet_connection.return_value = False
-        assert can_access_google_page(valid_url) == "Not accessible"
-
-    def test_cannot_access_with_incorrect_url(self,
-                                              internet_connection: bool,
-                                              valid_url: bool) -> None:
-        valid_url.return_value = False
-        assert can_access_google_page(valid_url) == "Not accessible"
+@pytest.mark.parametrize(
+    "internet_connection,valid_url,result",
+    [
+        (True, True, "Accessible"),
+        (False, True, "Not accessible"),
+        (True, False, "Not accessible")
+    ]
+)
+@mock.patch("app.main.has_internet_connection")
+@mock.patch("app.main.valid_google_url")
+def test_access(has_internet_func: bool, valid_google_func: str,
+                internet_connection: bool, valid_url: bool,
+                result: str) -> None:
+    has_internet_func.return_value = internet_connection
+    valid_google_func.return_value = valid_url
+    assert can_access_google_page(valid_google_func) == result
