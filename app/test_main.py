29fa3d693012f5635d155e5249db10e05122bd10
Yaroslav Ivanov <extazzzyprofit@gmail.com>
2024-08-30 11:16:36 +0300

    fixed flake8

--- app/test_main.py
+++ app/test_main.py
@@ -4,21 +4,23 @@ from app.main import can_access_google_page
 
 
 @pytest.fixture()
-def valid_url():
-    with mock.patch("app.main.valid_google_url") as valid_url:
+def valid_url() -> None:
+    with (mock.patch("app.main.valid_google_url")
+          as valid_url):
         yield valid_url
 
 
 @pytest.fixture()
-def internet_connection():
-    with mock.patch("app.main.has_internet_connection") as internet_connection:
+def internet_connection() -> None:
+    with (mock.patch("app.main.has_internet_connection")
+          as internet_connection):
         yield internet_connection
 
 
 url = "https://google.com"
 
 
-def test_with_correct_url_and_internet_connection(internet_connection, valid_url):
+def test_with_correct_url_and_internet_connection(internet_connection, valid_url): # noqa
     valid_url.return_value = True
     internet_connection.return_value = True
     can_access_google_page(url)
