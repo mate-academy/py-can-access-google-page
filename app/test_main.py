a8b9e02d1d29a81e06b32537f379c25169b81fdb
Yaroslav Ivanov <extazzzyprofit@gmail.com>
2024-08-30 16:47:27 +0300

    updated

--- app/test_main.py
+++ app/test_main.py
@@ -17,13 +17,31 @@ def internet_connection() -> None:
         yield internet_connection
 
 
-url = "https://google.com"
+@pytest.mark.parametrize(
+    "url_return_value,connection_return_value,expected",
+    [
+        (True, True, "Accessible"),
+        (False, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (True, False, "Not accessible")
+    ]
+)
+def test_can_access_google_page_functionality(
+        internet_connection: mock,
+        valid_url: mock,
+        url_return_value: bool,
+        connection_return_value: bool,
+        expected: str) -> None:
 
+    url = "https://google.com"
 
-def test_with_correct_url_and_internet_connection(internet_connection, valid_url): # noqa
-    valid_url.return_value = True
-    internet_connection.return_value = True
+    valid_url.return_value = url_return_value
+    internet_connection.return_value = connection_return_value
     can_access_google_page(url)
-    internet_connection.assert_called_once()
-    valid_url.assert_called_once_with(url)
-    assert can_access_google_page(url) == "Accessible"
+    assert can_access_google_page(url) == expected
+
+    if url_return_value == connection_return_value is True:
+        internet_connection.assert_called()
+        valid_url.assert_called_with(url)
+    else:
+        internet_connection.assert_called()
