fe0cf9d188a6cb3f0af17e0e21b88a62f86cc776
Anastasiia Karpenko <karpenko.anastasiya@gmail.com>
2024-09-02 12:19:54 +0300

    code refactor

--- app/test_main.py
+++ app/test_main.py
@@ -1,18 +1,34 @@
+import pytest
 from unittest import mock
 from app.main import can_access_google_page
 
 
-def test_can_access_google_page() -> None:
-    url = "https://example.com"
+@pytest.mark.parametrize(
+    "url, connection, expected",
+    [
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible"),
+    ],
+)
+class TestClass():
+    def test_can_access_google_page(self,
+                                    url: bool,
+                                    connection: bool,
+                                    expected: str) -> None:
+        test_url = "https://example.com"
+        with (
+            mock.patch("app.main.valid_google_url") as valid_url,
+            mock.patch("app.main.has_internet_connection") as has_connection
+        ):
+            valid_url.return_value = url
+            has_connection.return_value = connection
 
-    with (mock.patch("app.main.valid_google_url") as valid_url,
-          mock.patch("app.main.has_internet_connection") as has_connection):
-        valid_url.return_value = True
-        has_connection.return_value = True
+            result = can_access_google_page(test_url)
 
-        result = can_access_google_page(url)
+            has_connection.assert_called_once()
+            if connection:
+                valid_url.assert_called_once_with(test_url)
 
-        valid_url.assert_called_once_with(url)
-        has_connection.assert_called_once()
-
-        assert result == "Accessible"
+            assert result == expected
