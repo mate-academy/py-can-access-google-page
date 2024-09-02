e82b2dce5680ee25fb59c115d5096c7ff1b17543
Anastasiia Karpenko <karpenko.anastasiya@gmail.com>
2024-09-02 10:18:20 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,18 @@
-# write your code here
+from unittest import mock
+from app.main import can_access_google_page
+
+
+def test_can_access_google_page() -> None:
+    url = "https://example.com"
+
+    with (mock.patch("app.main.valid_google_url") as valid_url,
+          mock.patch("app.main.has_internet_connection") as has_connection):
+        valid_url.return_value = True
+        has_connection.return_value = True
+
+        result = can_access_google_page(url)
+
+        valid_url.assert_called_once_with(url)
+        has_connection.assert_called_once()
+
+        assert result == "Accessible"
