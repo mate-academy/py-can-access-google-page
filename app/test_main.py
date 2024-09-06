62c79a61cbf2f2cb14e2bfc5a7f5046d9e2a4bac
pavel_t <{89582095}+{PavelTym}@users.noreply.github.com>
2024-09-06 18:00:46 +0300

    Solution 2.0

--- app/test_main.py
+++ app/test_main.py
@@ -1,28 +1,27 @@
 import pytest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
+
 from app.main import can_access_google_page
 
 
 @pytest.mark.parametrize(
-    "url, internet_connection, valid_url, expected",
+    "internet_connection, valid_url, expected",
     [
-        ("https://www.google.com", True, True, "Accessible"),
-        ("https://www.google.com", True, False, "Not accessible"),
-        ("https://www.google.com", False, True, "Not accessible"),
-        ("https://www.google.com", False, False, "Not accessible")
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible")
     ]
 )
+@patch("app.main.has_internet_connection")
+@patch("app.main.valid_google_url")
 def test_can_access_google_page(
-        url: str,
+        mock_valid_google_url: MagicMock,
+        mock_has_internet_connection: MagicMock,
         internet_connection: bool,
         valid_url: bool,
         expected: str
 ) -> None:
-    with patch(
-            "app.main.has_internet_connection",
-            return_value=internet_connection
-    ):
-        with patch(
-                "app.main.valid_google_url", return_value=valid_url
-        ):
-            assert can_access_google_page(url) == expected
+    mock_has_internet_connection.return_value = internet_connection
+    mock_valid_google_url.return_value = valid_url
+    assert can_access_google_page("https://www.google.com") == expected
