5243b07d61d002fa41d5cfbfbc63e34a62c55b01
Andriy <grushevoyandrey@gmail.com>
2024-09-08 14:21:29 +0300

    fixed error annotations

--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,25 @@
-import unittest.mock
-from unittest.mock import patch
-from unittest import mock
-
 import pytest
-
+from unittest import mock
+from unittest.mock import MagicMock
 from app.main import can_access_google_page
 
 
 @pytest.fixture()
-def mock_url():
+def mock_url() -> MagicMock:
     with mock.patch("app.main.valid_google_url") as url:
         yield url
 
 
 @pytest.fixture()
-def mock_connection():
+def mock_connection() -> MagicMock:
     with mock.patch("app.main.has_internet_connection") as connection:
         yield connection
 
 
-def test_connection(mock_url, mock_connection):
+def test_connection(
+        mock_url: str,
+        mock_connection: bool
+) -> None:
     mock_url.return_value = True
     mock_connection.return_value = True
     result = can_access_google_page("https://www.google.com")
@@ -27,7 +27,10 @@ def test_connection(mock_url, mock_connection):
     assert result == "Accessible"
 
 
-def test_has_not_internet_connection(mock_url, mock_connection):
+def test_has_not_internet_connection(
+        mock_url: str,
+        mock_connection: bool
+) -> None:
     mock_url.return_value = True
     mock_connection.return_value = False
     result = can_access_google_page("https://www.google.com")
@@ -35,7 +38,10 @@ def test_has_not_internet_connection(mock_url, mock_connection):
     assert result == "Not accessible"
 
 
-def test_not_valid_url(mock_url, mock_connection):
+def test_not_valid_url(
+        mock_url: str,
+        mock_connection: bool
+) -> None:
     mock_url.return_value = False
     mock_connection.return_value = True
     result = can_access_google_page("invalid.url")
