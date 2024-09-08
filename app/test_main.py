1674da68ee64584ff799f1468fe7188c4de4eed6
Andriy <grushevoyandrey@gmail.com>
2024-09-08 17:12:49 +0300

    сÑchanged some parameteÐrs

--- app/test_main.py
+++ app/test_main.py
@@ -1,49 +1,29 @@
+from typing import Any
+
 import pytest
 from unittest import mock
-from unittest.mock import MagicMock
 from app.main import can_access_google_page
 
 
-@pytest.fixture()
-def mock_url() -> MagicMock:
-    with mock.patch("app.main.valid_google_url") as url:
-        yield url
+@pytest.mark.parametrize(
+    "test_link,test_connection,expected_result",
+    [
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible")
+    ]
 
-
-@pytest.fixture()
-def mock_connection() -> MagicMock:
-    with mock.patch("app.main.has_internet_connection") as connection:
-        yield connection
-
-
+)
+@mock.patch("app.main.has_internet_connection")
+@mock.patch("app.main.valid_google_url")
 def test_connection(
-        mock_url: str,
-        mock_connection: bool
+        mock_valid_url: Any,
+        mock_connection: Any,
+        test_connection: bool,
+        test_link: str,
+        expected_result: str
 ) -> None:
-    mock_url.return_value = True
-    mock_connection.return_value = True
-    result = can_access_google_page("https://www.google.com")
-
-    assert result == "Accessible"
-
-
-def test_has_not_internet_connection(
-        mock_url: str,
-        mock_connection: bool
-) -> None:
-    mock_url.return_value = True
-    mock_connection.return_value = False
-    result = can_access_google_page("https://www.google.com")
-
-    assert result == "Not accessible"
-
-
-def test_not_valid_url(
-        mock_url: str,
-        mock_connection: bool
-) -> None:
-    mock_url.return_value = False
-    mock_connection.return_value = True
-    result = can_access_google_page("invalid.url")
-
-    assert result == "Not accessible"
+    mock_valid_url.return_value = test_link
+    mock_connection.return_value = test_connection
+    result = can_access_google_page(mock_valid_url)
+    assert result == expected_result
