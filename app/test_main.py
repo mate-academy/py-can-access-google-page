a3a9def05ab03d80ac79ec3cdc80e17df02b9b0c
Andriy <grushevoyandrey@gmail.com>
2024-09-08 21:03:22 +0300

    done

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,3 @@
-from typing import Any
-
 import pytest
 from unittest import mock
 from app.main import can_access_google_page
@@ -18,13 +16,12 @@ from app.main import can_access_google_page
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
 def test_connection(
-        mock_valid_url: Any,
-        mock_connection: Any,
+        mock_valid_url: mock.MagicMock,
+        mock_connection: mock.MagicMock,
         test_connection: bool,
         test_link: str,
         expected_result: str
 ) -> None:
     mock_valid_url.return_value = test_link
     mock_connection.return_value = test_connection
-    result = can_access_google_page(mock_valid_url)
-    assert result == expected_result
+    assert can_access_google_page(mock_valid_url) == expected_result
