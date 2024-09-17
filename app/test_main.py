1e6216b0766001303ce6fd2fc1d104468a90211e
Tetiana Sobko <sobkotiana@gmail.com>
2024-09-17 11:31:39 +0300

    Refactor test_can_access_google_page to use patch decorators

--- app/test_main.py
+++ app/test_main.py
@@ -4,7 +4,8 @@ import pytest
 
 from app.main import can_access_google_page
 
-
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
 @pytest.mark.parametrize(
     "url_check,connection_check,result",
     [
@@ -15,23 +16,19 @@ from app.main import can_access_google_page
     ]
 )
 def test_can_access_google_page(
-        url_check: bool, connection_check: bool, result: str
+        mock_has_internet_connection,
+        mock_valid_google_url,
+        url_check: bool,
+        connection_check: bool,
+        result: str,
 ) -> None:
-    with (
-        mock.patch(
-            "app.main.valid_google_url"
-        ) as mock_valid_google_url,
-        mock.patch(
-            "app.main.has_internet_connection"
-        ) as mock_has_internet_connection
-    ):
-        mock_valid_google_url.return_value = url_check
-        mock_has_internet_connection.return_value = connection_check
+    mock_valid_google_url.return_value = url_check
+    mock_has_internet_connection.return_value = connection_check
 
-        assert can_access_google_page("https://www.youtube.com") == result
+    assert can_access_google_page("https://www.youtube.com") == result
 
-        mock_has_internet_connection.assert_called_once()
-        if connection_check:
-            mock_valid_google_url.assert_called_once_with(
-                "https://www.youtube.com"
-            )
+    mock_has_internet_connection.assert_called_once()
+    if connection_check:
+        mock_valid_google_url.assert_called_once_with(
+            "https://www.youtube.com"
+        )
