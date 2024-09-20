41dbb21bc1b912c17348d1bf1fd50d411b861043
Mykyta Paziuka <nikvitka.it@gmail.com>
2024-09-20 13:36:31 +0300

    fix flake8 problems

--- app/test_main.py
+++ app/test_main.py
@@ -1,12 +1,15 @@
-import pytest
 from unittest import mock
+from typing import Callable
 
 from app.main import can_access_google_page
 
-# write your code here
+
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page(mock_has_internet_connection, mock_valid_google_url):
+def test_can_access_google_page(
+        mock_has_internet_connection: Callable,
+        mock_valid_google_url: Callable
+) -> None:
     mock_valid_google_url.return_value = True
     mock_has_internet_connection.return_value = True
     result = can_access_google_page("https://www.google.com")
