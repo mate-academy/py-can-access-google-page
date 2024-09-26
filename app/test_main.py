9bb98434540d6ba9a673527c60f68c934f35f1c0
Maksym Rozdoba <rozdobamax@gmail.com>
2024-09-26 20:48:51 +0300

    add improvements

--- app/test_main.py
+++ app/test_main.py
@@ -1,15 +1,28 @@
+import pytest
+from typing import Callable
 from unittest import mock
 
 from app.main import can_access_google_page
 
 
-@mock.patch("app.main.valid_google_url")
-def test_valid_url(mocked_valid_url: callable) -> None:
-    can_access_google_page("https://www.google.com/")
-    mocked_valid_url.assert_called_once_with("https://www.google.com/")
-
-
+@pytest.mark.parametrize(
+    "expected_valid_url,expected_connection,expected_result",
+    [
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible")
+    ]
+)
 @mock.patch("app.main.has_internet_connection")
-def test_connection_exists(mocked_connection: callable) -> None:
-    can_access_google_page("https://www.google.com/")
-    mocked_connection.assert_called_once()
+@mock.patch("app.main.valid_google_url")
+def test_valid_url_and_connection_exists(
+        mocked_url: Callable,
+        mocked_connection: Callable,
+        expected_valid_url: bool,
+        expected_connection: bool,
+        expected_result: str
+) -> None:
+    mocked_url.return_value = expected_valid_url
+    mocked_connection.return_value = expected_connection
+    assert can_access_google_page("https://www.google.com/") == expected_result
