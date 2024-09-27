2882ffc24fb732f62445e6e69062c3f9b2a3f1d6
limbxbx <ilya.msd2003@gmail.com>
2024-09-27 11:33:46 +0300

    Fix Flake8 errors

--- app/test_main.py
+++ app/test_main.py
@@ -6,17 +6,17 @@ from app.main import can_access_google_page
 @pytest.mark.parametrize(
     "mock_internet_value, mock_valid_url_value, expected_result, url",
     [
-        (True, True, "Accessible", "https://www.google.com"),  # Accessible page
-        (False, True, "Not accessible", "https://www.google.com"),  # No internet
-        (True, False, "Not accessible", "https://invalid-url.com"),  # Invalid URL
-        (False, False, "Not accessible", "https://invalid-url.com"),  # No internet & invalid URL
+        (True, True, "Accessible", "https://www.google.com"),
+        (False, True, "Not accessible", "https://www.google.com"),
+        (True, False, "Not accessible", "https://invalid-url.com"),
+        (False, False, "Not accessible", "https://invalid-url.com"),
     ]
 )
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
-def test_can_access_google_page(mock_internet, mock_valid_url,
-                                mock_internet_value, mock_valid_url_value,
-                                expected_result, url):
+def test_can_access_google_page(mock_internet: callable, mock_valid_url: callable,
+                                mock_internet_value: callable, mock_valid_url_value: callable,
+                                expected_result: callable, url: callable) -> None:
     mock_internet.return_value = mock_internet_value
     mock_valid_url.return_value = mock_valid_url_value
 
