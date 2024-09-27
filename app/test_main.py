0f640b732233ff646af7f34f0b1bf46e6f0f505b
p1tonidze <p1t0nidze422@gmail.com>
2024-09-27 11:56:40 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,38 @@
-# write your code here
+import unittest
+from unittest import mock
+from app.main import can_access_google_page
+
+
+class TestCanAccessGooglePage(unittest.TestCase):
+
+    @mock.patch("app.main.valid_google_url")
+    @mock.patch("app.main.has_internet_connection")
+    def test_accessible_google_page(
+            self, mock_has_internet_connection: bool,
+            mock_valid_google_url: bool) -> None:
+        mock_valid_google_url.return_value = True
+        mock_has_internet_connection.return_value = True
+        result = can_access_google_page("https://www.google.com")
+        self.assertEqual(result, "Accessible")
+
+    @mock.patch("app.main.valid_google_url")
+    @mock.patch("app.main.has_internet_connection")
+    def test_not_accessible_due_to_invalid_url(
+            self,
+            mock_has_internet_connection: bool,
+            mock_valid_google_url: bool) -> None:
+        mock_valid_google_url.return_value = False
+        mock_has_internet_connection.return_value = True
+        result = can_access_google_page("https://invalid-url.com")
+        self.assertEqual(result, "Not accessible")
+
+    @mock.patch("app.main.valid_google_url")
+    @mock.patch("app.main.has_internet_connection")
+    def test_not_accessible_due_to_no_internet(
+            self,
+            mock_has_internet_connection: bool,
+            mock_valid_google_url: bool) -> None:
+        mock_valid_google_url.return_value = True
+        mock_has_internet_connection.return_value = False
+        result = can_access_google_page("https://www.google.com")
+        self.assertEqual(result, "Not accessible")
