235b99a56a3ba53ffcab7d2d170774c03a87d0eb
dmytro <dmysamo@gmail.com>
2024-10-03 18:27:45 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -4,8 +4,11 @@ from unittest import mock
 from app.main import can_access_google_page
 
 
+GOOGLE_URL = "https://www.google.com/"
+
+
 @mock.patch("app.main.has_internet_connection")
-def test_has_been_has_internet_connection_called(
+def test_internet_connection_called(
         m_has_internet_connection: object
 ) -> None:
     m_has_internet_connection.return_value = True
@@ -16,5 +19,5 @@ def test_has_been_has_internet_connection_called(
 @mock.patch("app.main.valid_google_url")
 def test_has_been_valid_google_url_called(m_valid_google_url: object) -> None:
     m_valid_google_url.return_value = True
-    can_access_google_page("https://www.google.com/")
-    m_valid_google_url.assert_called_once_with("https://www.google.com/")
+    can_access_google_page(GOOGLE_URL)
+    m_valid_google_url.assert_called_once_with(GOOGLE_URL)
