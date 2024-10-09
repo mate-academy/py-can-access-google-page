99064888c433d3924756167495de281b2b0196f9
Vladyslav Kucherenko <vlados.soc@gmail.com>
2024-10-09 16:55:37 +0300

    Fix tests

--- app/test_main.py
+++ app/test_main.py
@@ -3,21 +3,28 @@ from unittest.mock import patch, MagicMock
 
 from app.main import can_access_google_page
 
+URL_GOOGLE = "https://www.google.com/"
+
 
 @pytest.mark.parametrize(
     "url, has_internet_connection, valid_google_url, can_access",
     [
         pytest.param(
-            "https://www.google.com/", True, True, "Accessible",
+            URL_GOOGLE, True, True, "Accessible",
             id="Accessible if have internet connection and valid url"
         ),
         pytest.param(
-            "https://www.google.com", False, True, "Not accessible",
+            URL_GOOGLE, False, True, "Not accessible",
             id="Not accessible if no internet connection"
         ),
         pytest.param(
             "https://www.youtube.com", True, False, "Not accessible",
             id="Not accessible if no valid google url"
+        ),
+        pytest.param(
+            "https://www.youtube.com", False, False, "Not accessible",
+            id="Not accessible if no internet connection"
+               " and no valid google url"
         )
     ]
 )
@@ -35,3 +42,15 @@ def test_can_access_google_page(
     mock_valid_url.return_value = valid_google_url
 
     assert can_access_google_page(url) == can_access
+
+
+@patch("app.main.has_internet_connection")
+@patch("app.main.valid_google_url")
+def test_call_functions(
+        mock_valid_url: MagicMock,
+        mock_has_internet: MagicMock,
+) -> None:
+    can_access_google_page(URL_GOOGLE)
+
+    mock_has_internet.assert_called_once()
+    mock_valid_url.assert_called_once()
