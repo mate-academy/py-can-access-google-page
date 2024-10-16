312036e17d2e81f62594bd53a1ac911cebef0a1e
roman <skyfoxwork@gmail.com>
2024-10-16 18:17:22 +0300

    Solution-py-can-access-google-page v1.0

--- app/test_main.py
+++ app/test_main.py
@@ -1,14 +1,16 @@
 from unittest import mock
+from typing import Any
 
 from app.main import can_access_google_page
 
 
 @mock.patch("app.main.valid_google_url")
-def test_should_valid_google_url(mocked_url):
+def test_valid_google_url_exist(mocked_url: Any) -> None:
     can_access_google_page("http://google.com")
     mocked_url.assert_called_once()
 
+
 @mock.patch("app.main.has_internet_connection")
-def test_should_has_internet_connection(mocked_connection):
+def test_has_internet_connection_exist(mocked_connection: Any) -> None:
     can_access_google_page("http://google.com")
     mocked_connection.assert_called_once()
