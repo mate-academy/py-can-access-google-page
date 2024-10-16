961dccf2c8460a3dc9ca907a4e1f3be90fb05196
Dima Havrylenko <94nj111@gmail.com>
2024-10-16 14:31:57 +0300

    correct annotations

--- app/test_main.py
+++ app/test_main.py
@@ -1,19 +1,18 @@
-from typing import Callable
+from unittest import mock
 
 import pytest
-from unittest import mock
 
 from app.main import can_access_google_page
 
 
 @pytest.fixture()
-def mocked_valid_url() -> None:
+def mocked_valid_url() -> mock.MagicMock:
     with mock.patch("app.main.valid_google_url") as mock_valid_url:
         yield mock_valid_url
 
 
 @pytest.fixture()
-def mocked_has_connection() -> None:
+def mocked_has_internet_connection() -> mock.MagicMock:
     with mock.patch("app.main.has_internet_connection") as mock_has_connection:
         yield mock_has_connection
 
@@ -48,30 +47,31 @@ def mocked_has_connection() -> None:
             "https://www.google.com.ua",
             "Accessible",
             id="Should be ACCESSIBLE when connection and valid url"
-        )]
+        )
+    ]
 )
 def test_can_access_google_page(
-        mocked_valid_url: Callable,
-        mocked_has_connection: Callable,
+        mocked_valid_url: mock.MagicMock,
+        mocked_has_internet_connection: mock.MagicMock,
         validation: bool,
         connection: bool,
         url: str,
         result: str
 ) -> None:
     mocked_valid_url.return_value = validation
-    mocked_has_connection.return_value = connection
+    mocked_has_internet_connection.return_value = connection
     assert can_access_google_page(url) == result
 
 
 def test_valid_google_url_called(
-        mocked_valid_url: Callable
+        mocked_valid_url: mock.MagicMock
 ) -> None:
     can_access_google_page("https://www.google.com.ua")
     mocked_valid_url.assert_called_once(), "Must be called once"
 
 
 def test_has_internet_connection_called(
-        mocked_has_connection: Callable
+        mocked_has_internet_connection: mock.MagicMock
 ) -> None:
     can_access_google_page("https://www.google.com.ua")
-    mocked_has_connection.assert_called_once(), "Must be called once"
+    mocked_has_internet_connection.assert_called_once(), "Must be called once"
