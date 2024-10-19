87827b47326b3a5a84a26dd774958494ea8708b5
Volodymyr Pankiv <ivovan4ik@gmail.com>
2024-10-19 17:25:18 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,31 +1,35 @@
+import pytest
 from unittest import mock
 from app.main import can_access_google_page
 
-
-def test_can_access_google_page() -> None:
-    with mock.patch("app.main.has_internet_connection") as hic:
-        with mock.patch("app.main.valid_google_url") as vgu:
-            hic.return_value = True
-            vgu.return_value = True
-            result = can_access_google_page("http://www.google.com")
-            assert result == "Accessible"
+CONNECTION_URL = "http://www.google.com"
 
 
-def test_cannot_access_if_only_connection() -> None:
-    with mock.patch("app.main.has_internet_connection") as hic:
-        with mock.patch("app.main.valid_google_url") as vgu:
+@pytest.mark.parametrize(
+    "has_internet, is_valid_url, expected_result",
+    [
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible")
+    ]
+)
+def test_can_access_google_page(has_internet: bool,
+                                is_valid_url: bool,
+                                expected_result: str) -> None:
+    with mock.patch("app.main.has_internet_connection") as mock_has_internet:
+        with mock.patch("app.main.valid_google_url") as mock_is_valid_url:
+            mock_has_internet.return_value = has_internet
 
-            hic.return_value = True
-            vgu.return_value = False
-            result = can_access_google_page("http://www.google.com")
-            assert result == "Not accessible"
+            if has_internet:
+                mock_is_valid_url.return_value = is_valid_url
+            else:
+                mock_is_valid_url.return_value = False
 
+            result = can_access_google_page(CONNECTION_URL)
+            assert result == expected_result
 
-def test_cannot_access_if_only_valid_url() -> None:
-    with mock.patch("app.main.has_internet_connection") as hic:
-        with mock.patch("app.main.valid_google_url") as vgu:
-
-            hic.return_value = False
-            vgu.return_value = True
-            result = can_access_google_page("http://www.google.com")
-            assert result == "Not accessible"
+            if has_internet:
+                assert mock_is_valid_url.called
+            else:
+                assert not mock_is_valid_url.called
