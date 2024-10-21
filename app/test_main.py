a628080d63d1e0248af1f3e1d8680b46b6d61ac7
Ivan Fursyk <ivan.fursyk@gmail.com>
2024-10-21 17:41:50 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,30 +1,28 @@
-from pytest_mock import MockerFixture
+from unittest.mock import patch
 from app.main import can_access_google_page
 
 
-def mock_functions(mocker: MockerFixture,
-                   valid_url: bool,
-                   has_internet: bool) -> None:
-    mocker.patch("app.main.valid_google_url", return_value=valid_url)
-    mocker.patch("app.main.has_internet_connection", return_value=has_internet)
+def mock_functions(valid_url: bool, has_internet: bool):
+    patch("app.main.valid_google_url", return_value=valid_url).start()
+    patch("app.main.has_internet_connection", return_value=has_internet).start()
 
 
-def test_accessible_page(mocker: MockerFixture) -> None:
-    mock_functions(mocker, valid_url=True, has_internet=True)
+def test_accessible_page() -> None:
+    mock_functions(valid_url=True, has_internet=True)
 
     result = can_access_google_page("url")
     assert result == "Accessible"
 
 
-def test_not_accessible_due_to_invalid_url(mocker: MockerFixture) -> None:
-    mock_functions(mocker, valid_url=False, has_internet=True)
+def test_not_accessible_due_to_invalid_url() -> None:
+    mock_functions(valid_url=False, has_internet=True)
 
     result = can_access_google_page("url")
     assert result == "Not accessible"
 
 
-def test_not_accessible_due_to_no_internet(mocker: MockerFixture) -> None:
-    mock_functions(mocker, valid_url=True, has_internet=False)
+def test_not_accessible_due_to_no_internet() -> None:
+    mock_functions(valid_url=True, has_internet=False)
 
     result = can_access_google_page("url")
     assert result == "Not accessible"
