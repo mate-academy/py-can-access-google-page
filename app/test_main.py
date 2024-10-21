d8eba35558bb293f9d9768783a396ea3355dfd33
DMYTRO YURCHYSHYN <gost0371337@gmail.com>
2024-10-21 12:39:57 +0300

    Fixing flake8 issues

--- app/test_main.py
+++ app/test_main.py
@@ -2,7 +2,7 @@ from unittest.mock import patch
 from app.main import can_access_google_page
 
 
-def test_can_access_google_page():
+def test_can_access_google_page() -> None:
     with (patch("app.main.has_internet_connection") as mock_connection,
           patch("app.main.valid_google_url") as mock_valid_url):
         url = "https://google.com/"
