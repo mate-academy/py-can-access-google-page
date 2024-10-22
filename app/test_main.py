a8be87d8bafef0b59d51ebe1e99a47d44286f54c
Sonya Savkova <silva.vvss12@gmail.com>
2024-10-22 13:36:11 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,29 +1,26 @@
-from unittest.mock import patch
+from unittest import TestCase
+from unittest.mock import patch, MagicMock
 from app.main import can_access_google_page
-from unittest.mock import MagicMock
 
 
-class TestCanAccessGooglePage:
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
+class TestCanAccessGooglePage(TestCase):
 
-    @patch("app.main.valid_google_url")
-    @patch("app.main.has_internet_connection")
     def test_accessible(
             self,
             mock_has_internet: MagicMock,
             mock_valid_url: MagicMock
     ) -> None:
+        # Arrange
         mock_has_internet.return_value = True
         mock_valid_url.return_value = True
         url = "https://www.google.com"
 
-        # Act
         result = can_access_google_page(url)
 
-        # Assert
-        assert result == "Accessible"
+        self.assertEqual(result, "Accessible")
 
-    @patch("app.main.valid_google_url")
-    @patch("app.main.has_internet_connection")
     def test_not_accessible_due_to_internet(
             self,
             mock_has_internet: MagicMock,
@@ -35,10 +32,8 @@ class TestCanAccessGooglePage:
 
         result = can_access_google_page(url)
 
-        assert result == "Not accessible"
+        self.assertEqual(result, "Not accessible")
 
-    @patch("app.main.valid_google_url")
-    @patch("app.main.has_internet_connection")
     def test_not_accessible_due_to_invalid_url(
             self,
             mock_has_internet: MagicMock,
@@ -50,4 +45,4 @@ class TestCanAccessGooglePage:
 
         result = can_access_google_page(url)
 
-        assert result == "Not accessible"
+        self.assertEqual(result, "Not accessible")
