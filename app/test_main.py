0163de3d3812c118b43fa57a33fbac5b157a182c
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2024-10-29 19:01:18 +0200

    Swapped the order of the decorators

--- app/test_main.py
+++ app/test_main.py
@@ -23,8 +23,8 @@ from app.main import can_access_google_page
         )
     ]
 )
-@patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
+@patch("app.main.valid_google_url")
 def test_can_access_google_page(
         mocked_url: Callable,
         mocked_web_connection: Callable,
