9ff627b84de7e32ba8cf0e52397a0225aba420c8
srx <srx92@protonmail.com>
2024-10-29 19:51:34 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,28 @@
-# write your code here
+from unittest import mock
+from unittest.mock import MagicMock
+
+from app.main import can_access_google_page
+
+
+url = "https://google.com"
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_access_google_page(
+        mocked_url: MagicMock,
+        mocked_connection: MagicMock
+) -> None:
+    possible_variants = [
+        [True, True],
+        [True, False],
+        [False, True],
+        [False, False]
+    ]
+    for variant in possible_variants:
+        mocked_url.return_value, mocked_connection.return_value = variant
+        print(mocked_url.return_value, mocked_connection.return_value)
+        if all([mocked_url.return_value, mocked_connection.return_value]):
+            assert can_access_google_page(url) == "Accessible"
+        else:
+            assert can_access_google_page(url) == "Not accessible"
