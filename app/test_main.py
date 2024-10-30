fe7e7f07d422b16fad5ce74511656340ba863529
srx <srx92@protonmail.com>
2024-10-30 11:16:56 +0200

    Solution fixes 2

--- app/test_main.py
+++ app/test_main.py
@@ -1,27 +1,58 @@
 from unittest import mock
 from unittest.mock import MagicMock
 
+import pytest
+
 from app.main import can_access_google_page
 
 
 url = "https://google.com"
 
-
+@pytest.mark.parametrize(
+    "mocked_url_return_value,"
+    "mocked_connection_return_value,"
+    "expected_result",
+    [
+        pytest.param(
+            True,
+            True,
+            "Accessible",
+            id = "should return `Accessible` "
+                 "if both mocked values are `True`"
+        ),
+        pytest.param(
+            True,
+            False,
+            "Not accessible",
+            id = "should return `Not accessible` "
+                 "if connection mocked value is `False`"
+        ),
+        pytest.param(
+            False,
+            True,
+            "Not accessible",
+            id = "should return `Not accessible` "
+                 "if url mocked value is `False`"
+        ),
+        pytest.param(
+            False,
+            False,
+            "Not accessible",
+            id = "should return `Not accessible` "
+                 "if both mocked values are `False`"
+        )
+    ]
+)
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 def test_can_access_google_page_with_mocked_different_dependencies(
-        mocked_url: MagicMock,
-        mocked_connection: MagicMock
+    mocked_url: MagicMock,
+    mocked_connection: MagicMock,
+    mocked_url_return_value: bool,
+    mocked_connection_return_value: bool,
+    expected_result: str
 ) -> None:
-    possible_variants = [
-        [True, True],
-        [True, False],
-        [False, True],
-        [False, False]
-    ]
-    for variant in possible_variants:
-        mocked_url.return_value, mocked_connection.return_value = variant
-        if all([mocked_url.return_value, mocked_connection.return_value]):
-            assert can_access_google_page(url) == "Accessible"
-        else:
-            assert can_access_google_page(url) == "Not accessible"
+    mocked_url.return_value = mocked_url_return_value
+    mocked_connection.return_value = mocked_connection_return_value
+
+    assert can_access_google_page(url) == expected_result
