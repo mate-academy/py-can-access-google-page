b0e653d7c24ebea5fe11c7b21745e92396575a1c
Andrii Ovod <ovodenok@mail.com>
2024-11-02 10:39:40 +0200

    Solution with addition 4 different test option, after testing flake8.

--- app/test_main.py
+++ app/test_main.py
@@ -1,22 +1,31 @@
 from _pytest.monkeypatch import MonkeyPatch
 import app.main
 
+
 def test_can_access_google_page(monkeypatch: MonkeyPatch) -> None:
     monkeypatch.setattr("app.main.has_internet_connection", lambda: True)
     monkeypatch.setattr("app.main.valid_google_url", lambda url: True)
-    assert app.main.can_access_google_page("https://www.google.com") == "Accessible"
+    assert (app.main.can_access_google_page("https://www.google.com")
+            == "Accessible")
+
 
 def test_cannot_access_if_only_connection(monkeypatch: MonkeyPatch) -> None:
     monkeypatch.setattr("app.main.has_internet_connection", lambda: True)
     monkeypatch.setattr("app.main.valid_google_url", lambda url: False)
-    assert app.main.can_access_google_page("https://www.google.com") == "Not accessible"
+    assert (app.main.can_access_google_page("https://www.google.com")
+            == "Not accessible")
+
 
 def test_cannot_access_if_only_valid_url(monkeypatch: MonkeyPatch) -> None:
     monkeypatch.setattr("app.main.has_internet_connection", lambda: False)
     monkeypatch.setattr("app.main.valid_google_url", lambda url: True)
-    assert app.main.can_access_google_page("https://www.google.com") == "Not accessible"
+    assert (app.main.can_access_google_page("https://www.google.com")
+            == "Not accessible")
 
-def test_cannot_access_if_no_connection_and_invalid_url(monkeypatch: MonkeyPatch) -> None:
+
+def test_cannot_access_if_no_connection_and_invalid_url(monkeypatch
+                                                        : MonkeyPatch) -> None:
     monkeypatch.setattr("app.main.has_internet_connection", lambda: False)
     monkeypatch.setattr("app.main.valid_google_url", lambda url: False)
-    assert app.main.can_access_google_page("https://www.google.com") == "Not accessible"
+    assert (app.main.can_access_google_page("https://www.google.com")
+            == "Not accessible")
