c8d136e2427c6d9d76f16f5128ca3ea3a5c47414
Andrii Ovod <ovodenok@mail.com>
2024-11-02 10:25:39 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,18 @@
-# write your code here
+from _pytest.monkeypatch import MonkeyPatch
+import app.main
+
+
+def test_valid_google_url(monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setattr("app.main.valid_google_url", lambda url: True)
+
+
+def test_has_internet(monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setattr("app.main.has_internet_connection",
+                        lambda url: True)
+
+
+def test_can_access_google_page(monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setattr("app.main.valid_google_url", lambda url: True)
+    monkeypatch.setattr("app.main.has_internet_connection", lambda: True)
+    assert (app.main.can_access_google_page("https://www.google.com")
+            == "Accessible")
