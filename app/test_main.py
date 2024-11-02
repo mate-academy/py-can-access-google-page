a2ba522c65bfa300f0205d12fa51fa38831f7873
Andrii Ovod <ovodenok@mail.com>
2024-11-02 10:35:05 +0200

    Solution with addition 4 different test option

--- app/test_main.py
+++ app/test_main.py
@@ -1,18 +1,22 @@
 from _pytest.monkeypatch import MonkeyPatch
 import app.main
 
-
-def test_valid_google_url(monkeypatch: MonkeyPatch) -> None:
-    monkeypatch.setattr("app.main.valid_google_url", lambda url: True)
-
-
-def test_has_internet(monkeypatch: MonkeyPatch) -> None:
-    monkeypatch.setattr("app.main.has_internet_connection",
-                        lambda url: True)
-
-
 def test_can_access_google_page(monkeypatch: MonkeyPatch) -> None:
-    monkeypatch.setattr("app.main.valid_google_url", lambda url: True)
     monkeypatch.setattr("app.main.has_internet_connection", lambda: True)
-    assert (app.main.can_access_google_page("https://www.google.com")
-            == "Accessible")
+    monkeypatch.setattr("app.main.valid_google_url", lambda url: True)
+    assert app.main.can_access_google_page("https://www.google.com") == "Accessible"
+
+def test_cannot_access_if_only_connection(monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setattr("app.main.has_internet_connection", lambda: True)
+    monkeypatch.setattr("app.main.valid_google_url", lambda url: False)
+    assert app.main.can_access_google_page("https://www.google.com") == "Not accessible"
+
+def test_cannot_access_if_only_valid_url(monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setattr("app.main.has_internet_connection", lambda: False)
+    monkeypatch.setattr("app.main.valid_google_url", lambda url: True)
+    assert app.main.can_access_google_page("https://www.google.com") == "Not accessible"
+
+def test_cannot_access_if_no_connection_and_invalid_url(monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setattr("app.main.has_internet_connection", lambda: False)
+    monkeypatch.setattr("app.main.valid_google_url", lambda url: False)
+    assert app.main.can_access_google_page("https://www.google.com") == "Not accessible"
