45eb32ff9d80d4463d9843560a352fa74280fbe1
yuliiapyrih <yuliia.pyrih.pm.2018@lpnu.ua>
2024-11-03 17:28:54 +0200

    implement py-can-access-google-page

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,38 @@
-# write your code here
+from unittest import mock
+
+import pytest
+
+from app.main import can_access_google_page
+
+
+@pytest.fixture
+def mocked_has_internet():
+    with mock.patch("app.main.has_internet_connection") as mock_test_internet:
+        yield mock_test_internet
+
+
+@pytest.fixture
+def mocked_valid_url():
+    with mock.patch("app.main.valid_google_url") as mock_test_valid_url:
+        yield mock_test_valid_url
+
+
+@pytest.mark.parametrize(
+    "internet_status, url_status, expected_result",
+    [
+        (True, True, "Accessible"),
+        (False, True, "Not accessible"),
+        (True, False, "Not accessible"),
+        (False, False, "Not accessible"),
+    ]
+)
+def test_valid_url_and_connection_exists(
+        mocked_has_internet,
+        mocked_valid_url,
+        internet_status,
+        url_status,
+        expected_result
+):
+    mocked_has_internet.return_value = internet_status
+    mocked_valid_url.return_value = url_status
+    assert can_access_google_page("https://www.google.com") == expected_result
