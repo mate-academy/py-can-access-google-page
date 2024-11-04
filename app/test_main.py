e1ec9ed09a578c177b1ae154737d7007e48f1e21
Ihor Chaikovskyy <Altairerko@gmail.com>
2024-11-04 23:08:01 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -5,24 +5,33 @@ from app.main import can_access_google_page
 
 @mock.patch("app.main.has_internet_connection", return_value=False)
 @mock.patch("app.main.valid_google_url", return_value=True)
-def test_cannot_access_if_only_valid_url(mock_valid_google_url, mock_has_internet_connection):
+def test_cannot_access_if_only_valid_url(
+        mock_valid_google_url: mock.Mock,
+        mock_has_internet_connection: mock.Mock) -> None:
     assert can_access_google_page("https://google.com") == "Not accessible"
 
 
 @mock.patch("app.main.has_internet_connection", return_value=True)
 @mock.patch("app.main.valid_google_url", return_value=False)
-def test_cannot_access_if_only_connection(mock_valid_google_url, mock_has_internet_connection):
+def test_cannot_access_if_only_connection(
+        mock_valid_google_url: mock.Mock,
+        mock_has_internet_connection: mock.Mock) -> None:
     assert can_access_google_page("https://google.com") == "Not accessible"
 
 
 @mock.patch("app.main.has_internet_connection", return_value=True)
 @mock.patch("app.main.valid_google_url", return_value=True)
-def test_can_access_if_both_conditions_true(mock_valid_google_url, mock_has_internet_connection):
+def test_can_access_if_both_conditions_true(
+        mock_valid_google_url: mock.Mock,
+        mock_has_internet_connection: mock.Mock) -> None:
     assert can_access_google_page("https://google.com") == "Accessible"
+
 
 @mock.patch("app.main.has_internet_connection", return_value=False)
 @mock.patch("app.main.valid_google_url", return_value=False)
-def test_cannot_access_if_both_conditions_false(mock_valid_google_url, mock_has_internet_connection):
+def test_cannot_access_if_both_conditions_false(
+        mock_valid_google_url: mock.Mock,
+        mock_has_internet_connection: mock.Mock) -> None:
     assert can_access_google_page("https://google.com") == "Not accessible"
 
 
