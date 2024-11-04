d5a2fbb3fc312f08b548d5e9bbc0cd9d1ba1f9c0
Ihor Chaikovskyy <Altairerko@gmail.com>
2024-11-04 23:03:32 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,30 @@
-# write your code here
+import pytest
+from unittest import mock
+from app.main import can_access_google_page
+
+
+@mock.patch("app.main.has_internet_connection", return_value=False)
+@mock.patch("app.main.valid_google_url", return_value=True)
+def test_cannot_access_if_only_valid_url(mock_valid_google_url, mock_has_internet_connection):
+    assert can_access_google_page("https://google.com") == "Not accessible"
+
+
+@mock.patch("app.main.has_internet_connection", return_value=True)
+@mock.patch("app.main.valid_google_url", return_value=False)
+def test_cannot_access_if_only_connection(mock_valid_google_url, mock_has_internet_connection):
+    assert can_access_google_page("https://google.com") == "Not accessible"
+
+
+@mock.patch("app.main.has_internet_connection", return_value=True)
+@mock.patch("app.main.valid_google_url", return_value=True)
+def test_can_access_if_both_conditions_true(mock_valid_google_url, mock_has_internet_connection):
+    assert can_access_google_page("https://google.com") == "Accessible"
+
+@mock.patch("app.main.has_internet_connection", return_value=False)
+@mock.patch("app.main.valid_google_url", return_value=False)
+def test_cannot_access_if_both_conditions_false(mock_valid_google_url, mock_has_internet_connection):
+    assert can_access_google_page("https://google.com") == "Not accessible"
+
+
+if __name__ == "__main__":
+    pytest.main()
