e6b32c0601eccae6a8747382b4c563d83c653f2c
Question <make772@ukr.net>
2024-12-02 12:21:10 +0200

    result

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,11 @@
-# write your code here
+from app.main import can_access_google_page
+
+
+def test_can_acces_google_page_for_acessible() -> None:
+    url = "https://example.com"
+    assert can_access_google_page(url) == "Accessible"
+
+
+def test_can_acces_google_page_for_not_acessible() -> None:
+    url = "https://example.com/nonexistentpage"
+    assert can_access_google_page(url) == "Not accessible"
