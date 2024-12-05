1691004eb98bdf1f5219eb030851d5dc8fef6e39
Beliar24 <blackdid7@gmail.com>
2024-12-05 23:24:07 +0200

    test

--- app/test_main.py
+++ app/test_main.py
@@ -10,10 +10,10 @@ from app.main import can_access_google_page
     ("https://invalid.url", False, False, "Not accessible"),
 ])
 def test_can_access_google_page(
-        url,
-        valid_url_mock,
-        internet_mock,
-        expected) -> None:
+        url: str,
+        valid_url_mock: bool,
+        internet_mock: bool,
+        expected: str) -> None:
     with patch("app.main.valid_google_url", return_value=valid_url_mock), \
             patch("app.main.has_internet_connection",
                   return_value=internet_mock):
