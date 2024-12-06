b6a98b5b8fd45fa92e709e60078a9d12a04da246
Maksym Kryvsha <KryivshaMaxim@gmail.com>
2024-12-06 12:41:06 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -5,22 +5,22 @@ from app.main import can_access_google_page
 def test_can_access_google_page_accessible() -> None:
     with patch("app.main.has_internet_connection", return_value=True), \
          patch("app.main.valid_google_url", return_value=True):
-        assert can_access_google_page("http://com") == "Accessible"
+        assert can_access_google_page("http://example.com") == "Accessible"
 
 
 def test_can_access_google_page_no_internet() -> None:
     with patch("app.main.has_internet_connection", return_value=False), \
          patch("app.main.valid_google_url", return_value=True):
-        assert can_access_google_page("http://com") == "Not accessible"
+        assert can_access_google_page("http://example.com") == "Not accessible"
 
 
 def test_can_access_google_page_invalid_url() -> None:
     with patch("app.main.has_internet_connection", return_value=True), \
          patch("app.main.valid_google_url", return_value=False):
-        assert can_access_google_page("http://com") == "Not accessible"
+        assert can_access_google_page("http://example.com") == "Not accessible"
 
 
 def test_can_access_google_page_no_internet_and_invalid_url() -> None:
     with patch("app.main.has_internet_connection", return_value=False), \
          patch("app.main.valid_google_url", return_value=False):
-        assert can_access_google_page("http://com") == "Not accessible"
+        assert can_access_google_page("http://example.com") == "Not accessible"
