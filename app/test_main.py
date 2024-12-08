c061f34166e9e92d27bd794bc5dd026e28961e72
Глеб Дубинин <gleb1395@gmail.com>
2024-12-08 19:43:38 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,18 @@
 from unittest import mock
 
+import pytest
+
 from app.main import can_access_google_page
+
+
+@pytest.fixture
+def valid_internet_page() -> str:
+    return "https://www.google.com"
+
+
+@pytest.fixture
+def wrong_internet_page() -> str:
+    return "https://www.Gwwosdfgle.caaam"
 
 
 @mock.patch("app.main.valid_google_url")
@@ -8,27 +20,30 @@ from app.main import can_access_google_page
 # Success case
 def test_can_access_google_page_success(
         mock_has_internet: mock.Mock,
-        mock_valid_google_url: mock.Mock
+        mock_valid_google_url: mock.Mock,
+        valid_internet_page: str
 ) -> None:
     mock_has_internet.return_value = True
     mock_valid_google_url.return_value = True
-    result = can_access_google_page("https://www.google.com")
+    result = can_access_google_page(valid_internet_page)
     assert result == "Accessible"
     mock_has_internet.assert_called_once()
-    mock_valid_google_url.assert_called_once_with("https://www.google.com")
+    mock_valid_google_url.assert_called_once_with(valid_internet_page)
 
 
 @mock.patch("app.main.has_internet_connection")
-def test_failed_internet_connection(mock_has_internet: mock.Mock) -> None:
+def test_failed_internet_connection(mock_has_internet: mock.Mock,
+                                    valid_internet_page: str) -> None:
     mock_has_internet.return_value = False
-    result = can_access_google_page("https://www.google.com")
+    result = can_access_google_page(valid_internet_page)
     assert result == "Not accessible"
     mock_has_internet.assert_called_once()
 
 
 @mock.patch("app.main.valid_google_url")
-def test_not_valid_url(mock_valid_google_url: mock.Mock) -> None:
+def test_not_valid_url(mock_valid_google_url: mock.Mock,
+                       wrong_internet_page: str) -> None:
     mock_valid_google_url.return_value = False
-    result = can_access_google_page("https://ww.gogle.com")
+    result = can_access_google_page(wrong_internet_page)
     assert result == "Not accessible"
-    mock_valid_google_url.assert_called_once_with("https://ww.gogle.com")
+    mock_valid_google_url.assert_called_once_with(wrong_internet_page)
