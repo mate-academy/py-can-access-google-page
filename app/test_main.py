0d2b17f5768fb6f8dbdaa70cac931593ba280794
ComfortCityBud <gleb@gmail.com>
2024-12-09 09:53:39 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -18,32 +18,59 @@ def wrong_internet_page() -> str:
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 # Success case
+@pytest.mark.parametrize(
+    "url, result",
+    [
+        ("https://www.google.com", True),
+        ("https://uk-ua.facebook.com/", True),
+        ("https://github.com/", True)
+    ]
+)
 def test_can_access_google_page_success(
         mock_has_internet: mock.Mock,
         mock_valid_google_url: mock.Mock,
-        valid_internet_page: str
+        url: str,
+        result: bool
 ) -> None:
     mock_has_internet.return_value = True
     mock_valid_google_url.return_value = True
-    result = can_access_google_page(valid_internet_page)
+    result = can_access_google_page(url=url)
     assert result == "Accessible"
     mock_has_internet.assert_called_once()
-    mock_valid_google_url.assert_called_once_with(valid_internet_page)
+    mock_valid_google_url.assert_called_once_with(url)
 
 
 @mock.patch("app.main.has_internet_connection")
+@pytest.mark.parametrize(
+    "url, result",
+    [
+        ("https://www.google.com", True),
+        ("https://uk-ua.facebook.com/", True),
+        ("https://github.com/", True)
+    ]
+)
 def test_failed_internet_connection(mock_has_internet: mock.Mock,
-                                    valid_internet_page: str) -> None:
+                                    url: str,
+                                    result: bool) -> None:
     mock_has_internet.return_value = False
-    result = can_access_google_page(valid_internet_page)
+    result = can_access_google_page(url)
     assert result == "Not accessible"
     mock_has_internet.assert_called_once()
 
 
 @mock.patch("app.main.valid_google_url")
+@pytest.mark.parametrize(
+    "url, result",
+    [
+        ("https://www.Gogle.com", False),
+        ("https://uk-ua.faaaCdebook.com/", False),
+        ("https://GiThsdfub.com/", False)
+    ]
+)
 def test_not_valid_url(mock_valid_google_url: mock.Mock,
-                       wrong_internet_page: str) -> None:
+                       url: str,
+                       result: bool) -> None:
     mock_valid_google_url.return_value = False
-    result = can_access_google_page(wrong_internet_page)
+    result = can_access_google_page(url)
     assert result == "Not accessible"
-    mock_valid_google_url.assert_called_once_with(wrong_internet_page)
+    mock_valid_google_url.assert_called_once_with(url)
