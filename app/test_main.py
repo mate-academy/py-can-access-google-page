5976a17c2d7bc81bf1cdfeb948ce49af3c9580a1
Rozdorozhnii <rozdorozhnii@ukr.net>
2024-12-10 22:09:39 +0200

    Solution_v1.0

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,34 @@
-# write your code here
+from unittest.mock import patch
+
+from app.main import can_access_google_page
+
+
+def test_accesability_with_no_connection() -> None:
+    with patch("app.main.has_internet_connection", return_value=False) as mock_connection:
+        response = can_access_google_page("https://www.google.com")
+
+        mock_connection.assert_called_once()
+
+        assert response == "Not accessible"
+
+
+def test_accesability_without_valid_url_and_good_connection() -> None:
+    with patch("app.main.valid_google_url", return_value=False) as mock_url, \
+            patch("app.main.has_internet_connection", return_value=True) as mock_connection:
+        response = can_access_google_page("https://www.google.com")
+
+        mock_url.assert_called_once_with("https://www.google.com")
+        mock_connection.assert_called_once()
+
+        assert response == "Not accessible"
+
+
+def test_accesability_with_valid_url_and_good_connection() -> None:
+    with patch("app.main.valid_google_url", return_value=True) as mock_url, \
+            patch("app.main.has_internet_connection", return_value=True) as mock_connection:
+        response = can_access_google_page("https://www.google.com")
+
+        mock_url.assert_called_once_with("https://www.google.com")
+        mock_connection.assert_called_once()
+
+        assert response == "Accessible"
