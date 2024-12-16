6a9ebc165be728710a7491b1ed1bb7d7efa398e2
GavrilenkoVladimir <vladimirvgavrilenko@gmail.com>
2024-12-16 10:44:28 +0000

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,15 @@
-# write your code here
+from unittest.mock import patch
+from typing import Callable
+
+from app.main import can_access_google_page
+
+
+@patch("app.main.valid_google_url", reutn_value=True)
+@patch("app.main.has_internet_connection", reutn_value=True)
+def test_valid_url_and_connection_exists(
+    valid: Callable, internet: Callable
+) -> None:
+    url = "https://www.google.com"
+    assert can_access_google_page(url) == "Accessible"
+    valid.assert_called_once_with(url)
+    internet.assert_called_once_with(url)
