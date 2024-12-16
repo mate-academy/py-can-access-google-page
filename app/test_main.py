9575c04de7df590500096749e04b8c15d396fce4
Katarzyna Ochmanska <kat.ochmanska@gmail.com>
2024-12-16 15:17:12 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,7 @@
-# write your code here
+from unittest import mock
+
+
+def test_can_access_google_page() -> None:
+    with mock.patch("requests.get") as mock_get:
+        mock_get.return_value.ok = True
+        mock_get.return_value.status_code = 200
