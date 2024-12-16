d36f9198af313e248917e66f057f925f03b28261
GavrilenkoVladimir <vladimirvgavrilenko@gmail.com>
2024-12-16 11:07:19 +0000

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -7,9 +7,9 @@ from app.main import can_access_google_page
 @patch("app.main.valid_google_url", return_value=True)
 @patch("app.main.has_internet_connection", return_value=True)
 def test_valid_url_and_connection_exists(
-    valid: Callable, internet: Callable
+    internet: Callable, valid: Callable
 ) -> None:
     url = "https://www.google.com"
     assert can_access_google_page(url) == "Accessible"
-    valid.assert_called_once_with(url)
     internet.assert_called_once()
+    valid.assert_called_once_with(url)
