d7f972889a2fd3913f304207627c15685f60d77a
ValeriyIvanovich <98410772+ValeriyIvanovich@users.noreply.github.com>
2024-12-17 23:40:24 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -20,10 +20,12 @@ def test_can_access_google_page(
 				connection: bool,
 				expected: str
 				):
-	with (mock.patch("app.main.valid_google_url") as \
-		  mocked_valid_google_url, \
-		  mock.patch("app.main.has_internet_connection") as \
-		  mocked_has_internet_connection):
+	with (
+	       mock.patch("app.main.valid_google_url") as
+	       mocked_valid_google_url,
+	       mock.patch("app.main.has_internet_connection") as
+	       mocked_has_internet_connection
+	      ):
 		mocked_valid_google_url.return_value = valid_url
 		mocked_has_internet_connection.return_value = connection
 		response = can_access_google_page("https://www.google.com")
