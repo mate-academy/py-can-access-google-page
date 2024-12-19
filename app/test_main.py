d44ae37142e0e87f742575ab063a27eb78111a3f
Max <makimov2004@gmail.com>
2024-12-19 16:08:08 +0200

    Fix

--- app/test_main.py
+++ app/test_main.py
@@ -2,9 +2,11 @@ import pytest
 from unittest import mock
 from app.main import can_access_google_page
 
-def  test_can_access_google_page():
-    with mock.patch("app.main.valid_google_url") as mock_valid_google_url, \
-            mock.patch("app.main.has_internet_connection") as mock_has_internet_connection:
+
+def test_can_access_google_page() -> None:
+    with (mock.patch("app.main.valid_google_url") as mock_valid_google_url,
+            mock.patch("app.main.has_internet_connection")
+            as mock_has_internet_connection):
 
         mock_valid_google_url.return_value = True
         mock_has_internet_connection.return_value = True
@@ -21,6 +23,7 @@ def  test_can_access_google_page():
         mock_valid_google_url.return_value = False
         mock_has_internet_connection.return_value = False
         assert can_access_google_page("https://google.com") == "Not accessible"
+
 
 if __name__ == "__name__":
     pytest.main()
