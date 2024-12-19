b1a49dbef34087660aeebf2b9aa9fcae2d94076d
evgeniy <evgeniy.chernyak1982@gmail.com>
2024-12-19 15:51:27 +0200

    Solution1

--- app/test_main.py
+++ app/test_main.py
@@ -1,19 +1,26 @@
-import datetime
-import requests
+import pytest
+from app.main import can_access_google_page
+from unittest import mock
 
 
-def valid_google_url(url: str) -> bool:
-    response = requests.get(url)
-    return True if response.status_code == 200 else False
-
-
-def has_internet_connection() -> bool:
-    current_time = datetime.datetime.now()
-    return True if current_time.hour in range(6, 23) else False
-
-
-def can_access_google_page(url: str) -> str:
-    if has_internet_connection() and valid_google_url(url):
-        return "Accessible"
-    else:
-        return "Not accessible"
+@pytest.mark.parametrize(
+    "valid_google_url,has_internet_connection,goal",
+    [
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible"),
+    ],
+)
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_can_access_google_page(
+    mock_valid_google_url: mock,
+    mock_has_internet_connection: mock,
+    valid_google_url: bool,
+    has_internet_connection: bool,
+    goal: str,
+) -> None:
+    mock_valid_google_url.return_value = valid_google_url
+    mock_has_internet_connection.return_value = has_internet_connection
+    assert can_access_google_page("https://google.com") == goal
