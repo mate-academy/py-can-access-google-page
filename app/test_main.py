4741d5f2e206237704880a7ff3aa9245e78a9216
evgeniy <evgeniy.chernyak1982@gmail.com>
2024-12-19 15:39:35 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,19 @@
-# write your code here
+import datetime
+import requests
+
+
+def valid_google_url(url: str) -> bool:
+    response = requests.get(url)
+    return True if response.status_code == 200 else False
+
+
+def has_internet_connection() -> bool:
+    current_time = datetime.datetime.now()
+    return True if current_time.hour in range(6, 23) else False
+
+
+def can_access_google_page(url: str) -> str:
+    if has_internet_connection() and valid_google_url(url):
+        return "Accessible"
+    else:
+        return "Not accessible"
