71a13ceaa9bb10b9c11912f56fd865dfd6778892
Arsenii Aristov <arseniyaristov07@gmail.com>
2024-12-20 18:12:57 +0200

    Solution_Access

--- app/test_main.py
+++ app/test_main.py
@@ -4,7 +4,6 @@ from app.main import can_access_google_page
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-
 def test_for_access_to_page(mock_has_internet_connection: mock.MagicMock,
                             mock_valid_google_url: mock.MagicMock) -> None:
     mock_has_internet_connection.return_value = True
