71aaeb99377a322d0a1f60c5b591274390696136
10kkyvl <ztkkre@gmail.com>
2024-12-20 16:34:52 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,15 +1,25 @@
-from unittest import mock
-
+import pytest
+from unittest.mock import patch
 from app.main import can_access_google_page
 
 
-@mock.patch("app.main.valid_google_url")
-def test_valid_google_url(mocked_url: mock.MagicMock) -> None:
-    can_access_google_page("https://google.com")
-    mocked_url.assert_called_once_with("https://google.com")
-
-
-@mock.patch("app.main.has_internet_connection")
-def test_has_internet_connection(mocked_internet: mock.MagicMock) -> None:
-    can_access_google_page("https://google.com")
-    mocked_internet.assert_called_once()
+@pytest.mark.parametrize(
+    "valid_url, has_connection, expected",
+    [
+        (True, True, "Accessible"),
+        (False, True, "Not accessible"),
+        (True, False, "Not accessible"),
+        (False, False, "Not accessible"),
+    ],
+)
+def test_can_access_google_page(
+        valid_url: bool,
+        has_connection: bool,
+        expected: str
+) -> None:
+    with patch("app.main.valid_google_url", return_value=valid_url):
+        with patch("app.main.has_internet_connection",
+                   return_value=has_connection):
+            result = can_access_google_page("https://test-url.com")
+            assert result == expected, (f"Expected '{expected}' but"
+                                        f" got '{result}'.")
