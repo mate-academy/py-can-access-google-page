f48865e8c35258bb51ab5ef61e4af3956fc17edc
Arsenii Aristov <arseniyaristov07@gmail.com>
2024-12-20 18:10:38 +0200

    Solution_Access

--- app/test_main.py
+++ app/test_main.py
@@ -19,5 +19,5 @@ def test_for_access_to_page(mock_has_internet_connection: mock.MagicMock,
 
     mock_has_internet_connection.return_value = True
     mock_valid_google_url.return_value = False
-    assert(can_access_google_page("https://www.google.com")
+    assert (can_access_google_page("https://www.google.com")
            == "Not accessible")
