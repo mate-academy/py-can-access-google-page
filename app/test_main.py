1d6ee1071e597dfe5692a6fc0eef360524f39d97
Arsenii Aristov <arseniyaristov07@gmail.com>
2024-12-20 18:02:02 +0200

    Solution_Access

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,21 @@
-# write your code here
+from unittest import mock
+from app.main import can_access_google_page
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+
+def test_for_access_to_page(
+                            mock_has_internet_connection: mock.MagicMock,
+                            mock_valid_google_url: mock.MagicMock
+                            ) -> None:
+    mock_has_internet_connection.return_value = True
+    mock_valid_google_url.return_value = True
+
+    assert (can_access_google_page("https://www.google.com") == "Accessible")
+
+    mock_has_internet_connection.return_value = False
+    assert (can_access_google_page("https://www.google.com") == "Not accessible")
+
+    mock_has_internet_connection.return_value = True
+    mock_valid_google_url.return_value = False
+    assert(can_access_google_page("https://www.google.com") == "Not accessible")
