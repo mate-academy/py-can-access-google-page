bdcc78e6f1884efa321953befda7a06ddb60b21d
Admin <mr.artem450@gmail.com>
2024-12-22 18:59:27 +0200

    Task Completed

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,19 @@
+from unittest import mock
 
+from app.main import can_access_google_page
+
+
+@mock.patch("app.main.valid_google_url")
+def test_can_access_google_page_with_valid_url(
+        mocked_url: mock.MagicMock
+) -> None:
+    can_access_google_page("https://www.google.com/")
+    mocked_url.assert_called_once_with("https://www.google.com/")
+
+
+@mock.patch("app.main.has_internet_connection")
+def test_can_access_google_page_with_internet_connection(
+        mocked_internet: mock.MagicMock
+) -> None:
+    can_access_google_page("https://www.google.com/")
+    mocked_internet.assert_called_once()
