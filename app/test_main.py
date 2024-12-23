c0591e91a29fac304f53d8eee3c6a97c9ee19bfc
Ihor Halytskiy <galytskiyihor@gmail.com>
2024-12-23 12:50:54 +0200

    try fex errors

--- app/test_main.py
+++ app/test_main.py
@@ -1,11 +1,15 @@
-import pytest
+from typing import Any
 from unittest import mock
 from app.main import can_access_google_page
 
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page(mocked_has_internet_connection, mocked_valid_google_url):
+def test_can_access_google_page(
+    mocked_has_internet_connection: Any,
+    mocked_valid_google_url: Any
+) -> None:
+    # Define test cases
     test_cases = [
         (False, False, "Not accessible"),
         (False, True, "Not accessible"),
@@ -20,9 +24,6 @@ def test_can_access_google_page(mocked_has_internet_connection, mocked_valid_goo
         result = can_access_google_page("https://google.com")
 
         assert result == expected_result, (
-            f"Failed for has_internet={has_internet} and is_valid_url={is_valid_url}"
+            f"Failed for has_internet={has_internet} "
+            f"and is_valid_url={is_valid_url}"
         )
-
-
-if __name__ == "__main__":
-    pytest.main()
