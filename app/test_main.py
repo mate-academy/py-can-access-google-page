f5d88e0659aa994f3f57112adf9c9f3bbf745f7b
Bohdan Khudiakov <b.khudiakov@gmail.com>
2024-12-23 07:50:25 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,24 @@
-# write your code here
+import pytest
+from app.main import can_access_google_page
+
+
+@pytest.mark.parametrize(
+    "mock_connection_result, mock_url_result, expected",
+    [
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible"),
+    ],
+)
+def test_can_access_google_page(
+    mocker, mock_connection_result: bool, mock_url_result: bool, expected: str
+) -> None:
+    mock_has_internet = mocker.patch("app.main.has_internet_connection")
+    mock_has_internet.return_value = mock_connection_result
+
+    mock_requests_get = mocker.patch("app.main.requests.get")
+    mock_requests_get.return_value.status_code = 200 if mock_url_result else 404
+
+    result = can_access_google_page("http://www.google.com")
+    assert result == expected
