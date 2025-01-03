3f48a7f0cddb5b8f99ec43d3dcc57abb1c36bbc2
Andrii Fedchuk <andriyfedchuk2006@gmail.com>
2025-01-03 13:10:24 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,10 +1,14 @@
 from unittest import mock
-
+from unittest.mock import MagicMock
 from app.main import can_access_google_page
+
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_function_has_been_called(mocked_valid_google_url, mocked_has_internet_connection):
+def test_function_has_been_called(
+    mocked_valid_google_url: MagicMock,
+    mocked_has_internet_connection: MagicMock,
+) -> None:
     can_access_google_page("url")
     mocked_valid_google_url.assert_called_once()
 
