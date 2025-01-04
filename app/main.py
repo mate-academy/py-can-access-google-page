bdb8faa8bc1111e516153fb1fa80397ae340ce81
Mazurkav <aleshkamazurkevich@gmail.com>
2025-01-04 17:20:44 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,15 +1,18 @@
 import requests
 from datetime import datetime
 
+
 def valid_google_url(url: str) -> bool:
     valid_urls = ["https://www.google.com", "http://www.google.com"]
     return url in valid_urls
+
 
 def has_internet_connection() -> bool:
     current_time = datetime.now().time()
     start_time = datetime.strptime("06:00:00", "%H:%M:%S").time()
     end_time = datetime.strptime("22:59:59", "%H:%M:%S").time()
     return start_time <= current_time <= end_time
+
 
 def can_access_google_page(url: str) -> str:
     if valid_google_url(url) and has_internet_connection():
