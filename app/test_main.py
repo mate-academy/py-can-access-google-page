69a4aec8ab7190edd74a6db5c91d33a2513093eb
shafran <shafran.andriy@gmail.com>
2025-01-04 21:52:34 +0200

    Created all tests

--- app/test_main.py
+++ app/test_main.py
@@ -6,18 +6,19 @@ from unittest import mock
 from app.main import valid_google_url, has_internet_connection
 
 
+@mock.patch("requests.get")
 def test_valid_google_url(url: str) -> None:
-    requests.get = mock.MagicMock()
     response = requests.get(url)
     assert response.status_code == 200
 
 
+@mock.patch("datetime.datetime.now")
 def test_has_internet_connection() -> None:
-    datetime.datetime.now = mock.MagicMock()
     current_time = datetime.datetime.now()
     assert current_time.hour in range(6, 23)
 
 
+@mock.patch("valid_google_url, has_internet_connection")
 def test_can_access_google_page(url: str) -> None:
     if has_internet_connection() and valid_google_url(url):
         assert "Accessible", "Not accessible"
