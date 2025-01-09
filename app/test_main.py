369de9e4a3a6866fdd72e47c015763dd65229dc5
Taras <t.kudinov90@gmail.com>
2025-01-09 15:22:25 +0200

    Created test functions for main function tests.

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,37 @@
-# write your code here
+from app.main import can_access_google_page
+from unittest.mock import patch
+import pytest
+
+
+@pytest.mark.parametrize("url, result", [
+    ("https://mate.academy", "Accessible"),
+    ("https://www.ukr.net/", "Accessible"),
+    ("https://www.youtube.com/", "Accessible"),
+    ("https://www.bing.com/", "Accessible")
+])
+def test_can_access_google_page(url: str, result: str) -> None:
+    with patch("app.main.has_internet_connection") as mock_connect, \
+         patch("app.main.valid_google_url") as mock_valid:
+        mock_connect.return_value = True
+        mock_valid.return_value = True
+        assert can_access_google_page(url) == result
+
+
+@pytest.mark.parametrize("url, result", [
+    ("https://malte.academy", "Not accessible"),
+    ("https://www.youtulbe.com/", "Not accessible"),
+])
+def test_can_access_not_google_page(url: str, result: str) -> None:
+    with patch("app.main.has_internet_connection") as mock_connect, \
+         patch("app.main.valid_google_url") as mock_valid:
+        mock_connect.return_value = True
+        mock_valid.return_value = False
+        assert can_access_google_page(url) == result
+
+
+def test_can_access_google_page_time() -> None:
+    with patch("app.main.has_internet_connection") as mock_connect, \
+         patch("app.main.valid_google_url") as mock_valid:
+        mock_connect.return_value = False
+        mock_valid.return_value = True
+        assert can_access_google_page("google.com") == "Not accessible"
