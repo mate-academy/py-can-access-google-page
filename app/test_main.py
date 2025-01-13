266954dee1c0088ed05c188cc1f2590efa1e5c01
misha-cw <bigun.mykhaylo@gmail.com>
2025-01-13 23:04:16 +0200

    change test logic

--- app/test_main.py
+++ app/test_main.py
@@ -1,14 +1,32 @@
+import pytest
+
 from unittest import mock
 from app.main import can_access_google_page
 
 
-@mock.patch("app.main.has_internet_connection")
-def test_valid_connection(mocked_connection: mock) -> None:
-    can_access_google_page("https://www.python.org")
+@pytest.fixture()
+def mocked_connection() -> mock:
+    with (mock.patch("app.main.has_internet_connection")
+          as moke_connection):
+        yield moke_connection
+
+
+@pytest.fixture()
+def mocked_url() -> mock:
+    with mock.patch("app.main.valid_google_url") as moke_test_url:
+        yield moke_test_url
+
+
+def test_valid_url_and_connection_exists(
+        mocked_url: mock,
+        mocked_connection: mock
+) -> None:
+    mocked_url.return_value = True
+    mocked_connection.return_value = True
+
+    result = can_access_google_page("https://www.python.org")
+
+    mocked_url.assert_called_once_with("https://www.python.org")
     mocked_connection.assert_called_once()
 
-
-@mock.patch("app.main.valid_google_url")
-def test_valid_url(mocked_url: mock) -> None:
-    can_access_google_page("https://www.python.org")
-    mocked_url.assert_called_once_with("https://www.python.org")
+    assert result == "Accessible"
