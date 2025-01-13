462e1aa3424e1cdca61dd4b18b9cd00d66f213ea
misha-cw <bigun.mykhaylo@gmail.com>
2025-01-13 23:07:19 +0200

    change name variable

--- app/test_main.py
+++ app/test_main.py
@@ -7,14 +7,14 @@ from app.main import can_access_google_page
 @pytest.fixture()
 def mocked_connection() -> mock:
     with (mock.patch("app.main.has_internet_connection")
-          as moke_connection):
-        yield moke_connection
+          as mock_connection):
+        yield mock_connection
 
 
 @pytest.fixture()
 def mocked_url() -> mock:
-    with mock.patch("app.main.valid_google_url") as moke_test_url:
-        yield moke_test_url
+    with mock.patch("app.main.valid_google_url") as mock_test_url:
+        yield mock_test_url
 
 
 def test_valid_url_and_connection_exists(
