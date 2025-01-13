120b420a9f94cade8dbec21ece0685b1d151fd60
misha-cw <bigun.mykhaylo@gmail.com>
2025-01-13 22:20:13 +0200

    fix some mistakes

--- app/test_main.py
+++ app/test_main.py
@@ -1,15 +1,14 @@
-import pytest
-
 from unittest import mock
 from app.main import can_access_google_page
 
 
 @mock.patch("app.main.has_internet_connection")
-def test_valid_connection(mocked_connection):
+def test_valid_connection(mocked_connection: mock) -> None:
     can_access_google_page("https://www.python.org")
     mocked_connection.assert_called_once()
 
+
 @mock.patch("app.main.valid_google_url")
-def test_valid_url(mocked_url):
+def test_valid_url(mocked_url: mock) -> None:
     can_access_google_page("https://www.python.org")
     mocked_url.assert_called_once_with("https://www.python.org")
