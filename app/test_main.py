71ae797bfc0ee2bcc6b3e755a117f79fd9440ebb
Fedir <fedor.agureev@gmail.com>
2025-01-29 22:03:01 +0200

    fixes

--- app/test_main.py
+++ app/test_main.py
@@ -9,7 +9,7 @@ from app.main import can_access_google_page
     (False, True, "Not accessible"),
     (False, False, "Not accessible"),
 ])
-def test_can_access_google_page(mock_valid_url, mock_internet, expected):
+def test_can_access_google_page(mock_valid_url: bool, mock_internet: bool, expected: str) -> None:
     with patch("app.main.valid_google_url", return_value=mock_valid_url), \
          patch("app.main.has_internet_connection", return_value=mock_internet):
         assert can_access_google_page("https://www.google.com") == expected
