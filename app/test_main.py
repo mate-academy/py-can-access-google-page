5819dd19439e9d330018cf559cbef743f22e9d58
Mykyta <mitina1900@gmail.com>
2025-02-02 17:54:22 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -4,13 +4,13 @@ from app.main import can_access_google_page
 
 
 @pytest.fixture()
-def mock_valid_google_url():
+def mock_valid_google_url() -> mock.Mock:
     with mock.patch("app.main.valid_google_url") as mock_url:
         yield mock_url
 
 
 @pytest.fixture()
-def mock_has_internet_connection():
+def mock_has_internet_connection() -> mock.Mock:
     with mock.patch("app.main.has_internet_connection") as mock_internet:
         yield mock_internet
 
@@ -24,13 +24,13 @@ def mock_has_internet_connection():
         (False, False, "Not accessible"),
     ]
 )
-def test_can_access_google_page(mock_valid_google_url,
-                            mock_has_internet_connection,
-                            mock_url_return: bool,
-                            mock_connection_return: bool,
+def test_can_access_google_page(mock_valid_google_url: mock.Mock,
+                            mock_has_internet_connection: mock.Mock,
+                            mock_url: bool,
+                            mock_internet: bool,
                             expected: str
                             ) -> None:
-    mock_valid_google_url.return_value = mock_url_return
-    mock_has_internet_connection.return_value = mock_connection_return
+    mock_valid_google_url.return_value = mock_url
+    mock_has_internet_connection.return_value = mock_internet
     result = can_access_google_page("https://www.google.com")
     assert result == expected
