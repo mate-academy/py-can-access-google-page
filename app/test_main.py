c4d3b9c332c4f36c6405a011a494c6a552f53851
Bartolomeu <bartoszokroj@wp.pl>
2025-02-11 00:27:47 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -13,12 +13,12 @@ from .main import can_access_google_page
         (True, True, "Accessible"),
     ]
 )
-def test_if_can_acces_google_page_return_correct_str(
+def test_if_can_access_google_page_return_correct_str(
         has_internet_connection_result,
         valid_url_result,
         result) -> None:
     with patch("app.main.has_internet_connection",
-               return_value = has_internet_connection_result), \
-         patch("app.main.valid_google_url",
-              return_value = valid_url_result):
+               return_value=has_internet_connection_result), \
+        patch("app.main.valid_google_url",
+              return_value=valid_url_result):
         assert can_access_google_page("url.example") == result
