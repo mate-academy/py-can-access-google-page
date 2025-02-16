7d234a7f51bf82535fe90260b49d86a5ce26aefb
Marek Smolana <msmolana@gmail.com>
2025-02-16 11:49:55 +0100

    draft

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,34 @@
-# write your code here
+from unittest import mock
+import pytest
+from app.main import can_access_google_page
+
+
+@pytest.fixture
+def mock_has_con(result: bool) -> bool:
+    with mock.patch("app.main.has_internet_connection") as mock_has_con:
+        return result
+
+
+@pytest.fixture
+def mock_valid_url(result: bool) -> bool:
+    with mock.patch("app.main.valid_google_url") as mock_valid_url:
+        return result
+
+
+@pytest.mark.parametrize(
+    "valid_url,has_con,result",
+    [
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+    ],
+)
+def test_can_access_google_page(
+        mock_has_con,
+        mock_valid_url,
+        valid_url: bool,
+        has_con: bool,
+        result: str):
+    mock_has_con.return_value = has_con
+    mock_valid_url.return_value = valid_url
+    assert can_access_google_page("") == result
