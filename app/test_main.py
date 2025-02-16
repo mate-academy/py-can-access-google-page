7e3b3e6c201d87ee9531effef080548931ba512c
Marek Smolana <msmolana@gmail.com>
2025-02-16 12:03:42 +0100

    flake8 errors

--- app/test_main.py
+++ app/test_main.py
@@ -4,13 +4,13 @@ from app.main import can_access_google_page
 
 
 @pytest.fixture
-def mock_has_con():
+def mock_has_con() -> bool:
     with mock.patch("app.main.has_internet_connection") as mock_has_con:
         yield mock_has_con
 
 
 @pytest.fixture
-def mock_valid_url():
+def mock_valid_url() -> bool:
     with mock.patch("app.main.valid_google_url") as mock_valid_url:
         yield mock_valid_url
 
@@ -23,12 +23,12 @@ def mock_valid_url():
         (False, True, "Not accessible"),
     ],
 )
-def test_can_access_google_page(
-        mock_has_con,
-        mock_valid_url,
+def test_valid_url_and_connection_exists(
+        mock_has_con: callable,
+        mock_valid_url: callable,
         valid_url: bool,
         has_con: bool,
-        result: str):
+        result: str) -> None:
     mock_has_con.return_value = has_con
     mock_valid_url.return_value = valid_url
     assert can_access_google_page("") == result
