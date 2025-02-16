940e5f992de0763fee2b1f61e377e9da50c2b0dc
Your Name <nadeemrazani@gmail.com>
2025-02-16 22:04:39 +0800

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -3,27 +3,35 @@ from app.main import can_access_google_page
 
 
 def test_can_access_google_page_accessible() -> None:
-    with patch("app.main.valid_google_url", return_value=True), \
-         patch("app.main.has_internet_connection", return_value=True):
-        assert can_access_google_page("https://www.google.com") == "Accessible"
+    with patch("app.main.valid_google_url",
+               return_value=True), \
+         patch("app.main.has_internet_connection",
+               return_value=True):
+        assert can_access_google_page(
+            "https://www.google.com") == "Accessible"
 
 
 def test_can_access_google_page_invalid_url() -> None:
-    with patch("app.main.valid_google_url", return_value=False), \
-         patch("app.main.has_internet_connection", return_value=True):
+    with patch("app.main.valid_google_url",
+               return_value=False), \
+         patch("app.main.has_internet_connection",
+               return_value=True):
         assert can_access_google_page(
             "https://invalid-url.com") == "Not accessible"
 
 
 def test_can_access_google_page_no_internet() -> None:
-    with (((patch("app.main.valid_google_url", return_value=True),
-         patch("app.main.has_internet_connection", return_value=False)))):
+    with patch("app.main.valid_google_url",
+               return_value=True), \
+         patch("app.main.has_internet_connection",
+               return_value=False):
         assert can_access_google_page(
             "https://www.google.com") == "Not accessible"
 
 
 def test_can_access_google_page_invalid_url_no_internet() -> None:
-    with patch("app.main.valid_google_url", return_value=False), \
+    with patch("app.main.valid_google_url",
+               return_value=False), \
          patch("app.main.has_internet_connection",
                return_value=False):
         assert can_access_google_page(
