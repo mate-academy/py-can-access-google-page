28cdf364dae7cbe72ca38116d0ef71eeb312e08d
Johnny <ivensmk@gmail.com>
2025-02-18 18:17:04 +0200

    'Solution.v2_add_description'

--- app/test_main.py
+++ app/test_main.py
@@ -6,10 +6,23 @@ from app.main import can_access_google_page
 @pytest.mark.parametrize(
     "mock_valid_url,mock_internet_connection, expected_result",
     [
-        (True, True, "Accessible"),
-        (True, False, "Not accessible"),
-        (False, True, "Not accessible"),
-        (False, False, "Not accessible"),
+        pytest.param(
+            True, True, "Accessible",
+            id="should return 'Accessible' when both function 'True'"
+        ),
+        pytest.param(
+            True, False, "Not accessible",
+            id="should return 'Not accessible'"
+               " when mock_internet_connection 'False'"
+        ),
+        pytest.param(
+            False, True, "Not accessible",
+            id="should return 'Not accessible' when mock_valid_url 'False'"
+        ),
+        pytest.param(
+            False, False, "Not accessible",
+            id="should return ' Not accessible' when both function 'False'"
+        )
     ]
 )
 def test_can_access_google_page(
