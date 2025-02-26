7bf988db03801f4a9124388160169526098c7608
Vitaliy Tetianichenko <vitaliy2work@icloud.com>
2025-02-26 17:11:12 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -4,26 +4,34 @@ from app.main import can_access_google_page
 
 
 class GoogleTest(unittest.TestCase):
-    def test_can_access_google_page_two_true(self):
-        with mock.patch("app.main.has_internet_connection", return_value=True), \
-             mock.patch("app.main.valid_google_url", return_value=True):
+    def test_valid_url_and_connection_exists(self) -> None:
+        with (
+            mock.patch("app.main.has_internet_connection", return_value=True),
+            mock.patch("app.main.valid_google_url", return_value=True)
+        ):
             result = can_access_google_page("test_site")
             self.assertEqual(result, "Accessible")
 
-    def test_can_access_google_page_two_false(self):
-        with mock.patch("app.main.has_internet_connection", return_value=False), \
-             mock.patch("app.main.valid_google_url", return_value=False):
+    def test_invalid_url_and_no_connection(self) -> None:
+        with (
+            mock.patch("app.main.has_internet_connection", return_value=False),
+            mock.patch("app.main.valid_google_url", return_value=False)
+        ):
             result = can_access_google_page("test_site")
             self.assertEqual(result, "Not accessible")
 
-    def test_can_access_google_page_true_false(self):
-        with mock.patch("app.main.has_internet_connection", return_value=True), \
-             mock.patch("app.main.valid_google_url", return_value=False):
+    def test_valid_url_but_no_connection(self) -> None:
+        with (
+            mock.patch("app.main.has_internet_connection", return_value=True),
+            mock.patch("app.main.valid_google_url", return_value=False)
+        ):
             result = can_access_google_page("test_site")
             self.assertEqual(result, "Not accessible")
 
-    def test_can_access_google_page_false_true(self):
-        with mock.patch("app.main.has_internet_connection", return_value=False), \
-                mock.patch("app.main.valid_google_url", return_value=True):
+    def test_invalid_url_but_connection_exists(self) -> None:
+        with (
+            mock.patch("app.main.has_internet_connection", return_value=False),
+            mock.patch("app.main.valid_google_url", return_value=True)
+        ):
             result = can_access_google_page("test_site")
             self.assertEqual(result, "Not accessible")
