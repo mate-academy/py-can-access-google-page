a96156dcbc025caeefc278d3ccee2835c0048a0f
Vitaliy Tetianichenko <vitaliy2work@icloud.com>
2025-02-26 16:55:46 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,29 @@
-# write your code here
+import unittest
+from unittest import mock
+from app.main import can_access_google_page
+
+
+class GoogleTest(unittest.TestCase):
+    def test_can_access_google_page_two_true(self):
+        with mock.patch("app.main.has_internet_connection", return_value=True), \
+             mock.patch("app.main.valid_google_url", return_value=True):
+            result = can_access_google_page("test_site")
+            self.assertEqual(result, "Accessible")
+
+    def test_can_access_google_page_two_false(self):
+        with mock.patch("app.main.has_internet_connection", return_value=False), \
+             mock.patch("app.main.valid_google_url", return_value=False):
+            result = can_access_google_page("test_site")
+            self.assertEqual(result, "Not accessible")
+
+    def test_can_access_google_page_true_false(self):
+        with mock.patch("app.main.has_internet_connection", return_value=True), \
+             mock.patch("app.main.valid_google_url", return_value=False):
+            result = can_access_google_page("test_site")
+            self.assertEqual(result, "Not accessible")
+
+    def test_can_access_google_page_false_true(self):
+        with mock.patch("app.main.has_internet_connection", return_value=False), \
+                mock.patch("app.main.valid_google_url", return_value=True):
+            result = can_access_google_page("test_site")
+            self.assertEqual(result, "Not accessible")
