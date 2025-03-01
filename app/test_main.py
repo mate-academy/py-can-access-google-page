b9f868a71e3469f5985be447920782be314bdf95
Taras Shevchyk <tarashevchik27@gmail.com>
2025-03-01 14:53:09 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,13 @@
-# write your code here
+
+def test_can_access_google_page(monkeypatch: any) -> None:
+    def mock_valid_google_url(url: str) -> bool:
+        return True
+
+    def mock_has_internet_connection() -> bool:
+        return True
+
+    monkeypatch.setattr("app.main.valid_google_url", mock_valid_google_url)
+    monkeypatch.setattr(
+        "app.main.has_internet_connection",
+        mock_has_internet_connection
+    )
