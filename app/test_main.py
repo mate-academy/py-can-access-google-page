8018eddbc100733e3f400e743f4c8fb8fcafdbd8
Dolhyi Valerii <v.dolhyi@gmail.com>
2025-03-06 23:27:19 +0100

    Solution_2

--- app/test_main.py
+++ app/test_main.py
@@ -7,8 +7,8 @@ from unittest import mock
 @mock.patch("app.main.valid_google_url")
 def test_access_google_with_success(mocked_connection: MagicMock,
                                     mocked_url: MagicMock) -> None:
-    mocked_connection.value = True
-    mocked_url.value = True
+    mocked_connection.return_value = True
+    mocked_url.return_value = True
 
     assert can_access_google_page("http://ggo") == "Accessible"
 
@@ -17,8 +17,8 @@ def test_access_google_with_success(mocked_connection: MagicMock,
 @mock.patch("app.main.valid_google_url")
 def test_access_google_with_wrong_url(mocked_connection: MagicMock,
                                       mocked_url: MagicMock) -> None:
-    mocked_connection.value = True
-    mocked_url.value = False
+    mocked_connection.return_value = True
+    mocked_url.return_value = False
 
     assert can_access_google_page("http://ggo") == "Not accessible"
 
@@ -27,8 +27,8 @@ def test_access_google_with_wrong_url(mocked_connection: MagicMock,
 @mock.patch("app.main.valid_google_url")
 def test_access_google_with_wrong_time(mocked_connection: MagicMock,
                                        mocked_url: MagicMock) -> None:
-    mocked_connection.value = False
-    mocked_url.value = True
+    mocked_connection.return_value = False
+    mocked_url.return_value = True
 
     assert can_access_google_page("http://ggo") == "Not accessible"
 
@@ -37,7 +37,7 @@ def test_access_google_with_wrong_time(mocked_connection: MagicMock,
 @mock.patch("app.main.valid_google_url")
 def test_access_google_with_all_wrong(mocked_connection: MagicMock,
                                       mocked_url: MagicMock) -> None:
-    mocked_connection.value = False
-    mocked_url.value = False
+    mocked_connection.return_value = False
+    mocked_url.return_value = False
 
     assert can_access_google_page("http://ggo") == "Not accessible"
