bdb4fcd1f369c4e78d31e22a3233d39c5355cfc5
Polina Haltseva <polinayut@gmail.com>
2025-03-07 19:05:30 +0200

    'Solution1'

--- app/test_main.py
+++ app/test_main.py
@@ -1,8 +1,6 @@
 import pytest
-from app.main import (can_access_google_page,
-                      has_internet_connection,
-                      valid_google_url
-                     )
+from typing import Any
+from app.main import can_access_google_page
 
 
 @pytest.mark.parametrize(
@@ -15,13 +13,15 @@ from app.main import (can_access_google_page,
     ]
 )
 def test_should_return_if_can_access_to_page(
-        monkeypatch,
+        monkeypatch: Any,
         valid_url: bool,
         internet_connection: bool,
         result: str,
 ) -> None:
     monkeypatch.setattr("app.main.valid_google_url", lambda url: valid_url)
-    monkeypatch.setattr("app.main.has_internet_connection", lambda: internet_connection)
+    monkeypatch.setattr("app.main.has_internet_connection",
+                        lambda: internet_connection
+                        )
     assert (
         can_access_google_page("https://www.google.com") == result
     )
