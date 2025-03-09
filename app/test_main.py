3217bfeeba909027b0ee78d62072fae6e9d46861
Maksym Omelkov <omelkov85@gmail.com>
2025-03-09 16:25:21 +0200

    Can access Google page (1.0)

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,35 @@
-# write your code here
+from app import main
+
+
+def test_accessible_if_hic_and_vgu(monkeypatch):
+    def mock_h_i_c() -> bool:
+        return True
+
+    def mock_v_g_u(url: str) -> bool:
+        return True
+
+    monkeypatch.setattr(main, "valid_google_url", mock_v_g_u)
+    monkeypatch.setattr(main, "has_internet_connection", mock_h_i_c)
+    assert main.can_access_google_page("zdfbzdf") == "Accessible"
+
+def test_accessible_if_only_hic(monkeypatch):
+    def mock_h_i_c() -> bool:
+        return True
+
+    def mock_v_g_u(url: str) -> bool:
+        return False
+
+    monkeypatch.setattr(main, "valid_google_url", mock_v_g_u)
+    monkeypatch.setattr(main, "has_internet_connection", mock_h_i_c)
+    assert main.can_access_google_page("zdfbzdf") == "Not accessible"
+
+def test_accessible_if_only_vgu(monkeypatch):
+    def mock_h_i_c() -> bool:
+        return False
+
+    def mock_v_g_u(url: str) -> bool:
+        return True
+
+    monkeypatch.setattr(main, "valid_google_url", mock_v_g_u)
+    monkeypatch.setattr(main, "has_internet_connection", mock_h_i_c)
+    assert main.can_access_google_page("zdfbzdf") == "Not accessible"
