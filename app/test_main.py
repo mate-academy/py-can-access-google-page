1b3a7a88414a9232076179804553e367745f3140
Volodymyr Vitsenko <your@mail.com>
2025-03-13 11:18:31 +0100

    test_if_can_acess_google

--- app/test_main.py
+++ app/test_main.py
@@ -7,7 +7,7 @@ from app.main import can_access_google_page
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page(valid_url: Any, has_connection: Any) -> None:
+def test_can_access_google_page(has_connection: Any, valid_url: Any) -> None:
     url = "https://www.google.com/"
     assert can_access_google_page(url) == "Accessible"
     has_connection.assert_called_once()
