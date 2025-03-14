2694bd509d8c68a0c1c44c8ca1655468b44e6fa6
ViktorBl734 <harbourprogress@gmail.com>
2025-03-14 19:00:08 +0200

    Can access Google page

--- app/test_main.py
+++ app/test_main.py
@@ -5,18 +5,22 @@ from app.main import (
 
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
-def test_valid_url_and_connection_exists(mocked_url: str, mocked_connection: str) -> None:
+def test_valid_url_and_connection_exists(
+        mocked_url: str, mocked_connection: str) -> None:
     mocked_connection.return_value = True
     mocked_url.return_value = True
-    assert can_access_google_page("https://www.google.com") == "Accessible"
+    assert (can_access_google_page
+            ("https://www.google.com") == "Accessible")
 
     mocked_connection.return_value = False
     mocked_url.return_value = True
-    assert can_access_google_page("https://www.google.com") == "Not accessible"
+    assert can_access_google_page(
+        "https://www.google.com") == "Not accessible"
 
     mocked_connection.return_value = True
     mocked_url.return_value = False
-    assert can_access_google_page("https://www.google.com") == "Not accessible"
+    assert can_access_google_page(
+        "https://www.google.com") == "Not accessible"
 
     mocked_connection.return_value = False
     mocked_url.return_value = False
