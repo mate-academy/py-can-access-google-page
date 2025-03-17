6d9c7ca896b331183d37dd028f35ad9622e7116a
Oleh Kozdryn <kozdryn.oleg@gmail.com>
2025-03-17 19:32:00 +0200

    solution v2

--- app/test_main.py
+++ app/test_main.py
@@ -2,14 +2,14 @@ from unittest.mock import patch
 from app.main import can_access_google_page
 
 
-def test_accessible() -> None:
+def test_can_access_google_page_when_internet_and_url_valid() -> None:
     with patch("app.main.has_internet_connection", return_value=True), patch(
         "app.main.valid_google_url", return_value=True
     ):
         assert can_access_google_page("https://www.google.com") == "Accessible"
 
 
-def test_not_accessible_due_to_no_internet() -> None:
+def test_can_not_access_google_page_when_no_internet_connection() -> None:
     with patch("app.main.has_internet_connection", return_value=False), patch(
         "app.main.valid_google_url", return_value=True
     ):
@@ -19,7 +19,7 @@ def test_not_accessible_due_to_no_internet() -> None:
         )
 
 
-def test_not_accessible_due_to_invalid_url() -> None:
+def test_can_not_access_google_page_when_no_valid_google_url() -> None:
     with patch("app.main.has_internet_connection", return_value=True), patch(
         "app.main.valid_google_url", return_value=False
     ):
@@ -29,7 +29,7 @@ def test_not_accessible_due_to_invalid_url() -> None:
         )
 
 
-def test_not_accessible_due_to_both_false() -> None:
+def test_can_not_access_when_no_internet_and_no_valid_google_url() -> None:
     with patch("app.main.has_internet_connection", return_value=False), patch(
         "app.main.valid_google_url", return_value=False
     ):
