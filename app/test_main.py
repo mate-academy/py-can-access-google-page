2542e6d958b520dcc77bf797a0246e260cddf623
Roman Kostiv <rkostiv@gmail.com>
2025-03-17 19:57:55 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,23 +1,31 @@
 import unittest
 from unittest.mock import patch, Mock
-
 import random
-from app.main import can_access_google_page
+import datetime
+from app.main import can_access_google_page, valid_google_url, has_internet_connection
 
 
-@patch("requests.get")
-@patch("datetime.datetime.now")
-def test_can_access_google_page(mock_get: any, mock_time: any) -> None:
-    mock_time.return_value.hour = random.randint(6, 23)
+class TestUrlAccess(unittest.TestCase):
+    @patch("requests.get")
+    @patch("datetime.datetime.now")
+    @patch("app.main.valid_google_url", "app.main.has_internet_connection")
+    def test_valid_url_and_connection_time(self, mock_internet: any,
+                                           mock_url: any,
+                                           mock_get: any,
+                                           mock_time: any) -> None:
 
-    mock_response = Mock()
-    mock_response.status_code = 200
-    mock_get.return_value = mock_response
+        mock_time.now.return_value = datetime.datetime(2024, 1, 1, random.randint(6, 23))
 
-    result = can_access_google_page("google.com")
+        mock_url.return_value = True
+        mock_internet.return_value = True
 
-    assert result is True
+        mock_response = Mock()
+        mock_response.status_code = 200
+        mock_get.return_value = mock_response
 
+        result = can_access_google_page(mock_url(), mock_internet())
+
+        self.assertTrue(result)
 
 if __name__ == "__main__":
     unittest.main()
