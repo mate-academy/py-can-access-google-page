5e2b87defb6aaa6a7d855d8e49ff33f9b921f967
Zahar <galushkozahar>
2025-03-17 17:14:29 +0200

    Develop

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,27 @@
-# write your code here
+import pytest
+
+from unittest.mock import patch
+
+from app.main import can_access_google_page
+
+
+@pytest.mark.parametrize(
+    "internet_status, url_status, extended",
+    [
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible"),
+
+    ]
+)
+def test_can_access_google_page(
+        internet_status: bool,
+        url_status: bool,
+        extended: bool
+) -> None:
+    with patch(
+            "app.main.has_internet_connection",
+            return_value=internet_status), \
+            patch("app.main.valid_google_url", return_value=url_status):
+        assert can_access_google_page("https://www.google.com") == extended
