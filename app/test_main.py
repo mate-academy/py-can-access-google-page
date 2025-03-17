d0714eb8465e551a84807472d7689eb92eb6b558
Roman Kostiv <rkostiv@gmail.com>
2025-03-17 19:59:26 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,7 +2,7 @@ import unittest
 from unittest.mock import patch, Mock
 import random
 import datetime
-from app.main import can_access_google_page, valid_google_url, has_internet_connection
+from app.main import can_access_google_page
 
 
 class TestUrlAccess(unittest.TestCase):
@@ -14,7 +14,10 @@ class TestUrlAccess(unittest.TestCase):
                                            mock_get: any,
                                            mock_time: any) -> None:
 
-        mock_time.now.return_value = datetime.datetime(2024, 1, 1, random.randint(6, 23))
+        mock_time.now.return_value = datetime.datetime(
+            2024, 1, 1,
+            random.randint(6, 23)
+        )
 
         mock_url.return_value = True
         mock_internet.return_value = True
@@ -26,6 +29,7 @@ class TestUrlAccess(unittest.TestCase):
         result = can_access_google_page(mock_url(), mock_internet())
 
         self.assertTrue(result)
+
 
 if __name__ == "__main__":
     unittest.main()
