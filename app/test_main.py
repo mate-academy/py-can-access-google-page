d12ff9a5e6fd09dbf99392684943cf26790f093f
Max <smolinskijmaksim1@gmail.com>
2025-03-20 17:01:09 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -12,12 +12,14 @@ from unittest.mock import patch
         ("invalid_url", False, False, "Not accessible"),
     ]
 )
-def test_can_access_google_page(
+def test_valid_url_and_connection_exists(
         url: str,
         valid_url: bool,
         has_connection: bool,
         expected: str
 ) -> None:
     with patch("app.main.valid_google_url", return_value=valid_url), \
-         patch("app.main.has_internet_connection", return_value=has_connection):
+         patch(
+             "app.main.has_internet_connection",
+               return_value=has_connection):
         assert can_access_google_page(url) == expected
