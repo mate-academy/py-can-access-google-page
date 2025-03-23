823328fbc36388d4b2aa2ba4fc401eaca306a801
Anton Vynychenko <kinid@ukr.net>
2025-03-23 15:47:04 +0200

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -2,28 +2,28 @@ from unittest import mock
 from app import main
 
 
-def test_can_access_google_page_accessible() -> None:
+def test_can_access_google_page_when_internet_and_url_are_valid() -> None:
     with mock.patch("app.main.has_internet_connection", return_value=True), \
          mock.patch("app.main.valid_google_url", return_value=True):
         result = main.can_access_google_page("https://www.google.com")
         assert result == "Accessible"
 
 
-def test_can_access_google_page_not_accessible_no_internet() -> None:
+def test_cannot_access_google_page_when_no_internet() -> None:
     with mock.patch("app.main.has_internet_connection", return_value=False), \
          mock.patch("app.main.valid_google_url", return_value=True):
         result = main.can_access_google_page("https://www.google.com")
         assert result == "Not accessible"
 
 
-def test_can_access_google_page_not_accessible_invalid_url() -> None:
+def test_cannot_access_google_page_when_url_is_invalid() -> None:
     with mock.patch("app.main.has_internet_connection", return_value=True), \
          mock.patch("app.main.valid_google_url", return_value=False):
         result = main.can_access_google_page("https://fake-google.com")
         assert result == "Not accessible"
 
 
-def test_can_access_google_page_not_accessible_no_internet_and_invalid_url() \
+def test_cannot_access_google_page_when_no_internet_and_url_is_invalid() \
         -> None:
     with mock.patch("app.main.has_internet_connection", return_value=False), \
          mock.patch("app.main.valid_google_url", return_value=False):
