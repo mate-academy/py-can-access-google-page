34405baa89d8ca2b9e8b14ae3a8fc8229609976a
Ostap <ostap.burij@gmail.com>
2025-03-24 22:26:38 +0200

    implemented return value

--- app/test_main.py
+++ app/test_main.py
@@ -1,16 +1,25 @@
 from unittest import mock
-
+import pytest
 from app.main import can_access_google_page
 
 
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
+@pytest.mark.parametrize(
+    "url, valid_google_url, has_internet_connection, result",
+    [
+        ("https://www.google.com", True, True, "Accessible"),
+        ("https://www.google.com", True, False, "Not accessible"),
+        ("fake_url", False, True, "Not accessible"),
+        ("fake_url", False, False, "Not accessible"),
+    ]
+)
 def test_valid_url_and_connection_exists(
-        mocked_valid_google_url: mock.MagicMock,
-        mocked_has_internet_connection: mock.MagicMock
+    url: str,
+    valid_google_url: str,
+    has_internet_connection: bool,
+    result: str
 ) -> None:
-    can_access_google_page(mocked_valid_google_url)
-    mocked_valid_google_url.assert_called_once()
-    mocked_has_internet_connection.assert_called_once()
-    assert mocked_valid_google_url
-    assert mocked_has_internet_connection
+    with mock.patch("app.main.valid_google_url",
+                    return_value=valid_google_url), \
+            mock.patch("app.main.has_internet_connection",
+                       return_value=has_internet_connection):
+        assert can_access_google_page(url) == result
