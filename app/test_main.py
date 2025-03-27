d904c4dcb91f6a650b75e79bd5a347dc2ef8b4ca
Your Name <stas101203rakitin@gmail.com>
2025-03-27 14:59:45 +0200

    Tests written

--- app/test_main.py
+++ app/test_main.py
@@ -4,8 +4,8 @@ from app.main import can_access_google_page
 
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
-def test_accessible(mock_has_internet: MagicMock,
-                    mock_valid_url: MagicMock) -> None:
+def test_can_access_google_when_internet_and_url_are_valid(
+        mock_has_internet: MagicMock, mock_valid_url: MagicMock) -> None:
     mock_valid_url.return_value = True
     mock_has_internet.return_value = True
     assert can_access_google_page("https://www.google.com") == "Accessible"
@@ -13,8 +13,8 @@ def test_accessible(mock_has_internet: MagicMock,
 
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
-def test_not_accessible_invalid_url(mock_has_internet: MagicMock,
-                                    mock_valid_url: MagicMock) -> None:
+def test_cannot_access_google_when_url_is_invalid(
+        mock_has_internet: MagicMock, mock_valid_url: MagicMock) -> None:
     mock_valid_url.return_value = False
     mock_has_internet.return_value = True
     assert (can_access_google_page("https://www.fakegoogle.com")
@@ -23,8 +23,8 @@ def test_not_accessible_invalid_url(mock_has_internet: MagicMock,
 
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
-def test_not_accessible_no_internet(mock_has_internet: MagicMock,
-                                    mock_valid_url: MagicMock) -> None:
+def test_cannot_access_google_when_no_internet(
+        mock_has_internet: MagicMock, mock_valid_url: MagicMock) -> None:
     mock_valid_url.return_value = True
     mock_has_internet.return_value = False
     assert can_access_google_page("https://www.google.com") == "Not accessible"
@@ -32,9 +32,8 @@ def test_not_accessible_no_internet(mock_has_internet: MagicMock,
 
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
-def test_not_accessible_invalid_url_no_internet(mock_has_internet: MagicMock,
-                                                mock_valid_url: MagicMock)\
-        -> None:
+def test_cannot_access_google_when_url_is_invalid_and_no_internet(
+        mock_has_internet: MagicMock, mock_valid_url: MagicMock) -> None:
     mock_valid_url.return_value = False
     mock_has_internet.return_value = False
     assert (can_access_google_page("https://www.fakegoogle.com")
