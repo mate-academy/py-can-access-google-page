321acd1258e4ad8765195b94d470af5d1127d593
oleanto <olegantonenko100@gmail.com>
2025-04-02 22:49:04 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,21 @@
-# write your code here
+import pytest
+from unittest.mock import patch
+from app.main import can_access_google_page
+
+
+@pytest.mark.parametrize(
+    "valid_url,internet_connection,expected", [
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible")
+    ]
+)
+def test_can_access_google_page(valid_url: bool,
+                                internet_connection: bool,
+                                expected: str
+                                ) -> None:
+    with patch("app.main.valid_google_url", return_value=valid_url), \
+            patch("app.main.has_internet_connection", return_value=internet_connection):
+        test_result: str = can_access_google_page("https://www.google.com/")
+        assert test_result == expected
