c6e958c1aa50ee687257f958371ab084a6f6e1d3
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-04-03 14:23:00 +0300

    test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -2,18 +2,15 @@ from app.main import can_access_google_page
 from _pytest.monkeypatch import MonkeyPatch
 import pytest
 
-
 @pytest.mark.parametrize(
-    "internet,"
-    "google,"
-    "result",
+    "internet, google, result",
     [
         (True, True, "Accessible"),
         (True, False, "Not accessible"),
         (False, True, "Not accessible"),
     ]
 )
-def test_function_acces(
+def test_can_access_google_page_with_internet_and_valid_url(
         monkeypatch: MonkeyPatch,
         internet: bool,
         google: bool,
