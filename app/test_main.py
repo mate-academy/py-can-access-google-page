6924fca4d81632cdde7bc9f0803da9bf11db725a
DawidWiater <wiater344@gmail.com>
2025-04-07 12:45:58 +0200

    Solution1

--- app/test_main.py
+++ app/test_main.py
@@ -5,20 +5,20 @@ from app.main import can_access_google_page
 
 @mock.patch("app.main.valid_google_url", return_value=True)
 @mock.patch("app.main.has_internet_connection", return_value=True)
-def test_can_access_google_page(_: bool, ___: str) -> None:
+def test_valid_url_and_connection_exists(_: bool, ___: str) -> None:
     result = can_access_google_page("https://www.google.com")
     assert result == "Accessible"
 
 
 @mock.patch("app.main.valid_google_url", return_value=True)
 @mock.patch("app.main.has_internet_connection", return_value=False)
-def test_no_internet(_: bool, ___: str) -> None:
+def test_valid_url_but_no_connection(_: bool, ___: str) -> None:
     result = can_access_google_page("http://www.google.com")
     assert result == "Not accessible"
 
 
 @mock.patch("app.main.valid_google_url", return_value=False)
 @mock.patch("app.main.has_internet_connection", return_value=True)
-def test_cant_access_google_page(_: bool, ___: str) -> None:
+def test_connection_exists_but_invalid_url(_: bool, ___: str) -> None:
     result = can_access_google_page("http://www.google.com")
     assert result == "Not accessible"
