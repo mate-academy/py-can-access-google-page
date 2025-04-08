7e49b0d8ce7efbef06a418716b793351f8898f1c
Bohdan Mykyta <justkexitmail@gmail.com>
2025-04-08 19:11:03 +0300

    Solution [fix return annotation]

--- app/test_main.py
+++ app/test_main.py
@@ -3,18 +3,20 @@ from app.main import can_access_google_page
 
 url = "http://google.com/"
 
+
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
-def test_if_has_internet_false(mock_valid_url, mock_has_internet):
+def test_if_has_internet_false(mock_valid_url, mock_has_internet) -> None:
     mock_valid_url.return_value = False
     mock_has_internet.return_value = True
 
     result = can_access_google_page(url)
     assert result == "Not accessible"
 
+
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
-def test_if_not_valid_url(mock_valid_url, mock_has_internet):
+def test_if_not_valid_url(mock_valid_url, mock_has_internet) -> None:
     mock_valid_url.return_value = True
     mock_has_internet.return_value = False
 
