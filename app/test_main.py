f4aea0f3bf0e36bf2adbcf684657ed94f9f9380c
Bohdan Mykyta <justkexitmail@gmail.com>
2025-04-08 19:13:37 +0300

    Solution fix retard annotation

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,6 @@
 from unittest import mock
+from unittest.mock import MagicMock
+
 from app.main import can_access_google_page
 
 url = "http://google.com/"
@@ -6,7 +8,7 @@ url = "http://google.com/"
 
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
-def test_if_has_internet_false(mock_valid_url, mock_has_internet) -> None:
+def test_if_has_internet_false(mock_valid_url: MagicMock, mock_has_internet: MagicMock) -> None:
     mock_valid_url.return_value = False
     mock_has_internet.return_value = True
 
@@ -16,7 +18,7 @@ def test_if_has_internet_false(mock_valid_url, mock_has_internet) -> None:
 
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
-def test_if_not_valid_url(mock_valid_url, mock_has_internet) -> None:
+def test_if_not_valid_url(mock_valid_url: MagicMock, mock_has_internet: MagicMock) -> None:
     mock_valid_url.return_value = True
     mock_has_internet.return_value = False
 
