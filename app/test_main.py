92b7a93a4f966169602a9333651c2a762f7ad595
Alexandr <oleksandrglushen2005@gmail.com>
2025-04-14 22:41:19 +0300

    fixed code

--- app/test_main.py
+++ app/test_main.py
@@ -14,8 +14,8 @@ def test_valid_url_and_connection_exists(mock_valid_url: bool,
 
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
-def test_can_access_google_page(mock_valid_url: bool,
-                                mock_internet: bool) -> None:
+def test_can_access_google_page_valid_input(mock_valid_url: bool,
+                                            mock_internet: bool) -> None:
     mock_internet.return_value = True
     mock_valid_url.return_value = True
 
