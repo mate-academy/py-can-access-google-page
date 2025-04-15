75e5a258b68012184fad87de74b35474af11bbe8
Michal-Naron <michanaron@gmail.com>
2025-04-15 13:32:05 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,14 +1,26 @@
-from app.main import valid_google_url, has_internet_connection, \
-    can_access_google_page
+import pytest
 from unittest import mock
+from app.main import can_access_google_page
 
-def test_can_access_google_page():
+@pytest.mark.parametrize("valid_google, has_internet, result", [
+    (True, True, "Accessible"),
+    (True, False, "Not accessible"),
+    (False, True, "Not accessible"),
+    (False, False, "Not accessible"),
+])
+def test_can_access_google_page_should_return_true(
+    valid_google: bool,
+    has_internet: bool,
+    result: str
+) -> None:
     with mock.patch("valid_google_url") as mock_valid_google_url, \
-            mock.patch("has_internet_connection") as \
-                    mock_has_internet_connection:
+         mock.patch("has_internet_connection") as mock_has_internet_connection:
 
-        can_access_google_page("https://www.google.pl/?hl=pl")
-        mock_valid_google_url.assert_called_once_with("https://www.google.pl/?hl=pl")
+        mock_valid_google_url.return_value = valid_google
+        mock_has_internet_connection.return_value = has_internet
+
+        url = "https://www.google.pl/?hl=pl"
+        assert can_access_google_page(url) == result
+
+        mock_valid_google_url.assert_called_once_with(url)
         mock_has_internet_connection.assert_called_once()
-
-
