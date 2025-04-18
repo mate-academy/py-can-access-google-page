d799e91f845fcaa5bd45c3d7d3de29581978b93d
RomanSapunGit <sapunroma23@gmail.com>
2025-04-18 20:49:19 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,16 @@
-# write your code here
+from unittest import mock, TestCase
+from unittest.mock import MagicMock
+from app.main import can_access_google_page
+
+
+class TestThirdFunction(TestCase):
+    @mock.patch("app.main.has_internet_connection")
+    @mock.patch("app.main.valid_google_url")
+    def test_access_google_page_should_return_accessible(
+            self,
+            mock_valid_google_url: MagicMock,
+            mock_has_internet_connection: MagicMock
+    ) -> None:
+        assert can_access_google_page("Hello") == "Accessible"
+        mock_valid_google_url.assert_called_once()
+        mock_has_internet_connection.assert_called_once()
