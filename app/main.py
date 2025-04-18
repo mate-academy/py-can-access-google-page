c54d76962f6f85038065fdaa249c1bac5142110a
nikita-plotnytskyi <nikita.plotnitckiy@gmail.com>
2025-04-18 21:42:39 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -11,7 +11,6 @@ def has_internet_connection() -> bool:
     current_time = datetime.datetime.now()
     return True if current_time.hour in range(6, 23) else False
 
-
 def can_access_google_page(url: str) -> str:
     if has_internet_connection() and valid_google_url(url):
         return "Accessible"
--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,25 @@
-# write your code here
+import pytest
+
+from app.main import can_access_google_page
+from unittest import mock
+
+
+@pytest.mark.parametrize(
+    "bool_result_url, bool_result_has_internet, result",
+     [
+         (True, True, "Accessible"),
+         (False, True, "Not accessible"),
+         (True, False, "Not accessible"),
+         (False, False, "Not accessible")
+     ],
+     ids=[
+        "internet=True & url_accessible=True → Accessible",
+        "internet=True & url_accessible=False → Not accessible",
+        "internet=False & url_accessible=True → Not accessible",
+        "internet=False & url_accessible=False → Not accessible"
+     ]
+)
+def test_can_access_google_page(bool_result_url, bool_result_has_internet, result):
+    with (mock.patch("app.main.valid_google_url", return_value=bool_result_url),
+          mock.patch("app.main.has_internet_connection", return_value=bool_result_has_internet)):
+        assert can_access_google_page("url") == result
