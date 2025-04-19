d5cb225e0635f318d5cb9029587dd52db7e0fd17
Andrii Rykhliuk <andr.ruhx@gmail.com>
2025-04-19 13:12:42 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,26 @@
-# write your code here
+from unittest import mock
+
+import pytest
+
+from app.main import can_access_google_page
+
+
+@pytest.fixture()
+def valid_url() -> None:
+    with mock.patch("app.main.valid_google_url") as mocked_valid_url:
+        yield mocked_valid_url
+
+
+@pytest.fixture()
+def internet_connection_exists() -> None:
+    with mock.patch("app.main.has_internet_connection")\
+            as mocked_internet_connection:
+        yield mocked_internet_connection
+
+
+def test_valid_url_and_connection_exists(
+        valid_url: mock.MagicMock,
+        internet_connection_exists: mock.MagicMock) -> None:
+    can_access_google_page("https://www.google.com/")
+    valid_url.assert_called_once()
+    internet_connection_exists.assert_called_once()
