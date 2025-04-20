077b5c8e844fb47c799a222d9456de093810639b
Oleksandr Tarnavsky <sasha.tarnavsky@gmail.com>
2025-04-20 14:40:01 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -3,8 +3,8 @@ import pytest
 from unittest.mock import MagicMock, patch
 
 
-@patch("app.main.has_internet_connection")
 @patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
 @pytest.mark.parametrize(
     "internet, valid_url, expected",
     [
@@ -15,15 +15,15 @@ from unittest.mock import MagicMock, patch
     ],
 )
 def test_can_access_google_page(
-    mock_valid_url: MagicMock,
     mock_internet: MagicMock,
+    mock_valid_url: MagicMock,
     internet: bool,
     valid_url: bool,
     expected: str,
 ) -> None:
     """
     Test can_access_google_page() by mocking valid_google_url and
-    has_internet_connection to simulate all possible access conditions.
+    has_internet_connection to simulate all access scenarios.
     """
     mock_internet.return_value = internet
     mock_valid_url.return_value = valid_url
