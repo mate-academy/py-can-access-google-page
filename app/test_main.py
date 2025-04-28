a841fa8998b476f65f820b2e7ca394f12d9002a1
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-04-28 23:55:59 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -7,7 +7,8 @@ from unittest import mock
 
 @mock.patch("main.valid_google_url", return_value=True)
 @mock.patch("main.has_internet_connection", return_value=True)
-def test_can_access_google_page_success(mock_internet: Mock, mock_url: Mock) -> None:
+def test_can_access_google_page_success(mock_internet: Mock,
+                                        mock_url: Mock) -> None:
     result = can_access_google_page("https://google.com")
     assert result == "Accessible"
     mock_internet.assert_called_once()
