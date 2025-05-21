66db6e5816e27f1d3473f8def2df3b2968075b7c
Artur Drzazga <artur.drzazga03@gmail.com>
2025-05-21 12:32:39 +0200

    Flake8

--- app/test_main.py
+++ app/test_main.py
@@ -1,18 +1,24 @@
+from unittest.mock import MagicMock
+
 import pytest
 from unittest import mock
 from app.main import can_access_google_page
 
 
-@pytest.mark.parametrize("valid_url, has_internet, result",[
+@pytest.mark.parametrize("valid_url, has_internet, result", [
     (True, True, "Accessible"),
     (True, False, "Not accessible"),
     (False, True, "Not accessible"),
     (False, False, "Not accessible"),
 ])
-
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page(mock_valid_google_url, mock_has_internet_connection, valid_url, has_internet, result):
+def test_can_access_google_page(mock_valid_google_url : MagicMock,
+                                mock_has_internet_connection : MagicMock,
+                                valid_url : bool,
+                                has_internet : bool,
+                                result: str) -> None:
+
     mock_valid_google_url.return_value = valid_url
     mock_has_internet_connection.return_value = has_internet
 
@@ -22,6 +28,3 @@ def test_can_access_google_page(mock_valid_google_url, mock_has_internet_connect
     if has_internet:
         mock_valid_google_url.assert_called_once_with(url)
     mock_has_internet_connection.assert_called_once()
-
-
-
