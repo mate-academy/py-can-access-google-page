899869aebd1ae64de76ac4f0f1da9a064b5c4ea8
MiKPo4eLiK <stassojcuk@gmail.com>
2025-05-23 19:23:44 +0300

    wellll

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,5 @@
-from unittest import mock
 import pytest
+from unittest import mock
 from app.main import can_access_google_page
 
 
@@ -15,31 +15,33 @@ from app.main import can_access_google_page
         (
             "https://www.google.com",
             True,
+            False,
+            "Not accessible"
+        ),
+        (
+            "https://www.google.com",
+            False,
             True,
-            "Not Accessible"
+            "Not accessible"
         ),
         (
             "https://invalid-url.com",
-            True,
-            True,
-            "Not Accessible"
+            False,
+            False,
+            "Not accessible"
         ),
-        (
-            "https://invalid-url.com",
-            True,
-            True,
-            "Not Accessible"
-        )
     ]
 )
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page(mock_valid_url_function: object,
-                                mock_internet_connection: object,
-                                url: str,
-                                internet: bool,
-                                valid_url: bool,
-                                expected: str) -> None:
+def test_can_access_google_page(
+    mock_internet_connection: mock.MagicMock,
+    mock_valid_url_function: mock.MagicMock,
+    url: str,
+    internet: bool,
+    valid_url: bool,
+    expected: str
+) -> None:
     mock_internet_connection.return_value = internet
     mock_valid_url_function.return_value = valid_url
     assert can_access_google_page(url) == expected
