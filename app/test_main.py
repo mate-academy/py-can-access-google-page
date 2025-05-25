3aa937d0a90b4bbc77e4b4081cfe47292cf833fd
Antonov Andrew <prestigemilitary@gmail.com>
2025-05-25 23:05:25 +0300

    second try

--- app/test_main.py
+++ app/test_main.py
@@ -3,8 +3,8 @@ from unittest import mock
 from app.main import can_access_google_page
 
 
-@mock.patch('app.main.valid_google_url')
-@mock.patch('app.main.has_internet_connection')
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
 def test_can_access_google_page(
         mocked_url: mock.MagicMock,
         mocked_connection: mock.MagicMock) -> None:
