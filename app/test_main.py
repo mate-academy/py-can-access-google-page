a61722ef25ac1e305602eaf83809013ff11478ba
Antonov Andrew <prestigemilitary@gmail.com>
2025-05-25 23:04:35 +0300

    second try

--- app/test_main.py
+++ app/test_main.py
@@ -5,7 +5,9 @@ from app.main import can_access_google_page
 
 @mock.patch('app.main.valid_google_url')
 @mock.patch('app.main.has_internet_connection')
-def test_can_access_google_page(mocked_url, mocked_connection):
+def test_can_access_google_page(
+        mocked_url: mock.MagicMock,
+        mocked_connection: mock.MagicMock) -> None:
     mocked_connection.return_value = True
     mocked_url.return_value = True
     assert can_access_google_page("http://google.com") == True
