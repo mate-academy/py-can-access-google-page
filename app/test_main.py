5a266b06385ce9ebed805a3411c65936a768fb35
Antonov Andrew <prestigemilitary@gmail.com>
2025-05-25 23:00:42 +0300

    first try

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,11 @@
-# write your code here
+from unittest import mock
+
+from app.main import can_access_google_page
+
+
+@mock.patch('app.main.valid_google_url')
+@mock.patch('app.main.has_internet_connection')
+def test_can_access_google_page(mocked_url, mocked_connection):
+    mocked_connection.return_value = True
+    mocked_url.return_value = True
+    assert can_access_google_page("http://google.com") == True
