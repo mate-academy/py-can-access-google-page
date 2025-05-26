9ec114f4fd0edac9b51e468e6f6331eb28e4c4d0
Khlivniuk Oleksandr <sasha2056083@gmail.com>
2025-05-26 17:16:37 +0300

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -12,8 +12,8 @@ def test_can_access_google_page(
     assert can_access_google_page("https://www.google.com") == "Accessible"
 
 
-@mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
+@mock.patch("app.main.valid_google_url")
 def test_can_access_with_invalid_url(
         mock_valid_url: bool,
         mock_internet: bool) -> None:
@@ -23,8 +23,8 @@ def test_can_access_with_invalid_url(
     assert result == "Not accessible"
 
 
-@mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
+@mock.patch("app.main.valid_google_url")
 def test_can_access_google_page_has_no_internet(
         mock_valid_url: bool,
         mock_internet: bool) -> None:
