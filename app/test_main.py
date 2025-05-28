4b2427bbd5ef6f2427158d3341e89f0bbb384448
Vitalii <vitaliy.honchar@gmail.com>
2025-05-28 22:32:37 +0300

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -4,26 +4,26 @@ from unittest import mock
 
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
-def test_can_access_google_page(mock_url: bool,
-                                mock_internet: bool) -> None:
-    mock_internet.return_value = True
-    mock_url.return_value = True
+def test_can_access_google_page(valid_google_url: bool,
+                                has_internet_connection: bool) -> None:
+    has_internet_connection.return_value = True
+    valid_google_url.return_value = True
     assert can_access_google_page("https://google.com") == "Accessible"
 
 
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
-def test_can_access_without_internet(mock_url: bool,
-                                     mock_internet: bool) -> None:
-    mock_internet.return_value = False
-    mock_url.return_value = True
+def test_can_access_without_internet(valid_google_url: bool,
+                                     has_internet_connection: bool) -> None:
+    has_internet_connection.return_value = False
+    valid_google_url.return_value = True
     assert can_access_google_page("https://google.com") == "Not accessible"
 
 
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
-def test_can_access_url_is_error(mock_url: bool,
-                                 mock_internet: bool) -> None:
-    mock_internet.return_value = True
-    mock_url.return_value = False
+def test_can_access_url_is_error(valid_google_url: bool,
+                                 has_internet_connection: bool) -> None:
+    has_internet_connection.return_value = True
+    valid_google_url.return_value = False
     assert can_access_google_page("https://google.com") == "Not accessible"
