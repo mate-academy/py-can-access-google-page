98c149d9d7a8e9ddcee54ca18145696c9335ca51
Vitalii <vitaliy.honchar@gmail.com>
2025-05-28 22:25:34 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,29 @@
-# write your code here
+from app.main import can_access_google_page
+from unittest import mock
+
+
+@mock.patch("app.main.has_internet_connection")
+@mock.patch("app.main.valid_google_url")
+def test_can_access_google_page(mock_url: bool,
+                                mock_internet: bool) -> None:
+    mock_internet.return_value = True
+    mock_url.return_value = True
+    assert can_access_google_page("https://google.com") == "Accessible"
+
+
+@mock.patch("app.main.has_internet_connection")
+@mock.patch("app.main.valid_google_url")
+def test_can_access_without_internet(mock_url: bool,
+                                     mock_internet: bool) -> None:
+    mock_internet.return_value = False
+    mock_url.return_value = True
+    assert can_access_google_page("https://google.com") == "Not accessible"
+
+
+@mock.patch("app.main.has_internet_connection")
+@mock.patch("app.main.valid_google_url")
+def test_can_access_url_is_error(mock_url: bool,
+                                 mock_internet: bool) -> None:
+    mock_internet.return_value = True
+    mock_url.return_value = False
+    assert can_access_google_page("https://google.com") == "Not accessible"
