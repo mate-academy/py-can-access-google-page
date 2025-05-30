1dab37344e4e0b6c6b4f396e9d9aef839c9bda2d
Kyrylo Mikushyn <mikushin98@gmail.com>
2025-05-30 21:57:37 +0200

    test access google page

--- app/test_main.py
+++ app/test_main.py
@@ -11,8 +11,7 @@ from app.main import can_access_google_page
 ])
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_main_can_access_google_page(
-                                     mock_has_internet_connection: bool,
+def test_main_can_access_google_page(mock_has_internet_connection: bool,
                                      mock_valid_google_url: bool,
                                      param1: bool,
                                      param2: bool, expected: str) -> None:
