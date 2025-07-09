9f7d8dbea76e123ea2d4839b6e68fb0d85480a78
viannik <ivan.nikitski@gmail.com>
2025-07-09 23:42:38 +0300

    refactor unit tests for can_access_google_page function to use MagicMock

--- app/test_main.py
+++ app/test_main.py
@@ -1,37 +1,50 @@
+from unittest.mock import patch, MagicMock
+
 from app.main import can_access_google_page
-from unittest import mock
 
 
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_accessible_case(mock_internet, mock_valid_url):
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
+def test_accessible_case(
+    mock_internet: MagicMock,
+    mock_valid_url: MagicMock,
+) -> None:
     mock_valid_url.return_value = True
     mock_internet.return_value = True
     result = can_access_google_page("https://www.google.com")
     assert result == "Accessible"
 
 
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_not_accessible_invalid_url(mock_internet, mock_valid_url):
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
+def test_not_accessible_invalid_url(
+    mock_internet: MagicMock,
+    mock_valid_url: MagicMock,
+) -> None:
     mock_valid_url.return_value = False
     mock_internet.return_value = True
     result = can_access_google_page("https://www.google.com")
     assert result == "Not accessible"
 
 
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_not_accessible_no_internet(mock_internet, mock_valid_url):
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
+def test_not_accessible_no_internet(
+    mock_internet: MagicMock,
+    mock_valid_url: MagicMock,
+) -> None:
     mock_valid_url.return_value = True
     mock_internet.return_value = False
     result = can_access_google_page("https://www.google.com")
     assert result == "Not accessible"
 
 
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_not_accessible_both_invalid(mock_internet, mock_valid_url):
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
+def test_not_accessible_both_invalid(
+    mock_internet: MagicMock,
+    mock_valid_url: MagicMock,
+) -> None:
     mock_valid_url.return_value = False
     mock_internet.return_value = False
     result = can_access_google_page("https://www.google.com")
