743963786926459143bb256a4625565059ebb66b
Otstoy <otstoy.sxe@gmail.com>
2025-07-11 09:19:25 +0300

    Changed solution.

--- app/test_main.py
+++ app/test_main.py
@@ -1,58 +1,31 @@
-from typing import Generator
-from unittest import mock
-from unittest.mock import MagicMock
-
-import pytest
+from unittest.mock import patch
 
 from app.main import can_access_google_page
 
 
-@pytest.fixture()
-def mocks() -> Generator[list[MagicMock], None, None]:
-    with (
-        mock.patch("app.main.valid_google_url")
-        as mock_valid_google_url,
-        mock.patch("app.main.has_internet_connection")
-        as mock_has_internet_connection
-    ):
-        yield [mock_valid_google_url, mock_has_internet_connection]
-
-
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
 def test_can_access_page_if_both_are_true(
-        mocks: Generator[list[MagicMock], None, None]
+        mock_valid_google_url: bool,
+        mock_has_internet_connection: bool
 ) -> None:
-    mocks[0].return_value = True
-    mocks[1].return_value = True
 
-    result = can_access_google_page("google.com")
-    assert result == "Accessible"
+    mock_has_internet_connection.return_value = True
+    mock_valid_google_url.return_value = True
 
+    assert can_access_google_page("google.com") == "Accessible"
 
-def test_can_not_access_page_if_valid_url_is_false(
-        mocks: Generator[list[MagicMock], None, None]
-) -> None:
-    mocks[0].return_value = False
-    mocks[1].return_value = True
+    mock_has_internet_connection.return_value = False
+    mock_valid_google_url.return_value = True
 
-    result = can_access_google_page("google.com")
-    assert result == "Not accessible"
+    assert can_access_google_page("google.com") == "Not accessible"
 
+    mock_has_internet_connection.return_value = True
+    mock_valid_google_url.return_value = False
 
-def test_can_not_access_page_if_internet_connection_is_false(
-        mocks: Generator[list[MagicMock], None, None]
-) -> None:
-    mocks[0].return_value = True
-    mocks[1].return_value = False
+    assert can_access_google_page("google.com") == "Not accessible"
 
-    result = can_access_google_page("google.com")
-    assert result == "Not accessible"
+    mock_has_internet_connection.return_value = False
+    mock_valid_google_url.return_value = False
 
-
-def test_can_not_access_page_if_both_are_false(
-    mocks: Generator[list[MagicMock], None, None]
-) -> None:
-    mocks[0].return_value = False
-    mocks[0].return_value = False
-
-    result = can_access_google_page("google.com")
-    assert result == "Not accessible"
+    assert can_access_google_page("google.com") == "Not accessible"
