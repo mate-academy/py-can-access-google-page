04518811fd4807aeabf0e63c1fd5bcb5f88be971
Vasyl Boliukh <vskesha@gmail.com>
2025-07-12 22:44:57 +0300

    rewritten with parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -1,53 +1,34 @@
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 
-from pytest import fixture
+import pytest
 
 from app.main import can_access_google_page
 
 
-@fixture
-def mocked_url() -> MagicMock:
-    with patch("app.main.valid_google_url") as mocked_valid_url:
-        yield mocked_valid_url
-
-
-@fixture()
-def mocked_internet() -> MagicMock:
-    with patch("app.main.has_internet_connection") as mocked_connection:
-        yield mocked_connection
-
-
+@pytest.mark.parametrize(
+    "valid_url, has_connection, result",
+    [
+        (True, True, "Accessible"),
+        (False, True, "Not accessible"),
+        (True, False, "Not accessible"),
+        (False, False, "Not accessible")
+    ],
+    ids=[
+        "valid url and has connection",
+        "url is not valid",
+        "no internet connection",
+        "url is not valid and no internet connection"
+    ]
+)
 def test_can_access_google_page_valid_url_and_has_connection(
-        mocked_url: MagicMock,
-        mocked_internet: MagicMock
+        valid_url: bool,
+        has_connection: bool,
+        result: str
 ) -> None:
-    mocked_url.return_value = True
-    mocked_internet.return_value = True
-    assert can_access_google_page("http://example.com") == "Accessible"
-
-
-def test_can_access_google_page_not_valid_url_and_has_connection(
-        mocked_url: MagicMock,
-        mocked_internet: MagicMock
-) -> None:
-    mocked_url.return_value = False
-    mocked_internet.return_value = True
-    assert can_access_google_page("http://example.com") == "Not accessible"
-
-
-def test_can_access_google_page_valid_url_and_has_no_connection(
-        mocked_url: MagicMock,
-        mocked_internet: MagicMock
-) -> None:
-    mocked_url.return_value = True
-    mocked_internet.return_value = False
-    assert can_access_google_page("http://example.com") == "Not accessible"
-
-
-def test_can_access_google_page_not_valid_url_and_has_no_connection(
-        mocked_url: MagicMock,
-        mocked_internet: MagicMock
-) -> None:
-    mocked_url.return_value = False
-    mocked_internet.return_value = False
-    assert can_access_google_page("http://example.com") == "Not accessible"
+    with (
+        patch("app.main.valid_google_url") as mocked_valid_url,
+        patch("app.main.has_internet_connection") as mocked_connection
+    ):
+        mocked_valid_url.return_value = valid_url
+        mocked_connection.return_value = has_connection
+        assert can_access_google_page("http://example.com") == result
