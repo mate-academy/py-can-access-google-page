8a6cc54190633faf4817b18e34223ce04dda7052
Andrii Novikov <forytube2023@gmail.com>
2025-07-13 13:37:24 +0300

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,7 @@
 import pytest
 from unittest.mock import patch
 
+
 from app.main import can_access_google_page
 
 
@@ -14,9 +15,11 @@ from app.main import can_access_google_page
     ],
 )
 def test_can_access_google_page_різні_умови(
-    url, valid_url_mock, connection_mock, expected_result
-):
-    with patch("app.main.valid_google_url", return_value=valid_url_mock), patch(
-        "app.main.has_internet_connection", return_value=connection_mock
-    ):
+    url: str,
+    valid_url_mock: bool,
+    connection_mock: bool,
+    expected_result: str,
+) -> None:
+    with patch("app.main.valid_google_url", return_value=valid_url_mock), \
+         patch("app.main.has_internet_connection", return_value=connection_mock):
         assert can_access_google_page(url) == expected_result
