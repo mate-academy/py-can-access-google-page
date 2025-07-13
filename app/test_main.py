189a9cea3bb4cc84a8133c05c3379ca00158ab6f
Vovas <vovaafon25@gmail.com>
2025-07-13 19:40:11 +0300

    Solution improved.v2

--- app/test_main.py
+++ app/test_main.py
@@ -1,8 +1,11 @@
 from unittest import mock
+
+import pytest
+
 from app.main import can_access_google_page
 
 
-def test_can_access_google_page() -> None:
+def test_can_if_functions_called_once() -> None:
     with (mock.patch("app.main.has_internet_connection")
           as mock_internet_connection,
           mock.patch("app.main.valid_google_url") as mock_valid_url):
@@ -12,3 +15,18 @@ def test_can_access_google_page() -> None:
         mock_valid_url.assert_called_once_with("https://www.youtube.com/")
 
     assert can_access_google_page("https://www.youtube.com/") == "Accessible"
+
+
+@pytest.mark.parametrize(
+    "bool1, bool2, result",
+    [
+        (False, True, "Not accessible"),
+        (True, False, "Not accessible"),
+        (False, False, "Not accessible"),
+        (True, True, "Accessible")
+    ]
+)
+def test_different_cases(bool1: bool, bool2: bool, result: str) -> None:
+    with (mock.patch("app.main.has_internet_connection", return_value=bool1),
+          mock.patch("app.main.valid_google_url", return_value=bool2)):
+        assert can_access_google_page("https://www.youtube.com/") == result
