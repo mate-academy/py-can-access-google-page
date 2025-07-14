452c820c92a661fa35d09f41f085546a1d3370f4
Viktor Kartashov <if.artcraft@gmail.com>
2025-07-14 09:25:15 +0300

    Update app/test_main.py
    Co-authored-by: Bohdan Marchenko <114533548+yagginov@users.noreply.github.com>

--- app/test_main.py
+++ app/test_main.py
@@ -24,8 +24,10 @@ def test_can_access_google_page(
         has_connection: bool,
         expected: str
 ) -> None:
-    with mock.patch("app.main.valid_google_url") as mocked_test_url, \
-         mock.patch("app.main.has_internet_connection") as mocked_connection:
+    with (
+        mock.patch("app.main.valid_google_url") as mocked_test_url,
+        mock.patch("app.main.has_internet_connection") as mocked_connection
+    ):
         mocked_test_url.return_value = test_url
         mocked_connection.return_value = has_connection
 
