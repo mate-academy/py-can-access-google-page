f88b82953b890181b589042f2b6c0ee2137e2970
Vadym Vizirov <vadimplus57@gmail.com>
2025-07-14 14:49:15 +0300

    fix: access google

--- app/test_main.py
+++ app/test_main.py
@@ -4,7 +4,7 @@ from unittest import mock
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_main_func(mock_has_internet: bool, mock_valid: bool) -> None:
+def test_main_func(mock_valid: bool, mock_has_internet: bool) -> None:
     mock_valid.return_value = True
     mock_has_internet.return_value = True
     result = can_access_google_page("https://www.google.com.ua/")
