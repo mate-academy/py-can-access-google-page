4f7b74d8f6cabbf5482e8dbb717b451a4fc8c52c
SensibleN00B <sensible.noob@gmail.com>
2025-07-14 16:34:15 +0300

    Added type annotations

--- app/test_main.py
+++ app/test_main.py
@@ -14,7 +14,19 @@ from app.main import can_access_google_page
         (False, True, "Not accessible")
     ]
 )
-def test_can_access_google_page(has_internet, valid_url, expected):
-    with (mock.patch("app.main.has_internet_connection", return_value=has_internet),
-          mock.patch("app.main.valid_google_url", return_value=valid_url)):
+def test_can_access_google_page(
+        has_internet: bool,
+        valid_url: bool,
+        expected: str
+) -> None:
+    with (
+        mock.patch(
+            "app.main.has_internet_connection",
+            return_value=has_internet
+        ),
+        mock.patch(
+            "app.main.valid_google_url",
+            return_value=valid_url
+        )
+    ):
         assert can_access_google_page("https://www.google.com") == expected
