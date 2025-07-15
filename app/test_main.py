8836ede471f5c2e5029cdd15ef6df79f7a483bc5
benfict <benfict@gmail.com>
2025-07-16 01:25:06 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,34 @@
-# write your code here
+from unittest.mock import Mock
+import app.main as main
+
+
+def test_accessible_when_both_true(monkeypatch):
+    monkeypatch.setattr(main, "valid_google_url", Mock(return_value=True))
+    monkeypatch.setattr(main, "has_internet_connection", Mock(return_value=True))
+
+    result = main.can_access_google_page("https://www.google.com")
+    assert result == "Accessible"
+
+
+def test_not_accessible_if_only_connection_true(monkeypatch):
+    monkeypatch.setattr(main, "valid_google_url", Mock(return_value=False))
+    monkeypatch.setattr(main, "has_internet_connection", Mock(return_value=True))
+
+    result = main.can_access_google_page("https://google.com")
+    assert result == "Not accessible"
+
+
+def test_not_accessible_if_only_valid_url_true(monkeypatch):
+    monkeypatch.setattr(main, "valid_google_url", Mock(return_value=True))
+    monkeypatch.setattr(main, "has_internet_connection", Mock(return_value=False))
+
+    result = main.can_access_google_page("https://google.com")
+    assert result == "Not accessible"
+
+
+def test_not_accessible_if_both_false(monkeypatch):
+    monkeypatch.setattr(main, "valid_google_url", Mock(return_value=False))
+    monkeypatch.setattr(main, "has_internet_connection", Mock(return_value=False))
+
+    result = main.can_access_google_page("https://bad.com")
+    assert result == "Not accessible"
