602fd7a5a8eacaface64432cae1f28ea9c274305
helix09825 <maksilczuk@gmail.com>
2025-07-16 17:35:17 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,11 +2,14 @@ from unittest.mock import patch, MagicMock
 from app.main import can_access_google_page
 
 
-@patch("app.main.has_internet_connection")
 @patch("app.main.valid_google_url")
-def test_can_access_google_page(mock_valid_url: MagicMock,
-                                mock_internet: MagicMock) -> None:
-    mock_internet.return_value = True
+@patch("app.main.has_internet_connection")
+def test_can_access_google_page(
+    mock_valid_url: MagicMock,
+    mock_internet: MagicMock
+) -> None:
     mock_valid_url.return_value = True
+    mock_internet.return_value = True
 
-    assert can_access_google_page("https://www.google.com") == "Accessible"
+    result = can_access_google_page("https://www.google.com")
+    assert result == "Accessible"
