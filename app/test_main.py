d1cc4c04aca440b445f475179f5dc5daa6c090da
Nik <your@mail.com>
2025-07-16 16:58:15 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -20,7 +20,7 @@ def test_input_invalid_url() -> None:
 @mock.patch("datetime.datetime")
 def test_if_one_of_function_is_false(mock_time: datetime) -> None:
 
-    mock_time.datetime.now.return_value = (
+    mock_time.now.return_value = (
         datetime.datetime(2000, 1, 1, 0, 0, 0))
 
     assert (can_access_google_page("https://www.youtube.com/")
