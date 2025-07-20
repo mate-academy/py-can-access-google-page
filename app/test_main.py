ed9dd9b52150dd93136b241c0bed0dd83cd66f1f
Anton Yakovenko <yakovenkoanton2007@gmail.com>
2025-07-20 18:40:27 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,25 @@
 from unittest import mock
 import pytest
 import requests
-from typing import Any
+from typing import Any, Type
 import datetime
-from app.main import valid_google_url, has_internet_connection, can_access_google_page
+from app.main import (
+    valid_google_url,
+    has_internet_connection,
+    can_access_google_page
+)
 
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page(has_connection: mock.MagicMock, valid_url: mock.MagicMock):
+def test_can_access_google_page(
+        has_connection: mock.MagicMock,
+        valid_url: mock.MagicMock
+) -> None:
     can_access_google_page("url")
     has_connection.assert_called_once()
     valid_url.assert_called_once_with("url")
+
 
 @pytest.mark.parametrize(
     "input_of_expected_url,expected_result",
@@ -20,7 +28,10 @@ def test_can_access_google_page(has_connection: mock.MagicMock, valid_url: mock.
         pytest.param("https://httpbin.org/status/404", False, id="404 page")
     ]
 )
-def test_valid_google_url(input_of_expected_url: str, expected_result: bool) -> None:
+def test_valid_google_url(
+        input_of_expected_url: str,
+        expected_result: bool
+) -> None:
     assert valid_google_url(input_of_expected_url) == expected_result
 
 
@@ -30,12 +41,15 @@ def test_valid_google_url(input_of_expected_url: str, expected_result: bool) -> 
         ("", requests.exceptions.MissingSchema)
     ]
 )
-def test_exceptions_valid_google_url(argument:Any, expected_error):
+def test_exceptions_valid_google_url(
+        argument: Any,
+        expected_error: Type[BaseException]
+) -> None:
     with pytest.raises(expected_error):
         valid_google_url(argument)
 
 
-def test_has_internet_connection():
+def test_has_internet_connection() -> None:
     if datetime.datetime.now().hour in range(6, 23):
         expected_result = True
     else:
