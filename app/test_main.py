eead46bf544a7169ab7cda54687fc23ce382ab0d
8ajarz <8ajarz_zaprasza@protonmail.com>
2025-07-26 00:37:50 +0700

    it is funny night ahead, if it'll fail...

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,39 @@
-# write your code here
+from app import main
+from unittest import mock
+
+
+@mock.patch("app.main.requests.get")
+def test_valid_google_url(mock_get: object) -> bool:
+    mock_response = mock.Mock()
+    mock_response.status_code = 200
+    mock_get.return_value = mock_response
+    assert main.valid_google_url("google")
+
+
+@mock.patch("app.main.datetime.datetime")
+def test_has_internet_connection(mock_datetime: object) -> bool:
+    fake_now = mock.Mock()
+    fake_now.hour = 7
+    mock_datetime.now.return_value = fake_now
+    assert main.has_internet_connection()
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_can_access_google_page(
+        mocked_connection: object,
+        mocked_url: object
+) -> bool:
+    mocked_connection.return_value = True
+    mocked_url.return_value = True
+    assert main.can_access_google_page("google") == "Accessible"
+    mocked_connection.return_value = False
+    mocked_url.return_value = True
+    assert main.can_access_google_page("google") == "Not accessible"
+    mocked_connection.return_value = True
+    mocked_url.return_value = False
+    assert main.can_access_google_page("google") == "Not accessible"
+
+
+if __name__ == "__main__":
+    pass
