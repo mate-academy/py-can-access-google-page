dbb70b73907c042a1c85235789e11cfacec496c8
NoroiMusha37 <vo.melnyk37@gmail.com>
2025-07-26 17:02:04 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -6,7 +6,10 @@ from app.main import can_access_google_page
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page(mocked_connection: Any, mocked_url: Any) -> None:
+def test_can_access_google_page(
+        mocked_connection: Any,
+        mocked_url: Any
+)-> None:
     mocked_connection.return_value = True
     mocked_url.return_value = True
 
