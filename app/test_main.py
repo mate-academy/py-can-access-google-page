f9b9383f96cc23549b81fbe1e75691bd0dd1b094
Oleksandr Skoromnov <solalex005@gmail.com>
2025-08-07 21:07:32 +0300

    fix

--- app/test_main.py
+++ app/test_main.py
@@ -3,36 +3,36 @@ from app.main import can_access_google_page
 from unittest import mock
 
 
-class TestResponse:
-    def __init__(self, code: int) -> None:
-        self.status_code = code
-
-
-class TestDatetime:
-    def __init__(self, hour: int) -> None:
-        self.hour = hour
+# class TestResponse:
+#     def __init__(self, code: int) -> None:
+#         self.status_code = code
+#
+#
+# class TestDatetime:
+#     def __init__(self, hour: int) -> None:
+#         self.hour = hour
 
 
 @pytest.mark.parametrize(
-    "test_hour,test_code,result",
+    "is_valid_url,is_connection,result",
     [
-        (15, 200, "Accessible"),
-        (22, 200, "Accessible"),
-        (5, 200, "Not accessible"),
-        (23, 200, "Not accessible"),
-        (15, 404, "Not accessible"),
-        (2, 500, "Not accessible"),
+        (True, True, "Accessible"),
+        (True, True, "Accessible"),
+        (False, True, "Not accessible"),
+        (False, True, "Not accessible"),
+        (True, False, "Not accessible"),
+        (False, False, "Not accessible"),
     ]
 )
-def test_can_access_if_both_true(
-        test_hour: int,
-        test_code: int,
+def test_can_access(
+        is_valid_url: int,
+        is_connection: int,
         result: str
 ) -> None:
     with (
-        mock.patch("datetime.datetime") as mocked_dtt,
-        mock.patch("requests.get") as mocked_get
+        mock.patch("app.main.valid_google_url") as mocked_url,
+        mock.patch("app.main.has_internet_connection") as mocked_connection
     ):
-        mocked_dtt.now.return_value = TestDatetime(test_hour)
-        mocked_get.return_value = TestResponse(test_code)
+        mocked_url.return_value = is_valid_url
+        mocked_connection.return_value = is_connection
         assert can_access_google_page("some_url") == result
