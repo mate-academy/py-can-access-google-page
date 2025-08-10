75b3a46223544ff4e60e1ff23dc05135dd6de4b8
Maksym Pakula <gargantadoto@gmail.com>
2025-08-10 16:57:39 +0300

    solution for access pages

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,27 @@
-# write your code here
+from app.main import can_access_google_page
+
+
+def test_can_access_google_page(monkeypatch):
+    def mock_url(url: str) -> bool:
+        return False
+
+    def mock_internet_connection() -> bool:
+        return True
+
+    monkeypatch.setattr("app.main.valid_google_url", mock_url)
+    monkeypatch.setattr("app.main.has_internet_connection",
+                        mock_internet_connection)
+    assert can_access_google_page("https://google.com") == "Not accessible"
+
+
+def test_can_not_access_google_page(monkeypatch):
+    def mock_url(url: str) -> bool:
+        return True
+
+    def mock_internet_connection() -> bool:
+        return False
+
+    monkeypatch.setattr("app.main.valid_google_url", mock_url)
+    monkeypatch.setattr("app.main.has_internet_connection",
+                        mock_internet_connection)
+    assert can_access_google_page("https://google.com") == "Not accessible"
