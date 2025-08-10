9511b7f54e36228401c901fe2a59edea05afd4b6
Maksym Pakula <gargantadoto@gmail.com>
2025-08-10 17:03:08 +0300

    fixed issues

--- app/test_main.py
+++ app/test_main.py
@@ -1,9 +1,10 @@
 from app.main import can_access_google_page
+from typing import Any
 
 
-def test_can_access_google_page(monkeypatch):
+def test_can_access_google_page(monkeypatch: Any) -> None:
     def mock_url(url: str) -> bool:
-        return False
+        return True
 
     def mock_internet_connection() -> bool:
         return True
@@ -11,15 +12,28 @@ def test_can_access_google_page(monkeypatch):
     monkeypatch.setattr("app.main.valid_google_url", mock_url)
     monkeypatch.setattr("app.main.has_internet_connection",
                         mock_internet_connection)
-    assert can_access_google_page("https://google.com") == "Not accessible"
+    assert can_access_google_page("https://google.com") == "Accessible"
 
 
-def test_can_not_access_google_page(monkeypatch):
+def test_no_internet_connection(monkeypatch: Any) -> None:
     def mock_url(url: str) -> bool:
         return True
 
     def mock_internet_connection() -> bool:
         return False
+
+    monkeypatch.setattr("app.main.valid_google_url", mock_url)
+    monkeypatch.setattr("app.main.has_internet_connection",
+                        mock_internet_connection)
+    assert can_access_google_page("https://google.com") == "Not accessible"
+
+
+def test_not_200_status_code(monkeypatch: Any) -> None:
+    def mock_url(url: str) -> bool:
+        return False
+
+    def mock_internet_connection() -> bool:
+        return True
 
     monkeypatch.setattr("app.main.valid_google_url", mock_url)
     monkeypatch.setattr("app.main.has_internet_connection",
