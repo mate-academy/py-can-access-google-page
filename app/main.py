a6b7bd0b9f413c8202848031b2503ae7c959efe9
Tomasz Herman <tomekh2015@gmail.com>
2025-08-18 14:45:59 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -4,16 +4,15 @@ import requests
 
 def valid_google_url(url: str) -> bool:
     response = requests.get(url)
-    return True if response.status_code == 200 else False
+    return response.status_code == 200
 
 
 def has_internet_connection() -> bool:
     current_time = datetime.datetime.now()
-    return True if current_time.hour in range(6, 23) else False
+    return 6 <= current_time.hour <= 22
 
 
 def can_access_google_page(url: str) -> str:
     if has_internet_connection() and valid_google_url(url):
         return "Accessible"
-    else:
-        return "Not accessible"
+    return "Not accessible"
--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,33 @@
-# write your code here
+from app import main
+from unittest.mock import patch
+
+
+def test_cannot_access_if_connection_or_valid_url_is_true() -> None:
+    # tylko jedno z kryteriów prawdziwe -> brak dostępu
+    with patch("app.main.has_internet_connection", return_value=True), \
+         patch("app.main.valid_google_url", return_value=False):
+        url = "https://www.google.com"
+        result = main.can_access_google_page(url)
+        assert result == "Not accessible"
+
+    with patch("app.main.has_internet_connection", return_value=False), \
+         patch("app.main.valid_google_url", return_value=True):
+        url = "https://www.google.com"
+        result = main.can_access_google_page(url)
+        assert result == "Not accessible"
+
+
+def test_cannot_access_if_only_connection() -> None:
+    with patch("app.main.has_internet_connection", return_value=True), \
+         patch("app.main.valid_google_url", return_value=False):
+        url = "https://www.google.com"
+        result = main.can_access_google_page(url)
+        assert result == "Not accessible"
+
+
+def test_cannot_access_if_only_valid_url() -> None:
+    with patch("app.main.has_internet_connection", return_value=False), \
+         patch("app.main.valid_google_url", return_value=True):
+        url = "https://www.google.com"
+        result = main.can_access_google_page(url)
+        assert result == "Not accessible"
