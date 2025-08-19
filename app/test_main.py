d581173eae75ae60f8ddee6d802f45bb36bc15f4
Konstantin S. <kostaho16@gmail.com>
2025-08-19 20:42:27 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -6,8 +6,28 @@ from unittest import mock
 from app.main import can_access_google_page
 
 
+@pytest.fixture()
+def mocking_validator(
+    request: pytest.FixtureRequest,
+) -> Generator[mock.MagicMock | mock.AsyncMock, Any, None]:
+    validator_return = getattr(request, "param", True)
+    with mock.patch("app.main.valid_google_url") as mocked_validator:
+        mocked_validator.return_value = validator_return
+        yield mocked_validator
+
+
+@pytest.fixture()
+def mocking_connection(
+    request: pytest.FixtureRequest,
+) -> Generator[mock.MagicMock | mock.AsyncMock, Any, None]:
+    connection_return = getattr(request, "param", True)
+    with mock.patch("app.main.has_internet_connection") as mocked_connection:
+        mocked_connection.return_value = connection_return
+        yield mocked_connection
+
+
 @pytest.mark.parametrize(
-    "validator_response, connection_response, access_response",
+    "mocking_validator, mocking_connection, access_response",
     [
         pytest.param(
             True,
@@ -38,60 +58,56 @@ from app.main import can_access_google_page
             is not valid and no internet connection",
         ),
     ],
+    indirect=["mocking_validator", "mocking_connection"],
 )
 def test_access_with_mocked_page_validation_and_connection(
-    validator_response: bool, connection_response: bool, access_response: str
+    mocking_validator: mock.MagicMock,
+    mocking_connection: mock.MagicMock,
+    access_response: str,
 ) -> None:
-    with mock.patch(
-        "app.main.valid_google_url"
-    ) as mocked_validator, mock.patch(
-        "app.main.has_internet_connection"
-    ) as mocked_connection:
-        mocked_validator.return_value = validator_response
-        mocked_connection.return_value = connection_response
-        assert can_access_google_page("url_sample") == access_response
+    assert can_access_google_page("url_sample") == access_response
 
 
 class TestFunctionsCall:
-    @pytest.fixture(scope="class")
-    def mocking_validator(
-        self,
-    ) -> Generator[mock.MagicMock | mock.AsyncMock, Any, None]:
-        with mock.patch("app.main.valid_google_url") as mocked_validator:
-            yield mocked_validator
-
-    @pytest.fixture(scope="class")
-    def mocking_connection(
-        self,
-    ) -> Generator[mock.MagicMock | mock.AsyncMock, Any, None]:
-        with mock.patch(
-            "app.main.has_internet_connection"
-        ) as mocked_connection:
-            yield mocked_connection
-
     def test_should_call_validator_function(
         self, mocking_validator: mock.MagicMock
     ) -> None:
         can_access_google_page("url_sample")
-        mocking_validator.assert_called_once
+        mocking_validator.assert_called_once()
 
     def test_should_call_connection_function(
-        self, mocking_connection: mock.MagicMock
+        self,
+        mocking_connection: mock.MagicMock,
+        mocking_validator: mock.MagicMock
     ) -> None:
         can_access_google_page("url_sample")
-        mocking_connection.assert_called_once
+        mocking_connection.assert_called_once()
 
-    def test_should_call_both_functions(
+    def test_should_call_both_functions_if_conditions_true(
         self,
         mocking_validator: mock.MagicMock,
         mocking_connection: mock.MagicMock,
     ) -> None:
         can_access_google_page("url_sample")
-        mocking_validator.assert_called
-        mocking_connection.assert_called
+        mocking_validator.assert_called_once()
+        mocking_connection.assert_called_once()
+
+    @pytest.mark.parametrize(
+        "mocking_connection, mocking_validator",
+        [pytest.param(False, True)],
+        indirect=["mocking_connection", "mocking_validator"],
+    )
+    def test_should_not_call_validator_function_if_connection_false(
+        self,
+        mocking_connection: mock.MagicMock,
+        mocking_validator: mock.MagicMock,
+    ) -> None:
+        can_access_google_page("url_sample")
+        mocking_connection.assert_called_once()
+        mocking_validator.assert_not_called()
 
     def test_should_transfer_url_to_page_validator(
         self, mocking_validator: mock.MagicMock
     ) -> None:
         can_access_google_page("url_sample")
-        mocking_validator.assert_called_with("url_sample")
+        mocking_validator.assert_called_once_with("url_sample")
