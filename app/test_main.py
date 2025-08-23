9008a89eaa97dd074b55e526fb4103f4957d0e3d
Alex <zigzaksasha@gmail.com>
2025-08-23 22:47:00 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,28 +1,25 @@
 from app.main import can_access_google_page
 import pytest
-import datetime
 from unittest import mock
 
 
 @pytest.mark.parametrize(
-    "url_page,status_code,fake_datetime,expected",
+    "url,val_url,internet,expected",
     [
-        ("https://googlecom", 404,
-         datetime.datetime(2222, 2, 2, 6, 00, 00), "Not accessible"),
-        ("https://www.google.com", 200,
-         datetime.datetime(2222, 2, 2, 6, 00, 00), "Accessible"),
-        ("https://www.google.com", 200,
-         datetime.datetime(2222, 2, 2, 22, 59, 59), "Accessible"),
-        ("https://www.google.com", 200,
-         datetime.datetime(2222, 2, 2, 23, 00, 00), "Not accessible")
-    ]
+        ("https://www.google.com", True, True, "Accessible"),
+        ("https://www.google.com", True, False, "Not accessible"),
+        ("https://www.badsite.com", False, True, "Not accessible"),
+        ("https://www.badsite.com", False, False, "Not accessible"),
+    ],
 )
-def test_can_access_google_page(url_page: str,
-                                status_code: int,
-                                fake_datetime: datetime.datetime,
-                                expected: str) -> None:
-    with mock.patch("app.main.valid_google_url") as mock_requests:
-        mock_requests.get.return_value.status_code = status_code
-        with mock.patch("app.main.has_internet_connection") as mock_datetime:
-            mock_datetime.datetime.now.return_value = fake_datetime
-            assert can_access_google_page(url_page) == expected
+def test_valid_url_and_internet_connection(
+        url,
+        val_url,
+        internet,
+        expected
+) -> None:
+    with mock.patch("app.main.valid_google_url", return_value=val_url), \
+        mock.patch("app.main.has_internet_connection",
+                   return_value=internet):
+        result = can_access_google_page(url)
+    assert result == expected
