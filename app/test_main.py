80c2d30a966673fbe83d7ce51bdf3a1f17f8baf1
Alex_Lushenko <mralealex9@gmail.com>
2025-08-25 20:49:39 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -20,17 +20,11 @@ def test_can_access_when_url_and_connection_are_valid() -> None:
 
 def test_cannot_access_when_no_connection() -> None:
     url = "https://google.com"
-    with (
-        mock.patch(
-            "app.main.valid_google_url", return_value=True
-        ) as valid_url_mock,
-        mock.patch(
-            "app.main.has_internet_connection", return_value=False
-        ) as internet_mock
-    ):
+    with mock.patch(
+        "app.main.has_internet_connection", return_value=False
+    ) as internet_mock:
         result = can_access_google_page(url)
         assert result == "Not accessible"
-        valid_url_mock.assert_called_once_with(url)
         internet_mock.assert_called_once_with()
 
 
@@ -52,15 +46,9 @@ def test_cannot_access_when_invalid_url() -> None:
 
 def test_cannot_access_when_no_connection_and_invalid_url() -> None:
     url = "https://invalid-url.com"
-    with (
-        mock.patch(
-            "app.main.valid_google_url", return_value=False
-        ) as valid_url_mock,
-        mock.patch(
-            "app.main.has_internet_connection", return_value=False
-        ) as internet_mock
-    ):
+    with mock.patch(
+        "app.main.has_internet_connection", return_value=False
+    ) as internet_mock:
         result = can_access_google_page(url)
         assert result == "Not accessible"
-        valid_url_mock.assert_called_once_with(url)
         internet_mock.assert_called_once_with()
