db94dfa760778cef5a59bdda73c2a4086a64e9de
Olexii Babii <babiy0617@gmail.com>
2025-08-26 17:09:02 +0300

    fix_2

--- app/test_main.py
+++ app/test_main.py
@@ -1,36 +1,48 @@
-from app.main import can_access_google_page, valid_google_url, has_internet_connection
+from app.main import can_access_google_page
 from unittest import mock
 
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page_connecting_is_False(mocked_connection, mocked_url):
+def test_can_access_google_page_connecting_is_false(
+        mocked_connection: bool,
+        mocked_url: bool) -> None:
     mocked_url.return_value = True
     mocked_connection.return_value = False
-    assert can_access_google_page(
-        "https://github.com/Olexii-Babii/py-can-access-google-page") == "Not accessible"
+    assert (can_access_google_page(
+        "https://github.com/Olexii-Babii/py-can-access-google-page")
+        == "Not accessible")
 
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page_url_is_False(mocked_connection, mocked_url):
+def test_can_access_google_page_url_is_false(
+        mocked_connection: bool,
+        mocked_url: bool) -> None:
     mocked_url.return_value = False
     mocked_connection.return_value = True
-    assert can_access_google_page(
-        "https://github.com/Olexii-Babii/py-can-access-google-page") == "Not accessible"
+    assert (can_access_google_page(
+        "https://github.com/Olexii-Babii/py-can-access-google-page")
+        == "Not accessible")
 
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page_connecting_all_is_True(mocked_connection, mocked_url):
+def test_can_access_google_page_connecting_all_is_true(
+        mocked_connection: bool,
+        mocked_url: bool) -> None:
     mocked_url.return_value = True
     mocked_connection.return_value = True
-    assert can_access_google_page(
-        "https://github.com/Olexii-Babii/py-can-access-google-page") == "Accessible"
+    assert (can_access_google_page(
+        "https://github.com/Olexii-Babii/py-can-access-google-page")
+        == "Accessible")
 
 
 @mock.patch("app.main.valid_google_url", return_value=False)
-@mock.patch("app.main.has_internet_connection", return_valur=False)
-def test_can_access_google_page_connecting_all_is_False(mocked_connection, mocked_url):
-    assert can_access_google_page(
-        "https://github.com/Olexii-Babii/py-can-access-google-page") == "Not accessible"
+@mock.patch("app.main.has_internet_connection", return_value=False)
+def test_can_access_google_page_connecting_all_is_false(
+        mocked_connection: bool,
+        mocked_url: bool) -> None:
+    assert (can_access_google_page(
+        "https://github.com/Olexii-Babii/py-can-access-google-page")
+        == "Not accessible")
