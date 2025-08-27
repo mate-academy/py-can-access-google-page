3e943a2a8b24991732163129736eef3cd5951460
Arseniy Kushnir <arseniykushnir2007@gmail.com>
2025-08-27 10:19:45 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -12,12 +12,14 @@ from app.main import can_access_google_page
         (False, False, "Not accessible"),
     ]
 )
-def test_can_access_google_page(valid_google_url: bool,
-                                has_internet_connection: bool,
-                                result: str) -> None:
+def test_can_access_google_page(
+        valid_google_url: bool,
+        has_internet_connection: bool,
+        result: str) -> None:
 
     with (patch("app.main.valid_google_url",
-               return_value=valid_google_url),
+                return_value=valid_google_url),
+
           patch("app.main.has_internet_connection",
                 return_value=has_internet_connection)):
 
