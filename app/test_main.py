c5dc6169a0f085829cc67dc5ac7309c4174dfd5b
Sviatoslav Pokhvalenko <pokhvalenkosvyat@gmail.com>
2025-09-09 20:53:59 +0200

    Resolved issues

--- app/test_main.py
+++ app/test_main.py
@@ -1,22 +1,8 @@
-from unittest import mock
 from typing import Callable
 import pytest
 
 
 from app.main import can_access_google_page
-
-
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page_calls_both_functions(
-    mocked_has_internet_connection: mock.MagicMock,
-    mocked_valid_google_url: mock.MagicMock
-) -> None:
-    can_access_google_page("url::some_url")
-    mocked_valid_google_url.assert_called_once_with(
-        "url::some_url"
-    )
-    mocked_has_internet_connection.assert_called_once()
 
 
 @pytest.fixture()
