8a663ac7991c56bf65a173edac1af5ba5f8c4485
Sviatoslav Pokhvalenko <pokhvalenkosvyat@gmail.com>
2025-09-09 19:25:26 +0200

    Fix flake8

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,4 @@
 from unittest import mock
-from typing import Callable
 
 
 from app.main import can_access_google_page
@@ -8,8 +7,8 @@ from app.main import can_access_google_page
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 def test_can_access_google_page_calls_both_functions(
-    mocked_has_internet_connection,
-    mocked_valid_google_url
+    mocked_has_internet_connection: mock.MagicMock,
+    mocked_valid_google_url: mock.MagicMock
 ) -> None:
     can_access_google_page("url::some_url")
     mocked_valid_google_url.assert_called_once_with("url::some_url")
