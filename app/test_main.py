a98124087f45236b92549033af47e7bcf91bae2a
Vasyl <vasiliy.azarov.vl1@gmail.com>
2025-09-10 17:05:44 +0200

    Solution #2

--- app/test_main.py
+++ app/test_main.py
@@ -4,26 +4,35 @@ from app.main import can_access_google_page
 
 def test_accessible() -> None:
     with patch("app.main.has_internet_connection", return_value=True):
-        with patch("app.main.valid_google_url", return_value=True):
-            assert can_access_google_page("http://google.com") == "Accessible"
+        with patch("app.main.valid_google_url",
+                   return_value=True) as mock_valid:
+            result = can_access_google_page("http://google.com")
+            assert result == "Accessible"
+            mock_valid.assert_called_once_with("http://google.com")
 
 
 def test_not_accessible_invalid_url() -> None:
     with patch("app.main.has_internet_connection", return_value=True):
-        with patch("app.main.valid_google_url", return_value=False):
-            assert can_access_google_page("http://google.com") \
-                == "Not accessible"
+        with patch("app.main.valid_google_url",
+                   return_value=False) as mock_valid:
+            result = can_access_google_page("http://google.com")
+            assert result == "Not accessible"
+            mock_valid.assert_called_once_with("http://google.com")
 
 
 def test_not_accessible_no_internet() -> None:
     with patch("app.main.has_internet_connection", return_value=False):
-        with patch("app.main.valid_google_url", return_value=True):
-            assert can_access_google_page("http://google.com") \
-                == "Not accessible"
+        with patch("app.main.valid_google_url",
+                   return_value=True) as mock_valid:
+            result = can_access_google_page("http://google.com")
+            assert result == "Not accessible"
+            mock_valid.assert_called_once_with("http://google.com")
 
 
 def test_not_accessible_both_false() -> None:
     with patch("app.main.has_internet_connection", return_value=False):
-        with patch("app.main.valid_google_url", return_value=False):
-            assert can_access_google_page("http://google.com") \
-                == "Not accessible"
+        with patch("app.main.valid_google_url",
+                   return_value=False) as mock_valid:
+            result = can_access_google_page("http://google.com")
+            assert result == "Not accessible"
+            mock_valid.assert_called_once_with("http://google.com")
