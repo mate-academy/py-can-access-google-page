a81fcf869522853f1f32899941759739657b63ce
Maksym Kiporenko <kiporenkomaks8@gmail.com>
2025-09-17 21:17:41 +0300

    Complete3

--- app/test_main.py
+++ app/test_main.py
@@ -5,7 +5,10 @@ from app.main import can_access_google_page
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_can_access_google_page(mock_has_internet: MagicMock, mock_valid_url: MagicMock) -> None:
+def test_can_access_google_page(
+        mock_has_internet: MagicMock,
+        mock_valid_url: MagicMock
+) -> None:
     mock_has_internet.return_value = True
     mock_valid_url.return_value = True
 
