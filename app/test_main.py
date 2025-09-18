cfdf56021128ed63f3ef2e28d8cf131b959e029a
Ihor Moskalenko <ingvarsvensson7@gmail.com>
2025-09-19 00:36:51 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -6,9 +6,12 @@ from app.main import can_access_google_page
 
 
 @pytest.mark.parametrize(
-    "has_net_connection, valid_url",
+    "has_net_connection, valid_url, is_connect",
     [
-        (True, True)
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible"),
     ]
 )
 @mock.patch("app.main.valid_google_url")
@@ -16,30 +19,8 @@ from app.main import can_access_google_page
 def test_can_access_google_page(mock_has_internet_connection: mock,
                                 mock_valid_google_url: mock,
                                 has_net_connection: bool | int,
-                                valid_url: bool | int) -> None:
+                                valid_url: bool | int,
+                                is_connect: str) -> None:
     mock_has_internet_connection.return_value = has_net_connection
     mock_valid_google_url.return_value = valid_url
-    can_access_google_page("https://www.google.com")
-    mock_has_internet_connection.assert_called_once()
-    mock_valid_google_url.assert_called_once()
-
-
-@pytest.mark.parametrize(
-    "has_net_connection, valid_url",
-    [
-        (False, True),
-        (False, False)
-    ]
-)
-@mock.patch("app.main.valid_google_url")
-@mock.patch("app.main.has_internet_connection")
-def test_can_not_access_google_page(mock_has_internet_connection: mock,
-                                    mock_valid_google_url: mock,
-                                    has_net_connection: bool | int,
-                                    valid_url: bool | int) -> None:
-    with pytest.raises(AssertionError):
-        mock_has_internet_connection.return_value = has_net_connection
-        mock_valid_google_url.return_value = valid_url
-        can_access_google_page("https://www.google.com")
-        mock_has_internet_connection.assert_called_once()
-        mock_valid_google_url.assert_called_once()
+    assert (can_access_google_page("https://www.google.com") == is_connect)
