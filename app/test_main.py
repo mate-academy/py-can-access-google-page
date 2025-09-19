d9a131fe449e9e38b41aa36d1bdfc9984b9a724b
Radoslaw Ptak <radek1026@interi.pl>
2025-09-19 21:32:50 +0200

    Change in implement py-can-access-google-page

--- app/test_main.py
+++ app/test_main.py
@@ -1,23 +1,46 @@
 from unittest import mock
-
 from main import can_access_google_page
 
 
 @mock.patch("main.valid_google_url")
 @mock.patch("main.has_internet_connection")
-def test_can_access_google_page(
+def test_accessible_when_url_and_connection_are_true(
         mock_has_conn: mock.MagicMock,
         mock_valid_url: mock.MagicMock
 ) -> None:
     mock_has_conn.return_value = True
     mock_valid_url.return_value = True
     assert can_access_google_page("https://google.com") == "Accessible"
+
+
+@mock.patch("main.valid_google_url")
+@mock.patch("main.has_internet_connection")
+def test_not_accessible_when_only_url_is_true(
+        mock_has_conn: mock.MagicMock,
+        mock_valid_url: mock.MagicMock
+) -> None:
     mock_has_conn.return_value = False
     mock_valid_url.return_value = True
     assert can_access_google_page("https://google.com") == "Not accessible"
+
+
+@mock.patch("main.valid_google_url")
+@mock.patch("main.has_internet_connection")
+def test_not_accessible_when_only_connection_is_true(
+        mock_has_conn: mock.MagicMock,
+        mock_valid_url: mock.MagicMock
+) -> None:
     mock_has_conn.return_value = True
     mock_valid_url.return_value = False
     assert can_access_google_page("https://google.com") == "Not accessible"
+
+
+@mock.patch("main.valid_google_url")
+@mock.patch("main.has_internet_connection")
+def test_not_accessible_when_both_are_false(
+        mock_has_conn: mock.MagicMock,
+        mock_valid_url: mock.MagicMock
+) -> None:
     mock_has_conn.return_value = False
     mock_valid_url.return_value = False
     assert can_access_google_page("https://google.com") == "Not accessible"
