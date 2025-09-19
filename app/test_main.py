d895006a4b18b2b4710e9e91415ca2b23312a6a8
Radoslaw Ptak <radek1026@interi.pl>
2025-09-19 21:02:15 +0200

    Implement py-can-access-google-page

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,23 @@
-# write your code here
+from unittest import mock
+
+from main import can_access_google_page
+
+
+@mock.patch("main.valid_google_url")
+@mock.patch("main.has_internet_connection")
+def test_can_access_google_page(
+        mock_has_conn: mock.MagicMock,
+        mock_valid_url: mock.MagicMock
+) -> None:
+    mock_has_conn.return_value = True
+    mock_valid_url.return_value = True
+    assert can_access_google_page("https://google.com") == "Accessible"
+    mock_has_conn.return_value = False
+    mock_valid_url.return_value = True
+    assert can_access_google_page("https://google.com") == "Not accessible"
+    mock_has_conn.return_value = True
+    mock_valid_url.return_value = False
+    assert can_access_google_page("https://google.com") == "Not accessible"
+    mock_has_conn.return_value = False
+    mock_valid_url.return_value = False
+    assert can_access_google_page("https://google.com") == "Not accessible"
