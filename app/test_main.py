e510dea28ac59f757983a830933b74b619a61602
Gena <gena.anzin@ukr.net>
2025-09-19 13:43:14 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -7,19 +7,19 @@ def test_valid_url_and_connection_returns_accessible(mocker: any) -> None:
     assert can_access_google_page("google.com") == "Accessible"
 
 
-def test_valid_url_and_connection_exists_with_first_False(mocker: any) -> None:
+def test_valid_url_and_connection_exists_with_first_false(mocker: any) -> None:
     mocker.patch("app.main.valid_google_url", return_value=True)
     mocker.patch("app.main.has_internet_connection", return_value=False)
     assert can_access_google_page("google.com") == "Not accessible"
 
 
-def test_valid_url_and_connection_exists_with_oll_False(mocker: any) -> None:
+def test_valid_url_and_connection_exists_with_oll_false(mocker: any) -> None:
     mocker.patch("app.main.valid_google_url", return_value=False)
     mocker.patch("app.main.has_internet_connection", return_value=False)
     assert can_access_google_page("google.com") == "Not accessible"
 
 
-def test_valid_url_and_connection_exists_with_second_False(mocker: any) -> None:
+def test_valid_url_and_connection_exists_with_two_false(mocker: any) -> None:
     mocker.patch("app.main.valid_google_url", return_value=False)
     mocker.patch("app.main.has_internet_connection", return_value=True)
     assert can_access_google_page("google.com") == "Not accessible"
