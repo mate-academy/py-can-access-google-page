0d73fe0b8a0636f02370beed2581a70695e515bf
Iago Vinícius Souza de Sales <pristonvinicius@hotmail.com>
2025-09-27 16:46:35 -0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,13 @@
-# write your code here
+from app import main
+
+def test_can_access_google_page(monkeypatch):
+    def mock_valid_google_url(url):
+        return True
+    
+    def mock_has_internet_connection():
+        return True
+
+    monkeypatch.setattr(main, 'valid_google_url', mock_valid_google_url)
+    monkeypatch.setattr(main, 'has_internet_connection', mock_has_internet_connection)
+
+    assert main.can_access_google_page("http://www.google.com") == "Accessible"
