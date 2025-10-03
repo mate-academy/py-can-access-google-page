114725512570c161aa524b855157a6cf8f41efa5
Oleksandra Tymoshenko <aleksacat13@gmail.com>
2025-10-03 22:50:03 +0200

    flake8 fixed

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,7 @@
+from collections.abc import Generator
+
+from unittest.mock import Mock
+
 from unittest import mock
 
 import pytest
@@ -6,29 +10,45 @@ from app.main import can_access_google_page
 
 
 @pytest.fixture
-def mocked_valid_google_url():
+def mocked_valid_google_url() -> Generator:
     with mock.patch("app.main.valid_google_url") as mock_valid_google_url:
         yield mock_valid_google_url
 
+
 @pytest.fixture
-def mocked_has_internet_connection():
-    with mock.patch("app.main.has_internet_connection") as mock_has_internet_connection:
+def mocked_has_internet_connection() -> Generator:
+    with (
+        mock.patch("app.main.has_internet_connection")
+        as mock_has_internet_connection
+    ):
         yield mock_has_internet_connection
 
-def test_func_usage_valid_google(mocked_valid_google_url):
+
+def test_func_usage_valid_google(mocked_valid_google_url: Mock) -> None:
     can_access_google_page("https://www.google.com")
     mocked_valid_google_url.assert_called_once()
 
-def test_func_usage_has_internet_connection(mocked_has_internet_connection):
+
+def test_func_usage_has_internet_connection(
+        mocked_has_internet_connection: Mock
+) -> None:
     can_access_google_page("https://www.google.com")
     mocked_has_internet_connection.assert_called_once()
 
-def test_main_func_access(mocked_valid_google_url, mocked_has_internet_connection):
+
+def test_main_func_access(
+        mocked_valid_google_url: Mock,
+        mocked_has_internet_connection: Mock
+) -> None:
     mocked_valid_google_url.return_value = True
     mocked_has_internet_connection.return_value = True
     assert can_access_google_page("https://www.google.com") == "Accessible"
 
-def test_main_func_not_access(mocked_valid_google_url, mocked_has_internet_connection):
+
+def test_main_func_not_access(
+        mocked_valid_google_url: Mock,
+        mocked_has_internet_connection: Mock
+) -> None:
     mocked_valid_google_url.return_value = False
     mocked_has_internet_connection.return_value = False
     assert can_access_google_page("https://www.google.com") == "Not accessible"
