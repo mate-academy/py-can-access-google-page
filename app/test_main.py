87faa886bd29f712dff23c73f0772230fcf320d8
Kacper Kogut <kogutkacper7@gmail.com>
2025-10-12 21:50:31 +0200

    Soluton

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,68 @@
-# write your code here
+from unittest import mock
+from unittest.mock import MagicMock
+from app.main import can_access_google_page
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_url_true_internet_true(
+        mocked_internet: MagicMock,
+        mocked_url: MagicMock
+) -> None:
+    mocked_internet.return_value = True
+    mocked_url.return_value = True
+
+    result = can_access_google_page(mocked_url)
+
+    assert result == "Accessible"
+
+    mocked_internet.assert_called_once()
+    mocked_url.assert_called_once()
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_url_false_internet_true(mocked_internet: MagicMock,
+                                 mocked_url: MagicMock
+                                 ) -> None:
+    mocked_internet.return_value = True
+    mocked_url.return_value = False
+
+    results = can_access_google_page(mocked_url)
+
+    assert results == "Not accessible"
+
+    mocked_internet.assert_called_once()
+    mocked_url.assert_called_once()
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_url_true_internet_false(mocked_internet: MagicMock,
+                                 mocked_url: MagicMock
+                                 ) -> None:
+    mocked_internet.return_value = False
+    mocked_url.return_value = True
+
+    results = can_access_google_page(mocked_url)
+
+    assert results == "Not accessible"
+
+    mocked_internet.assert_called_once()
+    mocked_url.assert_not_called()
+
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_url_false_internet_false(mocked_internet: MagicMock,
+                                  mocked_url: MagicMock
+                                  ) -> None:
+    mocked_internet.return_value = False
+    mocked_url.return_value = False
+
+    results = can_access_google_page(mocked_url)
+
+    assert results == "Not accessible"
+
+    mocked_internet.assert_called_once()
+    mocked_url.assert_not_called()
