ab368ff5edf2508946166bf9cdfcfb6db5b44a79
Dawid <kucharzjaw@gmail.com>
2025-10-16 10:20:42 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,38 @@
-# write your code here
+from unittest.mock import patch, MagicMock
+from app.main import can_access_google_page
+
+
+@patch("app.main.has_internet_connection")
+@patch("app.main.valid_google_url")
+def test_can_access_google_page(
+        mock_valid_google_url: MagicMock,
+        mock_has_internet_connection: MagicMock
+) -> None:
+    mock_valid_google_url.return_value = True
+    mock_has_internet_connection.return_value = True
+    result = can_access_google_page("https://google.com")
+    assert result == "Accessible"
+
+
+@patch("app.main.has_internet_connection")
+@patch("app.main.valid_google_url")
+def test_can_access_google_page_invalid_url(
+        mock_valid_google_url: MagicMock,
+        mock_has_internet_connection: MagicMock
+) -> None:
+    mock_valid_google_url.return_value = False
+    mock_has_internet_connection.return_value = False
+    result = can_access_google_page("https://fake.com")
+    assert result == "Not accessible"
+
+
+@patch("app.main.has_internet_connection")
+@patch("app.main.valid_google_url")
+def test_can_access_google_page_not_internet_connection(
+        mock_valid_google_url: MagicMock,
+        mock_has_internet_connection: MagicMock
+) -> None:
+    mock_valid_google_url.return_value = True
+    mock_has_internet_connection.return_value = False
+    result = can_access_google_page("https://google.com")
+    assert result == "Not accessible"
