7e0f16a976888cde274b9581ad51283590c9bf30
acidcor <deadfoxua1337@gmail.com>
2025-10-20 16:11:57 +0200

    style fix

--- app/test_main.py
+++ app/test_main.py
@@ -8,8 +8,8 @@ from app.main import can_access_google_page
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
 def test_valid_url_and_connection_exists(
-        mocked_url_validator: mock.MagicMock,
-        mocked_internet_connection: mock.MagicMock
+    mocked_url_validator: mock.MagicMock,
+    mocked_internet_connection: mock.MagicMock
 ) -> None:
     url = "test.url/"
     can_access_google_page(url)
@@ -23,17 +23,18 @@ def test_valid_url_and_connection_exists(
         pytest.param(True, False, "Not accessible", id="TF"),
         pytest.param(False, True, "Not accessible", id="FT"),
         pytest.param(True, True, "Accessible", id="TT"),
-        pytest.param(False, False, "Not accessible", id="FF")
-    ]
+        pytest.param(False, False, "Not accessible", id="FF"),
+    ],
 )
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
-def test_can_access_google_page_by_bool(mocked_valid_google_url: mock.MagicMock,
-                                        mocked_has_internet_connection: mock.MagicMock,
-                                        valid_google_url: bool,
-                                        has_internet_connection: bool,
-                                        expected_result: str
-                                        ) -> None:
+def test_can_access_google_page_by_bool(
+    mocked_valid_google_url: mock.MagicMock,
+    mocked_has_internet_connection: mock.MagicMock,
+    valid_google_url: bool,
+    has_internet_connection: bool,
+    expected_result: str,
+) -> None:
     mocked_valid_google_url.return_value = valid_google_url
     mocked_has_internet_connection.return_value = has_internet_connection
     assert can_access_google_page("") == expected_result
