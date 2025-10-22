2cbdec63d0b6934442c2a935b0f94389f336a307
Yevhen Dmytrenko <zdmitrenko26@gmail.com>
2025-10-23 01:58:38 +0300

    Fix flake8 errors

--- app/test_main.py
+++ app/test_main.py
@@ -1,13 +1,14 @@
 from unittest import mock
+from typing import Any
 
 from app.main import can_access_google_page
 
 
 @mock.patch("app.main.has_internet_connection")
 def test_can_access_google_page(
-        mocked_has_internet_connection
-        ) -> None:
-    with mock.patch("app.main.valid_google_url") as mocked_valid_google_url:
+        mocked_has_internet_connection: Any
+) -> None:
+    with mock.patch("app.main.valid_google_url") as mocked_valid_url:
         can_access_google_page("https://www.google.com/")
         mocked_has_internet_connection.assert_called_once()
-        mocked_valid_google_url.assert_called_once_with("https://www.google.com/")
+        mocked_valid_url.assert_called_once_with("https://www.google.com/")
