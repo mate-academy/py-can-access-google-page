6e44febaa4fe1a9271bf36d417297a6b4a7bce9e
Vitalii Stetsenko <stvitalik01062006@gmail.com>
2025-10-23 15:52:41 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,30 @@
-# write your code here
+from unittest import mock
+
+from app.main import can_access_google_page
+
+
+@mock.patch("app.main.valid_google_url", return_value=True)
+@mock.patch("app.main.has_internet_connection", return_value=True)
+def test_can_access_google_page_with_true_true(
+        mock_connection: mock.MagicMock,
+        mock_valid: mock.MagicMock
+) -> None:
+    assert can_access_google_page("https://google.com") == "Accessible"
+
+
+@mock.patch("app.main.valid_google_url", return_value=False)
+@mock.patch("app.main.has_internet_connection", return_value=True)
+def test_can_access_google_page_with_false_true(
+        mock_connection: mock.MagicMock,
+        mock_valid: mock.MagicMock
+) -> None:
+    assert can_access_google_page("https://google.com") == "Not accessible"
+
+
+@mock.patch("app.main.valid_google_url", return_value=True)
+@mock.patch("app.main.has_internet_connection", return_value=False)
+def test_can_access_google_page_with_true_false(
+        mock_connection: mock.MagicMock,
+        mock_valid: mock.MagicMock
+) -> None:
+    assert can_access_google_page("https://google.com") == "Not accessible"
