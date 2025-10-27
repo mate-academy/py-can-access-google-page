46d2b3d776e64c8d3af4de90a28bd1472767054d
Rodion <r.demianetss@gmail.com>
2025-10-27 19:26:27 +0200

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -13,14 +13,14 @@ def test_page_is_accessible_when_url_is_valid_and_connection_exists() -> None:
 
 
 def test_page_is_not_accessible_when_url_is_invalid() -> None:
-    with patch("app.main.has_internet_connection", return_value=False), \
-            patch("app.main.valid_google_url", return_value=True):
+    with patch("app.main.has_internet_connection", return_value=True), \
+            patch("app.main.valid_google_url", return_value=False):
         assert can_access_google_page(url) == "Not accessible"
 
 
 def test_page_is_not_accessible_when_connection_missing() -> None:
-    with patch("app.main.has_internet_connection", return_value=True), \
-            patch("app.main.valid_google_url", return_value=False):
+    with patch("app.main.has_internet_connection", return_value=False), \
+            patch("app.main.valid_google_url", return_value=True):
         assert can_access_google_page(url) == "Not accessible"
 
 
