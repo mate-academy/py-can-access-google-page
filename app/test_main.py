0cf4e4d3f88f2bdaebf2b899b38b1089e9f6a72e
Olena Pyslar <pislar33@gmail.com>
2025-10-29 15:15:01 +0200

    Fix Solution 1

--- app/test_main.py
+++ app/test_main.py
@@ -5,8 +5,9 @@ from app.main import can_access_google_page
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_access_is_good(mock_has_internet_connection: Callable,
-                        mock_valid_google_url: Callable) -> None:
+def est_accessible_when_url_is_valid_and_connection_exists(
+        mock_has_internet_connection: Callable,
+        mock_valid_google_url: Callable) -> None:
     mock_valid_google_url.return_value = True
     mock_has_internet_connection.return_value = True
     assert can_access_google_page("https://www.google.com") == "Accessible"
@@ -14,8 +15,9 @@ def test_access_is_good(mock_has_internet_connection: Callable,
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_access_is_not_good(mock_has_internet_connection: Callable,
-                            mock_valid_google_url: Callable) -> None:
+def test_not_accessible_when_url_and_connection_is_invalid(
+        mock_has_internet_connection: Callable,
+        mock_valid_google_url: Callable) -> None:
     mock_valid_google_url.return_value = False
     mock_has_internet_connection.return_value = False
     assert can_access_google_page("//www.google") == "Not accessible"
@@ -23,7 +25,7 @@ def test_access_is_not_good(mock_has_internet_connection: Callable,
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_access_is_not_good_from_one_error(
+def test_not_accessible_when_url_is_invalid_but_connection_exists(
         mock_has_internet_connection: Callable,
         mock_valid_google_url: Callable) -> None:
     mock_valid_google_url.return_value = False
@@ -33,7 +35,7 @@ def test_access_is_not_good_from_one_error(
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_access_is_not_good_from_another_error(
+def test_not_accessible_when_url_exist_but_connection_is_invalid(
         mock_has_internet_connection: Callable,
         mock_valid_google_url: Callable) -> None:
     mock_valid_google_url.return_value = True
