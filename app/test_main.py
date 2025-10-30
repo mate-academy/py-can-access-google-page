681e585498d1f58cf0dafbb2103a339a56c64b31
Vladislav <grechkosij.vlados@gmail.com>
2025-10-30 17:29:06 +0200

    Fix1

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,22 @@
-from pytest import MonkeyPatch, mark
+from pytest import MonkeyPatch, mark, param
 import app.main as main
 from app.main import can_access_google_page
 
 
-@mark.parametrize("valid_url, has_internet, result", [
-    (True, True, "Accessible"),
-    (False, False, "Not accessible"),
-    (True, False, "Not accessible"),
-    (False, True, "Not accessible")
+@mark.parametrize("is_valid_url, has_connection, expected", [
+    param(True, True, "Accessible", id="valid-url_with_internet"),
+    param(False, False, "Not accessible", id="invalid-url_no-internet"),
+    param(True, False, "Not accessible", id="valid-url_no-internet"),
+    param(False, True, "Not accessible", id="invalid-url_with-internet")
 ])
-def test_can_access_google_page(
+def test_can_access_google_page_various_scenarios(
         monkeypatch: MonkeyPatch,
-        valid_url: bool,
-        has_internet: bool,
-        result: str
+        is_valid_url: bool,
+        has_connection: bool,
+        expected: str
 ) -> None:
-    monkeypatch.setattr(main, "valid_google_url", lambda x: valid_url)
-    monkeypatch.setattr(main, "has_internet_connection", lambda: has_internet)
+    monkeypatch.setattr(main, "valid_google_url", lambda x: is_valid_url)
+    monkeypatch.setattr(main,
+                        "has_internet_connection", lambda: has_connection)
 
-    assert can_access_google_page("url") == result
+    assert can_access_google_page("url") == expected
