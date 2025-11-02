2719a8cfac964cc4282780e275c801692a72ec29
Lana Topchii <topchii.svitlana@gmail.com>
2025-11-02 22:23:39 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -7,7 +7,7 @@ from app.main import can_access_google_page
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 def test_valid_url_and_connection_exists(
-    mocked_connection, mocked_valid
+    mocked_connection: mock.MagicMock, mocked_valid: mock.MagicMock
 ) -> None:
     mocked_valid.return_value = True
     mocked_connection.return_value = True
@@ -18,7 +18,7 @@ def test_valid_url_and_connection_exists(
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 def test_valid_url_but_no_connection(
-    mocked_connection, mocked_valid
+    mocked_connection: mock.MagicMock, mocked_valid: mock.MagicMock
 ) -> None:
     mocked_valid.return_value = True
     mocked_connection.return_value = False
@@ -29,7 +29,7 @@ def test_valid_url_but_no_connection(
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 def test_invalid_url_even_with_connection(
-    mocked_connection, mocked_valid
+    mocked_connection: mock.MagicMock, mocked_valid: mock.MagicMock
 ) -> None:
     mocked_valid.return_value = False
     mocked_connection.return_value = True
@@ -40,7 +40,7 @@ def test_invalid_url_even_with_connection(
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 def test_invalid_url_and_no_connection(
-    mocked_connection, mocked_valid
+    mocked_connection: mock.MagicMock, mocked_valid: mock.MagicMock
 ) -> None:
     mocked_valid.return_value = False
     mocked_connection.return_value = False
