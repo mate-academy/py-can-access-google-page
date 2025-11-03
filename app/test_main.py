c6fa62916ac94d0dd5e350a65ac8625c45913ab6
Artur <arturvovk780@gmail.com>
2025-11-03 14:43:45 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,29 @@
-# write your code here
+import pytest
+from app.main import can_access_google_page
+
+
+@pytest.mark.parametrize(
+    "has_internet, valid_url, expected",
+    [
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible"),
+    ]
+)
+def test_can_access_google_page(
+        monkeypatch: pytest.MonkeyPatch,
+        has_internet: bool,
+        valid_url: bool,
+        expected: bool
+) -> None:
+    monkeypatch.setattr(
+        "app.main.has_internet_connection",
+        lambda: has_internet
+    )
+    monkeypatch.setattr(
+        "app.main.valid_google_url",
+        lambda url: valid_url
+    )
+    result = can_access_google_page("https://www.google.com")
+    assert result == expected
