2c170e219685fdc97931a4cc9fc15077c5bc9a2b
Artur <arturvovk780@gmail.com>
2025-11-03 14:49:07 +0200

    fixed name test and type

--- app/test_main.py
+++ app/test_main.py
@@ -11,11 +11,11 @@ from app.main import can_access_google_page
         (False, False, "Not accessible"),
     ]
 )
-def test_can_access_google_page(
+def test_can_access_google_page_scenarios(
         monkeypatch: pytest.MonkeyPatch,
         has_internet: bool,
         valid_url: bool,
-        expected: bool
+        expected: str
 ) -> None:
     monkeypatch.setattr(
         "app.main.has_internet_connection",
