d55e83b295fc68fa029a24c908e6055c704ea504
Vadym Vynokur <king.krollie@gmail.com>
2025-11-07 13:23:50 +0200

    add tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,15 @@
-# write your code here
+from unittest import mock
+
+from app.main import can_access_google_page
+
+
+@mock.patch("requests.get")
+def test_valid_google_url_called(mock_get: mock.MagicMock) -> None:
+    can_access_google_page("https://google.com")
+    mock_get.assert_called_with("https://google.com")
+
+
+@mock.patch("app.main.datetime")
+def test_has_internet_connection_called(mock_datetime: mock.MagicMock) -> None:
+    can_access_google_page("https://google.com")
+    mock_datetime.datetime.now.assert_called()
