3989ba36085ba5318a2b3a0f6f5f94b2f4761669
Vorongor <mr.maddarknes@gmail.com>
2025-11-07 08:32:15 +0200

    Change url to constants, replase / with ()

--- app/test_main.py
+++ app/test_main.py
@@ -5,6 +5,7 @@ from app.main import can_access_google_page
 valid_url = "https://google.com"
 invalid_url = "https://fake.com"
 
+
 def test_can_access_google_page() -> None:
     with (mock.patch("app.main.valid_google_url", return_value=True),
          mock.patch("app.main.has_internet_connection", return_value=True)):
@@ -28,5 +29,5 @@ def test_can_access_google_page_not_accessible_no_internet() -> None:
 
 def test_can_access_to_page_no_internet_and_fake_page() -> None:
     with (mock.patch("app.main.valid_google_url", return_value=False),
-            mock.patch("app.main.has_internet_connection", return_value=False)):
+          mock.patch("app.main.has_internet_connection", return_value=False)):
         assert can_access_google_page(valid_url) == "Not accessible"
