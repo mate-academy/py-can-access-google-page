21d0fbb156d01a6635e011b4d5c48a0154ec6a4e
Wojciech Witkowski <witus85@gmail.com>
2025-11-09 16:59:03 +0100

    updated names of test functions

--- app/test_main.py
+++ app/test_main.py
@@ -18,37 +18,37 @@ def mock_connection() -> Generator[MagicMock | AsyncMock]:
         yield mocked_connection
 
 
-def test_valid_url_and_connected(
+def test_returns_accessible_when_url_is_valid_and_is_connected(
         mock_connection: MagicMock | AsyncMock,
         mock_url_validation: MagicMock | AsyncMock
 ) -> None:
-    mock_connection.return_value = True
     mock_url_validation.return_value = True
+    mock_connection.return_value = True
     assert can_access_google_page("url.com") == "Accessible"
 
 
-def test_valid_url_and_no_connection(
+def test_returns_not_accessible_when_url_is_valid_but_no_connection(
         mock_connection: mock.MagicMock | mock.AsyncMock,
         mock_url_validation: mock.MagicMock | mock.AsyncMock
 ) -> None:
-    mock_connection.return_value = False
     mock_url_validation.return_value = True
-    assert can_access_google_page("url") == "Not accessible"
-
-
-def test_connected_and_invalid_url(
-        mock_connection: mock.MagicMock | mock.AsyncMock,
-        mock_url_validation: mock.MagicMock | mock.AsyncMock
-) -> None:
-    mock_connection.return_value = True
-    mock_url_validation.return_value = False
-    assert can_access_google_page("url") == "Not accessible"
-
-
-def test_invalid_url_and_no_connection(
-        mock_connection: mock.MagicMock | mock.AsyncMock,
-        mock_url_validation: mock.MagicMock | mock.AsyncMock
-) -> None:
     mock_connection.return_value = False
+    assert can_access_google_page("url") == "Not accessible"
+
+
+def test_returns_not_accessible_when_is_connected_but_url_is_not_valid(
+        mock_connection: mock.MagicMock | mock.AsyncMock,
+        mock_url_validation: mock.MagicMock | mock.AsyncMock
+) -> None:
     mock_url_validation.return_value = False
+    mock_connection.return_value = True
+    assert can_access_google_page("url") == "Not accessible"
+
+
+def test_returns_not_accessible_when_url_is_not_valid_and_no_connection(
+        mock_connection: mock.MagicMock | mock.AsyncMock,
+        mock_url_validation: mock.MagicMock | mock.AsyncMock
+) -> None:
+    mock_url_validation.return_value = False
+    mock_connection.return_value = False
     assert can_access_google_page("url") == "Not accessible"
