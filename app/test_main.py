0b5256a4729cd4cb30532b65848de27545988d76
Oleh Skibinskyi <oleg.sk2002@gmail.com>
2025-11-10 16:53:23 +0200

    Updated according to review

--- app/test_main.py
+++ app/test_main.py
@@ -8,6 +8,9 @@ import pytest
 from app.main import can_access_google_page
 
 
+link = "https://google.com"
+
+
 @pytest.fixture()
 def mocked_valid_google_url() -> Any:
     with mock.patch("app.main.valid_google_url") as mock_valid_google_url:
@@ -25,9 +28,9 @@ def test_access_google_page_calls_functions(
         mocked_valid_google_url: Any,
         mocked_has_internet_connection: Any
 ) -> None:
-    can_access_google_page("https://google.com")
+    can_access_google_page(link)
     mocked_has_internet_connection.assert_called_once()
-    mocked_valid_google_url.assert_called_once_with("https://google.com")
+    mocked_valid_google_url.assert_called_once_with(link)
 
 
 def test_access_google_page_no_internet_connection(
@@ -36,7 +39,7 @@ def test_access_google_page_no_internet_connection(
 ) -> None:
     mocked_has_internet_connection.return_value = False
     mocked_valid_google_url.return_value = True
-    assert can_access_google_page("https://google.com") == "Not accessible"
+    assert can_access_google_page(link) == "Not accessible"
 
 
 def test_access_google_page_invalid_google_url(
@@ -45,8 +48,13 @@ def test_access_google_page_invalid_google_url(
 ) -> None:
     mocked_has_internet_connection.return_value = True
     mocked_valid_google_url.return_value = False
-    assert can_access_google_page("https://gyyhli.cim") == "Not accessible"
+    assert can_access_google_page(link) == "Not accessible"
 
 
-def test_access_google_page_correct_return() -> None:
-    assert can_access_google_page("https://google.com") == "Accessible"
+def test_access_google_page_when_accessible(
+        mocked_valid_google_url: Any,
+        mocked_has_internet_connection: Any
+) -> None:
+    mocked_has_internet_connection.return_value = True
+    mocked_valid_google_url.return_value = True
+    assert can_access_google_page("") == "Accessible"
