f83ff2728aad2add6e0934b12cb9b9f3b66d0082
Siarhei Nestsiaronak <nesterka375@gmail.com>
2025-11-10 18:37:24 +0100

    solved py-can-access-google-page

--- app/test_main.py
+++ app/test_main.py
@@ -5,8 +5,8 @@ from app.main import can_access_google_page
 @patch("app.main.has_internet_connection")
 @patch("app.main.valid_google_url")
 def test_can_access_google_page_accessible(
+        mock_has_internet_connection: MagicMock,
         mock_valid_google_url: MagicMock,
-        mock_has_internet_connection: MagicMock
 ) -> None:
     mock_valid_google_url.return_value = True
     mock_has_internet_connection.return_value = True
@@ -16,9 +16,9 @@ def test_can_access_google_page_accessible(
 
 @patch("app.main.has_internet_connection")
 @patch("app.main.valid_google_url")
-def test_can_access_google_page_not_accessible_first(
+def test_can_access_google_page_not_accessible_invalid_internet_connection(
+        mock_has_internet_connection: MagicMock,
         mock_valid_google_url: MagicMock,
-        mock_has_internet_connection: MagicMock
 ) -> None:
     mock_valid_google_url.return_value = True
     mock_has_internet_connection.return_value = False
@@ -28,9 +28,9 @@ def test_can_access_google_page_not_accessible_first(
 
 @patch("app.main.has_internet_connection")
 @patch("app.main.valid_google_url")
-def test_can_access_google_page_not_accessible_second(
+def test_can_access_google_page_not_accessible_invalid_google_url(
+        mock_has_internet_connection: MagicMock,
         mock_valid_google_url: MagicMock,
-        mock_has_internet_connection: MagicMock
 ) -> None:
     mock_valid_google_url.return_value = False
     mock_has_internet_connection.return_value = True
@@ -40,9 +40,9 @@ def test_can_access_google_page_not_accessible_second(
 
 @patch("app.main.has_internet_connection")
 @patch("app.main.valid_google_url")
-def test_can_access_google_page_not_accessible_third(
+def test_can_access_google_page_not_accessible_invalid_connection_and_url(
+        mock_has_internet_connection: MagicMock,
         mock_valid_google_url: MagicMock,
-        mock_has_internet_connection: MagicMock
 ) -> None:
     mock_valid_google_url.return_value = False
     mock_has_internet_connection.return_value = False
