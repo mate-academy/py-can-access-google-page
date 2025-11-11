25a374f60ae47f2377212473bf60c1e3d860760c
Andrii Yermolenko <vakt159@gmail.com>
2025-11-11 20:03:39 +0200

    Changed order of parameters in tests

--- app/test_main.py
+++ app/test_main.py
@@ -7,8 +7,8 @@ from app.main import can_access_google_page
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 def test_can_access_google_page_if_valid_url_and_has_internet_connection(
-        valid_google_url: Mock,
-        has_internet_connection: Mock
+        has_internet_connection : Mock,
+        valid_google_url: Mock
 ) -> None:
     valid_google_url.return_value = True
     has_internet_connection.return_value = True
@@ -18,8 +18,8 @@ def test_can_access_google_page_if_valid_url_and_has_internet_connection(
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 def test_cant_access_google_page_if_not_valid_url_and_has_internet_connection(
-        valid_google_url: Mock,
-        has_internet_connection: Mock
+        has_internet_connection: Mock,
+        valid_google_url: Mock
 ) -> None:
     valid_google_url.return_value = False
     has_internet_connection.return_value = True
@@ -29,8 +29,8 @@ def test_cant_access_google_page_if_not_valid_url_and_has_internet_connection(
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 def test_cant_access_google_page_if_not_valid_url_and_no_internet_connection(
-        valid_google_url: Mock,
-        has_internet_connection: Mock
+        has_internet_connection: Mock,
+        valid_google_url: Mock
 ) -> None:
     valid_google_url.return_value = False
     has_internet_connection.return_value = False
@@ -40,8 +40,8 @@ def test_cant_access_google_page_if_not_valid_url_and_no_internet_connection(
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 def test_cant_access_google_page_if_valid_url_and_no_internet_connection(
-        valid_google_url: Mock,
-        has_internet_connection: Mock
+        has_internet_connection: Mock,
+        valid_google_url: Mock
 ) -> None:
     valid_google_url.return_value = True
     has_internet_connection.return_value = False
