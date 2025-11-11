64da534d5a43d4952ce0c3700e2a90eb86432fe7
Oleh-Fr <frasyniuk.oleh.dev@gmail.com>
2025-11-11 22:34:34 +0000

    finish test

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,31 @@
-# write your code here
+from unittest import mock
+import pytest
+from app.main import can_access_google_page
+
+
+@pytest.fixture()
+def url():
+    return "https://www.google.com"
+
+
+@mock.patch("app.main.has_internet_connection", return_value=True)
+@mock.patch("app.main.valid_google_url", return_value=True)
+def test_can_access_google_page(mock_valid, mock_internet, url) -> None:
+    assert can_access_google_page(url) == "Accessible"
+
+
+@mock.patch("app.main.has_internet_connection", return_value=False)
+def test_can_not_access_google_page_no_internet(mock_internet, url) -> None:
+    assert can_access_google_page(url) == "Not accessible"
+
+
+@mock.patch("app.main.valid_google_url", return_value=False)
+@mock.patch("app.main.has_internet_connection", return_value=True)
+def test_can_not_access_google_page_no_valid_url(mock_internet, mock_valid, url) -> None:
+    assert can_access_google_page(url) == "Not accessible"
+
+
+@mock.patch("app.main.has_internet_connection", return_value=True)
+@mock.patch("app.main.valid_google_url", return_value=False)
+def test_can_not_access_google_page_only_with_internet(mock_valid, mock_internet, url) -> None:
+    assert can_access_google_page(url) == "Not accessible"
