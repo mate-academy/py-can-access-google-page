e235c4fc69b64a2df25587c559f2ecdbe3f56d55
Oleksii Mishchenko <alex3hard@gmail.com>
2025-11-13 16:35:26 +0200

    tests implemented

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,26 @@
-# write your code here
+from app import main
+
+
+def test_accessible(monkeypatch):
+    monkeypatch.setattr(main, "has_internet_connection", lambda: True)
+    monkeypatch.setattr(main, "valid_google_url", lambda url: True)
+    assert main.can_access_google_page("https://www.google.com") == "Accessible"
+
+
+def test_not_accessible_no_internet(monkeypatch):
+    monkeypatch.setattr(main, "has_internet_connection", lambda: False)
+    monkeypatch.setattr(main, "valid_google_url", lambda url: True)
+    assert main.can_access_google_page("https://www.google.com") == "Not accessible"
+
+
+def test_not_accessible_invalid_url(monkeypatch):
+    monkeypatch.setattr(main, "has_internet_connection", lambda: True)
+    monkeypatch.setattr(main, "valid_google_url", lambda url: False)
+    assert main.can_access_google_page("https://www.google.com") == "Not accessible"
+
+
+def test_not_accessible_both_false(monkeypatch):
+    monkeypatch.setattr(main, "has_internet_connection", lambda: False)
+    monkeypatch.setattr(main, "valid_google_url", lambda url: False)
+    assert main.can_access_google_page("https://www.google.com") == "Not accessible"
+
