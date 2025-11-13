451b272fa01894eaef6595070c96bac30b60c2c2
Alex <Rieznik.O.A@nmu.one>
2025-11-13 14:11:18 +0200

    Solution#2

--- app/test_main.py
+++ app/test_main.py
@@ -5,11 +5,12 @@ from app.main import can_access_google_page
 
 @mock.patch("app.main.valid_google_url", return_value=True)
 @mock.patch("app.main.has_internet_connection", return_value=True)
-def test_can_access_google_page(mock_1: Any, mock_2: Any) -> None:
+def test_returns_accessible_with_valid_url_and_internet(
+        mock_1: Any, mock_2: Any) -> None:
     assert can_access_google_page("https://google.com") == "Accessible"
 
 
-def test_cant_access_wrong_page() -> None:
+def test_returns_not_accessible_with_invalid_url() -> None:
     with (mock.patch("app.main.valid_google_url") as m_valid,
           mock.patch("app.main.has_internet_connection") as m_net):
         m_valid.return_value = False
@@ -17,7 +18,7 @@ def test_cant_access_wrong_page() -> None:
         assert can_access_google_page("hts://gogle.com") == "Not accessible"
 
 
-def test_cannot_access_page_w_o_internet() -> None:
+def test_returns_not_accessible_without_internet_connection() -> None:
     with (mock.patch("app.main.valid_google_url") as m_valid,
           mock.patch("app.main.has_internet_connection") as m_net):
         m_valid.return_value = True
@@ -27,7 +28,7 @@ def test_cannot_access_page_w_o_internet() -> None:
 
 @mock.patch("app.main.valid_google_url", return_value=False)
 @mock.patch("app.main.has_internet_connection", return_value=False)
-def test_cant_access_wrong_page_without_internet(
+def test_returns_not_accessible_with_invalid_url_and_no_internet(
         mock_1: Any, mock_2: Any
 ) -> None:
     assert can_access_google_page("htp://ggle.cosn") == "Not accessible"
