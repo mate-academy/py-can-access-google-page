527441d6524f6db493a3842cf4d3a72af6b1e5ef
Lukian <lukian55d@gmail.com>
2025-11-18 23:51:07 +0000

    asads

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,6 @@
-# write your code here
+from unittest.mock import patch
+from app.main import can_access_google_page
+
+
+def test_if_can_it_connect_to_internet_and_first_condition():
+    with patch("app.main.valid_google_url")
