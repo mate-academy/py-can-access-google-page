a19656a1ed10ade64e4b66dd0eb5d874b317aa8d
Lukian <lukian55d@gmail.com>
2025-11-19 00:27:31 +0000

    i understood the task properly

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,24 @@
+import pytest
 from unittest.mock import patch
 from app.main import can_access_google_page
 
 
-def test_if_can_it_connect_to_internet_and_first_condition():
-    with patch("app.main.valid_google_url")
+
+@pytest.mark.parametrize(
+    "validation,internet,expected_return",
+    [
+        (True, True, "Accessible"),
+        (False, True, "Not accessible"),
+        (True, False, "Not accessible"),
+        (False, False, "Not accessible")
+    ]
+)
+def test_output_from_valid_url_and_network(validation: bool,
+                                           internet: bool,
+                                           expected_return: str) -> None:
+
+    with patch("app.main.valid_google_url", return_value=validation), \
+        patch("app.main.has_internet_connection", return_value=internet):
+
+        result = can_access_google_page("https://google.com")
+        assert result == expected_return
