799eebdf2a2e1f872e1d971e74ffbf771d1450dd
Lukian <lukian55d@gmail.com>
2025-11-19 00:50:16 +0000

    flake8 commited

--- app/test_main.py
+++ app/test_main.py
@@ -3,7 +3,6 @@ from unittest.mock import patch
 from app.main import can_access_google_page
 
 
-
 @pytest.mark.parametrize(
     "validation,internet,expected_return",
     [
@@ -13,12 +12,18 @@ from app.main import can_access_google_page
         (False, False, "Not accessible")
     ]
 )
-def test_output_from_valid_url_and_network(validation: bool,
-                                           internet: bool,
-                                           expected_return: str) -> None:
+def test_output_from_valid_url_and_network(
+        validation: bool,
+        internet: bool,
+        expected_return: str
+) -> None:
 
-    with patch("app.main.valid_google_url", return_value=validation), \
-        patch("app.main.has_internet_connection", return_value=internet):
-
-        result = can_access_google_page("https://google.com")
-        assert result == expected_return
+    with patch(
+        "app.main.valid_google_url",
+               return_value=validation
+    ):
+        with patch(
+        "app.main.has_internet_connection",
+                return_value=internet):
+            result = can_access_google_page("https://google.com")
+    assert result == expected_return
