4748f18f96a01807cd9f09a290d250804c8b8335
Qbaldo <durowski@gmail.com>
2025-11-23 16:40:49 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,29 @@
-# write your code here
+import pytest
+from _pytest.monkeypatch import MonkeyPatch
+
+from app import main
+
+
+@pytest.mark.parametrize(
+    "valid_value, internet_value, expected_result",
+    (
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible"),
+    )
+)
+def test_can_access_google_page(valid_value: bool,
+                                internet_value: bool,
+                                expected_result: str,
+                                monkeypatch: MonkeyPatch) -> None:
+    def fake_valid(url: str) -> bool:
+        return valid_value
+
+    def fake_internet() -> bool:
+        return internet_value
+
+    monkeypatch.setattr("app.main.valid_google_url", fake_valid)
+    monkeypatch.setattr("app.main.has_internet_connection", fake_internet)
+    assert (main.can_access_google_page
+            ("https://www.google.com") == expected_result)
