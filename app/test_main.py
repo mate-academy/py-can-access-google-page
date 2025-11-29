8954c3458eee9da943ef24b1fe1c9b74670d3c44
Tomasz Szatałowicz <szatalowicztomek@gmail.com>
2025-11-29 09:42:46 +0100

    quest

--- app/test_main.py
+++ app/test_main.py
@@ -4,7 +4,10 @@ from app.main import can_access_google_page
 
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
-def test_can_access_google_page(mock_has_internet, mock_valid_url):
+def test_can_access_google_page(
+        mock_has_internet: None,
+        mock_valid_url: None
+) -> None:
     mock_valid_url.return_value = True
     mock_has_internet.return_value = True
     assert can_access_google_page("https://www.google.com") == "Accessible"
@@ -19,4 +22,3 @@ def test_can_access_google_page(mock_has_internet, mock_valid_url):
 
     mock_valid_url.return_value = False
     mock_has_internet.return_value = False
-
