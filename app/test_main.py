07b1b7aec9d73b76f267762d68d5d885927eb0eb
Tomasz Szatałowicz <szatalowicztomek@gmail.com>
2025-11-29 09:54:17 +0100

    quest

--- app/test_main.py
+++ app/test_main.py
@@ -1,12 +1,11 @@
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 from app.main import can_access_google_page
 
 
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
 def test_can_access_google_page(
-        mock_has_internet: None,
-        mock_valid_url: None
+    mock_has_internet: MagicMock, mock_valid_url: MagicMock
 ) -> None:
     mock_valid_url.return_value = True
     mock_has_internet.return_value = True
@@ -22,3 +21,4 @@ def test_can_access_google_page(
 
     mock_valid_url.return_value = False
     mock_has_internet.return_value = False
+    assert can_access_google_page("https://www.google.com") == "Not accessible"
