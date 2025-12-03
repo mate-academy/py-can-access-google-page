6448c808aa11b335b3af6745248b00c280474040
Vladislav <ilichoff88@gmail.com>
2025-12-03 22:38:57 +0200

    'Solution2'

--- app/test_main.py
+++ app/test_main.py
@@ -5,7 +5,9 @@ from app.main import can_access_google_page
 
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
-def test_can_access_google_page_accessible(mock_internet: Mock, mock_valid_url: Mock) -> None:
+def test_can_access_google_page_accessible(
+    mock_internet: Mock, mock_valid_url: Mock
+) -> None:
     mock_internet.return_value = True
     mock_valid_url.return_value = True
 
@@ -16,7 +18,9 @@ def test_can_access_google_page_accessible(mock_internet: Mock, mock_valid_url: 
 
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
-def test_can_access_google_page_invalid_url(mock_internet: Mock, mock_valid_url: Mock) -> None:
+def test_can_access_google_page_invalid_url(
+    mock_internet: Mock, mock_valid_url: Mock
+) -> None:
     mock_internet.return_value = True
     mock_valid_url.return_value = False
 
@@ -27,7 +31,9 @@ def test_can_access_google_page_invalid_url(mock_internet: Mock, mock_valid_url:
 
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
-def test_can_access_google_page_no_internet(mock_internet: Mock, mock_valid_url: Mock) -> None:
+def test_can_access_google_page_no_internet(
+    mock_internet: Mock, mock_valid_url: Mock
+) -> None:
     mock_internet.return_value = False
     mock_valid_url.return_value = True
 
@@ -38,7 +44,9 @@ def test_can_access_google_page_no_internet(mock_internet: Mock, mock_valid_url:
 
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
-def test_can_access_google_page_not_accessible(mock_internet: Mock, mock_valid_url: Mock) -> None:
+def test_can_access_google_page_not_accessible(
+    mock_internet: Mock, mock_valid_url: Mock
+) -> None:
     mock_internet.return_value = False
     mock_valid_url.return_value = False
 
