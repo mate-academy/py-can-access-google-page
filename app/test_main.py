17699182bc66413e96ed93cead9799b7dbb100c1
lassisterv16 <lassisterv16@gmail.com>
2025-12-05 14:57:53 +0100

    fixed flake8 errors

--- app/test_main.py
+++ app/test_main.py
@@ -14,12 +14,14 @@ from app.main import can_access_google_page
     ]
 )
 def test_should_return_correct_result_with_difference_values(
-        url_validator,
-        connection_validator,
-        result,
+        url_validator: bool,
+        connection_validator: bool,
+        result: str,
 ) -> None:
-    with (mock.patch("app.main.valid_google_url") as mocked_url_validator,
-          mock.patch("app.main.has_internet_connection") as mocked_connection_validator):
-        mocked_url_validator.return_value = url_validator
-        mocked_connection_validator.return_value = connection_validator
+    with (
+        mock.patch("app.main.valid_google_url") as mocked_url,
+        mock.patch("app.main.has_internet_connection") as mocked_connection
+    ):
+        mocked_url.return_value = url_validator
+        mocked_connection.return_value = connection_validator
         assert can_access_google_page("google.com") == result
