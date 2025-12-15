eefe6957c598821bac040f357d13ad24c753fb34
Sasha <alex.acmate@gmail.com>
2025-12-15 13:41:52 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -9,20 +9,6 @@ def url() -> str:
     return "http://google.com.ua"
 
 
-@mock.patch("app.main.valid_google_url")
-def test_mocked_valid_google_url(mocked_valid_google_url2: MagicMock,
-                                 url: str) -> None:
-    mocked_valid_google_url2(url)
-    mocked_valid_google_url2.assert_called_once_with(url)
-
-
-@mock.patch("app.main.has_internet_connection")
-def test_mocked_has_internet_connection(
-        mocked_has_internet_connection2: MagicMock, url: str) -> None:
-    can_access_google_page(url)
-    mocked_has_internet_connection2.assert_called_once()
-
-
 @pytest.mark.parametrize(
     "valid_google_url_return,has_internet_connection_return,result",
     [
@@ -44,6 +30,5 @@ def test_can_access_google_page(mocked_valid_google_url2: MagicMock,
                                 has_internet_connection_return: bool,
                                 result: str, url: str) -> None:
     mocked_valid_google_url2.return_value = valid_google_url_return
-    mocked_has_internet_connection2.return_value = (
-        has_internet_connection_return)
+    mocked_has_internet_connection2.return_value = has_internet_connection_return
     assert can_access_google_page(url) == result
