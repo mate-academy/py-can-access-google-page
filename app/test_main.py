66c557fc22f467aa4951e66cc0bfe84d0e95ea0e
Sasha <alex.acmate@gmail.com>
2025-12-15 13:28:26 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -26,18 +26,24 @@ def test_mocked_has_internet_connection(
 @pytest.mark.parametrize(
     "valid_google_url_return,has_internet_connection_return,result",
     [
-        pytest.param(True, True, "Accessible", id="test_if_two_dependencies_are_True"),
-        pytest.param(True, False, "Not accessible", id="test_cannot_access_if_only_url"),
-        pytest.param(False, True, "Not accessible", id="test_cannot_access_if_only_connection"),
-        pytest.param(False, False, "Not accessible", id="test_if_two_dependencies_are_False")
+        pytest.param(True, True, "Accessible",
+                     id="test_if_two_dependencies_are_True"),
+        pytest.param(True, False, "Not accessible",
+                     id="test_cannot_access_if_only_url"),
+        pytest.param(False, True, "Not accessible",
+                     id="test_cannot_access_if_only_connection"),
+        pytest.param(False, False, "Not accessible",
+                     id="test_if_two_dependencies_are_False")
     ]
 )
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
 def test_can_access_google_page(mocked_valid_google_url2: MagicMock,
                                 mocked_has_internet_connection2: MagicMock,
-                                valid_google_url_return: bool, has_internet_connection_return: bool,
+                                valid_google_url_return: bool,
+                                has_internet_connection_return: bool,
                                 result: str, url: str) -> None:
     mocked_valid_google_url2.return_value = valid_google_url_return
-    mocked_has_internet_connection2.return_value = has_internet_connection_return
+    mocked_has_internet_connection2.return_value = (
+        has_internet_connection_return)
     assert can_access_google_page(url) == result
