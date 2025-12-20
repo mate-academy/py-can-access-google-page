7d56399fcc13d4a0fc33bfb4d2a79861b1d308ad
Farlinder <sashakost2006e@gmail.com>
2025-12-20 13:39:15 +0100

    Solution1

--- app/test_main.py
+++ app/test_main.py
@@ -8,9 +8,10 @@ def test_url_have_connection() -> None:
         mocked_valid.return_value = True
         mock_connection.return_value = True
         result = can_access_google_page(url="https://www.google.com")
+        mock_connection.assert_called_once()
+        mocked_valid.assert_called_once()
         assert result == "Accessible"
-        mock_connection.called_once()
-        mocked_valid.called_once()
+
 
 
 def test_url_have_no_connection() -> None:
@@ -20,5 +21,3 @@ def test_url_have_no_connection() -> None:
         mock_connection.return_value = False
         result = can_access_google_page(url="googgle.com")
         assert result == "Not accessible"
-        mock_connection.called_once()
-        mocked_valid.called_once()
