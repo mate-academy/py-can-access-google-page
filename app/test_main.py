d17cac1dbbc3477fdaf90e3a5755b182af95ac65
Farlinder <sashakost2006e@gmail.com>
2025-12-20 13:08:56 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -5,6 +5,7 @@ from app.main import can_access_google_page
 def test_valid_url_and_connection_exists() -> None:
     with mock.patch("app.main.valid_google_url") as mocked_valid, \
             mock.patch("app.main.has_internet_connection") as mock_connection:
-        can_access_google_page(url="https://www.google.com")
+        result = can_access_google_page(url="https://www.google.com")
         mocked_valid.assert_called_once()
         mock_connection.assert_called_once()
+        assert result == "Accessible"
