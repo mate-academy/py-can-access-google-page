9add4720c05db2901c211f2dbbfa08281515c32c
Denys Herasymchuk <herasymchukdenyss@gmail.com>
2025-12-22 22:17:54 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,13 +1,38 @@
 from unittest import mock
 
+import pytest
+
 from app.main import can_access_google_page
 
+@pytest.fixture
+def mocked_url():
+    with mock.patch("app.main.valid_google_url") as mocked_url:
+        yield mocked_url
 
-def test_can_access_google_page() -> None:
-    with mock.patch("app.main.valid_google_url") as mocked_url_check:
-        with mock.patch(
-                "app.main.has_internet_connection"
-        ) as mocked_internet_check:
-            can_access_google_page("1.1.1.1")
-            mocked_url_check.assert_called_with("1.1.1.1")
-            mocked_internet_check.assert_called()
+
+@pytest.fixture
+def mocked_internet():
+    with mock.patch("app.main.has_internet_connection") as mocked_internet:
+        yield mocked_internet
+
+
+def test_can_access_google_page_when_valid_url(mocked_url, mocked_internet) -> None:
+    mocked_url.return_value = True
+    mocked_url.return_value = True
+
+    result = can_access_google_page("https://www.google.com/")
+
+    mocked_url.assert_called_with("https://www.google.com/")
+    mocked_internet.assert_called_once()
+    assert result == "Accessible"
+
+
+def test_can_access_google_page_when_fake_url(mocked_url, mocked_internet) -> None:
+    mocked_url.return_value = False
+    mocked_internet.return_value = True
+
+    result = can_access_google_page("fake_url")
+
+    mocked_url.assert_called_with("fake_url")
+    mocked_internet.assert_called_once()
+    assert result == "Not accessible"
