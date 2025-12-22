427dbab59c9fe1fca44b9f675e7f0da998bb10fd
tomech <109527747+tomech4@users.noreply.github.com>
2025-12-22 23:49:58 +0100

    implement test_can_access_google_page

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,13 @@
-# write your code here
+from unittest import mock
+
+from app.main import can_access_google_page
+
+def test_can_access_google_page():
+    with (
+        mock.patch("app.main.has_internet_connection") as mock_connection,
+        mock.patch("app.main.valid_google_url") as mock_validity
+    ):
+        url = "test"
+        can_access_google_page(url)
+        mock_connection.assert_called_once()
+        mock_validity.assert_called_once_with(url)
