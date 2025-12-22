0f2d7aad4919efbe114409f0f00f9f70c8953741
tomech <109527747+tomech4@users.noreply.github.com>
2025-12-22 23:51:24 +0100

    fix style as per flake8

--- app/test_main.py
+++ app/test_main.py
@@ -2,7 +2,8 @@ from unittest import mock
 
 from app.main import can_access_google_page
 
-def test_can_access_google_page():
+
+def test_can_access_google_page() -> None:
     with (
         mock.patch("app.main.has_internet_connection") as mock_connection,
         mock.patch("app.main.valid_google_url") as mock_validity
