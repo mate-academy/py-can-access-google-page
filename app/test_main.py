8ba8372008f4de3bc054cbb011aef65337159915
Andrii Yarotskiy <andriiyarotskiy@gmail,com>
2025-12-22 15:05:20 +0000

    fixed comments

--- app/test_main.py
+++ app/test_main.py
@@ -24,4 +24,6 @@ def test_valid_url_and_connection_exists(
     if mocked_has_internet_connection.return_value:
         (mocked_valid_google_url
          .assert_called_with("https://www.google.com"))
+    else:
+        mocked_valid_google_url.assert_not_called()
     assert response == expected
