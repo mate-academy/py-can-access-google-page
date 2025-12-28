88a822ec4a2a3869f62ead4f3f637e6ee0a10659
Sasha <sasha.ryabokon14@gmail.com>
2025-12-28 15:33:55 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,7 @@
 from app.main import can_access_google_page
 from unittest.mock import patch
 
+
 def test_can_access_google_page() -> None:
     with patch("app.main.has_internet_connection", return_value=True):
         with patch("app.main.valid_google_url", return_value=True):
@@ -8,18 +9,21 @@ def test_can_access_google_page() -> None:
 
 
 def test_can_access_google_page_invalid_url() -> None:
-    with patch("app.main.has_internet_connection", return_value=True):
+    with (patch("app.main.has_internet_connection", return_value=True)):
         with patch("app.main.valid_google_url", return_value=False):
-            assert can_access_google_page("https://google.com") == "Not accessible"
+            assert can_access_google_page("https://google.com"
+                                          ) == "Not accessible"
 
 
 def test_can_access_google_page_invalid_internet() -> None:
-    with patch("app.main.has_internet_connection", return_value=False):
+    with (patch("app.main.has_internet_connection", return_value=False)):
         with patch("app.main.valid_google_url", return_value=True):
-            assert can_access_google_page("https://google.com") == "Not accessible"
+            assert can_access_google_page("https://google.com"
+                                          ) == "Not accessible"
 
 
 def test_can_access_google_page_all_invalid() -> None:
-    with patch("app.main.has_internet_connection", return_value=False):
+    with (patch("app.main.has_internet_connection", return_value=False)):
         with patch("app.main.valid_google_url", return_value=False):
-            assert can_access_google_page("https://google.com") == "Not accessible"
+            assert can_access_google_page("https://google.com"
+                                          ) == "Not accessible"
