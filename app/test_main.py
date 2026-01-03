7dc4701953060a45456690082be2c68466fdd15c
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-01-03 15:13:31 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,71 @@
-# write your code here
+import pytest
+
+import app
+from app.main import can_access_google_page
+
+
+adress_url = "https://google.com"
+
+
+def test_valid_url_and_connection_exists(
+        monkeypatch: pytest.MonkeyPatch
+) -> None:
+    monkeypatch.setattr(
+        app.main,
+        "valid_google_url",
+        lambda url: True
+    )
+    monkeypatch.setattr(
+        app.main,
+        "has_internet_connection",
+        lambda: True
+    )
+    assert can_access_google_page(url=adress_url) == "Accessible"
+
+
+def test_valid_url_but_no_connection(
+        monkeypatch: pytest.MonkeyPatch
+) -> None:
+    monkeypatch.setattr(
+        app.main,
+        "valid_google_url",
+        lambda url: True
+    )
+    monkeypatch.setattr(
+        app.main,
+        "has_internet_connection",
+        lambda: False
+    )
+    assert can_access_google_page(url=adress_url) == "Not accessible"
+
+
+def test_invalid_url_but_connection_exists(
+        monkeypatch: pytest.MonkeyPatch
+) -> None:
+    monkeypatch.setattr(
+        app.main,
+        "valid_google_url",
+        lambda url: False
+    )
+    monkeypatch.setattr(
+        app.main,
+        "has_internet_connection",
+        lambda: True
+    )
+    assert can_access_google_page(url=adress_url) == "Not accessible"
+
+
+def test_invalid_url_and_no_connection(
+        monkeypatch: pytest.MonkeyPatch
+) -> None:
+    monkeypatch.setattr(
+        app.main,
+        "valid_google_url",
+        lambda url: False
+    )
+    monkeypatch.setattr(
+        app.main,
+        "has_internet_connection",
+        lambda: False
+    )
+    assert can_access_google_page(url=adress_url) == "Not accessible"
