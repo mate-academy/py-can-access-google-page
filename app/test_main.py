4e494f16c44a0d31608dd343ffaa087f98d2c511
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-01-03 20:06:35 +0200

    Solution3

--- app/test_main.py
+++ app/test_main.py
@@ -4,78 +4,6 @@ import app
 from app.main import can_access_google_page
 
 
-adress_url = "https://google.com"
-
-# use mocking monkeypatch
-
-
-def test_valid_url_and_connection_exists(
-        monkeypatch: pytest.MonkeyPatch
-) -> None:
-    monkeypatch.setattr(
-        app.main,
-        "valid_google_url",
-        lambda url: True
-    )
-    monkeypatch.setattr(
-        app.main,
-        "has_internet_connection",
-        lambda: True
-    )
-    assert can_access_google_page(url=adress_url) == "Accessible"
-
-
-def test_valid_url_but_no_connection(
-        monkeypatch: pytest.MonkeyPatch
-) -> None:
-    monkeypatch.setattr(
-        app.main,
-        "valid_google_url",
-        lambda url: True
-    )
-    monkeypatch.setattr(
-        app.main,
-        "has_internet_connection",
-        lambda: False
-    )
-    assert can_access_google_page(url=adress_url) == "Not accessible"
-
-
-def test_invalid_url_but_connection_exists(
-        monkeypatch: pytest.MonkeyPatch
-) -> None:
-    monkeypatch.setattr(
-        app.main,
-        "valid_google_url",
-        lambda url: False
-    )
-    monkeypatch.setattr(
-        app.main,
-        "has_internet_connection",
-        lambda: True
-    )
-    assert can_access_google_page(url=adress_url) == "Not accessible"
-
-
-def test_invalid_url_and_no_connection(
-        monkeypatch: pytest.MonkeyPatch
-) -> None:
-    monkeypatch.setattr(
-        app.main,
-        "valid_google_url",
-        lambda url: False
-    )
-    monkeypatch.setattr(
-        app.main,
-        "has_internet_connection",
-        lambda: False
-    )
-    assert can_access_google_page(url=adress_url) == "Not accessible"
-
-
-# use @pytest.mark.parametrize
-
-
 @pytest.mark.parametrize(
     "url_valid, internet_connection, expected",
     [
@@ -102,4 +30,4 @@ def test_can_access_google_page(
         "has_internet_connection",
         lambda: internet_connection
     )
-    assert can_access_google_page(url=adress_url) == expected
+    assert can_access_google_page(url="https://google.com") == expected
