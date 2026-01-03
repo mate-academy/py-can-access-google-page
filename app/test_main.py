e1fbf894d567d493bb518a7642aa771cc93872a8
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-01-03 17:31:42 +0200

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -6,6 +6,8 @@ from app.main import can_access_google_page
 
 adress_url = "https://google.com"
 
+# use mocking monkeypatch
+
 
 def test_valid_url_and_connection_exists(
         monkeypatch: pytest.MonkeyPatch
@@ -69,3 +71,35 @@ def test_invalid_url_and_no_connection(
         lambda: False
     )
     assert can_access_google_page(url=adress_url) == "Not accessible"
+
+
+# use @pytest.mark.parametrize
+
+
+@pytest.mark.parametrize(
+    "url_valid, internet_connection, expected",
+    [
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible")
+
+    ]
+)
+def test_can_access_google_page(
+        monkeypatch: pytest.MonkeyPatch,
+        url_valid: bool,
+        internet_connection: bool,
+        expected: str
+) -> None:
+    monkeypatch.setattr(
+        app.main,
+        "valid_google_url",
+        lambda url: url_valid
+    )
+    monkeypatch.setattr(
+        app.main,
+        "has_internet_connection",
+        lambda: internet_connection
+    )
+    assert can_access_google_page(url=adress_url) == expected
