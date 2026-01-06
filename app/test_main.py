317132288811a5a65a29f4e4a5da55d283b1d541
Mateusz <mateusz.rudnicki05@wp.pl>
2026-01-06 23:13:28 +0100

    poprawionev2

--- app/test_main.py
+++ app/test_main.py
@@ -4,7 +4,10 @@ from app.main import can_access_google_page
 
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
-def test_can_access_google_page_accessible(mock_internet, mock_valid_url):
+def test_can_access_google_page_accessible(
+        mock_internet: bool,
+        mock_valid_url: bool
+) -> None:
     mock_valid_url.return_value = True
     mock_internet.return_value = True
     assert can_access_google_page("test.pl") == "Accessible"
@@ -12,7 +15,10 @@ def test_can_access_google_page_accessible(mock_internet, mock_valid_url):
 
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
-def test_cant_access_google_page_when_no_internet(mock_internet, mock_valid_url):
+def test_cant_access_google_page_when_no_internet(
+        mock_internet: bool,
+        mock_valid_url: bool
+) -> None:
     mock_valid_url.return_value = True
     mock_internet.return_value = False
     assert can_access_google_page("test.pl") == "Not accessible"
@@ -20,7 +26,10 @@ def test_cant_access_google_page_when_no_internet(mock_internet, mock_valid_url)
 
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
-def test_cant_access_google_page_when_bad_link(mock_internet, mock_valid_url):
+def test_cant_access_google_page_when_bad_link(
+        mock_internet: bool,
+        mock_valid_url: bool
+) -> None:
     mock_valid_url.return_value = False
     mock_internet.return_value = True
     assert can_access_google_page("test.pl") == "Not accessible"
@@ -28,7 +37,10 @@ def test_cant_access_google_page_when_bad_link(mock_internet, mock_valid_url):
 
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
-def test_cant_access_google_page_when_bad_link_and_no_internet(mock_internet, mock_valid_url):
+def test_cant_access_google_page_when_bad_link_and_no_internet(
+        mock_internet: bool,
+        mock_valid_url: bool
+) -> None:
     mock_valid_url.return_value = False
     mock_internet.return_value = False
     assert can_access_google_page("test.pl") == "Not accessible"
