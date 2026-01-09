c407a6b58bacb949d2140b563a29d2649fea39e8
Oleg <bagrijoleh@gmail.com>
2026-01-09 11:23:21 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -18,8 +18,8 @@ def test_can_access_google_page_both_function_are_true(
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
 def test_can_access_google_page_internet_connection_are_false(
-        mock_has_internet_connection,
-        mock_valid_google_url
+        mock_has_internet_connection: MagicMock,
+        mock_valid_google_url: MagicMock
 ) -> None:
     mock_valid_google_url.return_value = True
     mock_has_internet_connection.return_value = False
@@ -29,8 +29,8 @@ def test_can_access_google_page_internet_connection_are_false(
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
 def test_can_access_google_page_google_url_are_false(
-        mock_has_internet_connection,
-        mock_valid_google_url
+        mock_has_internet_connection: MagicMock,
+        mock_valid_google_url: MagicMock
 ) -> None:
     mock_valid_google_url.return_value = False
     mock_has_internet_connection.return_value = True
