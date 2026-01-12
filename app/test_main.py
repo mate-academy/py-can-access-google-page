13bd56096916d1eeb4979eb2130e9a019a47b230
lykhorovych <31163897+lykhorovych@users.noreply.github.com>
2026-01-12 23:06:11 +0200

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -1,16 +1,18 @@
-from datetime import datetime
 from unittest import mock
 from app import main as app
 
 
 def test_can_access_if_url_valid_and_has_connection() -> None:
     with (
-        mock.patch("app.main.datetime") as mock_datetime,
-        mock.patch("app.main.requests") as mock_requests,
+        mock.patch(
+            "app.main.valid_google_url"
+        ) as mock_valid_google_url,
+        mock.patch(
+            "app.main.has_internet_connection"
+        ) as mock_has_internet_connections,
     ):
-        mock_datetime.datetime.now.return_value = datetime(
-            2026, 12, 1, 7, 0, 0)
-        mock_requests.get.return_value.status_code = 200
+        mock_valid_google_url.return_value = True
+        mock_has_internet_connections.return_value = True
 
         assert app.can_access_google_page(
             "https://google.com.ua") == "Accessible"
@@ -18,12 +20,15 @@ def test_can_access_if_url_valid_and_has_connection() -> None:
 
 def test_can_access_if_url_valid_and_has_not_connection() -> None:
     with (
-        mock.patch("app.main.datetime") as mock_datetime,
-        mock.patch("app.main.requests") as mock_requests,
+        mock.patch(
+            "app.main.valid_google_url"
+        ) as mock_valid_google_url,
+        mock.patch(
+            "app.main.has_internet_connection"
+        ) as mock_has_internet_connections,
     ):
-        mock_datetime.datetime.now.return_value = datetime(
-            2026, 12, 1, 5, 0, 0)
-        mock_requests.get.return_value.status_code = 200
+        mock_valid_google_url.return_value = True
+        mock_has_internet_connections.return_value = False
 
         assert app.can_access_google_page(
             "https://google.com.ua") == "Not accessible"
@@ -31,12 +36,15 @@ def test_can_access_if_url_valid_and_has_not_connection() -> None:
 
 def test_can_access_if_url_not_valid_and_has_connection() -> None:
     with (
-        mock.patch("app.main.datetime") as mock_datetime,
-        mock.patch("app.main.requests") as mock_requests,
+        mock.patch(
+            "app.main.valid_google_url"
+        ) as mock_valid_google_url,
+        mock.patch(
+            "app.main.has_internet_connection"
+        ) as mock_has_internet_connections,
     ):
-        mock_datetime.datetime.now.return_value = datetime(
-            2026, 12, 1, 7, 0, 0)
-        mock_requests.get.return_value.status_code = 404
+        mock_valid_google_url.return_value = False
+        mock_has_internet_connections.return_value = True
 
         assert app.can_access_google_page(
             "https://google.com.ua") == "Not accessible"
