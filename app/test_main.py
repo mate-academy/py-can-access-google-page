b470f5c304f0ee479a80c70529ed539582caf3fa
lykhorovych <31163897+lykhorovych@users.noreply.github.com>
2026-01-12 21:56:25 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,17 @@
-# write your code here
+from datetime import datetime
+from unittest import mock
+from app import main as app
+
+
+def test_can_access_to_google_page() -> None:
+    with (
+        mock.patch("app.main.datetime") as mock_datetime,
+        mock.patch("app.main.requests") as mock_requests,
+    ):
+        mock_datetime.datetime.now.return_value = datetime(
+            2026, 12, 1, 7, 0, 0
+        )
+        mock_requests.get.return_value.status_code = 200
+
+        assert app.can_access_google_page(
+            "https://google.com.ua") == "Accessible"
