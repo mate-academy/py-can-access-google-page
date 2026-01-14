47c07156f6b3225608e1b8836577b6d220f86f40
chojanpineapple <johan.apr99@gmail.com>
2026-01-14 11:51:36 +0000

    test

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,13 @@
-# write your code here
+from unittest import mock
+from .main import can_access_google_page
+
+
+def test_can_access_google_page_accessible() -> None:
+    with mock.patch("app.main.has_internet_connection",
+                    return_value=True), \
+         mock.patch("app.main.valid_google_url",
+                    return_value=True):
+
+        result = can_access_google_page("https://www.google.com")
+
+        assert result == "Accessible"
