4bf193ccf55ee87f4c4b77ac72c3580994d1dee2
chojanpineapple <johan.apr99@gmail.com>
2026-01-14 11:56:07 +0000

    test

--- app/test_main.py
+++ app/test_main.py
@@ -2,12 +2,28 @@ from unittest import mock
 from .main import can_access_google_page
 
 
-def test_can_access_google_page_accessible() -> None:
-    with mock.patch("app.main.has_internet_connection",
-                    return_value=True), \
-         mock.patch("app.main.valid_google_url",
-                    return_value=True):
+def test_can_access_google_page_accessible():
+    with mock.patch("app.main.has_internet_connection", return_value=True), \
+         mock.patch("app.main.valid_google_url", return_value=True):
 
         result = can_access_google_page("https://www.google.com")
 
         assert result == "Accessible"
+
+
+def test_can_access_google_page_not_accessible_when_url_invalid():
+    with mock.patch("app.main.has_internet_connection", return_value=True), \
+         mock.patch("app.main.valid_google_url", return_value=False):
+
+        result = can_access_google_page("https://www.google.com")
+
+        assert result == "Not accessible"
+
+
+def test_can_access_google_page_not_accessible_when_no_internet():
+    with mock.patch("app.main.has_internet_connection", return_value=False), \
+         mock.patch("app.main.valid_google_url", return_value=True):
+
+        result = can_access_google_page("https://www.google.com")
+
+        assert result == "Not accessible"
