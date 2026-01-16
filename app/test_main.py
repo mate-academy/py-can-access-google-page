2c8249d84684a3444714b81758fa4e81e5c098c3
viki <v.yavdoschuk@gmail.com>
2026-01-16 11:17:50 +0100

    'solution'

--- app/test_main.py
+++ app/test_main.py
@@ -7,8 +7,8 @@ from app.main import can_access_google_page
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 def test_valid_url_and_connection_exists(
-        mocked_valid_url: MagicMock,
-        mocked_has_connection: MagicMock) -> None:
+        mocked_has_connection: MagicMock,
+        mocked_valid_url: MagicMock) -> None:
     mocked_valid_url.return_value = True
     mocked_has_connection.return_value = True
 
@@ -16,3 +16,14 @@ def test_valid_url_and_connection_exists(
 
     mocked_valid_url.assert_called()
     mocked_has_connection.assert_called()
+
+@mock.patch("app.main.valid_google_url")
+@mock.patch("app.main.has_internet_connection")
+def test_valid_url_and_no_connection(
+        mocked_has_connection: MagicMock,
+        mocked_valid_url: MagicMock) -> None:
+    mocked_valid_url.return_value = True
+    mocked_has_connection.return_value = False
+
+    assert can_access_google_page("https//:www.google.com") == "Not accessible"
+
