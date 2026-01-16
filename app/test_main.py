2b8d25df557832ffa357239c05ffe48b0261d791
viki <v.yavdoschuk@gmail.com>
2026-01-16 11:07:43 +0100

    'solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,10 +1,14 @@
 from unittest import mock
+from unittest.mock import MagicMock
+
 from app.main import can_access_google_page
 
 
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_valid_url_and_connection_exists(mocked_valid_url, mocked_has_connection):
+def test_valid_url_and_connection_exists(
+        mocked_valid_url: MagicMock,
+        mocked_has_connection: MagicMock) -> None:
     mocked_valid_url.return_value = True
     mocked_has_connection.return_value = True
 
