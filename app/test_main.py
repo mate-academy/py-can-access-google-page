f117ea37f187d5d58359c9662c989fa73657e7be
viki <v.yavdoschuk@gmail.com>
2026-01-16 11:20:20 +0100

    'solution'

--- app/test_main.py
+++ app/test_main.py
@@ -17,6 +17,7 @@ def test_valid_url_and_connection_exists(
     mocked_valid_url.assert_called()
     mocked_has_connection.assert_called()
 
+
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 def test_valid_url_and_no_connection(
@@ -26,4 +27,3 @@ def test_valid_url_and_no_connection(
     mocked_has_connection.return_value = False
 
     assert can_access_google_page("https//:www.google.com") == "Not accessible"
-
