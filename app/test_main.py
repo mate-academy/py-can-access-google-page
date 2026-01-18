1a4114f0db397f4b6c44e36340d22ef18f3851a6
Ivan Lavrinenko <lavrinenkoivan51@gmail.com>
2026-01-18 14:33:02 +0100

    added parametrized test for creating patches

--- app/test_main.py
+++ app/test_main.py
@@ -1,7 +1,10 @@
 from typing import Callable
 from unittest import mock
 
+import pytest
+
 from app.main import can_access_google_page
+from unittest.mock import patch
 
 
 @mock.patch("app.main.has_internet_connection")
@@ -13,3 +16,25 @@ def test_functions_has_internet_connection_and_valid_google_url_were_called(
     can_access_google_page("https://www.google.com")
     mocked_has_internet_connection.assert_called_once()
     mocked_valid_google_url.assert_called_once()
+
+
+@pytest.mark.parametrize(
+    "internet, valid_url, result",
+    [
+        (True, True, "Accessible"),
+        (False, True, "Not accessible"),
+        (True, False, "Not accessible"),
+        (False, False, "Not accessible"),
+    ],
+)
+def test_accessible_address_has_internet_connection(
+        internet: bool,
+        valid_url: bool,
+        result: str
+) -> None:
+    with (patch("app.main.has_internet_connection")
+          as mocked_has_internet_connection,
+            patch("app.main.valid_google_url") as mocked_valid_google_url):
+        mocked_has_internet_connection.return_value = internet
+        mocked_valid_google_url.return_value = valid_url
+        assert can_access_google_page("some.address") == result
