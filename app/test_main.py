4d1dab72a6b6e791780c624ff57daa67c7b1bd2c
Ivan Lavrinenko <lavrinenkoivan51@gmail.com>
2026-01-18 13:12:06 +0100

    mocked internally used functions

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,10 @@
-# write your code here
+from unittest import mock
+
+from app.main import can_access_google_page
+
+@mock.patch("app.main.has_internet_connection")
+@mock.patch("app.main.valid_google_url")
+def test_functions_has_internet_connection_and_valid_google_url_were_called(mocked_valid_google_url, mocked_has_internet_connection):
+    can_access_google_page("https://www.google.com")
+    mocked_has_internet_connection.assert_called_once()
+    mocked_valid_google_url.assert_called_once()
