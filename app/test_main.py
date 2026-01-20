9fd3b7bd38d812f9866c172b07f31a79d3fb5eae
stadnikruslan168-hub <stadnikruslan168@gmail.com>
2026-01-20 16:23:18 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -9,12 +9,12 @@ import pytest
         (True, True, "Accessible"),
         (True, False, "Not accessible"),
         (False, True, "Not accessible"),
-        (True, True, "Accessible"),
+        (False, False, "Not accessible"),
     ]
 )
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
-def test_cannot_access_if_connection_or_valid_url_is_true(
+def test_can_access_url(
     mock_valid_url: mock,
     mock_has_internet_connection: mock, valid_url: bool,
         has_internet_connection: bool, expected_result: str) -> None:
