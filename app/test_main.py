1f2f02137129ec1f438a28bfd3f29f8043482389
Dmytro <ys33ys33ys55@gmail.com>
2026-01-20 14:55:27 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,19 @@
-# write your code here
+import pytest
+from unittest import mock
+
+
+@pytest.fixture()
+def mocking_valid_google_url():
+    with mock.patch("valid_google_url") as func:
+        yield func
+
+
+@pytest.fixture()
+def mocked_has_internet_connection():
+    with mock.patch("has_internet_connection") as func:
+            yield func
+
+
+def test_can_access_google_page(mocking_valid_google_url, mocked_has_internet_connection) -> None:
+    mocking_valid_google_url.assert_called_once()
+    mocked_has_internet_connection.assert_called()
