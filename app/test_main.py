796933a1229ac7c28b9b6efddb5ece9b2c668f30
Dmytro <ys33ys33ys55@gmail.com>
2026-01-20 15:08:06 +0100

    Solution 2

--- app/test_main.py
+++ app/test_main.py
@@ -1,19 +1,28 @@
 import pytest
 from unittest import mock
+from typing import Any, Callable
+
+from app.main import can_access_google_page
 
 
 @pytest.fixture()
-def mocking_valid_google_url():
-    with mock.patch("valid_google_url") as func:
+def mocking_valid_google_url() -> Any:
+    with mock.patch("app.main.valid_google_url") as func:
+        func.return_value = True
         yield func
 
 
 @pytest.fixture()
-def mocked_has_internet_connection():
-    with mock.patch("has_internet_connection") as func:
-            yield func
+def mocked_has_internet_connection() -> Any:
+    with mock.patch("app.main.has_internet_connection") as func:
+        func.return_value = True
+        yield func
 
 
-def test_can_access_google_page(mocking_valid_google_url, mocked_has_internet_connection) -> None:
+def test_main_func_using_required_funcs(
+    mocking_valid_google_url: Callable,
+    mocked_has_internet_connection: Callable
+) -> None:
+    can_access_google_page("")
     mocking_valid_google_url.assert_called_once()
-    mocked_has_internet_connection.assert_called()
+    mocked_has_internet_connection.assert_called_once()
