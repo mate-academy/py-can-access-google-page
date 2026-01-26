b09b3f96ba5fe5210a18e20b4f3f506d7be69f8f
Yuliia Zahoruulko <yzahorulko@gmail.com>
2026-01-26 23:25:30 +0200

    clarified test names and annotations

--- app/test_main.py
+++ app/test_main.py
@@ -5,7 +5,7 @@ from app.main import can_access_google_page
 
 
 @pytest.fixture()
-def mocked_valid_google_url() -> None:
+def test_can_access_google_page_when_url_valid_and_connection_exists() -> None:
     with mock.patch("app.main.valid_google_url") as mocked_url:
         yield mocked_url
 
@@ -16,24 +16,24 @@ def mocked_internet_connection() -> None:
         yield mocked_connection
 
 
-def test_can_access_google_page(mocked_valid_google_url: bool,
-                                mocked_internet_connection: bool
+def test_can_access_google_page(mocked_valid_google_url: mock.MagicMock,
+                                mocked_internet_connection: mock.MagicMock
                                 ) -> None:
     mocked_valid_google_url.return_value = True
     mocked_internet_connection.return_value = True
     assert can_access_google_page("valid_url") == "Accessible"
 
 
-def test_cannot_access_if_only_valid_url(mocked_valid_google_url: bool,
-                                         mocked_internet_connection: bool
+def test_cannot_access_if_only_valid_url(mocked_valid_google_url: mock.MagicMock,
+                                         mocked_internet_connection: mock.MagicMock
                                          ) -> None:
     mocked_valid_google_url.return_value = True
     mocked_internet_connection.return_value = False
     assert can_access_google_page("valid_url") == "Not accessible"
 
 
-def test_cannot_access_if_only_connection(mocked_valid_google_url: bool,
-                                          mocked_internet_connection: bool
+def test_cannot_access_if_only_connection(mocked_valid_google_url: mock.MagicMock,
+                                          mocked_internet_connection: mock.MagicMock
                                           ) -> None:
     mocked_valid_google_url.return_value = False
     mocked_internet_connection.return_value = True
