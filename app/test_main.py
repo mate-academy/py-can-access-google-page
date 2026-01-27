06f802e62c4a636e147bd85a52245127f59fb34f
Yatsenko Dmytro <sonituensem@gmail.com>
2026-01-27 13:45:40 +0200

    Flake8 tests fixed

--- app/test_main.py
+++ app/test_main.py
@@ -2,6 +2,7 @@ from unittest import mock
 import pytest
 from app.main import can_access_google_page
 
+
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
 @pytest.mark.parametrize(
@@ -10,11 +11,17 @@ from app.main import can_access_google_page
         (True, True, "Accessible"),
         (True, False, "Not accessible"),
         (False, True, "Not accessible"),
-        (False, False, "Not accessible")
+        (False, False, "Not accessible"),
     ],
 )
-def test_can_access_google_page(mock_valid_url, mock_internet, internet, valid_url, expected):
+def test_can_access_google_page(
+    mock_valid_url: mock.MagicMock,
+    mock_internet: mock.MagicMock,
+    internet: bool,
+    valid_url: bool,
+    expected: str
+) -> None:
     mock_internet.return_value = internet
     mock_valid_url.return_value = valid_url
-    result = can_access_google_page("https://www.google.com")
+    result: str = can_access_google_page("https://www.google.com")
     assert result == expected
