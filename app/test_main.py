2ecbf99f699348d09453a20538a3216863d95a4d
Artur Bertin <arturbertin2006@gmail.com>
2026-01-28 17:55:37 -0300

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -14,3 +14,32 @@ def test_access_to_google_is_available(mock_has_internet_connection: Mock,
     expected = "Accessible"
     actual = can_access_google_page(url)
     assert actual == expected
+
+
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
+def test_access_google_is_unavailable_invalid_url(
+        mock_has_internet_connection: Mock,
+        mock_valid_google_url: Mock
+) -> None:
+    mock_valid_google_url.return_value = False
+    mock_has_internet_connection.return_value = True
+
+    expected = "Not accessible"
+    actual = can_access_google_page(url)
+    assert actual == expected
+
+
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
+def test_access_google_is_unavailable_no_internet_connection(
+        mock_has_internet_connection: Mock,
+        mock_valid_google_url: Mock
+) -> None:
+
+    mock_valid_google_url.return_value = True
+    mock_has_internet_connection.return_value = False
+
+    expected = "Not accessible"
+    actual = can_access_google_page(url)
+    assert actual == expected
