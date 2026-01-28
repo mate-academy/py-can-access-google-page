282a8155ff7c68abea3c33d3ec7d048232e0174c
Artur Bertin <arturbertin2006@gmail.com>
2026-01-28 17:34:00 -0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,16 @@
-# write your code here
+from app.main import can_access_google_page
+from unittest.mock import patch, Mock
+
+url = "https://google.com"
+
+
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
+def test_access_to_google_is_available(mock_has_internet_connection: Mock,
+                                       mock_valid_google_url: Mock) -> None:
+    mock_valid_google_url.return_value = True
+    mock_has_internet_connection.return_value = True
+
+    expected = "Accessible"
+    actual = can_access_google_page(url)
+    assert actual == expected
