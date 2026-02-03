ee36a782dec1f396424768b3a8462ab0087cac8d
Mafialate <mafialate@ukr.net>
2026-02-03 20:47:55 +0100

    Add test when no internet

--- app/test_main.py
+++ app/test_main.py
@@ -16,6 +16,18 @@ def test_can_access_google_page() -> None:
         mock_valid_url.assert_called_once_with("https://www.google.com")
 
 
+def test_can_not_access_google_page_when_no_internet() -> None:
+    with (mock.patch("app.main.valid_google_url") as mock_valid_url,
+          mock.patch("app.main.has_internet_connection") as mock_has_internet):
+        mock_has_internet.return_value = False
+        mock_valid_url.return_value = True
+
+        assert can_access_google_page(
+            "https://www.google.com") == "Not accessible"
+        mock_has_internet.assert_called_once()
+        mock_valid_url.assert_not_called()
+
+
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
 def test_can_not_access_google_page_when_invalid_url(
