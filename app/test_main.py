8fc719208c19ac5f512e0d39f90f9405b52f46cc
Seroga <ilchuk.cergey@gmail.com>
2026-02-04 14:07:45 +0200

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -1,20 +1,5 @@
 import pytest
-import datetime
-from unittest import mock
-from unittest.mock import MagicMock
 import app.main as main
-
-
-@mock.patch("requests.get")
-def test_valid_google_url(mock_get_response: MagicMock) -> None:
-    main.valid_google_url("https://google.com")
-    mock_get_response.assert_called_once_with("https://google.com")
-
-
-def test_has_internet_connection() -> None:
-    with mock.patch("datetime.datetime") as mock_datetime:
-        mock_datetime.now.return_value = datetime.datetime(2020, 1, 1, 3, 0, 0)
-        assert main.has_internet_connection() is False
 
 
 @pytest.mark.parametrize(
@@ -28,9 +13,9 @@ def test_has_internet_connection() -> None:
 )
 def test_can_access_google_page(
         monkeypatch: pytest.MonkeyPatch,
-        valid_url: str,
+        valid_url: bool,
         has_internet: bool,
-        result: bool
+        result: str
 ) -> None:
     monkeypatch.setattr(main, "valid_google_url", lambda url: valid_url)
     monkeypatch.setattr(main, "has_internet_connection", lambda: has_internet)
