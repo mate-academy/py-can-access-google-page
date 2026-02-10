ec1970ab19f9d98a08cd09f66417bd3a0447d783
Kamil <kmirosz6@gmail.com>
2026-02-10 21:37:48 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,6 @@
-# write your code here
+from app.main import can_access_google_page
+
+
+def test_valid_url_and_connection_exists() -> None:
+    assert can_access_google_page("https://google.com") == "Accessible"
+    assert can_access_google_page("https://not-exist.com") == "Not accessible"
