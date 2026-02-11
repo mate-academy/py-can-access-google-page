9dbb45eab7def1bccec2eb518a30eea519939ca9
Kyryl <sautakiki.com@gmail.com>
2026-02-11 19:26:19 +0200

    sol

--- app/test_main.py
+++ app/test_main.py
@@ -2,7 +2,7 @@ from unittest import mock
 from app.main import can_access_google_page
 
 
-def test_should_access_google_page() -> None:
+def test_should_validate_access_google_page() -> None:
     with (mock.patch("app.main.valid_google_url",
                      return_value=True),
             mock.patch("app.main.has_internet_connection",
@@ -19,6 +19,13 @@ def test_should_access_google_page() -> None:
 
     with (mock.patch("app.main.valid_google_url",
                      return_value=True),
+            mock.patch("app.main.has_internet_connection",
+                       return_value=False)):
+        result = can_access_google_page("https://www.google.com/")
+        assert result == "Not accessible"
+
+    with (mock.patch("app.main.valid_google_url",
+                     return_value=False),
             mock.patch("app.main.has_internet_connection",
                        return_value=False)):
         result = can_access_google_page("https://www.google.com/")
