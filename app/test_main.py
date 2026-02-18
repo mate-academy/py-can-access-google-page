894cbfd841fbc8503d5bf2d1387ecd4f89dec31b
Karolina Latko <caroline15@poczta.fm>
2026-02-18 22:24:35 +0100

    Amendment to solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,7 +2,17 @@ import pytest
 import app.main as main
 
 
-def test_not_accessible_without_connection(
+def test_not_accessible_when_both_false(
+        monkeypatch: pytest.MonkeyPatch
+) -> None:
+    monkeypatch.setattr("app.main.has_internet_connection", lambda: False)
+    monkeypatch.setattr("app.main.valid_google_url", lambda url: False)
+
+    result = main.can_access_google_page("https://google.com")
+    assert result == "Not accessible"
+
+
+def test_not_accessible_when_only_url_true(
         monkeypatch: pytest.MonkeyPatch
 ) -> None:
     monkeypatch.setattr("app.main.has_internet_connection", lambda: False)
@@ -12,7 +22,7 @@ def test_not_accessible_without_connection(
     assert result == "Not accessible"
 
 
-def test_not_accessible_with_invalid_url(
+def test_not_accessible_when_only_connection_true(
         monkeypatch: pytest.MonkeyPatch
 ) -> None:
     monkeypatch.setattr("app.main.has_internet_connection", lambda: True)
@@ -22,31 +32,9 @@ def test_not_accessible_with_invalid_url(
     assert result == "Not accessible"
 
 
-def test_accessible_only_when_both_true(
-        monkeypatch: pytest.MonkeyPatch
-) -> None:
+def test_accessible_when_both_true(monkeypatch: pytest.MonkeyPatch) -> None:
     monkeypatch.setattr("app.main.has_internet_connection", lambda: True)
     monkeypatch.setattr("app.main.valid_google_url", lambda url: True)
 
     result = main.can_access_google_page("https://google.com")
     assert result == "Accessible"
-
-
-def test_not_accessible_when_only_url_is_true(
-        monkeypatch: pytest.MonkeyPatch
-) -> None:
-    monkeypatch.setattr("app.main.has_internet_connection", lambda: False)
-    monkeypatch.setattr("app.main.valid_google_url", lambda url: True)
-
-    result = main.can_access_google_page("https://google.com")
-    assert result == "Not accessible"
-
-
-def test_not_accessible_when_only_connection_is_true(
-        monkeypatch: pytest.MonkeyPatch
-) -> None:
-    monkeypatch.setattr("app.main.has_internet_connection", lambda: True)
-    monkeypatch.setattr("app.main.valid_google_url", lambda url: False)
-
-    result = main.can_access_google_page("https://google.com")
-    assert result == "Not accessible"
