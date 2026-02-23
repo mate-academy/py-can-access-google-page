2c493035f93f634b2358ac232f3b9d0ee0819ca9
Artem Turchyn <Artem.at131@gmail.com>
2026-02-23 19:39:54 +0200

    update

--- app/test_main.py
+++ app/test_main.py
@@ -3,18 +3,18 @@ from app.main import can_access_google_page
 
 
 def test_can_access_google_page() -> None:
-    with (mock.patch("app.main.valid_google_url", return_value=True),
-          mock.patch("app.main.has_internet_connection", return_value=True)):
+    with mock.patch("app.main.valid_google_url", return_value=True), \
+         mock.patch("app.main.has_internet_connection", return_value=True):
         assert can_access_google_page("https://google.com") == "Accessible"
 
-    with (mock.patch("app.main.valid_google_url", return_value=True),
-          mock.patch("app.main.has_internet_connection", return_value=False)):
+    with mock.patch("app.main.valid_google_url", return_value=True), \
+         mock.patch("app.main.has_internet_connection", return_value=False):
         assert can_access_google_page("https://google.com") == "Not accessible"
 
-    with (mock.patch("app.main.valid_google_url", return_value=False),
-          mock.patch("app.main.has_internet_connection", return_value=True)):
+    with mock.patch("app.main.valid_google_url", return_value=False), \
+         mock.patch("app.main.has_internet_connection", return_value=True):
         assert can_access_google_page("Hello") == "Not accessible"
 
-    with (mock.patch("app.main.valid_google_url", return_value=False),
-          mock.patch("app.main.has_internet_connection", return_value=False)):
+    with mock.patch("app.main.valid_google_url", return_value=False), \
+         mock.patch("app.main.has_internet_connection", return_value=False):
         assert can_access_google_page("hello") == "Not accessible"
