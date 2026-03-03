9a497af9ea3cc895b3157cb4fb9f8467559c06c6
Serhii <petrenkosm13@gmail.com>
2026-03-03 09:07:11 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -4,7 +4,9 @@ from app.main import can_access_google_page
 
 @patch("app.main.valid_google_url", return_value=True)
 @patch("app.main.has_internet_connection", return_value=True)
-def test_accessible(mock_internet: object, mock_valid_url: object) -> None:
+def test_returns_accessible_when_url_is_valid_and_internet_is_connected(
+    mock_internet: object, mock_valid_url: object
+) -> None:
     result: str = can_access_google_page("https://google.com")
     assert result == "Accessible"
 
@@ -12,8 +14,7 @@ def test_accessible(mock_internet: object, mock_valid_url: object) -> None:
 @patch("app.main.valid_google_url", return_value=False)
 @patch("app.main.has_internet_connection", return_value=True)
 def test_not_accessible_invalid_url(
-        mock_internet: object,
-        mock_valid_url: object
+    mock_internet: object, mock_valid_url: object
 ) -> None:
     result: str = can_access_google_page("https://google.com")
     assert result == "Not accessible"
@@ -22,8 +23,7 @@ def test_not_accessible_invalid_url(
 @patch("app.main.valid_google_url", return_value=True)
 @patch("app.main.has_internet_connection", return_value=False)
 def test_not_accessible_no_internet(
-        mock_internet: object,
-        mock_valid_url: object
+    mock_internet: object, mock_valid_url: object
 ) -> None:
     result: str = can_access_google_page("https://google.com")
     assert result == "Not accessible"
@@ -32,8 +32,7 @@ def test_not_accessible_no_internet(
 @patch("app.main.valid_google_url", return_value=False)
 @patch("app.main.has_internet_connection", return_value=False)
 def test_not_accessible_both_fail(
-        mock_internet: object,
-        mock_valid_url: object
+    mock_internet: object, mock_valid_url: object
 ) -> None:
     result: str = can_access_google_page("https://google.com")
     assert result == "Not accessible"
