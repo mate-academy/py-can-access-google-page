b9fc5b970ba71bc6c3a83f0d92da15a3232fb5a3
Michal Lysakowski <michaslysakowski@gmail.com>
2026-03-07 12:28:48 +0100

    Solution: added type annotations

--- app/test_main.py
+++ app/test_main.py
@@ -5,8 +5,8 @@ from app.main import can_access_google_page
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
 def test_accessible_when_url_valid_and_internet_on(
-    mock_has_internet_connection,
-    mock_valid_google_url,
+    mock_has_internet_connection: bool,
+    mock_valid_google_url: bool,
 ) -> None:
     mock_has_internet_connection.return_value = True
     mock_valid_google_url.return_value = True
@@ -17,8 +17,7 @@ def test_accessible_when_url_valid_and_internet_on(
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
 def test_not_accessible_when_url_invalid(
-    mock_has_internet_connection,
-    mock_valid_google_url,
+    mock_valid_google_url: bool, mock_has_internet_connection: bool
 ) -> None:
     mock_has_internet_connection.return_value = True
     mock_valid_google_url.return_value = False
@@ -29,8 +28,7 @@ def test_not_accessible_when_url_invalid(
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
 def test_not_accessible_when_no_internet(
-    mock_has_internet_connection,
-    mock_valid_google_url,
+    mock_valid_google_url: bool, mock_has_internet_connection: bool
 ) -> None:
     mock_has_internet_connection.return_value = False
     mock_valid_google_url.return_value = True
