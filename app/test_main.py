0dec0453d507d4b4b06d808d8888f95e5a341e91
Michal Lysakowski <michaslysakowski@gmail.com>
2026-03-07 14:44:09 +0100

    Solution: added type annotations and reversed test order as Mate had some problems with the test

--- app/test_main.py
+++ app/test_main.py
@@ -17,7 +17,7 @@ def test_accessible_when_url_valid_and_internet_on(
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
 def test_not_accessible_when_url_invalid(
-    mock_valid_google_url: bool, mock_has_internet_connection: bool
+    mock_has_internet_connection: bool, mock_valid_google_url: bool
 ) -> None:
     mock_has_internet_connection.return_value = True
     mock_valid_google_url.return_value = False
@@ -28,7 +28,7 @@ def test_not_accessible_when_url_invalid(
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
 def test_not_accessible_when_no_internet(
-    mock_valid_google_url: bool, mock_has_internet_connection: bool
+    mock_has_internet_connection: bool, mock_valid_google_url: bool
 ) -> None:
     mock_has_internet_connection.return_value = False
     mock_valid_google_url.return_value = True
