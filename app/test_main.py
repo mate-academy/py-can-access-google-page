192f76ff7fb2859c52533f6b66cda520e47948eb
Eduardo Atae <e.atae05@gmail.com>
2026-03-07 21:15:05 -0300

    fix flake8 line length and add monkeypatch tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,34 @@
-# write your code here
+import pytest
+from app import main
+
+
+@pytest.mark.parametrize(
+    "url,is_valid_url,has_connection,expected",
+    [
+        ("https://google.com", True, True, "Accessible"),
+        ("https://google.com", True, False, "Not accessible"),
+        ("https://google.com", False, True, "Not accessible"),
+        ("https://google.com", False, False, "Not accessible"),
+    ],
+)
+def test_can_access_google_page(
+    monkeypatch: pytest.MonkeyPatch,
+    url: str,
+    is_valid_url: bool,
+    has_connection: bool,
+    expected: str,
+) -> None:
+
+    monkeypatch.setattr(
+        main,
+        "valid_google_url",
+        lambda x: is_valid_url,
+    )
+
+    monkeypatch.setattr(
+        main,
+        "has_internet_connection",
+        lambda: has_connection,
+    )
+
+    assert main.can_access_google_page(url) == expected
