18513924c7a4c3b73e8c65a91c539309ac944656
maxtort <topormaks3217@gmail.com>
2026-03-09 21:34:32 +0200

    Solution 1.02

--- app/test_main.py
+++ app/test_main.py
@@ -8,13 +8,13 @@ def test_accessible_with_valid_url_and_connection() -> None:
         assert can_access_google_page("https://google.com") == "Accessible"
 
 
-def test_with_not_internet_connection() -> None:
+def test_returns_not_accessible_when_no_internet_connection() -> None:
     with (mock.patch("app.main.valid_google_url", return_value=True),
           mock.patch("app.main.has_internet_connection", return_value=False)):
         assert can_access_google_page("https://google.com") == "Not accessible"
 
 
-def test_with_invalid_url() -> None:
+def test_returns_not_accessible_with_invalid_url() -> None:
     with (mock.patch("app.main.valid_google_url", return_value=False),
           mock.patch("app.main.has_internet_connection", return_value=True)):
         assert can_access_google_page("https://google.com") == "Not accessible"
