86d38dce7080412d8d2e23e459567dc5178201dd
Anton Bublykov <mw.anton2000@gmail.com>
2026-03-10 21:22:05 +0100

    change 'expected_value'

--- app/test_main.py
+++ app/test_main.py
@@ -7,31 +7,31 @@ from app.main import can_access_google_page
     "initial_internet, initial_url, expected",
     [
         pytest.param(
-            True, True, True,
-            id="should return True if are internet and correct url"
+            True, True, "Accessible",
+            id="should return 'Accessible' if are internet and correct url"
         ),
         pytest.param(
-            True, False, False,
-            id="should return False if is not correct url"
+            True, False, "Not accessible",
+            id="should return 'Not accessible' if is not correct url"
         ),
         pytest.param(
-            False, True, False,
-            id="should return False if is not internet"
+            False, True, "Not accessible",
+            id="should return 'Not accessible' if is not internet"
         ),
         pytest.param(
-            False, False, False,
-            id="should return False if are not internet and correct url"
+            False, False, "Not accessible",
+            id="should return 'Not accessible' if are not internet and correct url"
         )
     ]
 )
 @mock.patch("app.main.valid_google_url")
 @mock.patch("app.main.has_internet_connection")
-def test_return_access_to_url(
+def test_can_access_google_page_scenarios(
         mock_internet: bool,
         mock_url: bool,
         initial_internet: bool,
         initial_url: bool,
-        expected: bool,
+        expected: str,
 ) -> None:
 
     mock_internet.return_value = initial_internet
