63bf1ae22b5b93b3e92338eabd3173a5f046895d
Rustam Mamatov <Mamatovr31@gmail.com>
2026-03-10 18:55:55 +0200

    correct mistakes

--- app/test_main.py
+++ app/test_main.py
@@ -14,9 +14,9 @@ from app.main import can_access_google_page
 )
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
-def test_can_access(
-        mock_inet: MagicMock,
+def test_can_access_google_page_scenarios(
         mock_valid: MagicMock,
+        mock_inet: MagicMock,
         valid: bool,
         conn: bool,
         expected: str
