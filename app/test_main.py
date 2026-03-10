dec6d5c6aa9e99c018d5efd9dbe4e16bfe180555
Rustam Mamatov <Mamatovr31@gmail.com>
2026-03-10 18:37:42 +0200

    make home_work

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,26 @@
-# write your code here
+from unittest.mock import patch, MagicMock
+import pytest
+from app.main import can_access_google_page
+
+
+@pytest.mark.parametrize(
+    "valid,conn,expected",
+    [
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible"),
+    ]
+)
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
+def test_can_access(
+        mock_inet: MagicMock,
+        mock_valid: MagicMock,
+        valid: bool,
+        conn: bool,
+        expected: str
+) -> None:
+    mock_valid.return_value = valid
+    mock_inet.return_value = conn
+    assert can_access_google_page("https://mate.academy/home") == expected
