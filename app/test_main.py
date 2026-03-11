4965dfec3b72e5e7e451502d3a611ce87994a477
Kasjan Michalak <kasjan.michalak@gmail.com>
2026-03-11 17:12:33 +0000

    2nd save

--- app/test_main.py
+++ app/test_main.py
@@ -3,10 +3,18 @@ from unittest import mock
 from .main import can_access_google_page
 
 
-def test_can_access_to_google_page() -> None:
+def test_can_access_to_google_page_when_accessible() -> None:
     with mock.patch("app.main.valid_google_url") as mock_valid:
         with mock.patch("app.main.has_internet_connection") as mock_internet:
             mock_internet.return_value = True
             mock_valid.return_value = True
             result = can_access_google_page("https://google.com")
             assert result == "Accessible"
+
+def test_can_access_google_page_when_not_accessible() -> None:
+    with mock.patch("app.main.valid_google_url") as mock_valid:
+        with mock.patch("app.main.has_internet_connection") as mock_internet:
+            mock_internet.return_value = False
+            mock_valid.return_value = False
+            result = can_access_google_page("https://google.com")
+            assert result == "Not accessible"
