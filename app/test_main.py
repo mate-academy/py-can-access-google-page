7b1a17c3d954fac04e9f80649d1b057a796112c5
Artem Yur <artemonodon@gmail.com>
2026-03-14 14:01:20 +0100

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,28 +1,38 @@
-import pytest
+from pytest_mock import MockerFixture
+
 from app.main import can_access_google_page
 
 
-def test_should_be_accessible_when_url_is_valid_and_connection_exists(mocker) -> None:
+def test_accessible_when_url_valid_and_connection_exists(
+    mocker: MockerFixture,
+) -> None:
     mocker.patch("app.main.valid_google_url", return_value=True)
     mocker.patch("app.main.has_internet_connection", return_value=True)
 
     assert can_access_google_page("https://google.com") == "Accessible"
 
 
-def test_can_not_access_if_not_valid_url(mocker) -> None:
+def test_not_accessible_if_url_is_not_valid(
+    mocker: MockerFixture,
+) -> None:
     mocker.patch("app.main.valid_google_url", return_value=False)
     mocker.patch("app.main.has_internet_connection", return_value=True)
 
     assert can_access_google_page("https://google.com") == "Not accessible"
 
 
-def test_can_not_access_if_not_internet_connection(mocker) -> None:
+def test_not_accessible_if_no_internet_connection(
+    mocker: MockerFixture,
+) -> None:
     mocker.patch("app.main.valid_google_url", return_value=True)
     mocker.patch("app.main.has_internet_connection", return_value=False)
 
     assert can_access_google_page("https://google.com") == "Not accessible"
 
-def test_can_not_access_if_both_not_true(mocker) -> None:
+
+def test_not_accessible_if_url_invalid_and_no_connection(
+    mocker: MockerFixture,
+) -> None:
     mocker.patch("app.main.valid_google_url", return_value=False)
     mocker.patch("app.main.has_internet_connection", return_value=False)
 
