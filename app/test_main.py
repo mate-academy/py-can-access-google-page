e143b878d9486d8ffd0cade06edd6eafce94725c
Kott Q <constantinekuncel@gmail.com>
2026-03-17 10:16:10 +0100

    solve2

--- app/test_main.py
+++ app/test_main.py
@@ -4,21 +4,34 @@ from app import main
 
 
 @pytest.mark.parametrize(
-    "url, expected",
+    "url, internet, valid_url, expected",
     [
-        ("https://www.google.com", "Accessible"),
+        ("https://www.google.com", True, True, "Accessible"),
+        ("http://www.goole.com", True, False, "Not accessible"),
+        ("https://www.google.com", False, True, "Not accessible"),
+        ("http://www.goole.com", False, False, "Not accessible"),
     ],
 )
-def test_can_access_google_page(url: str, expected: str) -> None:
+def test_can_access_google_page(
+    url: str,
+    internet: bool,
+    valid_url: bool,
+    expected: str,
+) -> None:
     with (
         mock.patch("app.main.valid_google_url") as mock_valid_url,
         mock.patch("app.main.has_internet_connection") as mock_internet_connection,
     ):
-        mock_valid_url.return_value = True
-        mock_internet_connection.return_value = True
+        mock_valid_url.return_value = valid_url
+        mock_internet_connection.return_value = internet
 
         result = main.can_access_google_page(url)
 
         assert result == expected
         mock_internet_connection.assert_called_once()
-        mock_valid_url.assert_called_once_with(url)
+
+        # важно из-за short-circuit
+        if internet:
+            mock_valid_url.assert_called_once_with(url)
+        else:
+            mock_valid_url.assert_not_called()
