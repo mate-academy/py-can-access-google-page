983bfb1238a9d3c345a99cefc96486e94a0068c7
kuligambino01 <czaro923@gmail.com>
2026-03-23 11:27:28 +0100

    'fixed_flake8'

--- app/test_main.py
+++ app/test_main.py
@@ -6,8 +6,8 @@ from app.main import can_access_google_page
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
 def test_should_return_true_when_url_is_valid_and_connection_exists(
-        mocked_valid_google_url,
-        mocked_has_internet_connection,
+    mocked_valid_google_url: bool,
+    mocked_has_internet_connection: bool,
 ) -> None:
     mocked_valid_google_url.return_value = True
     mocked_has_internet_connection.return_value = True
