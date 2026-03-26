498e03a43fbd9a219b99ee7cc50c7f1428d31c73
Arseniy Petrenko <arsfix2004@gmail.com>
2026-03-26 15:46:57 +0200

    Fixed Solution 2

--- app/test_main.py
+++ app/test_main.py
@@ -7,9 +7,9 @@ from app.main import can_access_google_page
     "is_valid_url, has_connection, expected_result",
     [
         pytest.param(True, True, "Accessible", id="full_access"),
-        pytest.param(False, True, "Not accessible", id="no_internet"),
-        pytest.param(True, False, "Not accessible", id="invalid_url"),
-        pytest.param(False, False, "Not accessible", id="both_fail"),
+        pytest.param(False, True, "Not accessible", id="invalid_url"),
+        pytest.param(True, False, "Not accessible", id="no_internet"),
+        pytest.param(False, False, "Not accessible", id="both_fail")
     ]
 )
 @patch("app.main.valid_google_url")
@@ -19,9 +19,8 @@ def test_can_access_google_page(mocked_connection: any,
                                 has_connection: bool,
                                 is_valid_url: bool,
                                 expected_result: str) -> None:
-
-    mocked_connection.return_value = has_connection
     mocked_url.return_value = is_valid_url
+    mocked_connection.return_value = has_connection
 
     result = can_access_google_page("https://google.com")
 
