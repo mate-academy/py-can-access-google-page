c7dbac9bb8b95e3ffa1852fe140c83fe86432294
Nikita <nikita.polischukov@gmail.com>
2026-03-29 00:00:40 +0100

    Solution1

--- app/test_main.py
+++ app/test_main.py
@@ -2,9 +2,33 @@ from unittest.mock import patch
 from app.main import can_access_google_page
 
 
-def test_can_access_google_page() -> None:
+def test_can_access_page_when_url_is_valid_and_connection_exists() -> None:
     with (
         patch("app.main.valid_google_url", return_value=True),
         patch("app.main.has_internet_connection", return_value=True),
     ):
         assert can_access_google_page("google.com") == "Accessible"
+
+
+def test_can_not_access_page_when_url_is_invalid_and_connection_exists() -> None:
+    with (
+        patch("app.main.valid_google_url", return_value=False),
+        patch("app.main.has_internet_connection", return_value=True),
+    ):
+        assert can_access_google_page("google.com") == "Not accessible"
+
+
+def test_can_not_access_page_when_url_is_valid_and_connection_missing() -> None:
+    with (
+        patch("app.main.valid_google_url", return_value=True),
+        patch("app.main.has_internet_connection", return_value=False),
+    ):
+        assert can_access_google_page("google.com") == "Not accessible"
+
+
+def test_can_not_access_page_when_url_is_invalid_and_connection_missing() -> None:
+    with (
+        patch("app.main.valid_google_url", return_value=False),
+        patch("app.main.has_internet_connection", return_value=False),
+    ):
+        assert can_access_google_page("google.com") == "Not accessible"
