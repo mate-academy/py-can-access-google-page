e75ea7af20087d4e527d8b81416869e0bd8ca447
Vladislav <vladjigalo07@gmail.com>
2026-04-02 23:37:16 +0300

    develop v2

--- app/test_main.py
+++ app/test_main.py
@@ -1,32 +1,6 @@
 import datetime
 from unittest import mock
-from app.main import (
-    can_access_google_page,
-    has_internet_connection,
-    valid_google_url,
-)
-
-
-@mock.patch("app.main.requests.get")
-def test_valid_google_url(mock_get: mock.MagicMock) -> None:
-    mock_get.return_value.status_code = 200
-    assert valid_google_url("https://www.google.com") is True
-
-    mock_get.return_value.status_code = 404
-    assert valid_google_url("https://www.invalid-url.com") is False
-
-
-@mock.patch("app.main.datetime")
-def test_has_internet_connection(mock_datetime: mock.MagicMock) -> None:
-    mock_datetime.datetime.now.return_value = datetime.datetime(
-        2024, 1, 1, 6, 0
-    )
-    assert has_internet_connection() is True
-
-    mock_datetime.datetime.now.return_value = datetime.datetime(
-        2024, 1, 1, 23, 0
-    )
-    assert has_internet_connection() is False
+from app.main import can_access_google_page
 
 
 def test_can_access_google_page() -> None:
