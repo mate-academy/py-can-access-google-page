ec8591051c3cbae0f57f7f714f7b61fcd029f51c
Oleh M <oleh.murachov28@gmail.com>
2026-04-04 09:00:47 +0200

    oleh

--- app/test_main.py
+++ app/test_main.py
@@ -1,31 +1,43 @@
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 from app.main import can_access_google_page
 
 
 @patch("app.main.valid_google_url", return_value=True)
 @patch("app.main.has_internet_connection", return_value=True)
-def test_accessible(mock_internet, mock_valid) -> None:
+def test_accessible(
+    mock_internet: MagicMock,
+    mock_valid: MagicMock
+) -> None:
     result = can_access_google_page("https://google.com")
     assert result == "Accessible"
 
 
 @patch("app.main.valid_google_url", return_value=False)
 @patch("app.main.has_internet_connection", return_value=True)
-def test_invalid_url(mock_internet, mock_valid) -> None:
+def test_invalid_url(
+    mock_internet: MagicMock,
+    mock_valid: MagicMock
+) -> None:
     result = can_access_google_page("https://notgoogle.com")
     assert result == "Not accessible"
 
 
 @patch("app.main.valid_google_url", return_value=True)
 @patch("app.main.has_internet_connection", return_value=False)
-def test_no_internet(mock_internet, mock_valid) -> None:
+def test_no_internet(
+    mock_internet: MagicMock,
+    mock_valid: MagicMock
+) -> None:
     result = can_access_google_page("https://google.com")
     assert result == "Not accessible"
 
 
 @patch("app.main.valid_google_url", return_value=False)
 @patch("app.main.has_internet_connection", return_value=False)
-def test_invalid_url_and_no_internet(mock_internet, mock_valid) -> None:
+def test_invalid_url_and_no_internet(
+    mock_internet: MagicMock,
+    mock_valid: MagicMock,
+) -> None:
     result = can_access_google_page("https://bad.com")
     assert result == "Not accessible"
