e5dc8d947d2358aafe570b078ec91aa1b8157197
Denys Matusevych <denytop3@gmal.com>
2026-04-05 20:10:29 +0300

    Solution without pytest.mark.parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,36 @@
-# write your code here
+from unittest.mock import patch
+from app.main import can_access_google_page
+
+
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
+def test_success(mocked_internet_connection, mocked_valid_url):
+    mocked_internet_connection.return_value = True
+    mocked_valid_url.return_value = True
+
+    assert can_access_google_page("https://www.google.com") == "Accessible"
+
+
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
+def test_invalid_url(mocked_internet_connection, mocked_valid_url):
+    mocked_internet_connection.return_value = True
+    mocked_valid_url.return_value = False
+
+    assert can_access_google_page("https://www.google.com") == "Not accessible"
+
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
+def test_invalid_time(mocked_internet_connection, mocked_valid_url):
+    mocked_internet_connection.return_value = False
+    mocked_valid_url.return_value = True
+
+    assert can_access_google_page("https://www.google.com") == "Not accessible"
+
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
+def test_invalid_time_and_url(mocked_internet_connection, mocked_valid_url):
+    mocked_internet_connection.return_value = False
+    mocked_valid_url.return_value = False
+
+    assert can_access_google_page("https://www.google.com") == "Not accessible"
