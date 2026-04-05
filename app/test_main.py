60025a108a91f08134c85f472bc71e640e295f89
Bohdan3457 <bohdankhomenko04@gmail.com>
2026-04-05 10:40:51 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,33 @@
-# write your code here
+from pytest import MonkeyPatch
+from app.main import can_access_google_page
+
+
+url = "https://google.com"
+
+
+def test_accessible(monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setattr("app.main.valid_google_url", lambda x: True)
+    monkeypatch.setattr("app.main.has_internet_connection", lambda: True)
+    result = can_access_google_page(url)
+    assert result == "Accessible"
+
+
+def test_no_access_invalid_url(monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setattr("app.main.valid_google_url", lambda x: False)
+    monkeypatch.setattr("app.main.has_internet_connection", lambda: True)
+    result = can_access_google_page(url)
+    assert result == "Not accessible"
+
+
+def test_no_internet_valid_url(monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setattr("app.main.valid_google_url", lambda x: True)
+    monkeypatch.setattr("app.main.has_internet_connection", lambda: False)
+    result = can_access_google_page(url)
+    assert result == "Not accessible"
+
+
+def test_no_internet_invalid_url(monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setattr("app.main.valid_google_url", lambda x: False)
+    monkeypatch.setattr("app.main.has_internet_connection", lambda: False)
+    result = can_access_google_page(url)
+    assert result == "Not accessible"
