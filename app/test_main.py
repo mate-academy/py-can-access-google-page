27a52cae901e3578cfd45739efc1f764128dbfc4
Denys Matusevych <denytop3@gmal.com>
2026-04-05 20:12:48 +0300

    fixed flake8

--- app/test_main.py
+++ app/test_main.py
@@ -4,7 +4,8 @@ from app.main import can_access_google_page
 
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
-def test_success(mocked_internet_connection, mocked_valid_url):
+def test_success(mocked_internet_connection: None,
+                 mocked_valid_url: str) -> None:
     mocked_internet_connection.return_value = True
     mocked_valid_url.return_value = True
 
@@ -13,23 +14,28 @@ def test_success(mocked_internet_connection, mocked_valid_url):
 
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
-def test_invalid_url(mocked_internet_connection, mocked_valid_url):
+def test_invalid_url(mocked_internet_connection: None,
+                     mocked_valid_url: str) -> None:
     mocked_internet_connection.return_value = True
     mocked_valid_url.return_value = False
 
     assert can_access_google_page("https://www.google.com") == "Not accessible"
 
+
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
-def test_invalid_time(mocked_internet_connection, mocked_valid_url):
+def test_invalid_time(mocked_internet_connection: None,
+                      mocked_valid_url: str) -> None:
     mocked_internet_connection.return_value = False
     mocked_valid_url.return_value = True
 
     assert can_access_google_page("https://www.google.com") == "Not accessible"
 
+
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
-def test_invalid_time_and_url(mocked_internet_connection, mocked_valid_url):
+def test_invalid_time_and_url(mocked_internet_connection: None,
+                              mocked_valid_url: str) -> None:
     mocked_internet_connection.return_value = False
     mocked_valid_url.return_value = False
 
