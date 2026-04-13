46b5de455fdaa3860d59ebec519b11e0d729f405
Artem <artemradckevich@gmail.com>
2026-04-13 14:29:07 +0300

    Fix flake8 issues in test_main

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,6 @@
 from typing import Generator
 from unittest import mock
+from unittest.mock import MagicMock
 
 import pytest
 
@@ -9,20 +10,26 @@ url = "https://www.google.com"
 
 
 @pytest.fixture
-def mocks() -> Generator:
-    with mock.patch("app.main.valid_google_url") as mock_url, \
-        mock.patch("app.main.has_internet_connection") as mock_internet:
+def mocks() -> Generator[tuple[MagicMock, MagicMock], None, None]:
+    with (
+        mock.patch("app.main.valid_google_url") as mock_url,
+        mock.patch("app.main.has_internet_connection") as mock_internet,
+    ):
         yield mock_url, mock_internet
 
 
-def test_accessible(mocks) -> None:
+def test_accessible(
+        mocks: tuple[MagicMock, MagicMock]
+) -> None:
     mock_url, mock_internet = mocks
     mock_url.return_value = True
     mock_internet.return_value = True
     assert can_access_google_page(url) == "Accessible"
 
 
-def test_not_accessible_invalid_url(mocks) -> None:
+def test_not_accessible_invalid_url(
+        mocks: tuple[MagicMock, MagicMock]
+) -> None:
     mock_url, mock_internet = mocks
     mock_url.return_value = False
     mock_internet.return_value = True
@@ -30,7 +37,9 @@ def test_not_accessible_invalid_url(mocks) -> None:
     assert can_access_google_page(url) == "Not accessible"
 
 
-def test_not_accessible_no_internet(mocks) -> None:
+def test_not_accessible_no_internet(
+        mocks: tuple[MagicMock, MagicMock]
+) -> None:
     mock_url, mock_internet = mocks
     mock_url.return_value = True
     mock_internet.return_value = False
@@ -38,7 +47,9 @@ def test_not_accessible_no_internet(mocks) -> None:
     assert can_access_google_page(url) == "Not accessible"
 
 
-def test_not_accessible_both_false(mocks) -> None:
+def test_not_accessible_both_false(
+        mocks: tuple[MagicMock, MagicMock]
+) -> None:
     mock_url, mock_internet = mocks
     mock_url.return_value = False
     mock_internet.return_value = False
