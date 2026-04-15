9c403ab338e511246cae9b73fa9e2c525ea3ef53
PavelPereverzev1 <pereverzev.pavel1975@gmail.com>
2026-04-15 11:49:35 +0300

    Implemented tests for can_access_google_page function

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,28 @@
-# write your code here
+import pytest
+from unittest.mock import patch
+
+from app.main import can_access_google_page
+
+
+@pytest.mark.parametrize(
+    "url_status, internet_status, expected",
+    [
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, True, "Not accessible"),
+        (False, False, "Not accessible")
+    ]
+)
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
+def test_can_access_google_page(
+        mock_valid_google_url: bool,
+        mock_has_internet_connection: bool,
+        url_status: bool ,
+        internet_status: bool,
+        expected: str
+) -> None:
+    mock_valid_google_url.return_value = url_status
+    mock_has_internet_connection.return_value = internet_status
+
+    assert can_access_google_page("https://www.google.com") == expected
