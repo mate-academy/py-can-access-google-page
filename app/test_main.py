a124c6696ed1130023c10506c6369b75c9f3b164
Konstantin Nahorni <konstantinnahorni@gmail.com>
2026-04-17 12:23:56 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -23,8 +23,8 @@ def test_can_access_google_page_invalid_url(
         mock_valid: MagicMock,
         mock_internet: MagicMock
 ) -> None:
-    mock_valid.return_value = True
-    mock_internet.return_value = False
+    mock_valid.return_value = False
+    mock_internet.return_value = True
 
     result = can_access_google_page("https://google.com")
     assert result == "Not accessible"
@@ -39,5 +39,5 @@ def test_can_access_google_page_no_internet(
     mock_valid.return_value = True
     mock_internet.return_value = False
 
-    result = can_access_google_page("https://bad-url.com")
+    result = can_access_google_page("https://google.com")
     assert result == "Not accessible"
