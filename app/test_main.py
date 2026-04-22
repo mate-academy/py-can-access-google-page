765f66e228c086649ae240cbdf6ded605696d582
gunsts <137734013+gunsts@users.noreply.github.com>
2026-04-22 20:51:48 +0700

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,41 @@
-# write your code here
+import pytest
+import main
+
+
+def test_valid_google_url_with_internet_connection(
+        monkeypatch: pytest.MonkeyPatch) -> None:
+    # mock valid_google_url to always return True
+    monkeypatch.setattr("main.valid_google_url", lambda url: True)
+    # mock has_internet_connection to always return True
+    monkeypatch.setattr("main.has_internet_connection", lambda: True)
+    assert main.can_access_google_page("https://google.com") == "Accessible"
+
+
+def test_invalid_google_url_with_internet_connection(
+        monkeypatch: pytest.MonkeyPatch) -> None:
+    # mock valid_google_url to always return False
+    monkeypatch.setattr("main.valid_google_url", lambda url: False)
+    # mock has_internet_connection to always return True
+    monkeypatch.setattr("main.has_internet_connection", lambda: True)
+    assert main.can_access_google_page("https://google.com") == \
+        "Not accessible"
+
+
+def test_valid_google_url_with_no_internet_connection(
+        monkeypatch: pytest.MonkeyPatch) -> None:
+    # mock valid_google_url to always return True
+    monkeypatch.setattr("main.valid_google_url", lambda url: True)
+    # mock has_internet_connection to always return False
+    monkeypatch.setattr("main.has_internet_connection", lambda: False)
+    assert main.can_access_google_page("https://google.com") == \
+        "Not accessible"
+
+
+def test_invalid_google_url_with_no_internet_connection(
+        monkeypatch: pytest.MonkeyPatch) -> None:
+    # mock valid_google_url to always return False
+    monkeypatch.setattr("main.valid_google_url", lambda url: False)
+    # mock has_internet_connection to always return False
+    monkeypatch.setattr("main.has_internet_connection", lambda: False)
+    assert main.can_access_google_page("https://google.com") == \
+        "Not accessible"
