f13827317c27d68284fb564b7d1d09add6f67f94
Igor <zalugin.iv2@gmail.com>
2026-04-24 15:17:46 +0200

    all test written

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,27 @@
-# write your code here
+from unittest.mock import patch
+
+from app.main import can_access_google_page
+
+
+def test_can_access_google_and_internet_connection_true() -> None:
+    with patch("app.main.valid_google_url", return_value=True):
+        with patch("app.main.has_internet_connection", return_value=True):
+            assert can_access_google_page("correct url") == "Accessible"
+
+
+def test_can_access_google_true_has_internet_false() -> None:
+    with patch("app.main.valid_google_url", return_value=True):
+        with patch("app.main.has_internet_connection", return_value=False):
+            assert can_access_google_page("correct url") == "Not accessible"
+
+
+def test_can_access_google_false_has_internet_true() -> None:
+    with patch("app.main.valid_google_url", return_value=False):
+        with patch("app.main.has_internet_connection", return_value=True):
+            assert can_access_google_page("incorrect url") == "Not accessible"
+
+
+def test_can_access_google_false_has_internet_false() -> None:
+    with patch("app.main.valid_google_url", return_value=False):
+        with patch("app.main.has_internet_connection", return_value=False):
+            assert can_access_google_page("incorrect url") == "Not accessible"
