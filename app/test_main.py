df799154d1193a5efc7c003525a1366235f34efb
Daniil Baianov <bayanovdanil666@gmail>
2026-04-25 13:47:57 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,26 @@
-# write your code here
+import pytest
+
+
+from app.main import can_access_google_page
+
+
+@pytest.mark.parametrize("url_valid,has_connection,expected", [
+    (True, True, "Accessible"),
+    (True, False, "Not accessible"),
+    (False, True, "Not accessible"),
+    (False, False, "Not accessible")
+])
+def test_can_accessible(
+        url_valid: bool,
+        has_connection: bool,
+        expected: str,
+        mocker: object
+) -> None:
+    mocker.patch("app.main.valid_google_url", return_value=url_valid)
+    mocker.patch(
+        "app.main.has_internet_connection",
+        return_value=has_connection
+    )
+
+    result = can_access_google_page("https://google.com")
+    assert result == expected
