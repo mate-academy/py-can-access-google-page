6df554639a1900c1225b7c096993464eed277706
Raffaele Maccarrone <raffaele.alves.maccarrone@gmail.com>
2026-05-06 17:11:16 -0300

    Solução

--- app/test_main.py
+++ app/test_main.py
@@ -5,7 +5,10 @@ from app.main import can_access_google_page
 # O patch deve apontar para onde as funções são CHAMADAS (no arquivo main)
 @patch("app.main.has_internet_connection")
 @patch("app.main.valid_google_url")
-def test_can_access_google_page(mock_valid_url: bool, mock_has_internet: bool) -> None:
+def test_can_access_google_page(
+        mock_valid_url: bool,
+        mock_has_internet: bool
+) -> None:
     # Cenário 1: Tudo OK -> Deve retornar "Accessible"
     mock_valid_url.return_value = True
     mock_has_internet.return_value = True
