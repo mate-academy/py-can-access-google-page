1e43641f69f3cb53e72f4a9722bdcc2fa5b59f67
Tymur <t.balenko12008@gmail.com>
2026-05-09 16:25:47 +0200

    solvedd

--- app/test_main.py
+++ app/test_main.py
@@ -11,9 +11,15 @@ URL = [
 
 
 @pytest.mark.parametrize("inet_ok, url_ok, expected", URL)
-def test_can_access_google_page(inet_ok: bool, url_ok: bool, expected: str) -> None:
-    with mock.patch("app.main.valid_google_url") as mock_valid, \
-        mock.patch("app.main.has_internet_connection") as mock_conn:
+def test_can_access_google_page(
+    inet_ok: bool,
+    url_ok: bool,
+    expected: str
+) -> None:
+    with (
+        mock.patch("app.main.valid_google_url") as mock_valid,
+        mock.patch("app.main.has_internet_connection") as mock_conn,
+    ):
         mock_valid.return_value = url_ok
         mock_conn.return_value = inet_ok
         result = main.can_access_google_page("https://google.com")
