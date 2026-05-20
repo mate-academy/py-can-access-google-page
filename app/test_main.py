002f77a8d1aaef1f6c4b4c174c7b779c89be7810
Vadim Sukhsniuk <pilrou839@gmail.com>
2026-05-21 00:01:20 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -4,7 +4,7 @@ import pytest
 
 
 @pytest.mark.parametrize(
-    "mock_goog_url, mock_internet_conn, mock_result",
+    "google_url, internet_conn, mock_result",
     [
         (True, True, "Accessible"),
         (False, True, "Not accessible"),
@@ -12,7 +12,17 @@ import pytest
         (False, False, "Not accessible")
     ]
 )
-@patch("app.main.valid_google_url")
-@patch("has_internet_connection")
+def test_with_mock_parametrize_can_access_goog(
+        google_url: bool,
+        internet_conn: bool,
+        mock_result: str,
+) -> None:
+    with (
+        patch("app.main.has_internet_connection") as mock_internet,
+        patch("app.main.valid_google_url") as mock_googl_url
+    ):
+        mock_internet.return_value = internet_conn
+        mock_googl_url.return_value = google_url
 
-
+    result = can_access_google_page("https://google.com")
+    assert result == mock_result
