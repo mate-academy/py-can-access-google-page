6bcaf116ad61d0e03df2b09182640765299f0302
Dima Sukhariev <dmitriu.sy1303@gmail.com>
2026-05-26 13:49:19 +0300

    Added test case when has_internet_connection returns False

--- app/test_main.py
+++ app/test_main.py
@@ -22,3 +22,14 @@ def test_can_access_google_page_fail(mock_valid, mock_internet) -> None:
     assert can_access_google_page("https://google.com") == "Not accessible"
     mock_valid.assert_called_once()
     mock_internet.assert_called_once()
+
+
+@mock.patch("app.main.has_internet_connection")
+@mock.patch("app.main.valid_google_url")
+def test_can_access_google_page_fail(mock_valid, mock_internet) -> None:
+    mock_valid.return_value = True
+    mock_internet.return_value = False
+
+    assert can_access_google_page("https://google.com") == "Not accessible"
+    mock_valid.assert_called_once()
+    mock_internet.assert_called_once()
