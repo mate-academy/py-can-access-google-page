89e47ab116cf6bdf74ade562a633e770da185f2c
Dima Sukhariev <dmitriu.sy1303@gmail.com>
2026-05-26 13:59:36 +0300

    covered all boundary conditions, fix names and annotations

--- app/test_main.py
+++ app/test_main.py
@@ -1,10 +1,14 @@
 from unittest import mock
+from unittest.mock import MagicMock
 from app.main import can_access_google_page
 
 
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
-def test_can_access_google_page_success(mock_valid, mock_internet) -> None:
+def test_can_access_google_page_success(
+        mock_valid: MagicMock,
+        mock_internet: MagicMock
+) -> None:
     mock_valid.return_value = True
     mock_internet.return_value = True
 
@@ -15,7 +19,10 @@ def test_can_access_google_page_success(mock_valid, mock_internet) -> None:
 
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
-def test_can_access_google_page_fail(mock_valid, mock_internet) -> None:
+def test_can_access_google_page_failed_valid(
+        mock_valid: MagicMock,
+        mock_internet: MagicMock
+) -> None:
     mock_valid.return_value = False
     mock_internet.return_value = True
 
@@ -26,8 +33,25 @@ def test_can_access_google_page_fail(mock_valid, mock_internet) -> None:
 
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
-def test_can_access_google_page_fail(mock_valid, mock_internet) -> None:
+def test_can_access_google_page_failed_internet(
+        mock_valid: MagicMock,
+        mock_internet: MagicMock
+) -> None:
     mock_valid.return_value = True
+    mock_internet.return_value = False
+
+    assert can_access_google_page("https://google.com") == "Not accessible"
+    mock_valid.assert_called_once()
+    mock_internet.assert_called_once()
+
+
+@mock.patch("app.main.has_internet_connection")
+@mock.patch("app.main.valid_google_url")
+def test_can_access_google_page_failed_both(
+        mock_valid: MagicMock,
+        mock_internet: MagicMock
+) -> None:
+    mock_valid.return_value = False
     mock_internet.return_value = False
 
     assert can_access_google_page("https://google.com") == "Not accessible"
