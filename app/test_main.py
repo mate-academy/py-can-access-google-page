d0bec5b983358384ae3e7af387d2d8af0153d187
Dima Sukhariev <dmitriu.sy1303@gmail.com>
2026-05-27 13:32:00 +0300

    remove duplication

--- app/test_main.py
+++ app/test_main.py
@@ -1,59 +1,29 @@
+import pytest
 from unittest import mock
 from unittest.mock import MagicMock
 from app.main import can_access_google_page
 
 
+@pytest.mark.parametrize(
+    "valid_url, internet_status, result_expected", [
+        (True, True, "Accessible"),
+        (False, True, "Not accessible"),
+        (True, False, "Not accessible"),
+        (False, False, "Not accessible"),
+    ]
+)
 @mock.patch("app.main.has_internet_connection")
 @mock.patch("app.main.valid_google_url")
-def test_can_access_google_page_success(
+def test_can_access_google_page(
         mock_valid: MagicMock,
-        mock_internet: MagicMock
+        mock_internet: MagicMock,
+        valid_url: bool,
+        internet_status: bool,
+        result_expected: str
 ) -> None:
-    mock_valid.return_value = True
-    mock_internet.return_value = True
+    mock_valid.return_value = valid_url
+    mock_internet.return_value = internet_status
 
-    assert can_access_google_page("https://google.com") == "Accessible"
-    mock_valid.assert_called_once()
-    mock_internet.assert_called_once()
-
-
-@mock.patch("app.main.has_internet_connection")
-@mock.patch("app.main.valid_google_url")
-def test_can_access_google_page_failed_valid(
-        mock_valid: MagicMock,
-        mock_internet: MagicMock
-) -> None:
-    mock_valid.return_value = False
-    mock_internet.return_value = True
-
-    assert can_access_google_page("https://google.com") == "Not accessible"
-    mock_valid.assert_called_once()
-    mock_internet.assert_called_once()
-
-
-@mock.patch("app.main.has_internet_connection")
-@mock.patch("app.main.valid_google_url")
-def test_can_access_google_page_failed_internet(
-        mock_valid: MagicMock,
-        mock_internet: MagicMock
-) -> None:
-    mock_valid.return_value = True
-    mock_internet.return_value = False
-
-    assert can_access_google_page("https://google.com") == "Not accessible"
-    mock_valid.assert_called_once()
-    mock_internet.assert_called_once()
-
-
-@mock.patch("app.main.has_internet_connection")
-@mock.patch("app.main.valid_google_url")
-def test_can_access_google_page_failed_both(
-        mock_valid: MagicMock,
-        mock_internet: MagicMock
-) -> None:
-    mock_valid.return_value = False
-    mock_internet.return_value = False
-
-    assert can_access_google_page("https://google.com") == "Not accessible"
+    assert can_access_google_page("https://google.com") == result_expected
     mock_valid.assert_called_once()
     mock_internet.assert_called_once()
